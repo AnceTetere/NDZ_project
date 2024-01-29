0102994b66ccf22dc78ccc208b3b76fee721b0ea
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-01-29 20:24:15 +0200

    Create 4_NDZ_final.sql

---
+++ 4_NDZ_final.sql
@@ -0,0 +1,16 @@
+-- Pārnes īstās rindas uz datu tabulu, ar kuru strādāsi
+/* pēc tam izformētu tabulu pārnesīs uz šo
+CREATE TABLE ndz_2024 (
+	rkod varchar(???),
+	TB_PK_ALG varchar(?),
+	perk varchar(???),
+	sdB_PK_ALG varchar(???),
+	sandat varchar(???),
+	zkod varchar(???),
+	valsts varchar(???),
+	pro varchar(???),
+	periodT varchar(???));
+*/
+
+-- izdzēs prendz_2024
+DROP TABLE prendz_2024
