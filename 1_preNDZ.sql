1c619b52808937a8e0ac7d62fbba738011eef41d
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-01-29 20:12:15 +0200

    Create 1_preNDZ.sql

---
+++ 1_preNDZ.sql
@@ -0,0 +1,15 @@
+USE [Zinas ND]
+GO
+
+--Sākumā aiļu platumu neskatos, to pēc izformēs.
+-- Dati nāk ar daudzām tukšām rindām, kas jāiztīra.
+CREATE TABLE preNDZ_2024 (
+	rkod varchar(300),
+	NM_PK_ALG varchar(5),
+	perk varchar(300),
+	TB_PK_ALG varchar(300),
+	sandat varchar(300),
+	zkod varchar(300),
+	valsts varchar(300),
+	pro varchar(300),
+	periodT varchar(300));
