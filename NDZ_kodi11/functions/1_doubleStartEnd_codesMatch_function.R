34b703fa52be01bd03abe534eb0ee77a44a8e941
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-02-18 16:35:42 +0200

    Update 1_doubleStartEnd_codesMatch_function.R

--- NDZ_kodi11/functions/1_doubleStartEnd_codesMatch_function.R
+++ NDZ_kodi11/functions/1_doubleStartEnd_codesMatch_function.R
@@ -1,9 +1,3 @@
-# Ja vienam un tajam pašam vienumam, vienā un tajā pašā uzņēmumā 
-# ir divas atzīmes par darba sākšanu vai beigšanu, 
-# tad, ja ziņojuma datumi neatšķiras, saglabā tikai vienu no tiem.
-
-# Ja ziņojuma datumi atšķiras, atstāj vēlāko ierakstu datumu.
-
 F_doubleStartEnd_codesMatch <- function(x2) {
 rownames(x2) <- NULL
 n1 <- data.frame()
