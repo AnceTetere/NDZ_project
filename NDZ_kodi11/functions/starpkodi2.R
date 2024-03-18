c386e3393b597b5949bbae06ffffd393eebd1a53
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-03-18 16:07:25 +0200

    Update starpkodi2.R

--- NDZ_kodi11/functions/starpkodi2.R
+++ NDZ_kodi11/functions/starpkodi2.R
@@ -1,21 +1,21 @@
 starpkodi2 <- function(y2, t) {
   if (t$zinkod[1] == "51" &&
       t$zinkod[2] == "25" &&
-      t$ZDN_sanemsanas_datums[1] <= t$ZDN_sanemsanas_datums[2]) {
+      t$NDZ_sanemsanas_datums[1] <= t$NDZ_sanemsanas_datums[2]) {
     yt <- y2[v:(v + 1),]
     days <-
-      as.numeric(difftime(t$beidz_darbu[2], t$sak_darbu[1], units = "days")) + 1
-        yt <- yt[1,]
+      as.numeric(difftime(t$beidz[2], t$sak[1], units = "days")) + 1
+    yt <- yt[1,]
     yt$dienas <- days
   } else if (t$zinkod[1] == "25" && t$zinkod[2] == "51" &&
-             t$ZDN_sanemsanas_datums[1] == t$ZDN_sanemsanas_datums[2]) {
+             t$NDZ_sanemsanas_datums[1] == t$NDZ_sanemsanas_datums[2]) {
     yt <- y2[v,]
     yt$dienas <- 0
   } else if (t$zinkod[1] == "11" && t$zinkod[2] == "50" &&
-             t$ZDN_sanemsanas_datums[1] < t$ZDN_sanemsanas_datums[2]) {
+             t$NDZ_sanemsanas_datums[1] < t$NDZ_sanemsanas_datums[2]) {
     yt <- y2[v:(v + 1),]
     days <-
-      as.numeric(difftime(t$beidz_darbu[2], t$sak_darbu[1], units = "days"))
+      as.numeric(difftime(t$beidz[2], t$sak[1], units = "days"))
     yt <- yt[1,]
     yt$dienas <- days
   } else if (t$zinkod[1] == "50" && t$zinkod[2] == "25") {
@@ -24,71 +24,142 @@ starpkodi2 <- function(y2, t) {
   } else if (t$zinkod[1] == "51" && t$zinkod[2] == "21") {
     yt <- y2[v:(v + 1),]
     days <-
-      as.numeric(difftime(as.Date(t$beidz_darbu[2]), as.Date(t$sak_darbu)[1], units = "days"))
+      as.numeric(difftime(as.Date(t$beidz[2]), as.Date(t$sak)[1], units = "days"))
     yt <- yt[1,]
     yt$dienas <- days
   } else if (t$zinkod[1] == "54" && t$zinkod[2] == "21") {
     yt <- y2[v:(v + 1),]
     days <-
-      as.numeric(difftime(as.Date(t$beidz_darbu[2]), as.Date(t$sak_darbu)[1], units = "days")) + 1
+      as.numeric(difftime(as.Date(t$beidz[2]), as.Date(t$sak)[1], units = "days")) + 1
     yt <- yt[1,]
     yt$dienas <- days
   } else if (t$zinkod[1] == "21" && t$zinkod[2] == "51" &&
-             t$ZDN_sanemsanas_datums[1] == t$ZDN_sanemsanas_datums[2]) {
+             t$NDZ_sanemsanas_datums[1] == t$NDZ_sanemsanas_datums[2]) {
     yt <- y2[v,]
     yt$dienas <- 0
   } else if (t$zinkod[1] == "11" && t$zinkod[2] == "50" &&
-             t$ZDN_sanemsanas_datums[1] == t$ZDN_sanemsanas_datums[2]) {
+             t$NDZ_sanemsanas_datums[1] == t$NDZ_sanemsanas_datums[2]) {
     yt <- y2[v,]
     yt$dienas <- 0
   } else if (t$zinkod[1] == "21" && t$zinkod[2] == "53" &&
-             t$ZDN_sanemsanas_datums[1] == t$ZDN_sanemsanas_datums[2]){
+             t$NDZ_sanemsanas_datums[1] == t$NDZ_sanemsanas_datums[2]){
     yt <- y2[v:(v + 1),]
     yt <- yt[yt$zinkod == "11",]
   } else if (t$zinkod[1] == "51" && t$zinkod[2] == "23" &&
-            t$ZDN_sanemsanas_datums[1] != t$ZDN_sanemsanas_datums[2]) {
-    days <- as.numeric(difftime(as.Date(t$beidz_darbu)[2], as.Date(t$sak_darbu)[1], units = "days"))
+            t$NDZ_sanemsanas_datums[1] != t$NDZ_sanemsanas_datums[2]) {
+    days <- as.numeric(difftime(as.Date(t$beidz)[2], as.Date(t$sak)[1], units = "days"))
     yt <- y2[v,]
     yt$dienas <- days
   } else if (t$zinkod[1] == "53" && t$zinkod[2] == "21" &&
-            t$ZDN_sanemsanas_datums[1] != t$ZDN_sanemsanas_datums[2]) {
+            t$NDZ_sanemsanas_datums[1] != t$NDZ_sanemsanas_datums[2]) {
     yt <- y2[v:(v+1),] 
     yt <- yt[yt$zinkod == "53", ]
   } else if (t$zinkod[1] == "25" && t$zinkod[2] == "41" &&
-             t$ZDN_sanemsanas_datums[1] == t$ZDN_sanemsanas_datums[2]) {
+             t$NDZ_sanemsanas_datums[1] == t$NDZ_sanemsanas_datums[2]) {
     yt <- y2[v,] 
     yt$dienas <- 0
   } else if (t$zinkod[1] == "41" && t$zinkod[2] == "50" &&
-            t$ZDN_sanemsanas_datums[1] == t$ZDN_sanemsanas_datums[2]) {
+            t$NDZ_sanemsanas_datums[1] == t$NDZ_sanemsanas_datums[2]) {
     yt <- y2[v,] 
     yt$dienas <- 0
   } else if (t$zinkod[1] == "92" && t$zinkod[2] == "40" &&
-             t$ZDN_sanemsanas_datums[1] != t$ZDN_sanemsanas_datums[2]) {
-    days <- as.numeric(difftime(as.Date(t$beidz_darbu)[2], as.Date(t$sak_darbu)[1], units = "days"))
+             t$NDZ_sanemsanas_datums[1] != t$NDZ_sanemsanas_datums[2]) {
+    days <- as.numeric(difftime(as.Date(t$beidz)[2], as.Date(t$sak)[1], units = "days"))
     yt <- y2[v,] 
     yt$dienas <- days
   } else if (t$zinkod[1] == "50" && t$zinkod[2] == "82" &&
-            t$ZDN_sanemsanas_datums[1] != t$ZDN_sanemsanas_datums[2]) {
+            t$NDZ_sanemsanas_datums[1] != t$NDZ_sanemsanas_datums[2]) {
     yt <- y2[v:(v+1),] 
     yt <- yt[yt$zinkod == "50", ]
   } else if (t$zinkod[1] == "53" && t$zinkod[2] == "25" &&
-             t$ZDN_sanemsanas_datums[1] != t$ZDN_sanemsanas_datums[2]) {
+             t$NDZ_sanemsanas_datums[1] != t$NDZ_sanemsanas_datums[2]) {
     yt <- y2[v:(v+1),] 
     yt <- yt[yt$zinkod == "53", ]
   } else if (t$zinkod[1] == "25" && t$zinkod[2] == "50" &&
-             t$ZDN_sanemsanas_datums[1] == t$ZDN_sanemsanas_datums[2]) {
+             t$NDZ_sanemsanas_datums[1] == t$NDZ_sanemsanas_datums[2]) {
     yt <- y2[v:(v+1),] 
     yt <- yt[yt$zinkod == "50", ]
   } else if (t$zinkod[1] == "25" && t$zinkod[2] == "40" &&
-              t$ZDN_sanemsanas_datums[1] == t$ZDN_sanemsanas_datums[2]) {
+              t$NDZ_sanemsanas_datums[1] == t$NDZ_sanemsanas_datums[2]) {
     yt <- y2[v:(v+1),] 
     yt <- yt[yt$zinkod == "40", ]
   } else if (t$zinkod[1] == "50" && t$zinkod[2] == "21" &&
-             t$ZDN_sanemsanas_datums[1] != t$ZDN_sanemsanas_datums[2]) {
+             t$NDZ_sanemsanas_datums[1] != t$NDZ_sanemsanas_datums[2]) {
     yt <- y2[v:(v+1),] 
     yt <- yt[yt$zinkod == "50", ]
   } else if (t$zinkod[1] == "21" && t$zinkod[2] == "41" &&
-             t$ZDN_sanemsanas_datums[1] == t$ZDN_sanemsanas_datums[2]) {
+             t$NDZ_sanemsanas_datums[1] == t$NDZ_sanemsanas_datums[2]) {
+    yt <- y2[v,] 
+    yt$dienas <- 0
+  } else if (t$zinkod[1] == "41" && t$zinkod[2] == "25" &&
+             t$NDZ_sanemsanas_datums[1] != t$NDZ_sanemsanas_datums[2]) {
+    days <- as.numeric(difftime(as.Date(t$beidz[2]), as.Date(t$sak[1]), units = "days"))
+    yt <- y2[v,] 
+    yt$dienas <- days
+  } else if (t$zinkod[1] == "11" && t$zinkod[2] == "81" &&
+             t$NDZ_sanemsanas_datums[1] <= t$NDZ_sanemsanas_datums[2]) {
+    yt <- y2[v:(v+1),] 
+    yt <- yt[yt$zinkod == "11", ]
+  } else if (t$zinkod[1] == "25" && t$zinkod[2] == "92" &&
+             t$NDZ_sanemsanas_datums[1] == t$NDZ_sanemsanas_datums[2]) {
+    yt <- y2[v,] 
+    yt$dienas <- 0
+  } else if (t$zinkod[1] == "41" && t$zinkod[2] == "21" &&
+            t$NDZ_sanemsanas_datums[1] <= t$NDZ_sanemsanas_datums[2]) {
+    days <- as.numeric(difftime(as.Date(t$beidz[2]), as.Date(t$sak[1]), units = "days")) + 1
+    
+    yt <- y2[v,] 
+    yt$dienas <- days
+  } else if (t$zinkod[1] == "41" && t$zinkod[2] == "50" &&
+             t$NDZ_sanemsanas_datums[1] <= t$NDZ_sanemsanas_datums[2]) {
+    days <- as.numeric(difftime(as.Date(t$beidz[2]), as.Date(t$sak[1]), units = "days")) + 1
+    
+    yt <- y2[v,] 
+    yt$dienas <- days
+  } else if (t$zinkod[1] == "51" && t$zinkod[2] == "22" &&
+             t$NDZ_sanemsanas_datums[1] <= t$NDZ_sanemsanas_datums[2]) {
+    days <- as.numeric(difftime(as.Date(t$beidz[2]), as.Date(t$sak[1]), units = "days")) + 1
+    yt <- y2[v,] 
+    yt$dienas <- days
+  } else if (t$zinkod[1] == "11" && t$zinkod[2] == "40" &&
+            t$NDZ_sanemsanas_datums[1] == t$NDZ_sanemsanas_datums[2]) {
+    yt <- y2[v,] 
+    yt$dienas <- 0
+  } else if (t$zinkod[1] == "53" && t$zinkod[2] == "82" &&
+             t$NDZ_sanemsanas_datums[1] == t$NDZ_sanemsanas_datums[2]) {
+    yt <- y2[v:(v+1),] 
+    yt <- yt[yt$zinkod == "53", ]
+  } else if (t$zinkod[1] == "11" && t$zinkod[2] == "91" &&
+             t$NDZ_sanemsanas_datums[1] != t$NDZ_sanemsanas_datums[2]) {
+    days <- as.numeric(difftime(as.Date(t$beidz[2]), as.Date(t$sak[1]), units = "days"))
+    yt <- y2[v,] 
+    yt$dienas <- days
+  } else if (t$zinkod[1] == "54" && t$zinkod[2] == "25" &&
+            t$NDZ_sanemsanas_datums[1] != t$NDZ_sanemsanas_datums[2]) {
+    yt <- y2[v,] 
+    yt$dienas <- as.numeric(difftime(as.Date(t$beidz[2]), as.Date(t$sak[1]), units = "days")) + 1 #jo darbs
+  } else if (t$zinkod[1] == "25" && t$zinkod[2] == "53" &&
+             t$NDZ_sanemsanas_datums[1] == t$NDZ_sanemsanas_datums[2]) {
+    yt <- y2[v:(v+1),] 
+    yt <- yt[yt$zinkod == "53", ]
+  } else if (t$zinkod[1] == "26" && t$zinkod[2] == "82" &&
+             t$NDZ_sanemsanas_datums[1] == t$NDZ_sanemsanas_datums[2]) {
+    yt <- y2[v,] 
+    yt$dienas <- 0
+  } else if (t$zinkod[1] == "11" && t$zinkod[2] == "40" &&
+             t$NDZ_sanemsanas_datums[1] != t$NDZ_sanemsanas_datums[2]) {
+    yt <- y2[v,] 
+    yt$dienas <- as.numeric(difftime(as.Date(t$beidz[2]), as.Date(t$sak[1]), units = "days"))
+  } else if (t$zinkod[1] == "92" && t$zinkod[2] == "25" &&
+             t$NDZ_sanemsanas_datums[1] != t$NDZ_sanemsanas_datums[2]) {
+    yt <- y2[v,] 
+    yt$dienas <- as.numeric(difftime(as.Date(t$beidz[2]), as.Date(t$sak[1]), units = "days")) + 1 #jo darbs
+  } else if (t$zinkod[1] == "21" && t$zinkod[2] == "54" &&
+            t$NDZ_sanemsanas_datums[1] == t$NDZ_sanemsanas_datums[2]) {
+    yt <- y2[v,] 
+    yt$dienas <- 0
+  } else if (t$zinkod[1] == "22" && t$zinkod[2] == "51" &&
+             t$NDZ_sanemsanas_datums[1] == t$NDZ_sanemsanas_datums[2]) {
     yt <- y2[v,] 
     yt$dienas <- 0
   } else {
