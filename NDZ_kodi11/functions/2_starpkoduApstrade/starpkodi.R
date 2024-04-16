9f2cbd7cebdaa1f563c9fd9b3e57700bbdadd4f4
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-04-16 16:06:55 +0300

    Update starpkodi.R

--- NDZ_kodi11/functions/2_starpkoduApstrade/starpkodi.R
+++ NDZ_kodi11/functions/2_starpkoduApstrade/starpkodi.R
@@ -1,58 +1,27 @@
-starpkodi <- function(y2) {
+starpkodi <- function(y_2plus, n) { #y_2plus: apstrādā tabulas, kur ir divi atšķirīgi kodi vai vairāk; n: skaits, cik šo atšķirīgo kodu ir
   setwd(paste0(path, "data\\originals\\", year))
-  y2 <- y2[order(y2$PS_code, y2$DN_code, y2$NM_code), ]
+  y_2plus <- y_2plus[order(y_2plus$PS_code, y_2plus$DN_code, y_2plus$NM_code), ]
   
-  prev <- as.Date(paste0(substr(y2$period[1], 1, 4), "-", substr(y2$period[1], 5, 6), "-01")) - 1
+  prev <- as.Date(paste0(substr(y_2plus$period[1], 1, 4), "-", substr(y_2plus$period[1], 5, 6), "-01")) - 1
   z <- data.frame()
   
-  for(v in seq(1, nrow(y2), by = 2)) {
-    tName <- paste0("NDZ", year, month, "_", y2$zinkod[v])
-    load(paste0("starting_", tName, ".RData"))
-    t1 <- get(tName)
-    t1 <- t1[t1$PS_code == y2$PS_code[v] & t1$NM_code == y2$NM_code[v], ]
-    rm(list = tName, tName)
+  for(v in seq(1, nrow(y_2plus), by = n)) {
     
-    tName <- paste0("NDZ", year, month, "_", y2$zinkod[v+1])
-    load(paste0("starting_", tName, ".RData"))
-    t2 <- get(tName)
-    t2 <- t2[t2$PS_code == y2$PS_code[v+1] & t2$NM_code == y2$NM_code[v+1], ]
-    rm(list = tName, tName)
-    
-    t <- rbind(t1, t2)
-    t <- t[order(t$NDZ_sanemsanas_datums), ]
-    rownames(t) <- NULL
-    rm(t1, t2)
+    # izveido apakštabulu no oriģinālajiem datiem, pārrēķinam
+    t <- subTable(y_2plus, n, v)
     
     if(!(all(t$PS_code == t$PS_code[1]) && all(t$NM_code == t$NM_code[1]))) {
-      stop("ps_CODE nesakritība! Tabula: y2; rinda:", v)
+      stop("PS_code nesakritība! Tabula: y_2plus; rinda:", v)
     }
     
-    if (nrow(t) == 2) {
-      z <- rbind(z, starpkodi2(y2, t, prev))
-    } else if(nrow(t) == 3){
-      z <- rbind(z, starpkodi3(y2, t, prev))  
-    } else if (nrow(t) == 4) {
-      z <- rbind(z, starpkodi4(y2, t, prev))
-    } else if (nrow(t) == 5) {
-      z <- rbind(z, starpkodi5(y2, t, prev))
-    } else if (nrow(t) == 6) {
-      z <- rbind(z, starpkodi6(y2, t, prev))
-    } else if (nrow(t) == 7) {
-      z <- rbind(z, starpkodi7(y2, t, prev))
-    } else if (nrow(t) == 8) {
-      z <- rbind(z, starpkodi8(y2, t, prev))
-    } else if (nrow(t) == 9) {
-      z <- rbind(z, starpkodi9(y2, t, prev))
-    } else if (nrow(t) == 10) {
-      z <- rbind(z, starpkodi10(y2, t, prev))
-    } else if (nrow(t) == 13) {
-      z <- rbind(z, starpkodi13(y2, t, prev))
-    } else {
-      stop("Trūkst izstrādes koda.")
-    }
+    z <- switch(
+      as.character(n),
+      "2" = dubultkodi(y_2plus, t, prev, v, z),
+      "3" = tripletkodi(y_2plus, t, prev, v, z),
+      default = stop("Starpkodi neatpazīst atšķirīgo kodu tabulu y_2plus.")
+    )
   }
  
-  rm(prev, t, y2)
+  rm(prev, t, y_2plus)
   return(z) 
-  
 }
