b759d83dee3795530a7ceda230886666013a4a98
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-04-17 11:34:57 +0300

    Update starpkodi2_51.R

--- NDZ_kodi11/functions/2_starpkoduApstrade/starpkodi2_51.R
+++ NDZ_kodi11/functions/2_starpkoduApstrade/starpkodi2_51.R
@@ -1,6 +1,6 @@
-starpkodi2_51 <- function(y2, t) {
+starpkodi2_51 <- function(y2, t, prev, v) {
   if (t$zinkod[2] == "25" && t$NDZ_sanemsanas_datums[1] <= t$NDZ_sanemsanas_datums[2]) {
-    yt <- yt[v,]
+    yt <- y2[v,]
     yt$dienas <- as.numeric(difftime(t$beidz[2], t$sak[1], units = "days")) + 1
   } else if (t$zinkod[2] == "21") {
     yt <- y2[v,]
@@ -11,9 +11,13 @@ starpkodi2_51 <- function(y2, t) {
   } else if (t$zinkod[2] == "22" && t$NDZ_sanemsanas_datums[1] <= t$NDZ_sanemsanas_datums[2]) {
     yt <- y2[v,] 
     yt$dienas <- as.numeric(difftime(as.Date(t$beidz[2]), as.Date(t$sak[1]), units = "days")) + 1
+  } else if (t$zinkod[2] == "91" && t$NDZ_sanemsanas_datums[1] != t$NDZ_sanemsanas_datums[2]) {
+    yt <- y2[v,] 
+    yt$dienas <- as.numeric(difftime(as.Date(t$beidz[2]), as.Date(t$sak[1]), units = "days"))
   } else {
     stop("starpkodi2_51: Trūkst izstrādes koda starpkodi2.")
   }
-  
+ 
+  if(is.na(yt$PS_code[1])) {stop("Dienas NA.")}
   return(yt)
 }
