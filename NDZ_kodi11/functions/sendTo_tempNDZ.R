76cb9c6d1af4af6141c1cbb59501303b317848d2
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-04-19 11:35:22 +0300

    Update sendTo_tempNDZ.R

--- NDZ_kodi11/functions/sendTo_tempNDZ.R
+++ NDZ_kodi11/functions/sendTo_tempNDZ.R
@@ -1,24 +1,24 @@
 sendTo_tempNDZ <- function(x) {
-  x <- x[order(x$ps_code, x$dn_code, x$nm_code, x$NDZ_sanemsanas_datums, x$start), ]
-  x <- x[ , c("period", "ps_code", "dn_code", "nm_code", "dienas")]
+  x <- x[order(x$PS_code, x$DN_code, x$NM_code, x$NDZ_sanemsanas_datums, x$start), ]
+  x <- x[ , c("period", "PS_code", "DN_code", "NM_code", "dienas")]
   
   x <- x %>%
-    group_by(period, ps_code, dn_code, nm_code) %>%
+    group_by(period, PS_code, DN_code, NM_code) %>%
     summarise(
       dienas = sum(dienas, na.rm = TRUE)
     ) %>%
-    arrange(ps_code)
+    arrange(PS_code)
 
-  if ((nrow(x[duplicated(x[c("ps_code", "nm_code")]), ]) == 0) && (sum(x$dienas < 0) == 0)) {
+  if ((nrow(x[duplicated(x[c("PS_code", "NM_code")]), ]) == 0) && (sum(x$dienas < 0) == 0)) {
     setwd(paste0(path, "data\\intermediate_tables\\"))
     load("temp_NDZ.RData")
     
     temp_NDZ <-
       rbind(temp_NDZ, x[, c("period",
-                            "ps_code",
-                            "dn_code",
-                            "nm_code",
-                            "dienas")]) 
+                            "PS_code",
+                            "DN_code",
+                            "NM_code",
+                            "dienas")])
     save(temp_NDZ, file = "temp_NDZ.RData")
     
     temp_rows <- readRDS("temp_rows.rds")
