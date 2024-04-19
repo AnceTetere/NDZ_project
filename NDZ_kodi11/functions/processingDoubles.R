ba3ee3783f7d1b3842076d56dc506acbf18c04fd
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-04-19 11:32:40 +0300

    Update processingDoubles.R

--- NDZ_kodi11/functions/processingDoubles.R
+++ NDZ_kodi11/functions/processingDoubles.R
@@ -1,11 +1,12 @@
+
 processingDoubles <- function(x, o) {
 
-  x <- x[order(x$PS_codes, x$DN_codes, x$NM_codes, x$ndz_sanemsanas_datums, x$start, x$end), ]
+  x <- x[order(x$PS_code, x$DN_code, x$NM_code, x$NDZ_sanemsanas_datums, x$start, x$end), ]
   temp <- data.frame()
   
   for (k in seq(1, nrow(x), by = 2)) {
-    if ((is.na(x$sak_darbu)[k] && is.na(x$beidz_darbu[k + 1])
-         || is.na(x$sak_darbu)[k + 1] && is.na(x$beidz_darbu[k])) && (x$ndz_sanemsanas_datums[k] <= x$ndz_sanemsanas_datums[k+1])) {
+    if ((is.na(x$sak)[k] && is.na(x$beidz[k + 1])
+         || is.na(x$sak)[k + 1] && is.na(x$beidz[k])) && (x$NDZ_sanemsanas_datums[k] <= x$NDZ_sanemsanas_datums[k+1])) {
       temp <- rbind(temp, x[c(k, k + 1), ])
     }
   }
@@ -14,20 +15,12 @@ processingDoubles <- function(x, o) {
   if (nrow(temp) == nrow(x)) {
     cat(
       "PĀRBAUDE IZIETA: Visas rindas ir Īstie Divnieki.\n
-    Īstie Divnieki nozīmē, ka ierakstu pārī, kur PS_codes un NM_codes sakrīt, viena rinda ir ar sak_darbu
-    ierakstu otra ar beidz_darbu ierakstu, bet nevienā rindā nav abi: gan sak_darbu, gan beidz_darbu datumi.
-    Īstie Divnieki ir rindas, kuras ir savienojamas uz vienu ierakstu, kurā būs gan sak_darbu datums,
-    gan beidz_darbu datums.\n"
     )
     rm(temp)
   } else {
     stop(
       cat(
         "ERROR: Visas rindas NAV Īstie Divnieki. Pārskatīt tabulu.\n
-       Īstie Divnieki nozīmē, ka ierakstu pārī, kur PS_codes un NM_codes sakrīt, viena rinda ir ar sak_darbu
-       ierakstu otra ar beidz_darbu ierakstu, bet nevienā rindā nav abi: gan sak_darbu, gan beidz_darbu datumi.
-       Īstie Divnieki ir rindas, kuras ir savienojamas uz vienu ierakstu, kurā būs gan sak_darbu datums,
-       gan beidz_darbu datums.\n"
       )
     )
   }
@@ -36,21 +29,24 @@ processingDoubles <- function(x, o) {
   x_savienotieDivnieki <- data.frame()
   x <-
     x[order(x$period,
-            x$PS_codes,
-            x$DN_codes,
-            x$NM_codes,
-            x$ndz_sanemsanas_datums, x$start, x$end),]
+            x$PS_code,
+            x$DN_code,
+            x$NM_code,
+            x$NDZ_sanemsanas_datums, x$start, x$end),]
 
-  for (O in seq(1, nrow(x), by = 2)) {
+    for (O in seq(1, nrow(x), by = 2)) {
     y <- x[c(O, O + 1),]
   
-    if (y$ndz_sanemsanas_datums[1] <= y$ndz_sanemsanas_datums[2] && doublesTest(1, y)) {
+    if (y$NDZ_sanemsanas_datums[1] <= y$NDZ_sanemsanas_datums[2] && doublesTest(1, y)) {
       
-      y$sak_darbu[y$end == "2"] <- y$sak_darbu[y$start == "1"] 
-      y$beidz_darbu[y$start == "1"] <- y$beidz_darbu[y$end == "2"]
+      #pataisa abas rindas vienādas
+      if (y$zinkod[y$end == "2"] == "26") {y$zinkod <- "26"} 
+      y$sak[y$end == "2"] <- y$sak[y$start == "1"] 
+      y$beidz[y$start == "1"] <- y$beidz[y$end == "2"]
       y$start <- "1"
       y$end <- "2"
       
+      #Piešuj pirmo rindu x_savienotieDivnieki tabulai
       x_savienotieDivnieki <- rbind(x_savienotieDivnieki, y[1, ])
     } else {
       stop(cat("Apstrādājot dubultniekus caur processingDouble(), kas nāk no", o, "-nieku apstrādes, dubultnieku sākuma datums NAV agrāks vai vienāds ar beigu datumu. TrueDoubles rindas: ",
@@ -60,20 +56,20 @@ processingDoubles <- function(x, o) {
   
   rm(y)
   
-  #3 Pārbaudām vai izdalītās rindas sakrīt, vai ailēs [sak_darbu] un [beidz_darbu] nav NAs.
+  #3 Pārbaudām vai izdalītās rindas sakrīt, vai ailēs [sak] un [beidz] nav NAs.
   
   if (nrow(x_savienotieDivnieki) * 2 == nrow(x)) {
-    if (sum(is.na(x_savienotieDivnieki$sak_darbu)) == 0) {
-      if (sum(is.na(x_savienotieDivnieki$beidz_darbu)) == 0) {
+    if (sum(is.na(x_savienotieDivnieki$sak)) == 0) {
+      if (sum(is.na(x_savienotieDivnieki$beidz)) == 0) {
         cat(
           "PĀRBAUDE IZIETA:Funkcijā processingDoubles(), jaunizveidotā tabula x_savienotieDivnieki ir izgājusi pārbaudes un tālāk var sarēķināt dienas.\n"
         )
         rm(x)
       } else {
-        stop(cat("STOP: Datumu ailē [beidz_darbu] ir NA vienības."))
+        stop(cat("STOP: Datumu ailē [beidz] ir NA vienības."))
       }
     } else {
-      stop(cat("STOP: Datumu ailē [sak_darbu] ir NA vienības."))
+      stop(cat("STOP: Datumu ailē [sak] ir NA vienības."))
     }
   } else {
     stop(cat("STOP: No Īsto Divnieku tabulas izvilktāts rindas nesakrīt."))
@@ -84,25 +80,30 @@ processingDoubles <- function(x, o) {
   #4 Tabulu x_savienotieDivnieki apstrādā sekojošos punktos.
   
   x <- x_savienotieDivnieki
-  x <- x[order(x$PS_codes, x$DN_codes, x$NM_codes, x$ndz_sanemsanas_datums, x$start), ]
+  x <- x[order(x$PS_code, x$DN_code, x$NM_code, x$NDZ_sanemsanas_datums, x$start), ]
   rownames(x) <- NULL
   
-  #6 Aprēķini dienas no sak_darbu līdz beidz_darbu un ievieto tās ailē [dienas].
-  #LĒMUMS
+  #6 Aprēķini dienas no sak līdz beidz un ievieto tās ailē [dienas].
+
   x$dienas1 <-
-    as.numeric(difftime(as.Date(x$beidz_darbu), as.Date(x$sak_darbu), units = "days")) + 1
-  x$dienas <- x$dienas1[(!is.na(x$sak_darbu))]
+    as.numeric(difftime(as.Date(x$beidz), as.Date(x$sak), units = "days")) + 1
+  x$dienas <- x$dienas1[(!is.na(x$sak))]
   x$dienas1 <- NULL
   rm(x_savienotieDivnieki)
   
-  x <- x[order(x$PS_codes, x$DN_codes, x$NM_codes, x$ndz_sanemsanas_datums, x$start), ]
-  
-  #SKAT. Lēmums
+  x <- x[order(x$PS_code, x$DN_code, x$NM_code, x$NDZ_sanemsanas_datums, x$start), ]
+
   if("40" %in% x$zinkod || "41" %in% x$zinkod || "50" %in% x$zinkod || "51" %in% x$zinkod || "53" %in% x$zinkod || "54" %in% x$zinkod || "91" %in% x$zinkod || "92" %in% x$zinkod) {
     x$dienas <- x$dienas-1
   } else {
     cat("Šajā tabulā nav kodu 40 un 41 vai 50 un 51, vai 53 un 54, vai 91 un 92.")
   }
+  
+  # Kods 26
+  for (b in 1:nrow(x)){
+    ifelse(x$zinkod[b] == "26", x$dienas[b] <- 0, x$dienas[b]) 
+  }
+  rm(b)
   
   return(x)
 }
