6ebf9972591cc44ea3d76440800fd73fd5ee209a
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-04-22 12:02:31 +0300

    Update starpkodi2_vienadiDatumi.R

--- NDZ_kodi11/functions/2_starpkoduApstrade/starpkodi2_vienadiDatumi.R
+++ NDZ_kodi11/functions/2_starpkoduApstrade/starpkodi2_vienadiDatumi.R
@@ -1,23 +1,28 @@
 starpkodi2_vienadiDatumi <- function(y2, t, prev, v) {
+
 if ((t$zinkod[1] == "11" && t$zinkod[2]  %in% c("40", "50")) ||
     (t$zinkod[1] == "21" && t$zinkod[2] %in% c("51", "41", "92")) ||
     (t$zinkod[1] == "22" && t$zinkod[2] == "51") ||
     (t$zinkod[1] == "23" && t$zinkod[2] == "41") ||   
     (t$zinkod[1] == "24" && t$zinkod[2] == "51") ||   
-    (t$zinkod[1] == "25" && t$zinkod[2] %in% c("41", "54", "51", "50")) ||
+    (t$zinkod[1] == "25" && t$zinkod[2] %in% c("41", "54", "51", "50", "92")) ||
     (t$zinkod[1] == "40" && t$zinkod[2] == "51") ||
     (t$zinkod[1] == "41" && t$zinkod[2] == "50") ||
     (t$zinkod[1] == "51" && t$zinkod[2] == "53") ||
     (t$zinkod[1] == "92" && t$zinkod[2] == "50")) {
+
   yt <- y2[v,] 
   yt$dienas <- 0
 } else if (t$zinkod[1] == "21" && t$zinkod[2] == "53"){
+
   yt <- y2[v:(v + 1),]
   yt <- yt[yt$zinkod == "11",]
 } else if (t$zinkod[1] == "21" && t$zinkod[2] == "50") {
+
   yt <- y2[v:(v+1),] 
-  yt <- yt[yt$zinkod == "50", ] #jo ambiguity
+  yt <- yt[yt$zinkod == "50", ]
 } else if (t$zinkod[1] == "25" && t$zinkod[2] == "53") {
+
   yt <- y2[v:(v+1),] 
   yt <- yt[yt$zinkod == "53", ]
 } else {
