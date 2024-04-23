96049121ebbab088db39f05ff7b5039a28d92f44
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-04-23 11:48:04 +0300

    Update tripletkodi5.R

--- NDZ_kodi11/functions/2_starpkoduApstrade/tripletkodi5.R
+++ NDZ_kodi11/functions/2_starpkoduApstrade/tripletkodi5.R
@@ -6,9 +6,19 @@ tripletkodi5 <- function(y3, t, prev, v) {
       t$NDZ_sanemsanas_datums[2] != t$NDZ_sanemsanas_datums[3] &&
       t$NDZ_sanemsanas_datums[3] == t$NDZ_sanemsanas_datums[4] &&
       t$NDZ_sanemsanas_datums[4] != t$NDZ_sanemsanas_datums[5]) {
-
     yt <- y3[v, ]
     yt$dienas <- 0
+  } else if (t$zinkod[1] == "50" && t$zinkod[2] == "51" && 
+       t$zinkod[3] == "53" && t$zinkod[4] == "54" && t$zinkod[5] == "21" && 
+       t$NDZ_sanemsanas_datums[2] == t$NDZ_sanemsanas_datums[3] &&
+       t$NDZ_sanemsanas_datums[1] != t$NDZ_sanemsanas_datums[2] &&
+       all(diff(t$NDZ_sanemsanas_datums[3:5]) != 0)) {
+    days1 <- as.numeric(difftime(t$beidz[1], prev, units = "days")) - 1
+    days2 <- as.numeric(difftime(t$beidz[5], t$sak[4], units = "days")) + 1 
+    
+    yt <- y3[v, ]
+    yt$dienas <- sum(days1, days2)
+    rm(days1, days2)
   } else {
     stop("Tripletkodi5 iztrūkst apstrādes koda.")
   }
