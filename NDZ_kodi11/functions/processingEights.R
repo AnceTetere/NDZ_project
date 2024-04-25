141ef63b746559df4067b1ed538ec2c7899b9318
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-04-25 15:07:01 +0300

    Update processingEights.R

--- NDZ_kodi11/functions/processingEights.R
+++ NDZ_kodi11/functions/processingEights.R
@@ -1,8 +1,9 @@
 processingEights <- function(x, o) {
-  x <- x[order(x$PS_code, x$DN_code, x$NM_code, x$NDZ_sanemsanas_datums, x$start), ]
+  x <- x[order(x$PS_code, x$DN_code, x$NM_code, x$NDZ_sanemsanas_datums), ]
   
   x8_uzVieniniekiem <- data.frame()
   x8_uzCetriniekiem <- data.frame()
+  x8_uzSesi <- data.frame()
   x8_uzSeptini <- data.frame()
   check_rows <- 0
   
@@ -32,9 +33,16 @@ processingEights <- function(x, o) {
                all(diff(x8$NDZ_sanemsanas_datums[1:3]) != 0) && all(diff(x8$NDZ_sanemsanas_datums[4:6]) != 0) &&
                all(diff(x8$NDZ_sanemsanas_datums[3:4]) == 0) && all(diff(x8$NDZ_sanemsanas_datums[6:7]) == 0)) {
       x8_uzCetriniekiem <- rbind(x8_uzCetriniekiem, x8)
+    } else if (all(x8$start[c(1, 3:4, 6:8)] == "1") && 
+               all(diff(x8$NDZ_sanemsanas_datums[1:7]) != 0) && all(diff(x8$NDZ_sanemsanas_datums[7:8]) == 0) &&
+               x8$PS_code[1] == '----------' && x8$NM_code[1] == '----------') {
+
+      x8_uzVieniniekiem <- rbind(x8_uzVieniniekiem, x8[1,])
+    } else if (all(x8$start[c(1, 3, 5, 7, 8)] == "1") && all(diff(x8$NDZ_sanemsanas_datums) != 0)){
+      x8_uzVieniniekiem <- rbind(x8_uzVieniniekiem, x8[8,])
+      x8_uzSesi <- rbind(x8_uzSesi, x8[1:6,])
     } else {
-      stop(cat("Astoņnieku apstrādē nosacījums neizpildās attiecībā uz rindām", r, "līdz", r + 8,".
-             Iespējams, ka pietrūkst izstrādes koda."))
+      stop("processingEights: Astoņnieku apstrādē iztrūkst kods rindām", r, "līdz", r + 8,".\n")
     }
     check_rows <- check_rows + 8
   }
@@ -49,7 +57,7 @@ processingEights <- function(x, o) {
   
   #4) Apakštabulu x8_uzVieniniekiem sūta caur processingOnes().
   if(nrow(x8_uzVieniniekiem) > 0) {
-    x8_uzVieniniekiem <- x8_uzVieniniekiem [order(x8_uzVieniniekiem$PS_code, x8_uzVieniniekiem$NM_code, x8_uzVieniniekiem$NDZ_sanemsanas_datums, x8_uzVieniniekiem$start), ]
+    x8_uzVieniniekiem <- x8_uzVieniniekiem [order(x8_uzVieniniekiem$PS_code, x8_uzVieniniekiem$NM_code, x8_uzVieniniekiem$NDZ_sanemsanas_datums), ]
     sendTo_tempNDZ(processingOnes(x8_uzVieniniekiem, o))
     cat("No astoņniekiem atvasinātā tabula x8_uzVieniniekiem pārsūtīta apstrādei caur processingOnes().\n")
   } else {
@@ -60,7 +68,7 @@ processingEights <- function(x, o) {
   
   #5) Apakštabulu x8_uzCetriniekiem sūta caur processingFours.
   if(nrow(x8_uzCetriniekiem) > 0) {
-    x8_uzCetriniekiem <- x8_uzCetriniekiem[order(x8_uzCetriniekiem$PS_code, x8_uzCetriniekiem$NM_code, x8_uzCetriniekiem$NDZ_sanemsanas_datums, x8_uzCetriniekiem$start), ]
+    x8_uzCetriniekiem <- x8_uzCetriniekiem[order(x8_uzCetriniekiem$PS_code, x8_uzCetriniekiem$NM_code, x8_uzCetriniekiem$NDZ_sanemsanas_datums), ]
     processingFours(x8_uzCetriniekiem, o)
     cat("No astoņniekiem atvasinātā tabula x8_uzCetriniekiem pārsūtīta apstrādei caur processingFours.\n")
   } else {
@@ -69,9 +77,20 @@ processingEights <- function(x, o) {
   
   rm(x8_uzCetriniekiem)
   
-  #6) Apakštabulu x8_uzSeptini sūta caur processingSeven().
+  #6) Apakštabulu x8_uzSesi sūta caur processingSixes().
+  if(nrow(x8_uzSesi) > 0) {
+    x8_uzSesi <- x8_uzSesi[order(x8_uzSesi$PS_code, x8_uzSesi$NM_code, x8_uzSesi$NDZ_sanemsanas_datums), ]
+    processingSixes(x8_uzSesi, o)
+    cat("No astoņniekiem atvasinātā tabula x8_uzSesi pārsūtīta apstrādei caur processingSixes.\n")
+  } else {
+    cat("Tabula x8_uzSesi ir tukša.\n")
+  }
+  
+  rm(x8_uzSesi)
+  
+  #7) Apakštabulu x8_uzSeptini sūta caur processingSeven().
   if(nrow(x8_uzSeptini) > 0) {
-    x8_uzSeptini <- x8_uzSeptini[order(x8_uzSeptini$PS_code, x8_uzSeptini$NM_code, x8_uzSeptini$NDZ_sanemsanas_datums, x8_uzSeptini$start), ]
+    x8_uzSeptini <- x8_uzSeptini[order(x8_uzSeptini$PS_code, x8_uzSeptini$NM_code, x8_uzSeptini$NDZ_sanemsanas_datums), ]
     processingSeven(x8_uzSeptini, o)
     cat("No astoņniekiem atvasinātā tabula x8_uzSeptini pārsūtīta apstrādei caur processingSeven.\n")
   } else {
