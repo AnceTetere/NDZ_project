e3b0763dbeb63e6c1612ad8782cfd52cd0ea3e68
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-04-29 17:45:42 +0300

    Update starpkodi3.R

--- NDZ_kodi11/functions/2_starpkoduApstrade/starpkodi3.R
+++ NDZ_kodi11/functions/2_starpkoduApstrade/starpkodi3.R
@@ -26,7 +26,7 @@ starpkodi3 <- function(y2, t, prev, v) {
                t$NDZ_sanemsanas_datums[1] != t$NDZ_sanemsanas_datums[2] &&
                t$NDZ_sanemsanas_datums[2] == t$NDZ_sanemsanas_datums[3]) {
       yt <- y2[v, ]
-      yt$dienas <- as.numeric(difftime(t$beidz[1], prev, units = "days")) - 1 
+      yt$dienas <- as.numeric(difftime(t$beidz[1], prev, units = "days")) - 1
     } else if (t$zinkod[1] == "40" && t$zinkod[2] == "25" &&
                t$zinkod[3] == "41" && 
                all(!diff(t$NDZ_sanemsanas_datums[1:2]) == 0)&&
@@ -42,7 +42,7 @@ starpkodi3 <- function(y2, t, prev, v) {
       yt$dienas <- sum(days1, days2)
       rm(days1, days2)
     } else if (t$zinkod[1] == "92" && t$zinkod[2] == "25" && t$zinkod[3] == "11" && all(diff(t$NDZ_sanemsanas_datums) != 0)) {
-      days1 <- as.numeric(difftime(t$beidz[2], t$sak[1], units = "days")) + 1  
+      days1 <- as.numeric(difftime(t$beidz[2], t$sak[1], units = "days")) + 1 
       days2 <- as.numeric(difftime(t$last_date[3], t$sak[3], units = "days"))  + 1 
       
       yt <- y2[v, ]
@@ -55,16 +55,21 @@ starpkodi3 <- function(y2, t, prev, v) {
     } else if (t$zinkod[1] == "41" && t$zinkod[2] == "50" && t$zinkod[3] == "51" && 
                all(diff(t$NDZ_sanemsanas_datums) != 0)) {
       days1 <- as.numeric(difftime(t$beidz[2], t$sak[1], units = "days")) 
-      days2 <- as.numeric(difftime(t$last_date[3], t$sak[3], units = "days"))  + 1
+      days2 <- as.numeric(difftime(t$last_date[3], t$sak[3], units = "days"))  + 1 
       
       yt <- y2[v, ]
       yt$dienas <- sum(days1, days2)
       rm(days1, days2)
     } else if (t$zinkod[1] == "54" && t$zinkod[2] == "53" && t$zinkod[3] == "25" && 
                all(diff(t$NDZ_sanemsanas_datums[1:2]) == 0) &&
-               all(diff(t$NDZNDZ_sanemsanas_datums[2:3]) != 0)) {
+               all(diff(t$NDZ_sanemsanas_datums[2:3]) != 0)) {
       yt <- y2[v, ]
       yt$dienas <- 0
+    } else if (t$zinkod[1] == "21" && t$zinkod[2] == "50" && t$zinkod[3] == "51" && 
+               all(diff(t$NDZ_sanemsanas_datums[2:3]) == 0) &&
+               all(diff(t$NDZ_sanemsanas_datums[1:2]) != 0)) {
+      yt <- y2[v:(v+1), ]
+      yt <- yt[yt$zinkod == "11", ]
     } else {
     stop("Starpkodi3: Trūkst izstrādes koda.")
     }
