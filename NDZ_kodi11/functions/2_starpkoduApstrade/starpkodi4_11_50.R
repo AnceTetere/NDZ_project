bfc30c4dd536884a986a3df50d25cf8db2bf21fc
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-05-21 11:23:19 +0300

    Update starpkodi4_11_50.R

--- NDZ_kodi11/functions/2_starpkoduApstrade/starpkodi4_11_50.R
+++ NDZ_kodi11/functions/2_starpkoduApstrade/starpkodi4_11_50.R
@@ -2,10 +2,15 @@ starpkodi4_11_50 <- function(y2, t, prev, v) {
   
   if(t$zinkod[3] == "51") {
     yt <- starpkodi4_11_50_51(y2, t, prev, v)
-  }
-  if (t$NDZ_sanemsanas_datums[1] == t$NDZ_sanemsanas_datums[2] && t$zinkod[3] == "51" && t$zinkod[4] == "21" && t$NDZ_sanemsanas_datums[3] != t$NDZ_sanemsanas_datums[4]) {
+  } else if (t$zinkod[3] == "50" && t$zinkod[4] == "51" && 
+             all(sapply(c(1,3), function(i) all(diff(t$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) &&
+             all(diff(t$NDZ_sanemsanas_datums[2:3]) == 0)) {
+    days1 <- as.numeric(difftime(t$beidz[2], t$sak[1], units = "days"))
+    days2 <- as.numeric(difftime(t$last_date[4], t$sak[4], units = "days")) + 1
+    
     yt <- y2[v, ]
-    yt$dienas <- as.numeric(difftime(t$beidz[4], t$sak[3], units = "days"))
+    yt$dienas <- sum(days1, days2)
+    rm(days1, days2)
   } else if (t$zinkod[3] == "25" && t$zinkod[4] == "51" && t$NDZ_sanemsanas_datums[1] != t$NDZ_sanemsanas_datums[2] && t$NDZ_sanemsanas_datums[2] != t$NDZ_sanemsanas_datums[3] &&
              t$NDZ_sanemsanas_datums[3] == t$NDZ_sanemsanas_datums[4]) {
     yt <- y2[v, ]
@@ -35,14 +40,6 @@ starpkodi4_11_50 <- function(y2, t, prev, v) {
              diff(t$NDZ_sanemsanas_datums[2:3]) != 0) {
     yt <- y2[v, ]
     yt$dienas <- as.numeric(difftime(t$last_date[2], t$sak[4], units = "days")) + 1
-  } else if (t$zinkod[3] == "51" && t$zinkod[4] == "50" && 
-            all(diff(t$NDZ_sanemsanas_datums) != 0)) {
-    days1 <- as.numeric(difftime(t$beidz[2], t$sak[1], units = "days"))
-    days2 <- as.numeric(difftime(t$beidz[4], t$sak[3], units = "days"))
-    
-    yt <- y2[v, ]
-    yt$dienas <- sum(days1, days2)
-    rm(days1, days2)
   } else {
     stop("Starpkodi4_11_50: Trūkst izstrādes koda.")
   }
