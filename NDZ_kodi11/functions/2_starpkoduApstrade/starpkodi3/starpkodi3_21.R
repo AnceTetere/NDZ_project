b0b4199a38a85ccc706e09e2b182daf98574a65c
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-05-27 16:16:09 +0300

    Update starpkodi3_21.R

--- NDZ_kodi11/functions/2_starpkoduApstrade/starpkodi3/starpkodi3_21.R
+++ NDZ_kodi11/functions/2_starpkoduApstrade/starpkodi3/starpkodi3_21.R
@@ -1,34 +1,28 @@
 starpkodi3_21 <- function(y2, t, prev, v) {
   
   if (t$zinkod[2] == "50" && t$zinkod[3] == "51" && all(diff(t$NDZ_sanemsanas_datums) == 0)) {
-    #Indivīds it kā iet bezalgas atvaļinājumā, bet tomēr aiziet no darba pavisam.
     yt <- y2[v, ]
     yt$dienas <- as.numeric(difftime(t$beidz[1], prev, units = "days"))
   } else if (t$zinkod[2] == "50" && t$zinkod[3] == "51" && all(diff(t$NDZ_sanemsanas_datums[2:3]) == 0) &&
               all(diff(t$NDZ_sanemsanas_datums[1:2]) != 0)) {
-    #Bezalgas atvaļinājums sākas un beidzas tanī pat dienā.
-    #Tam tur nevajadzētu būt, jo indivīds jau ir aizgājis no darba.
     yt <- y2[v:(v+1), ]
     yt <- yt[yt$zinkod == "11", ]
   } else if (t$zinkod[2] == "50" && t$zinkod[3] == "51" && all(diff(t$NDZ_sanemsanas_datums) != 0)) {
-    #Nezinu, kaut kāds bezalgas atvaļinājums te cilvēkam paņēmies pēc aiziešanas no darba. Ignorēju to.
     yt <- y2[v:(v+1), ]
     yt <- yt[yt$zinkod == "11", ]
   } else if (t$zinkod[2] == "50" && t$zinkod[3] == "51" && all(diff(t$NDZ_sanemsanas_datums[2:3]) != 0) && all(diff(t$NDZ_sanemsanas_datums[1:2]) == 0)) {
-    #Nezinu, kaut kāds bezalgas atvaļinājums te cilvēkam paņēmies pēc aiziešanas no darba. Ignorēju to.
     yt <- y2[v:(v+1), ]
     yt <- yt[yt$zinkod == "11", ]
   } else if (t$zinkod[2] == "51" && t$zinkod[3] == "51" && all(diff(t$NDZ_sanemsanas_datums[2:3]) != 0) && all(diff(t$NDZ_sanemsanas_datums[1:2]) == 0)) {
-    #Dienā, kad jābeidzas bezalgas atvaļinājumam, indivīds aiziet no tā.
     yt <- y2[v:(v+1), ]
     yt <- yt[yt$zinkod == "11", ]
   } else if (t$zinkod[2] == "51" && t$zinkod[3] == "11" && 
              diff(t$NDZ_sanemsanas_datums[2:3]) != 0 && diff(t$NDZ_sanemsanas_datums[1:2]) == 0) {
-    #Dienā, kad jābeidzas bezalgas atvaļinājumam, indivīds aiziet no darba.
-    #Tad tiek pieņemts darbā no jauna.
-    #Pieņemu, ka pēc atvaļinājuma indivīds neatgriezās darbā, bet paziņoja par aiziešanu no tā.
     yt <- y2[v, ]
     yt$dienas <- as.numeric(difftime(t$last_date[3], t$sak[3], units = "days"))
+  } else if (t$zinkod[2] == "92" && t$zinkod[3] == "41" && all(diff(t$NDZ_sanemsanas_datums) == 0)) {
+    yt <- y2[v, ]
+    yt$dienas <- 0
   } else {
     stop("Starpkodi3_21: Trūkst izstrādes koda.")
   }
@@ -36,3 +30,4 @@ starpkodi3_21 <- function(y2, t, prev, v) {
   if(is.na(yt$PS_code[1])) {stop("Dienas NA.")}
   return(yt) 
 }
+
