10f39d88d506d4f23712644a601139d3648054cd
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-05-28 18:43:43 +0300

    Create starpkodi3_25.R

---
+++ NDZ_kodi11/functions/2_starpkoduApstrade/starpkodi3/starpkodi3_25.R
@@ -0,0 +1,51 @@
+starpkodi3_25 <- function(y2, t, prev, v) {
+  if (t$zinkod[2] == "51" && t$zinkod[3] == "50" && all(t$NDZ_sanemsanas_datums == t$NDZ_sanemsanas_datums[1])) {
+    yt <- y2[v:(v+1), ]
+    yt <- yt[yt$zinkod == "11", ]
+  } else if (t$zinkod[2] == "51" && t$zinkod[3] == "11" && t$NDZ_sanemsanas_datums[1] == t$NDZ_sanemsanas_datums[2] &&
+             t$NDZ_sanemsanas_datums[2] != t$NDZ_sanemsanas_datums[3]) {
+    
+    yt <- y2[v, ]
+    yt$dienas <- as.numeric(difftime(as.Date(t$last_date[3]), as.Date(t$sak[3]), units = "days")) + 1 + 1  
+  } else if (t$zinkod[2] == "50" && t$zinkod[3] == "51" && all(diff(t$NDZ_sanemsanas_datums) == 0)) {
+    yt <- y2[v, ]
+    yt$dienas <- as.numeric(difftime(t$beidz[1], prev, units = "days")) - 1 
+  } else if (t$zinkod[2] == "41" && t$zinkod[3] == "11" && 
+             all(diff(t$NDZ_sanemsanas_datums[1:2]) == 0) && 
+             all(diff(t$NDZ_sanemsanas_datums[2:3]) != 0)) {
+    yt <- y2[v, ]
+    yt$dienas <- as.numeric(difftime(t$last_date[3], t$sak[3], units = "days")) + 1 
+  } else if (t$zinkod[2] == "11" && t$zinkod[3] == "50" && all(diff(t$NDZ_sanemsanas_datums) != 0)) {
+    days1 <- as.numeric(difftime(t$beidz[1], prev, units = "days"))
+    days2 <- as.numeric(difftime(t$beidz[3], t$sak[2], units = "days"))
+    
+    yt <- y2[v, ]
+    yt$dienas <- sum(days1, days2)
+    rm(days1, days2)
+  } else if (t$zinkod[2] == "51" && t$zinkod[3] == "51" && all(diff(t$NDZ_sanemsanas_datums[2:3]) != 0) && all(diff(t$NDZ_sanemsanas_datums[1:2]) == 0)) {
+    yt <- y2[v:(v+1), ]
+    yt <- yt[yt$zinkod == "11", ]
+  } else if (t$zinkod[2] == "50" && t$zinkod[3] == "51" && all(diff(t$NDZ_sanemsanas_datums[2:3]) != 0) && all(diff(t$NDZ_sanemsanas_datums[1:2]) == 0)) {
+    yt <- y2[v, ]
+    yt$dienas <- as.numeric(difftime(t$beidz[1], prev, units = "days")) - 1 #jo ambiguous
+  } else if (t$zinkod[2] %in% c("92","41") && t$zinkod[3] %in% c("92","41") && all(diff(t$NDZ_sanemsanas_datums) == 0)) {
+    yt <- y2[v, ]
+    yt$dienas <- 0
+  } else if (t$zinkod[2] == "41" && t$zinkod[3] == "11" && all(diff(t$NDZ_sanemsanas_datums) != 0)) {
+    yt <- y2[v:(v+1), ]
+    yt <- yt[yt$zinkod == "11", ]
+  } else if (t$zinkod[2] == "41" && t$zinkod[3] == "41" && diff(t$NDZ_sanemsanas_datums[1:2]) == 0 &&
+             diff(t$NDZ_sanemsanas_datums[2:3]) != 0) {
+    yt <- y2[v:(v+1), ]
+    yt <- yt[yt$zinkod == "11", ]
+  } else if (t$zinkod[2] == "11" && t$zinkod[3] == "50" && diff(t$NDZ_sanemsanas_datums[2:3]) == 0 &&
+             diff(t$NDZ_sanemsanas_datums[1:2]) != 0) {
+    yt <- y2[v, ]
+    yt$dienas <- as.numeric(difftime(t$beidz[1], prev, units = "days"))
+  } else {
+    stop("Starpkodi3_25: Trūkst izstrādes koda.")
+  }
+
+  if(is.na(yt$PS_code[1])) {stop("Dienas NA.")}
+  return(yt) 
+}
