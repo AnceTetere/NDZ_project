ca3bb28cdf80f2853e8abfc7ab06070f661ae543
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-07-31 17:27:31 +0300

    Create calculations.R

---
+++ NDZ_kodi11/scripts/calculations.R
@@ -0,0 +1,6 @@
+for(kods in kodu_vektors) {
+  source(paste0(path, "R\\scripts\\1_NDZ_original.R"))
+  source(paste0(path, "R\\scripts\\2_processingCodes.R"))
+}
+
+source(paste0(path, "R\\scripts\\3_months.R"))
