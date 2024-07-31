619500b68b2b61f11af28c4e9688fd8751cfa532
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-07-31 17:21:23 +0300

    Update create_tempNDZ.R

--- NDZ_kodi11/functions/create_tempNDZ.R
+++ NDZ_kodi11/functions/create_tempNDZ.R
@@ -5,16 +5,11 @@ tempNDZ <- function(){
     PS_code = character(),
     DN_code = character(),
     NM_code = character(),
-    prof = character(),
     dienas = integer()
   )
   
-  setwd(paste0(path, "data\\intermediate_tables\\"))
-  save(temp_NDZ, file = "temp_NDZ.RData")
-  rm(temp_NDZ)
-  
   #Izveido tukšu vektoru, kas skaitīs rindas, kas ievietotas temp_NDZ tabulā
   temp_rows <- vector()
-  saveRDS(temp_rows, file = "temp_rows.RDS")
-  rm(temp_rows)
+  
+  return(list(temp_NDZ = temp_NDZ, temp_rows = temp_rows))
 }
