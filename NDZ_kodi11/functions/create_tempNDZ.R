11606e422c72861af74844b9e4ba3842ff342828
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-07-31 16:55:40 +0300

    Create create_tempNDZ.R

---
+++ NDZ_kodi11/functions/create_tempNDZ.R
@@ -0,0 +1,20 @@
+tempNDZ <- function(){
+  #Izveido tukšo tabulu, kurā būvēsi gala tabulu
+  temp_NDZ <- data.frame(
+    period = character(), 
+    PS_code = character(),
+    DN_code = character(),
+    NM_code = character(),
+    prof = character(),
+    dienas = integer()
+  )
+  
+  setwd(paste0(path, "data\\intermediate_tables\\"))
+  save(temp_NDZ, file = "temp_NDZ.RData")
+  rm(temp_NDZ)
+  
+  #Izveido tukšu vektoru, kas skaitīs rindas, kas ievietotas temp_NDZ tabulā
+  temp_rows <- vector()
+  saveRDS(temp_rows, file = "temp_rows.RDS")
+  rm(temp_rows)
+}
