4d696e56291d53a2ba793889e86711e5e398bcce
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-07-31 16:08:54 +0300

    Update occurencesSplit.R

--- NDZ_kodi11/functions/occurencesSplit.R
+++ NDZ_kodi11/functions/occurencesSplit.R
@@ -3,22 +3,22 @@ occurencesSplit <- function(NDZ) {
 setwd(paste0(path, "data\\intermediate_tables\\"))
 check_number <- nrow(NDZ)
 occurance_number <- 1
-tabs_NDZ <- vector("character")
+tabs_ndz <- vector("character")
 
 while (check_number > 0) {
   
   x <- NDZ %>%
-    group_by(ps_code, dn_code, nm_code) %>%
+    group_by(PS_code, DN_code, NM_code) %>%
     filter(n() == occurance_number) %>%
     ungroup() %>%
-    arrange(ps_code, NDZ_sanemsanas_datums)
+    arrange(PS_code, NDZ_sanemsanas_datums)
   
   tab_name <- paste0("NDZ_", occurance_number)
   
   if(nrow(x) > 0) {
     assign(tab_name, x)
     check_number <- check_number - nrow(x)
-    tabs_NDZ <- append(tabs_NDZ, tab_name)
+    tabs_ndz <- append(tabs_ndz, tab_name)
     
     save(list = tab_name, file = paste0(tab_name, ".RData"))
     rm(list = tab_name) 
@@ -28,9 +28,9 @@ while (check_number > 0) {
   occurance_number <- occurance_number + 1
 }
 
-saveRDS(tabs_NDZ, file = "tabs_NDZ.rds")
+saveRDS(tabs_ndz, file = "tabs_ndz.rds")
 rm(NDZ, check_number, occurance_number)
 
-return(tabs_NDZ)
+return(tabs_ndz)
 
 }
