fd3b2d218f772b27ff5f290fba68769e9b3f392b
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-08-01 16:04:25 +0300

    Update sendTo_tempNDZ.R

--- NDZ_kodi11/functions/sendTo_tempNDZ.R
+++ NDZ_kodi11/functions/sendTo_tempNDZ.R
@@ -1,5 +1,8 @@
+#Funkcija sendTo_tempNDZ() sasummē dienas uz oriģinālo indivīdu mēnesī,
+#veic papildus pārbaudes un pievieno apstrādātās rindas izstrādes tabulai temp_NDZ, ko šis kods būvē.
+
 sendTo_tempNDZ <- function(x) {
-  x <- x[order(x$PS_code, x$DN_code, x$NM_code, x$NDZ_sanemsanas_datums, x$start), ]
+  x <- x[order(x$PS_code, x$DN_code, x$NM_code, x$NDZ_sanemsanas_datums, x$sak_beidz), ]
   x <- x[ , c("period", "PS_code", "DN_code", "NM_code", "dienas")]
   
   x <- x %>%
@@ -9,23 +12,19 @@ sendTo_tempNDZ <- function(x) {
     ) %>%
     arrange(PS_code)
 
-  if ((nrow(x[duplicated(x[c("PS_code", "NM_code")]), ]) == 0) && (sum(x$dienas < 0) == 0)) {
-    setwd(paste0(path, "data\\intermediate_tables\\"))
-    load("temp_NDZ.RData")
+  if (nrow(x[duplicated(x[c("PS_code", "NM_code")]), ]) == 0 && sum(x$dienas < 0) == 0) {
+    #setwd(paste0(path, "data\\intermediate_tables\\"))
+    #load("temp_NDZ.RData")
     
-    temp_NDZ <-
-      rbind(temp_NDZ, x[, c("period",
-                            "PS_code",
-                            "DN_code",
-                            "NM_code",
-                            "dienas")])
-    save(temp_NDZ, file = "temp_NDZ.RData")
+    temp_NDZ <- rbind(temp_NDZ, x[, c("period", "PS_code", "DN_code", "NM_code", "dienas")])
+    #save(temp_NDZ, file = "temp_NDZ.RData")
     
-    temp_rows <- readRDS("temp_rows.rds")
+    #temp_rows <- readRDS("temp_rows.rds")
     temp_rows <- append(temp_rows, nrow(x))
-    saveRDS(temp_rows, file = "temp_rows.RDS")
-    rm(temp_NDZ, x, temp_rows)
+    #saveRDS(temp_rows, file = "temp_rows.RDS")
+    #rm(temp_NDZ, x, temp_rows)
+    rm(x)
   } else {
-    stop(cat("Tabulā ir dubultnieki vai negatīvas dienas. o ir", o, ".\n"))
+    stop("Tabulā ir dubultnieki vai negatīvas dienas. o ir", o, ".\n")
   }
 }
