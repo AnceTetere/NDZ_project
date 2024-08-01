df457cc74b2349cbfce61199b99957cc6d6af673
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-08-01 16:55:08 +0300

    Update 2_processingCodes.R

--- NDZ_kodi11/scripts/2_processingCodes.R
+++ NDZ_kodi11/scripts/2_processingCodes.R
@@ -1,38 +1,46 @@
-#NDZ <- get(kodu_tab_nos)
-#rm(list = kodu_tab_nos)  
-
+#1 Funkcija occurencesSplit iziet cauri visai mēneša datnei un sadala to 
+#  apakštabulās atkarībā no tā, cik reizes indivīds uzrādas dotajā mēnesī.
+#  Šo tabulu nosaukumi iekabinās vektorā tabs_ndz, kur pēdējais cipars nosaukumā norāda
+#  rindu skaitu, ko aizņem viens indivīds.
   result <- occurencesSplit(NDZ)
-  tabs_zdn <- result$tabs_zdn
+  tabs_ndz <- result$tabs_ndz
   NDZ_list <- result$NDZ_list
   rm(NDZ)
-  
+
+#2 Funkcija tempNDZ izveido tukšu datu rāmi. 
+#  Tā sevī uzkrās no apakštabulām sarēķinātās tabulas ar dienu skaitu.
+#  Vektors temp_rows dokumentē, cik rindas katrā solī tiek pievienotas datnei temp_NDZ.
+#  (Tā ir pirmā riņķa tabula, kurā būs gan pareizi sarēķini, gan tie, kas dublēsies.)  
   tNDZ <- tempNDZ()
   temp_NDZ <- tNDZ$temp_NDZ
   temp_rows <- tNDZ$temp_rows
   rm(tNDZ)
   
-  #tabs_zdn <- readRDS("tabs_zdn.rds")
-  #Tabulu pēdējais cipars norāda uz to, cik reizes vienā uzņēmumā strādājošs indivīds uzrādas
-  #dotajā tabulā. Nolasa to un saglabā uz burta 'o' (no vārda 'occurences')
 
-for(i in 1:length(tabs_zdn)) {
+#3 No funkcijas occurencesSplit atgriezto tabulu nosaukumos pēdējais cipars norāda, 
+#  cik reizes vienā uzņēmumā strādājošais indivīds uzrādas dotajā tabulā.
+#  To nolasa un saglabā uz burta 'o' (no vārda 'occurences').
+  
+#3.1 Attiecīgi, ejot cauri tabulu nosaukumu vektoram tabs_ndz, nolasa o un ielādē atbilstošo tabulu.
+#    Vadoties pēc, to sūta tālāk uz apstrādi.
+
+ #i <- 1 #testēšanai
+#for(i in 1:length(tabs_ndz)) {
+# Ielādē attiecīgo tabulu  
   x <- NDZ_list[[i]]
-  assign(tabs_zdn[i], x)
-  rm(x)
-    if(nchar(tabs_zdn[i]) == 5) {
-      o <- substr(tabs_zdn[i], 5, 5)
-    } else if (nchar(tabs_zdn[i]) == 6) {
-      o <- substr(tabs_zdn[i], 5, 6)
+  
+    if(nchar(tabs_ndz[i]) == 5) {
+      o <- substr(tabs_ndz[i], 5, 5)
+    } else if (nchar(tabs_ndz[i]) == 6) {
+      o <- substr(tabs_ndz[i], 5, 6)
     } else {
-      stop("Neatbilstošs tabulas nosaukuma garums") #TODO Izformē šo
+      stop("Neatbilstošs tabulas nosaukuma garums")
     }
-
-    o <- as.character(o) ###############!!!!!Paliku te
     
     switch(   
       o,
-      #1. Sarēķini dienas un noglabā gala tabulā, kuru būvē.
-      "1"= sendTo_tempNDZ(processingOnes(loadTable(o), o)),
+      # Caur sekojošo apstrādi sarēķina dienas un noglabā tabulā temp_NDZ, kuru būvē.
+      "1"= temp_NDZ <- sendTo_tempNDZ(processingOnes(x, o), temp_NDZ),
       "2"= processingTwoes(loadTable(o), o),
       "3"= processingThrees(loadTable(o), o),
       "4"= processingFours(loadTable(o), o),
@@ -58,18 +66,10 @@ for(i in 1:length(tabs_zdn)) {
       "24"= processingTwentyFour(loadTable(o), o),
       "default" = stop("NAV KODA PRIEKŠ NDZ_", o))
   
-    #Nokop aiz sevis.
-    file_name <- paste0("NDZ_", o, ".RData")
-    if (file.exists(file_name)) {
-      file.remove(file_name)
-      print(paste("File", file_name, "deleted successfully./n"))
-    } else {
-      print(paste("File", file_name, "does not exist."))
-    }
-    rm(file_name)
+  temp_rows <- append(temp_rows, nrow(temp_NDZ) - sum(temp_rows))
   }    
   
-  rm(tabs_zdn, o, i)
+  rm(NDZ_list, tabs_ndz, o, i)
 
   #----------------------------BŪVĒJAM PILNO MĒNESI
   
@@ -77,19 +77,19 @@ for(i in 1:length(tabs_zdn)) {
   
   load("temp_NDZ.RData")
   x <- temp_NDZ
-  x <- x[order(x$PS_code, x$dnperk, x$nmrkod), ]
+  x <- x[order(x$PS_code, x$DN_code, x$NM_code), ]
   
   cat("Kodu gala tabulā temp_NDZ pārbauda dubultos indivīdus, ja ir, tad sasummē to dienas.\n")
-  if(sum(duplicated(x[c("PS_code", "dnperk", "nmrkod")])) > 0) {
+  if(sum(duplicated(x[c("PS_code", "DN_code", "NM_code")])) > 0) {
     x <- x %>%
-      group_by(period, PS_code, dnperk, nmrkod) %>%
+      group_by(period, PS_code, DN_code, NM_code) %>%
       summarise(
         dienas = sum(dienas, na.rm = TRUE)
       ) %>%
       arrange(PS_code)
-    cat("Dubltnieki sasummēti.\n")
+    cat("Dubultnieki sasummēti.\n")
   } else {
-    cat("Kodu gala tabulā dubltnieku nav.\n")
+    cat("Kodu gala tabulā dubultnieku nav.\n")
   }
   
   #2. Pārbauda, vai kādam dienu skaits ir vairāk nekā mēnesī dienu.
@@ -121,4 +121,4 @@ for(i in 1:length(tabs_zdn)) {
   
   #4. Nokop aiz sevis.
   setwd(paste0(path, "data\\intermediate_tables\\"))
-  file.remove("tabs_zdn.rds", "temp_rows.RDS", "temp_NDZ.RData")  
+  file.remove("tabs_ndz.rds", "temp_rows.RDS", "temp_NDZ.RData")  
