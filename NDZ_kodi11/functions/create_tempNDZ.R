9c9986289973ff1abe40eba382cfc4a2e4ee8aff
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-08-02 17:03:26 +0300

    Update create_tempNDZ.R

--- NDZ_kodi11/functions/create_tempNDZ.R
+++ NDZ_kodi11/functions/create_tempNDZ.R
@@ -1,19 +1,29 @@
-tempNDZ <- function(){
-  #Izveido tukšo tabulu, kurā būvēsi gala tabulu
-  temp_NDZ <- data.frame(
-    period = character(), 
-    PS_code = character(),
-    DN_code = character(),
-    NM_code = character(),
-    dienas = integer()
-  )
+#Funkcija sendTo_tempNDZ() sasummē dienas uz oriģinālo indivīdu mēnesī,
+#veic papildus pārbaudes un pievieno apstrādātās rindas izstrādes tabulai temp_NDZ, ko šis kods būvē.
+
+sendTo_tempNDZ <- function(x, temp_NDZ) {
+  x <- x[order(x$PS_code, x$DN_code, x$NM_code, x$NDZ_sanemsanas_datums, x$sak_beidz), ]
+  x <- x[ , c("period", "PS_code", "DN_code", "NM_code", "dienas")]
   
-  setwd(paste0(path, "data\\intermediate_tables\\"))
-  save(temp_NDZ, file = "temp_NDZ.RData")
-  rm(temp_NDZ)
-  
-  #Izveido tukšu vektoru, kas skaitīs rindas, kas ievietotas temp_NDZ tabulā
-  temp_rows <- vector()
-  saveRDS(temp_rows, file = "temp_rows.RDS")
-  rm(temp_rows)
+  x <- x %>%
+    group_by(period, PS_code, DN_code, NM_code) %>%
+    summarise(
+      dienas = sum(dienas, na.rm = TRUE)
+    ) %>%
+    arrange(PS_code)
+
+  if (nrow(x[duplicated(x[c("PS_code", "NM_code")]), ]) == 0 && sum(x$dienas < 0) == 0) {
+    setwd(paste0(path, "data\\intermediate_tables\\"))
+    load("temp_NDZ.RData")
+    
+    temp_NDZ <- rbind(temp_NDZ, x[, c("period", "PS_code", "DN_code", "NM_code", "dienas")])
+    save(temp_NDZ, file = "temp_NDZ.RData")
+    
+    temp_rows <- readRDS("temp_rows.rds")
+    temp_rows <- append(temp_rows, nrow(x))
+    saveRDS(temp_rows, file = "temp_rows.RDS")
+    rm(temp_NDZ, x, temp_rows)
+  } else {
+    stop("Tabulā ir dubultnieki vai negatīvas dienas.\n")
+  }
 }
