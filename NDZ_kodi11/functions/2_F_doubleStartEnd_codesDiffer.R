e921efb71f5668b30f6a9bef760c587ca6fe8529
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-08-05 20:41:35 +0300

    Update 2_F_doubleStartEnd_codesDiffer.R

--- NDZ_kodi11/functions/2_F_doubleStartEnd_codesDiffer.R
+++ NDZ_kodi11/functions/2_F_doubleStartEnd_codesDiffer.R
@@ -1,154 +1,124 @@
+#GADĪJUMS 1:
+
 F_doubleStartEnd_codesDiffer <- function(x3) {
   rownames(x3) <- NULL
   
   x3 <- x3[order(x3$PS_code, x3$DN_code, x3$NM_code, x3$NDZ_sanemsanas_datums, x3$zinkod), ]
   x3_finish <- data.frame()
   check_rows <- 0
-  
+
   for (k in seq(1, nrow(x3), by = 2)) {
     if(doublesTest(k, x3)) {
       #Ja neviena no sekojošīem kodiem apakštabulā nav, turpini šajā sekcijā, citādi lec uz nākamo.
-      if(!("40" %in% x3$zinkod || "41" %in% x3$zinkod || "50" %in% x3$zinkod || "51" %in% x3$zinkod || "53" %in% x3$zinkod || "54" %in% x3$zinkod || "91" %in% x3$zinkod || "92" %in% x3$zinkod)) {
-        if (x3$zinkod[k] == '11' && x3$zinkod[k + 1] == '14') {
-          x3_finish <- rbind(x3_finish, x3[k + 1,])
-          check_rows <- check_rows + 2 
-        } else if (x3$zinkod[k] == '14' && x3$zinkod[k + 1] == '11') {
-          x3_finish <- rbind(x3_finish, x3[k,])
+      if(!any(x3$zinkod %in% c("40", "41", "50", "51", "53", "54", "91", "92"))) {
+        if ((x3$zinkod[k] == '11' && x3$zinkod[k + 1] == '14') || (x3$zinkod[k] == '14' && x3$zinkod[k + 1] == '11')) {
+          x3_finish <- rbind(x3_finish, x3[k + (x3$zinkod[k] == '11'),])
           check_rows <- check_rows + 2
-        } else {
-          #print(paste0("Meklējot tabulas x3 rindās kodu 11 un 14 kombinācijas, atzīmēju, ka rindās Nr.", k, " un Nr.", k + 1, " to nav."))
-        } 
-
-        if (x3$NDZ_sanemsanas_datums[k] == x3$NDZ_sanemsanas_datums[k + 1]) {
+        }
+        #GADĪJUMS 2:
+        if (diff(x3$NDZ_sanemsanas_datums[k:(k + 1)]) == 0) {
           #Ja datumi neatšķiras, un rinda k ir 21. kods un rinda k+1 ir 25,
-          if (x3$zinkod[k] == '21' && x3$zinkod[k + 1] == '25') {
+          if ((x3$zinkod[k] == '21' && x3$zinkod[k + 1] == '25') ||
+              (x3$zinkod[k] == '25' && x3$zinkod[k + 1] == '21')){
             #tad tabulā x3_finish iet rinda k+1 ar kodu 25, jo tas anulē kodu 21.
-            x3_finish <- rbind(x3_finish, x3[k + 1,])
-            check_rows <- check_rows + 2
-            #Un otrādi.
-          } else if (x3$zinkod[k] == '25' && x3$zinkod[k + 1] == '21') {
-            #Ja datumi neatšķiras, un rinda k ir 25. kods un rinda k+1 ir 21,
-            #tad tabulā x3_finish iet rinda k ar kodu 25, jo tas anulē kodu 21.
-            x3_finish <- rbind(x3_finish, x3[k,])
+            x3_finish <- rbind(x3_finish, x3[k + (x3$zinkod[k] == '21'),])
             check_rows <- check_rows + 2
           }
           
           #Taču...
-          #ja datumi kodiem atšķiras, tad tik un tā vēlreiz pārbaudam, ka tādi kodi tajās rindās ir.
-        } else if (x3$NDZ_sanemsanas_datums[k] > x3$NDZ_sanemsanas_datums[k + 1] && (x3$zinkod[k] == '21' || x3$zinkod[k] == '25') && (x3$zinkod[k + 1] == '21' || x3$zinkod[k + 1] == '25')) {
+          #ja datumi kodiem atšķiras, tad tik un tā vēlreiz pārbauda, ka tādi kodi tajās rindās ir.
+        } else if (x3$NDZ_sanemsanas_datums[k] > x3$NDZ_sanemsanas_datums[k + 1] && any(x3$zinkod[k:(k+1)] %in% c('21', '25'))) {
           #Tabulā x3_finish liksies tā rinda, kurā vēlaks saņemšanas datums.
           x3_finish <- rbind(x3_finish, x3[k,])
           check_rows <- check_rows + 2
-        } else if (x3$NDZ_sanemsanas_datums[k] < x3$NDZ_sanemsanas_datums[k + 1]) {
-          if ((x3$zinkod[k] == '21' || x3$zinkod[k] == '25') && (x3$zinkod[k + 1] == '21' || x3$zinkod[k + 1] == '25')) {
+        } else if (x3$NDZ_sanemsanas_datums[k] < x3$NDZ_sanemsanas_datums[k + 1] && any(x3$zinkod[k:(k+1)] %in% c('21', '25'))) {
             x3_finish <- rbind(x3_finish, x3[k + 1,])
-            check_rows <- check_rows + 2  
-          } 
+            check_rows <- check_rows + 2
         } else {
           #print("Šajās rindās nav kodi 21 vai 25.")
         }
-
         
-        if (x3$zinkod[k] == '11' && x3$zinkod[k + 1] == '61') {
-          x3_finish <- rbind(x3_finish, x3[k + 1,])
-          check_rows <- check_rows + 2
-        } else if (x3$zinkod[k] == '61' && x3$zinkod[k + 1] == '11') {
-          x3_finish <- rbind(x3_finish, x3[k,])
+        #GADĪJUMS 3: ...
+        if ((x3$zinkod[k] == '11' && x3$zinkod[k + 1] == '61') ||
+            (x3$zinkod[k] == '61' && x3$zinkod[k + 1] == '11')){
+          x3_finish <- rbind(x3_finish, x3[k + (x3$zinkod[k] == '11'),])
           check_rows <- check_rows + 2
         } else {
           #print(paste0("Meklējot tabulas x3 rindās kodu 11 un 61 kombinācijas, atzīmēju, ka rindās Nr.", k," un Nr.", k + 1," to nav."))
         }
-
-        if (x3$zinkod[k] == '26' && x3$zinkod[k + 1] == '25' && x3$NDZ_sanemsanas_datums[k] <= x3$NDZ_sanemsanas_datums[k+1]) {
-          z <- x3[k, ]
-          z$beidz_darbu <- x3$beidz_darbu[k+1]
-          z$NDZ_sanemsanas_datums <- x3$NDZ_sanemsanas_datums[k+1]
+        
+        #GADĪJUMS 4: ...
+        
+        if ((x3$zinkod[k] == '26' && x3$zinkod[k + 1] == '25') ||
+            (x3$zinkod[k] == '25' && x3$zinkod[k + 1] == '26') && 
+            diff(x3$NDZ_sanemsanas_datums[k:(k+1)]) <= 0) {
+          z <- x3[k + (x3$zinkod[k] == '25'), ]
+          z$NDZ_sanemsanas_datums <- x3$NDZ_sanemsanas_datums[k + (x3$zinkod[k] == '25')]
           x3_finish <- rbind(x3_finish, z)
           rm(z)
-          check_rows <- check_rows + 2
-        } else if  (x3$zinkod[k] == '25' && x3$zinkod[k + 1] == '26' && x3$NDZ_sanemsanas_datums[k] <= x3$NDZ_sanemsanas_datums[k+1]) {
-          x3_finish <- rbind(x3_finish, x3[k + 1,])
           check_rows <- check_rows + 2
         } else {
           #print(paste0("Meklējot tabulas x3 rindās kodu 25 un 26 kombinācijas, atzīmēju, ka rindās Nr.", k," un Nr.", k + 1," to nav."))
         }
-      } else {}
-
-      
-      if ((x3$zinkod[k] == '40' && x3$zinkod[k + 1] == '41') || (x3$zinkod[k] == '50' && x3$zinkod[k + 1] == '51' || (x3$zinkod[k] == '53' && x3$zinkod[k + 1] == '54') || (x3$zinkod[k] == '91' && x3$zinkod[k + 1] == '92'))&& (x3$NDZ_sanemsanas_datums[k] != x3$NDZ_sanemsanas_datums[k+1])) {
-        x3_finish <- rbind(x3_finish, x3[c(k, k + 1),])
-        check_rows <- check_rows + 2
-      } else {
-        # Nezinu ko ielikt
       }
       
-     
+      #GADĪJUMS 5: ...
+      valid_pairs <- list(c('40', '41'), c('50', '51'), c('53', '54'), c('91', '92'))
+      pair_check <- any(sapply(valid_pairs, function(pair) all(x3$zinkod[k:(k + 1)] == pair)))
       
-      if (x3$NDZ_sanemsanas_datums[k] == x3$NDZ_sanemsanas_datums[k + 1]) {
-        #Ja datumi neatšķiras, un rinda k ir 91. kods un rinda k+1 ir 40,
-        if (x3$zinkod[k] == '91' && x3$zinkod[k + 1] == '40') {
-          #tad tabulā x3_finish iet rinda k+1 ar kodu 40, jo tas anulē kodu 91.
+      if (pair_check && diff(x3$NDZ_sanemsanas_datums[k:(k + 1)]) != 0) {
+          x3_finish <- rbind(x3_finish, x3[k : (k + 1),])
+          check_rows <- check_rows + 2
+      } 
+      rm(valid_pairs, pair_check)
+      
+      #GADĪJUMS 6: ...
+      
+      if (x3$zinkod[k] %in% c('91','40') && x3$zinkod[k + 1] %in% c('91', '40')) {
+        if (diff(x3$NDZ_sanemsanas_datums[k:(k+1)]) == 0) {
+          #Ja datumi neatšķiras, un viena rinda ir 91. kods un 
+          # otra rinda ir 40, tad tabulā x3_finish iet rinda ar kodu 40, 
+          # jo tas anulē kodu 91.
+          x3_finish <- rbind(x3_finish, x3[k + (x3$zinkod[k] == "91"),])
+          #Taču...
+          #ja datumi kodiem atšķiras, tad tik un tā vēlreiz pārbauda, ka tādi kodi tajās rindās ir.
+        } else if (diff(x3$NDZ_sanemsanas_datums[k:(k+1)]) < 0) {
+          #Tabulā x3_finish liksies tā rinda, kurā vēlaks saņemšanas datums.
+          x3_finish <- rbind(x3_finish, x3[k,])
+        } else if (diff(x3$NDZ_sanemsanas_datums[k:(k+1)]) > 0) {
           x3_finish <- rbind(x3_finish, x3[k + 1,])
-          check_rows <- check_rows + 2
-          #Un otrādi.
-        } else if (x3$zinkod[k] == '40' && x3$zinkod[k + 1] == '91') {
-          #Ja datumi neatšķiras, un rinda k ir 40. kods un rinda k+1 ir 91,
-          #tad tabulā x3_finish iet rinda k ar kodu 40, jo tas anulē kodu 91.
-          x3_finish <- rbind(x3_finish, x3[k,])
-          check_rows <- check_rows + 2
-        }#Taču...
-        #ja datumi kodiem atšķiras, tad tik un tā vēlreiz pārbauda, ka tādi kodi tajās rindās ir.
-      } else if (x3$NDZ_sanemsanas_datums[k] > x3$NDZ_sanemsanas_datums[k + 1] && (x3$zinkod[k] == '91' || x3$zinkod[k] == '40') && (x3$zinkod[k + 1] == '91' || x3$zinkod[k + 1] == '40')) {
-        #Tabulā x3_finish liksies tā rinda, kurā vēlaks saņemšanas datums.
-        x3_finish <- rbind(x3_finish, x3[k,])
+        } 
         check_rows <- check_rows + 2
-      } else if ((x3$NDZ_sanemsanas_datums[k] < x3$NDZ_sanemsanas_datums[k + 1]) &&
-                 (x3$zinkod[k] == '40' || x3$zinkod[k] == '91') && (x3$zinkod[k + 1] == '40' || x3$zinkod[k + 1] == '91')) {
-        x3_finish <- rbind(x3_finish, x3[k + 1,])
-        check_rows <- check_rows + 2
-      } else {
-        #print("Šajās rindās nav kodi 40 vai 91.")
-      }
-      
+      } 
+        
+      #GADĪJUMS 7: ...
 
-      
-      if (x3$NDZ_sanemsanas_datums[k] == x3$NDZ_sanemsanas_datums[k + 1]) {
-        #Ja datumi neatšķiras, un rinda k ir 41. kods un rinda k+1 ir 92,
-        if (x3$zinkod[k] == '41' && x3$zinkod[k + 1] == '92') {
-          #tad tabulā x3_finish iet rinda k ar kodu 41, jo tas anulē kodu 92.
-          x3_finish <- rbind(x3_finish, x3[k,])
-          check_rows <- check_rows + 2
-          #Un otrādi.
-        } else if (x3$zinkod[k] == '92' && x3$zinkod[k + 1] == '41') {
-          #Ja datumi neatšķiras, un rinda k+1 ir 41. kods un rinda k ir 92,
-          #tad tabulā x3_finish iet rinda k+1 ar kodu 41, jo tas anulē kodu 92.
-          x3_finish <- rbind(x3_finish, x3[k+1,])
-          check_rows <- check_rows + 2
-        }#Taču... 
+      if (x3$zinkod[k] %in% c('92','41') && x3$zinkod[k + 1] %in% c('92', '41')) {
+       if (diff(x3$NDZ_sanemsanas_datums[k:(k+1)]) == 0) {
+        #Ja datumi neatšķiras, un viena rinda ir ar kodu 41 un 
+        # otra rinda ir ar kodu 92
+        #tad tabulā x3_finish iet rinda ar kodu 41, jo tas anulē kodu 92.
+          x3_finish <- rbind(x3_finish, x3[k + (x3$zinkod[k] == '92'),])
+        #Taču... 
         #ja datumi kodiem atšķiras, tad tik un tā vēlreiz pārbauda, ka tādi kodi tajās rindās ir.
-      } else if (x3$NDZ_sanemsanas_datums[k] < x3$NDZ_sanemsanas_datums[k + 1] && 
-                 (x3$zinkod[k] == '41' || x3$zinkod[k] == '92') && (x3$zinkod[k + 1] == '92' || x3$zinkod[k + 1] == '41')) {
+        } else if (diff(x3$NDZ_sanemsanas_datums[k:(k+1)]) > 0) {
         #Tabulā x3_finish liksies tā rinda, kurā vēlaks saņemšanas datums.
         x3_finish <- rbind(x3_finish, x3[k+1,])
-        check_rows <- check_rows + 2
-      } else if ((x3$NDZ_sanemsanas_datums[k] > x3$NDZ_sanemsanas_datums[k + 1]) &&
-                 (x3$zinkod[k] == '41' || x3$zinkod[k] == '92') && (x3$zinkod[k + 1] == '92' || x3$zinkod[k + 1] == '41')) {
+        } else if (diff(x3$NDZ_sanemsanas_datums[k:(k+1)]) < 0) {
         x3_finish <- rbind(x3_finish, x3[k,])
-        check_rows <- check_rows + 2 
-      } else {
-        #print("Šajās rindās nav kodi 40 vai 91.")
+        }  
+        check_rows <- check_rows + 2
       }
     } else {
-      stop(cat("ERROR: DoublesTest nav iziets. Rinda:", k))
+      stop("ERROR: Funkcijā F_doubleStartEnd_codesDiffer DoublesTest nav iziets. Rinda: ", k)
     }
   }
-  
-  
+    
   # Gala pārbaude
   
   rownames(x3_finish) <- NULL
   if (nrow(x3) != check_rows) {
-    stop(cat("ERROR: Atvasināto tabulu rindas nesakrīt ar mātes tabulu."))
+    stop("ERROR: Funkcijā F_doubleStartEnd_codesDiffer atvasināto tabulu rindas nesakrīt ar mātes tabulu.")
   } else {
     rm(check_rows, k, x3)
     return(x3_finish)
