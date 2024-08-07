b157161bfc5607fefdc53e1d8197f5ee98876c5d
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-08-07 17:45:33 +0300

    Update processingOnes.R

--- NDZ_kodi11/functions/processingOnes.R
+++ NDZ_kodi11/functions/processingOnes.R
@@ -6,28 +6,23 @@ processingOnes <- function(x, o) {
 
     if (nrow(x1) > 0) {
       x_uzVieniniekiem <- data.frame()
-      
-      #for (p in 1:floor((as.numeric(o) / 2))) {
-        x2 <- data.frame()
-        x3 <- data.frame()
+      x2 <- data.frame()
+      x3 <- data.frame()
         
-        
-        if (length(unique(x1$PS_code)) == nrow(x1)) {
+        if (length(unique(x1$PS_code)) == nrow(x1)) { 
           x_uzVieniniekiem <- x1
         } else {
-          r <- 1  # ! šo nedzēs, šis nav no testēšanas palicis r, bet ir nepieciešams while loopam.
+          r <- 1  # ! šis NAV no testēšanas
           while(r <= nrow(x1)) {
             if (ifelse(is.na(doublesTest(r, x1)), FALSE, doublesTest(r, x1))) {
               if (x1$zinkod[r] == x1$zinkod[r + 1]) {
                 x2 <- rbind(x2, x1[c(r, r + 1), ])
-                r <- r + 2
-              } else if (ifelse(is.na(x1$beidz_darbu[r] < x1$sak_darbu[r + 1]), FALSE, (x1$beidz_darbu[r] < x1$sak_darbu[r + 1])))  {
+              } else if (ifelse(is.na(diff(x1$NDZ_sanemsanas_datums[r:(r+1)]) > 0), FALSE, diff(x1$NDZ_sanemsanas_datums[r:(r+1)]) > 0))  {
                 x_uzVieniniekiem <- rbind(x_uzVieniniekiem, x1[c(r, r + 1), ])
-                r <- r + 2
               } else {
                 x3 <- rbind(x3, x1[c(r, r + 1), ])
-                r <- r + 2
               }
+              r <- r + 2
             } else {
               x_uzVieniniekiem <- rbind(x_uzVieniniekiem, x1[r, ])
               r <- r+1
@@ -44,16 +39,13 @@ processingOnes <- function(x, o) {
           }
           
           if (nrow(x2) > 0) {
-            x_uzVieniniekiem <-
-              rbind(x_uzVieniniekiem,
-                    F_doubleStartEnd_codesMatch(x2))
+            x_uzVieniniekiem <- rbind(x_uzVieniniekiem, F_doubleStartEnd_codesMatch(x2))
           }
           
           if (nrow(x3) > 0) {
-            x_uzVieniniekiem <-
-              rbind(x_uzVieniniekiem,
-                    F_doubleStartEnd_codesDiffer(x3))}
-          rm(x2, x3)
+            x_uzVieniniekiem <- rbind(x_uzVieniniekiem, F_doubleStartEnd_codesDiffer(x3))
+            }
+            rm(x2, x3)
         }
       }
       
@@ -61,34 +53,19 @@ processingOnes <- function(x, o) {
       rm(x_uzVieniniekiem)
   }
 
-  #1 Sakārto tabulu
   x <- x[order(x$PS_code, x$DN_code, x$NM_code, x$NDZ_sanemsanas_datums, x$sak_beidz), ]
   rownames(x) <- NULL
   
-  #2 Aprēķini dienas no darba sākšanas datuma līdz mēneša pēdējai dienai, un ievieto tās ailē [dienas].
-  #  Pieskaita 1, jo gan mēneša pēdējai datums, gan darba sākšanas diena piederās dienu skaitam.
   x$dienas[x$sak_beidz == "1"] <- as.numeric(difftime(x$last_date[x$sak_beidz == "1"], x$NDZ_sanemsanas_datums[x$sak_beidz == "1"], units = "days")) + 1
   
-  #3 Aprēķini dienas no iepriekšējā mēneša pēdējās dienas līdz beidz_darbu, un ievieto tās ailē [dienas]
-  #3.1 Izrēķini iepriekšējā mēneša pēdējo dienu
   prev <- as.Date(format(x$last_date[1], "%Y-%m-01")) - 1
   
-  #3.2 Aprēķina nostrādātās dienas pret iepriekšējā mēneša pēdējo dienu.
-  #    Dienas rēķina no iepriekšējā mēneša beigu datuma līdz darba beigšanas datumam (x$NDZ_sanemsanas_datums[x$sak_beidz == "2"])
   x$dienas[x$sak_beidz == "2"] <- as.numeric(difftime(x$NDZ_sanemsanas_datums[x$sak_beidz == "2"], prev, units = "days"))
   
-  # Ja darba beigšanās datums nozīmē atvaļinājuma vai dīkstāves sākšanos, tad tām dienām atņem 1, 
-  # jo to dienu indivīds vairs nestrādā. Tāpat arī, ja cilvēks 
-  # (1) atsāk darbu pēc bērnu kopšanas atvaļinājuma un tanī pat dienā aiziet bērnu kopšanas atvaļinājumā (kodi 40 un 41),
-  # vai (2) atgriežas darbā pēc bezalgas atvaļinājuma un tanī pat dienā aiziet nākamajā (kodi 50 un 51),
-  # tad šo vienu dienu neskaita. Mīnus 1 to nulificēs.
   x$dienas[x$zinkod %in% c("41", "50", "53", "92")] <- x$dienas[x$zinkod %in% c("41", "50", "53", "92")] - 1
   rm(prev)
 
-  #3.3 Kods 26 atceļ iepriekš sarēķinātās dienas darba ņēmēja statusā.
-  #    Kods 26: Darba ņēmēja statusa zaudēšana, ja persona nav uzsākusi darbu.
   x$dienas[x$zinkod == "26"] <- 0
-  
   return(x)
 }
 
