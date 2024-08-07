eace518072773cdd0e375b77eb78edb0434e0f7b
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-08-07 18:13:44 +0300

    Update 1_doubleStartEnd_codesMatch_function.R

--- NDZ_kodi11/functions/1_doubleStartEnd_codesMatch_function.R
+++ NDZ_kodi11/functions/1_doubleStartEnd_codesMatch_function.R
@@ -1,25 +1,24 @@
 F_doubleStartEnd_codesMatch <- function(x2) {
 rownames(x2) <- NULL
-n1 <- data.frame()
-n2 <- data.frame()
+
+n <- data.frame()
 
 for (k in seq(1, nrow(x2), by = 2)) {
-  if (doublesTest(k, x2) && x2$zinkod[k] == x2$zinkod[k + 1]) {
-    if (diff(x2$NDZ_sanemsanas_datums[k:(k+1)]) == 0 ||
-        x2$NDZ_sanemsanas_datums[k] > x2$NDZ_sanemsanas_datums[k + 1]) {
-      n1 <- rbind(n1, x2[k,])
+  if (doublesTest(k, x2) && x2$zinkod[k] == x2$zinkod[k + 1]) { 
+    if (diff(x2$NDZ_sanemsanas_datums[k:(k+1)]) <= 0) {
+      n <- rbind(n, x2[k,])
     } else {
-      n2 <- rbind(n2, x2[k + 1,])
+      n <- rbind(n, x2[k + 1,])
     }
+  } else {
+    stop("Pārbaude nav izieta: Funkcija F_doubleStartEnd_codesMatch(). \n")
   }
 } 
 
-x2_finish <- rbind(n1, n2)  
-
-if(nrow(x2_finish) * 2 != nrow(x2)) {
-  stop(cat("ERROR: Atvasināto tabulu rindas neatbilst mātes tabulai x2!"))
+if(nrow(n) * 2 != nrow(x2)) {
+  stop("ERROR: Funkcijā F_doubleStartEnd_codesMatch() atvasināto tabulu rindas neatbilst mātes tabulai x2! \n")
   } else {
-    rm(k, n1, n2)
-    return(x2_finish)
+    rm(k, x2)
+    return(n)
   }
 }
