a9f9f032db7526afb950fb605267de513cef796d
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-08-08 16:00:53 +0300

    Update processingDoubles.R

--- NDZ_kodi11/functions/processingDoubles.R
+++ NDZ_kodi11/functions/processingDoubles.R
@@ -1,38 +1,22 @@
-
 processingDoubles <- function(x, o) {
 
-  x <- x[order(x$PS_code, x$DN_code, x$NM_code, x$NDZ_sanemsanas_datums, x$start, x$end), ]
-  temp <- data.frame()
-  
-  for (k in seq(1, nrow(x), by = 2)) {
-    if ((is.na(x$sak)[k] && is.na(x$beidz[k + 1])
-         || is.na(x$sak)[k + 1] && is.na(x$beidz[k])) && (x$NDZ_sanemsanas_datums[k] <= x$NDZ_sanemsanas_datums[k+1])) {
-      temp <- rbind(temp, x[c(k, k + 1), ])
-    }
-  }
-  rm(k)
-  
-  if (nrow(temp) == nrow(x)) {
-    cat(
-      "PĀRBAUDE IZIETA: Visas rindas ir Īstie Divnieki.\n
-    )
-    rm(temp)
+  x <- x[order(x$PS_code, x$DN_code, x$NM_code, x$NDZ_sanemsanas_datums), ]
+
+  #1. Pārbauda vai neiztrūkst datumu vai ziņojumu kodu
+  if (sum(is.na(x$NDZ_sanemsanas_datums)) > 0 || sum(is.na(x$zinkod)) > 0) {
+    stop("processingDoubles() nepilnīgas ailes. \n")
   } else {
-    stop(
-      cat(
-        "ERROR: Visas rindas NAV Īstie Divnieki. Pārskatīt tabulu.\n
-      )
-    )
+    cat("PĀRBAUDE IZIETA: processingDoubles. \n")
   }
-  
-  #2 Savienojam duplikātu rindas tā, lai sākuma un beigu datumi ir vienā ailē
+
+  #2 Savieno duplikātu rindas tā, lai sākuma un beigu datumi ir vienā ailē
   x_savienotieDivnieki <- data.frame()
   x <-
     x[order(x$period,
             x$PS_code,
             x$DN_code,
             x$NM_code,
-            x$NDZ_sanemsanas_datums, x$start, x$end),]
+            x$NDZ_sanemsanas_datums),]
 
     for (O in seq(1, nrow(x), by = 2)) {
     y <- x[c(O, O + 1),]
@@ -80,26 +64,19 @@ processingDoubles <- function(x, o) {
   #4 Tabulu x_savienotieDivnieki apstrādā sekojošos punktos.
   
   x <- x_savienotieDivnieki
-  x <- x[order(x$PS_code, x$DN_code, x$NM_code, x$NDZ_sanemsanas_datums, x$start), ]
+  x <- x[order(x$PS_code, x$DN_code, x$NM_code, x$NDZ_sanemsanas_datums), ]
   rownames(x) <- NULL
   
   #6 Aprēķini dienas no sak līdz beidz un ievieto tās ailē [dienas].
-
+  
   x$dienas1 <-
     as.numeric(difftime(as.Date(x$beidz), as.Date(x$sak), units = "days")) + 1
   x$dienas <- x$dienas1[(!is.na(x$sak))]
   x$dienas1 <- NULL
   rm(x_savienotieDivnieki)
   
-  x <- x[order(x$PS_code, x$DN_code, x$NM_code, x$NDZ_sanemsanas_datums, x$start), ]
-
-  if("40" %in% x$zinkod || "41" %in% x$zinkod || "50" %in% x$zinkod || "51" %in% x$zinkod || "53" %in% x$zinkod || "54" %in% x$zinkod || "91" %in% x$zinkod || "92" %in% x$zinkod) {
-    x$dienas <- x$dienas-1
-  } else {
-    cat("Šajā tabulā nav kodu 40 un 41 vai 50 un 51, vai 53 un 54, vai 91 un 92.")
-  }
+  x <- x[order(x$PS_code, x$DN_code, x$NM_code, x$NDZ_sanemsanas_datums), ]
   
-  # Kods 26
   for (b in 1:nrow(x)){
     ifelse(x$zinkod[b] == "26", x$dienas[b] <- 0, x$dienas[b]) 
   }
