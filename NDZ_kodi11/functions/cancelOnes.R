c76444e893db2a66dfa3df58ad6049db2c3922f0
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-08-15 13:59:49 +0300

    Update cancelOnes.R

--- NDZ_kodi11/functions/cancelOnes.R
+++ NDZ_kodi11/functions/cancelOnes.R
@@ -2,11 +2,12 @@ cancelOnes <- function(y) {
   
   x_divnieki <- data.frame()
   
-  if (y$NDZ_sanemsanas_datums[y$sak_beidz == "1"][1] != y$NDZ_sanemsanas_datums[y$sak_beidz == "1"][2]) {
-    x_divnieki <- rbind(x_divnieki, y[c(which.max(y$NDZ_sanemsanas_datums[y$sak_beidz == "1"]), which.max(y$end == "2")), ])
-  } else if (y$NDZ_sanemsanas_datums[y$sak_beidz == "1"][1] == y$NDZ_sanemsanas_datums[y$sak_beidz == "1"][2]) {
-    x_divnieki <- rbind(x_divnieki, y[y$sak_beidz == '1', ][1, ], y[y$end == "2", ])
+  if (diff(y$NDZ_sanemsanas_datums[y$sak_beidz == "1"][1:2]) != 0) {
+    x_divnieki <- rbind(x_divnieki, y[c(which.max(y$NDZ_sanemsanas_datums[y$sak_beidz == "1"]), which.max(y$sak_beidz == "2")), ])
+
+  } else if (diff(y$NDZ_sanemsanas_datums[y$sak_beidz == "1"][1:2]) == 0) {
+    x_divnieki <- rbind(x_divnieki, y[y$sak_beidz == '1', ][1,], y[y$sak_beidz == "2", ])
   }
+  
   return(x_divnieki)
 }
-
