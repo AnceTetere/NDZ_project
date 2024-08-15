90f43ecb40b8c63ad73ccc6b95dd61f93a84f6d1
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-08-15 15:23:05 +0300

    Update codes_match.R

--- NDZ_kodi11/functions/codes_match.R
+++ NDZ_kodi11/functions/codes_match.R
@@ -1,47 +1,12 @@
 codes_match <- function(y) {
+  
 x_viens <- data.frame()
-  
-  test <- TRUE # test pārbauda datumus ailē [sak]
-  test1 <- TRUE # test1 pārbauda datumus ailē [beidz]
-  
-  for (r in 1:(as.numeric(o) - 1)) {
-    a <- (y$sak[r] == y$sak[r + 1])
-    if (is.na(a)) {
-      a <- FALSE
-    }
-    test <- test & a
-    rm(a)
-  }
-  
-  for (r in 1:(as.numeric(o) - 1)) {
-    a <- (y$beidz[r] == y$beidz[r + 1])
-    if (is.na(a)) {
-      a <- FALSE
-    }
-    test1 <- test1 & a
-    rm(a)
-  }
-  
-  if (test == TRUE || test1 == TRUE) {
-    x_viens <- rbind(x_viens, y[1,])
-    if (test == TRUE) {
-    } else {
-    }
-    
-  } else if (sum(y$start == "1") == as.numeric(o)) {
-    for (r in 1:(as.numeric(o) - 1)) {
-      b <- max(y$sak[r], y$sak[r + 1])
-    }
-    x_viens <- rbind(x_viens, y[y$sak == b,])
-    rm(b)
-  } else if (sum(y$end == "1") == as.numeric(o)) {
-    for (r in 1:(as.numeric(o) - 1)) {
-      c <- max(y$beidz[r], y$beidz[r + 1])
-    }
-    x_viens <-
-      rbind(x_viens, y[y$beidz == c,])
-    rm(c)
-  }
 
+  if (all(diff(y$NDZ_sanemsanas_datums) == 0)) {
+    x_viens <- rbind(x_viens, y[1,])
+  } else {
+    x_viens <- rbind(x_viens, y[y$NDZ_sanemsanas_datums == max(y$NDZ_sanemsanas_datums),])
+  } 
+  
   return(x_viens)
 }
