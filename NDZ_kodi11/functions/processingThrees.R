115975b3314f253ec0dfd627a59579a72d545b6c
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-10-03 16:45:59 +0300

    Update processingThrees.R

--- NDZ_kodi11/functions/processingThrees.R
+++ NDZ_kodi11/functions/processingThrees.R
@@ -1,17 +1,19 @@
 processingThrees <- function(x, o) {
 
-  x <- x[order(x$period, x$PS_code, x$DN_code, x$NM_code, x$NDZ_sanemsanas_datums), ] 
+  x <- arrange(x, PS_code, DN_code, NM_code, NDZ_sanemsanas_datums) 
   
   x3_uzDivniekiem <- data.frame()
   x3_uzVieniniekiem <- data.frame()
 
-for (k in seq(1, nrow(x), by = 3)) {
+  for (k in seq(1, nrow(x), by = 3)) {
 
   x3 <- x[k:(k+2),]
-  x3 <- x3[order(x3$period, x3$PS_code, x3$DN_code, x3$NM_code, x3$NDZ_sanemsanas_datums), ] 
+  x3 <- arrange(x3, PS_code, DN_code, NM_code, NDZ_sanemsanas_datums) 
   
-    if (all(x3$sak_beidz[1] == x3$sak_beidz[2], x3$sak_beidz[2] == x3$sak_beidz[3])) {
-        x3_uzVieniniekiem <- rbind(x3_uzVieniniekiem, codes_match(x3))
+  # Ja visām trim rindām ir sākuma kodi
+  if (all(x3$sak_beidz == "1")) {
+      x3_uzVieniniekiem <- rbind(x3_uzVieniniekiem, codes_match(x3))
+      # Ja pirmajām divām rindām ir sākuma kodi tabulā, kurā datumi ailē [NDZ_sanemsanas_datums] kārtoti augošā secībā
     } else if (all(x3$sak_beidz[1:2] == "1")) {
         if (diff(x3$NDZ_sanemsanas_datums[1:2]) == 0) {
         x3_uzDivniekiem <- rbind(x3_uzDivniekiem, cancelOnes(x3))
@@ -29,18 +31,16 @@ for (k in seq(1, nrow(x), by = 3)) {
   
   rm(k, x, x3)
   
+#1 Tabulu x3_uzVieniniekiem sūta caur processingOnes(x, o) funkciju.
   if(nrow(x3_uzVieniniekiem) > 0) {
     sendTo_tempNDZ(processingOnes(x3_uzVieniniekiem, o))
-  } else {
-    cat("No trijniekiem pārsūtāmajā vieninieku tabulā nebija nevienas rindas.\n")
-  }
+  } else {cat("No trijniekiem pārsūtāmajā vieninieku tabulā nebija nevienas rindas.\n")}
   rm(x3_uzVieniniekiem)
   
   
+#2 x3_uzDivniekiem sūta caur funkciju processingtwoes(x, o).
   if(nrow(x3_uzDivniekiem) > 0) {
     processingTwoes(x3_uzDivniekiem, o)
-  } else {
-    cat("No trijniekiem pārsūtāmajā divnieku tabulā nebija nevienas rindas.\n")
-  }
+  } else {cat("No trijniekiem pārsūtāmajā divnieku tabulā nebija nevienas rindas.\n")}
   rm(x3_uzDivniekiem)
 }
