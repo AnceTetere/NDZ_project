530ad184300949f2ce4ca7399063005744d38b5f
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-10-09 14:50:28 +0300

    Update processingOnes.R

--- NDZ_kodi11/functions/processingOnes.R
+++ NDZ_kodi11/functions/processingOnes.R
@@ -1,9 +1,9 @@
 processingOnes <- function(x, o) {
 
-    if (as.numeric(o) > 1) { #šo neņem ārā
+    if (as.numeric(o) > 1) { 
       x1 <- x
-      x1 <- x1[order(x1$PS_code, x1$DN_code, x1$NM_code, x1$NDZ_sanemsanas_datums, x1$sak_beidz), ]
-      
+      x1 <- arrange(x1, PS_code, DN_code, NM_code, NDZ_sanemsanas_datums)
+	  
       if (nrow(x1) > 0) {      
         x_uzVieniniekiem <- data.frame()
         
@@ -15,7 +15,7 @@ processingOnes <- function(x, o) {
         if (length(unique(x1$PS_code)) == nrow(x1)) { 
           x_uzVieniniekiem <- x1
         } else {
-          r <- 1  # ! šo nedzēs, šis nav no testēšanas 
+          r <- 1  # ! šo nedzēs, šis nav no testēšanas palicis r, bet ir nepieciešams while loopam.
           while(r <= nrow(x1)) {
             if (ifelse(is.na(doublesTest(r, x1)), FALSE, doublesTest(r, x1))) {
               if (x1$sak_beidz[r] == x1$sak_beidz[r + 1]) {
@@ -33,11 +33,12 @@ processingOnes <- function(x, o) {
               r <- r+1
             }
           }
+          rm(r)
         }
         
         
         if (nrow(x1) == sum(nrow(x2), nrow(x3), nrow(x_uzVieniniekiem))) {
-          rm(r, x1)
+          rm(x1)
         } else {
           stop("ERROR: Atvasināto tabulu x2 un x3 nesakrīt ar mātes tabulu x1.")
         }
@@ -56,19 +57,16 @@ processingOnes <- function(x, o) {
   }
 
   #1 Sakārto tabulu
-  x <- x[order(x$PS_code, x$DN_code, x$NM_code, x$NDZ_sanemsanas_datums, x$sak_beidz), ]
+  x <- arrange(x, PS_code, DN_code, NM_code, NDZ_sanemsanas_datums)
   rownames(x) <- NULL
   
   x$dienas[x$sak_beidz == "1"] <- as.numeric(difftime(x$last_date[x$sak_beidz == "1"], x$NDZ_sanemsanas_datums[x$sak_beidz == "1"], units = "days")) + 1
-  
   prev <- as.Date(format(x$last_date[1], "%Y-%m-01")) - 1
   
   x$dienas[x$sak_beidz == "2"] <- as.numeric(difftime(x$NDZ_sanemsanas_datums[x$sak_beidz == "2"], prev, units = "days"))
-  
-  x$dienas[x$zinkod %in% c("41", "50", "53", "92")] <- x$dienas[x$zinkod %in% c("41", "50", "53", "92")] - 1
+  x$dienas[x$zinkod %in% c("40", "41", "50", "51", "53", "54", "91", "92")] <- x$dienas[x$zinkod %in% c("40", "41", "50", "51", "53", "54", "91", "92")] - 1
   rm(prev)
 
   x$dienas[x$zinkod == "26"] <- 0
   return(x)
 }
-
