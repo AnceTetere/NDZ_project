4be43df74c0c76a349ac65c80194539421e831c0
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-10-09 14:32:59 +0300

    Update processingEights.R

--- NDZ_kodi11/functions/processingEights.R
+++ NDZ_kodi11/functions/processingEights.R
@@ -11,25 +11,25 @@ processingEights <- function(x, o){
   for (r in seq(1, nrow(x), by = 8)) {
     x8 <- x[r:(r+7), ]
     x8 <- arrange(x8, PS_code, DN_code, NM_code, NDZ_sanemsanas_datums)
-
-    if (sum(x8$sak_beidz == "1") == 4) {
+    
+    if(sum(x8$sak_beidz == "1") == 4) {
         result <- processingEights_s4(x8)
-    } else if (x8$sak_beidz[1] == "2" && x8$sak_beidz[2] == "1" && x8$NDZ_sanemsanas_datums[1] != x8$NDZ_sanemsanas_datums[2]) {
+    } else if (all(x8$sak_beidz[1:2] == c("2", "1")) && diff(x8$NDZ_sanemsanas_datums[1:2]) != 0) {
       x8_uzVieniniekiem <- rbind(x8_uzVieniniekiem, x8[1, ])
       x8_uzSeptini <- rbind(x8_uzSeptini, x8[-1, ])
     } else if (x8$sak_beidz[1] == x8$sak_beidz[2] && all(diff(x8$NDZ_sanemsanas_datums[1:3]) != 0)) {
       x8_uzSeptini <- rbind(x8_uzSeptini, x8[2:8, ])
-    } else if (x8$sak_beidz[1] == "1" && x8$sak_beidz[2] == "2" && x8$sak_beidz[3] == "2" && x8$sak_beidz[4] == "1" && 
-               all(diff(x8$NDZ_sanemsanas_datums[1:3]) != 0) && all(diff(x8$NDZ_sanemsanas_datums[4:6]) != 0) &&
-               all(diff(x8$NDZ_sanemsanas_datums[3:4]) == 0) && all(diff(x8$NDZ_sanemsanas_datums[6:7]) == 0)) {
+    } else if (all(x8$sak_beidz[c(1:2,4)] == c("1", "2", "1")) && 
+               all(diff(x8$NDZ_sanemsanas_datums[1:3]) != 0) && 
+               all(diff(x8$NDZ_sanemsanas_datums[4:6]) != 0) &&
+               diff(x8$NDZ_sanemsanas_datums[3:4]) == 0 && 
+               diff(x8$NDZ_sanemsanas_datums[6:7] == 0)) {
       x8_uzCetriniekiem <- rbind(x8_uzCetriniekiem, x8)
-    } else if (all(x8$sak_beidz[c(1, 3:4, 6:8)] == "1") && 
-               all(diff(x8$NDZ_sanemsanas_datums[1:7]) != 0) && all(diff(x8$NDZ_sanemsanas_datums[7:8]) == 0) &&
-               x8$PS_code[1] == '_________' && x8$NM_code[1] == '_____________') {
+    } else if (all(x8$sak_beidz == c("1", "2", "1", "1", "2", "1", "1", "1")) && 
+               all(diff(x8$NDZ_sanemsanas_datums[1:7]) != 0) && 
+               diff(x8$NDZ_sanemsanas_datums[7:8]) == 0 &&
+               x8$PS_code[1] == '________' && x8$NM_code[1] == '__________') {
       x8_uzVieniniekiem <- rbind(x8_uzVieniniekiem, x8[1,])
-    } else if (all(x8$sak_beidz[c(1, 3, 5, 7, 8)] == "1") && all(diff(x8$NDZ_sanemsanas_datums) != 0)){
-      x8_uzVieniniekiem <- rbind(x8_uzVieniniekiem, x8[8,])
-      x8_uzSesi <- rbind(x8_uzSesi, x8[1:6,])
     } else if (sum(x8$sak_beidz == "1") == 5) { 
       if(all(x8$sak_beidz[c(1, 3, 5, 7, 8)] == "1") && 
          all(sapply(c(3, 6), function(i) all(diff(x8$NDZ_sanemsanas_datums[i:(i+1)]) == 0))) &&
@@ -77,24 +77,24 @@ if (nrow(x8_uzDivniekiem) > 0) {
 #3 Apakštabulu x8_uzCetriniekiem sūta caur processingFours().
 if (nrow(x8_uzCetriniekiem) > 0) {
   x8_uzCetriniekiem <- arrange(x8_uzCetriniekiem, PS_code, NM_code, NDZ_sanemsanas_datums)
-  processingFours(x8_uzCetriniekiem, "4")
   cat("No astoņniekiem atvasinātā tabula x8_uzCetriniekiem pārsūtīta apstrādei caur processingFours.\n")
+  processingFours(x8_uzCetriniekiem, o)
 } else {cat("Tabula x8_uzCetriniekiem ir tukša.\n")}
   rm(x8_uzCetriniekiem)
   
 #4 Apakštabulu x8_uzSesi sūta caur processingSixes().
 if (nrow(x8_uzSesi) > 0) {
   x8_uzSesi <- arrange(x8_uzSesi, PS_code, NM_code, NDZ_sanemsanas_datums)
-  processingSixes(x8_uzSesi, "6")
   cat("No astoņniekiem atvasinātā tabula x8_uzSesi pārsūtīta apstrādei caur processingSixes().\n")
+  processingSixes(x8_uzSesi, o)
 } else {cat("Tabula x8_uzSesi ir tukša.\n")}
   rm(x8_uzSesi)
   
 #5 Apakštabulu x8_uzSeptini sūta caur processingSeven().
 if (nrow(x8_uzSeptini) > 0) {
   x8_uzSeptini <- arrange(x8_uzSeptini, PS_code, NM_code, NDZ_sanemsanas_datums)
-  processingSeven(x8_uzSeptini, "7")
   cat("No astoņniekiem atvasinātā tabula x8_uzSeptini pārsūtīta apstrādei caur processingSeven.\n")
+  processingSeven(x8_uzSeptini, o)
 } else {cat("Tabula x8_uzSeptini ir tukša.\n")}
   rm(x8_uzSeptini)
 }
