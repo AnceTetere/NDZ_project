f18be0c6eca782fbf8eb4de54a5d96d56e0e2d55
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-10-11 10:14:48 +0300

    Update menesa_dienu_skaits.R

--- NDZ_kodi11/functions/helper_functions/menesa_dienu_skaits.R
+++ NDZ_kodi11/functions/helper_functions/menesa_dienu_skaits.R
@@ -1,1 +1,21 @@
-
+menesa_dienu_skaits <- function(year, month) {
+  
+  #1 Pārbauda saņemto mēnesi
+  month <- as.numeric(month)
+  if (month < 1 || month > 12) {
+    stop("Neatbilstošs mēnesis!")
+  }  
+    
+  #2 Izveido R objektu, kas nes nākošā mēneša pirmo dienu.
+  year <- as.numeric(year)
+  if(month == 12) {
+    nakamais_menesis <- as.Date(paste(year + 1, 1, 1, sep = "-"))
+  } else {
+    nakamais_menesis <- as.Date(paste(year, month + 1, 1, sep = "-"))
+  }  
+    
+  #3 Sarēkina dienu skaitu
+  dienu_skaits <- as.integer(format(nakamais_menesis - 1, "%d"))
+  
+  return(dienu_skaits)
+}
