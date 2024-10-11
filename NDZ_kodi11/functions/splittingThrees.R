097da45c962a1d8f3048d58b63bf2047c2b3daae
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-10-11 15:06:51 +0300

    Update splittingThrees.R

--- NDZ_kodi11/functions/splittingThrees.R
+++ NDZ_kodi11/functions/splittingThrees.R
@@ -3,25 +3,26 @@ splittingThrees <- function(x3) {
   x_vieninieki <- data.frame()
   x_divnieki <- data.frame()
   
-  #if (all(diff(x3$NDZ_sanemsanas_datums) == 0) && all(x3$sak_beidz[c(1,3)] == "1") && x3$sak_beidz[2] == "2" && 
-  #    x3$zinkod[1] %in% c("92", "41") && x3$zinkod[3] %in% c("92", "41") && x3$PS_code[1] == '_________' && x3$NM_code[1] == '_________'){
-  #  x_vieninieki <- rbind(x_vieninieki, x3[3, ]) 
-  #} else if ((x3$zinkod[1] == "40" && x3$zinkod[2] == "41" && x3$zinkod[3] == "41") && (x3$NDZ_sanemsanas_datums[2] <= x3$NDZ_sanemsanas_datums[3]) && (x3$NDZ_sanemsanas_datums[1] != x3$NDZ_sanemsanas_datums[2])) {
-  #  x_vieninieki <- rbind(x_vieninieki, x3[c(1, 3), ]) 
-    # vv || (x3$sak_beidz[1] == "2" && x3$sak_beidz[2] == "1" && x3$sak_beidz[3] != "2" && diff(x3$NDZ_sanemsanas_datums[1:2]) == 0 && diff(x3$NDZ_sanemsanas_datums[2:3]) != 0)) {
-
-  #} else 
-    
   if (all(x3$sak_beidz == c("1", "2", "2")) && all(x3$zinkod != '26') && 
       all(diff(x3$NDZ_sanemsanas_datums[1:3]) >= 0)) {
       x_divnieki <- rbind(x_divnieki, x3[c(1,3), ])
-  } else if (all(x3$sak_beidz == c("1", "2", "1")) && 
-             diff(x3$NDZ_sanemsanas_datums[1:2]) >= 0 && diff(x3$NDZ_sanemsanas_datums[2:3]) > 0) {
-     x_divnieki <- rbind(x_divnieki, x3[1:2, ])
-     x_vieninieki <- rbind(x_vieninieki, x3[3, ])    
-  } else if (all(x3$sak_beidz == c("2", "1", "2")) && diff(x3$NDZ_sanemsanas_datums[1:2]) != 0) {
-     x_divnieki <- rbind(x_divnieki, x3[2:3, ])
-     x_vieninieki <- rbind(x_vieninieki, x3[1, ])
+  } else if (all(x3$sak_beidz == c("1", "2", "1"))) {
+    if (diff(x3$NDZ_sanemsanas_datums[1:2]) >= 0 && diff(x3$NDZ_sanemsanas_datums[2:3]) > 0) {
+      x_divnieki <- rbind(x_divnieki, x3[1:2, ])
+      x_vieninieki <- rbind(x_vieninieki, x3[3, ]) 
+    } else if (diff(x3$NDZ_sanemsanas_datums[1:2]) != 0 && diff(x3$NDZ_sanemsanas_datums[2:3]) == 0) {
+      # Sekojot lēmumam: ..\manual\lēmumi\kods50_112
+      x3 <- arrange(x3, sak_beidz)
+      x_divnieki <- rbind(x_divnieki, x3[2:3, ])
+    } else {stop("No trijniekiem sūtītajā tabulā uz splittingThrees trūkst apstrādes koda!")}
+  } else if (all(x3$sak_beidz == c("2", "1", "2"))) {
+    if (diff(x3$NDZ_sanemsanas_datums[1:2]) != 0) {
+      x_divnieki <- rbind(x_divnieki, x3[2:3, ])
+      x_vieninieki <- rbind(x_vieninieki, x3[1, ])
+    } else if (diff(x3$NDZ_sanemsanas_datums[1:2]) == 0 && diff(x3$NDZ_sanemsanas_datums[2:3]) != 0) {
+      # Sekojot lēmumam: ..\manual\lēmumi\kods50_122
+      x_divnieki <- rbind(x_divnieki, x3[2:3, ])
+    } else {stop("No trijniekiem sūtītajā tabulā uz splittingThrees trūkst apstrādes koda!")}
   } else if (all(x3$sak_beidz == c("2", "2", "1")) && all(x3$zinkod != "26")) {
      x_divnieki <- rbind(x_divnieki, x3[2:3, ])
   } else if (all(x3$sak_beidz == c("2", "2", "1")) && any(x3$zinkod %in% "26")) {
@@ -36,8 +37,6 @@ splittingThrees <- function(x3) {
              diff(x3$NDZ_sanemsanas_datums[2:3]) == 0 && "41" %in% x3$zinkod) {
     x_vieninieki <- rbind(x_vieninieki, x3[3, ])
     x_divnieki <- rbind(x_divnieki, x3[1:2, ])
-    # } else if (all(x3$sak_beidz == "2") && all(diff(x3$NDZ_sanemsanas_datums) == 0)) {                                                    
-    #  x_vieninieki <- rbind(x_vieninieki, x3[1, ])
   } else {
     stop("No trijniekiem sūtītajā tabulā uz splittingThrees trūkst apstrādes koda!")
   }
