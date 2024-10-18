89240896c15f5b928147fc3546ab3c822f0700fb
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-10-18 11:02:11 +0300

    Update processingTens.R

--- NDZ_kodi11/functions/processingTens.R
+++ NDZ_kodi11/functions/processingTens.R
@@ -1,16 +1,13 @@
 processingTens <- function(x, o) {
+  cat("-------------SĀK 10-nieku APSTRĀDI.")
   x <- arrange(x, PS_code, DN_code, NM_code, NDZ_sanemsanas_datums)
 
-  x10_uzVieniniekiem <- data.frame()
-  x10_uzDivniekiem <- data.frame()
-  x10_uzSeptini <- data.frame()
-  x10_uzAstoniekiem <- data.frame()
+  x10_uzVieniniekiem <- data.frame(); x10_uzDivniekiem <- data.frame(); x10_uzSeptini <- data.frame(); x10_uzAstoniekiem <- data.frame()
   check_rows <- 0
   
   for (r in seq(1, nrow(x), by = 10)) {
 
-    x10 <- x[r:(r+9), ]
-    x10 <- arrange(x10, PS_code, DN_code, NM_code, NDZ_sanemsanas_datums)
+    x10 <- x[r:(r+9),] %>% arrange(PS_code, DN_code, NM_code, NDZ_sanemsanas_datums)
     
     if(sum(x10$sak_beidz == "1") == 5) {
       if (all(x10$sak_beidz[1:2] == c("1","2")) || (all(x10$sak_beidz[1:2] == c("2","1")) && diff(x10$NDZ_sanemsanas_datums[1:2]) == 0)) {
@@ -50,11 +47,15 @@ processingTens <- function(x, o) {
       if (all(x10$sak_beidz[c(1, 2)] == "2") && all(x10$zinkod[c(1, 2)] == "26") &&
           all(sapply(seq(3, 10, by = 2), function(i) all(diff(x10$NDZ_sanemsanas_datums[i:i+1]) == 0))) &&
           all(sapply(seq(1, 3, by = 2), function(i) all(diff(x10$NDZ_sanemsanas_datums[i:i+1]) != 0)))) {
+        # Šis ir unikāla gadījuma, jo strādā ar tiem dubultajiem kodiem īslaicīgi kā fiziska persona, taču
+        # 2023. gadā un 2022. gadā neviens sākšanas kods pirms tiem pirmiem trim 26, 26 un 25 kodiem nav atrodams.
+        # Nu vispār pēc trīs gadiem neatlaiž par darba nesākšanu, bet ja atlaists ar 25 tad tur būs kļūda.
+        # Labo, ja vajag savādāk, bet no pašreizējās informācijas, es šo griežu nost.
         x10_uzAstoniekiem <- rbind(x10_uzAstoniekiem, x10[3:10, ])
       } else if (all(x10$sak_beidz[c(3:4, 7, 9)] == "1") && 
                  all(sapply(seq(1, 10, by = 2), function(i) all(diff(x10$NDZ_sanemsanas_datums[i:i+1]) == 0))) &&
                  all(sapply(seq(2, 9, by = 2), function(i) all(diff(x10$NDZ_sanemsanas_datums[i:i+1]) != 0)))&&
-                 x10$PS_code[1] == '________' & x10$NM_code[1] == '________') {
+                 x10$PS_code[1] == '__________' & x10$NM_code[1] == '__________') {
         p <- x10[1:6, ]
         p <- p[p$zinkod %in% c("40", "41"), ]
         x10_uzVieniniekiem <- rbind(x10_uzVieniniekiem, p[1, ])
@@ -85,33 +86,29 @@ if (nrow(x) == check_rows) {
   
 #1 Apakštabulu x10_uzVieniniekiem sūta caur processingOnes().
 if(nrow(x10_uzVieniniekiem) > 0) {
-  x10_uzVieniniekiem <- arrange(x10_uzVieniniekiem, PS_code, NM_code, NDZ_sanemsanas_datums)
   cat("No desmitniekiem atvasināto tabulu x10_uzVieniniekiem pārsūta uz processingOnes() un tad uz tempNDZ, ko būvējam.\n")
-  sendTo_tempNDZ(processingOnes(x10_uzVieniniekiem, o))
+  x10_uzVieniniekiem %>% arrange(PS_code, NM_code, NDZ_sanemsanas_datums) %>% processingOnes(o) %>% sendTo_tempNDZ()
 } else {cat("Tabula x10_uzVieniniekiem ir tukša.\n")}
 rm(x10_uzVieniniekiem) 
   
 #2 Apakštabulu x10_uzDivniekiem sūta caur processingTwoes().
 if(nrow(x10_uzDivniekiem) > 0) {
-  x10_uzDivniekiem <- arrange(x10_uzDivniekiem, PS_code, NM_code, NDZ_sanemsanas_datums)
   cat("No desmitniekiem atvasinātā tabula x10_uzDivniekiem pārsūtīta uz processingTwoes un caur to uz tempNDZ, ko būvējam.\n")
-  processingTwoes(x10_uzDivniekiem, o)
+  x10_uzDivniekiem %>% arrange(PS_code, NM_code, NDZ_sanemsanas_datums) %>% processingTwoes(o)
 } else {cat("Tabula x10_uzDivniekiem ir tukša.\n")}
 rm(x10_uzDivniekiem)
   
 #3 Apakštabulu x10_uzSeptini sūta caur processingSeven().
 if(nrow(x10_uzSeptini) > 0) {
-  x10_uzSeptini <- arrange(x10_uzSeptini, PS_code, NM_code, NDZ_sanemsanas_datums)
   cat("No desmitniekiem atvasinātā tabula x10_uzSeptini pārsūtīta uz processingSeven un caur to uz tempNDZ, ko būvējam.\n")
-  processingSeven(x10_uzSeptini, o)
+  x10_uzSeptini %>% arrange(PS_code, NM_code, NDZ_sanemsanas_datums) %>% processingSeven(o)
 } else {cat("Tabula x10_uzSeptini ir tukša.\n")}
 rm(x10_uzSeptini)
   
 #4 Apakštabulu x10_uzAstoniekiem sūta caur processingEights().
 if(nrow(x10_uzAstoniekiem) > 0) {
-  x10_uzAstoniekiem <- arrange(x10_uzAstoniekiem, PS_code,, NM_code, NDZ_sanemsanas_datums)
   cat("No desmitniekiem atvasinātā tabula x10_uzAstoniekiem pārsūtīta uz processingEights un tad uz tempNDZ, ko būvējam.\n")
-  processingEights(x10_uzAstoniekiem, o)
+  x10_uzAstoniekiem %>% arrange(PS_code,, NM_code, NDZ_sanemsanas_datums) %>% processingEights(o)
 } else {cat("Tabula x10_uzAstoniekiem ir tukša.\n")}
 rm(x10_uzAstoniekiem) 
 }
