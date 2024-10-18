c3e3f74313e75eb7f4b8451b1cb5eb891f6e31cf
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-10-18 16:58:26 +0300

    Update processingSixes.R

--- NDZ_kodi11/functions/processingSixes.R
+++ NDZ_kodi11/functions/processingSixes.R
@@ -1,16 +1,11 @@
 processingSixes <- function(x, o) {
   x <- x %>% arrange(PS_code, DN_code, NM_code, NDZ_sanemsanas_datums)
 
-  x6_uzVieniniekiem <- data.frame()
-  x6_uzDivniekiem <- data.frame()
-  x6_uzTris <- data.frame()
-  x6_uzCetri <- data.frame()
-  x6_uzPieciniekiem <- data.frame()
+  x6_uzVieniniekiem <- data.frame(); x6_uzDivniekiem <- data.frame(); x6_uzTris <- data.frame(); x6_uzCetri <- data.frame(); x6_uzPieciniekiem <- data.frame()
   check_rows <- 0
 
   for (r in seq(1, nrow(x), by = 6)) {
-      x6 <- x[r:(r+5),] %>% 
-        arrange(PS_code, DN_code, NM_code, NDZ_sanemsanas_datums, sak_beidz)
+      x6 <- x[r:(r+5),] %>% arrange(PS_code, DN_code, NM_code, NDZ_sanemsanas_datums, sak_beidz)
       
     if(all(x6$sak_beidz[1:2] == c("2", "1")) && diff(x6$NDZ_sanemsanas_datums[1:2]) != 0) {
         x6_uzVieniniekiem <- rbind(x6_uzVieniniekiem, x6[1, ])
@@ -42,7 +37,7 @@ processingSixes <- function(x, o) {
         } else if (all(x6$sak_beidz == c("2", "1", "2", "1", "2", "1")) && 
                   all(sapply(seq(1,4,by=2), function(i) all(diff(x6$NDZ_sanemsanas_datums[i:(i+1)]) == 0))) &&
                   diff(x6$NDZ_sanemsanas_datums[5:6]) != 0 &&
-                  x6$PS_code[1] == "___________" && x6$NM_code[1] == "___________") {
+                  x6$PS_code[1] == "__________" && x6$NM_code[1] == "_____________") {
           x6_uzVieniniekiem <- rbind(x6_uzVieniniekiem, x6[1, ])
           x6_uzPieciniekiem <- rbind(x6_uzPieciniekiem, x6[-1, ])
         } else if (all(x6$sak_beidz == c("2", "2", "1", "2", "1", "1")) && 
@@ -70,44 +65,43 @@ processingSixes <- function(x, o) {
   
 #Pārbaude
 if(check_rows == nrow(x)) {
-  cat("PĀRBAUDE IZIETA:\n
+  cat("PĀRBAUDE IZIETA:
       Rindu summa no sešiniekiem atvasinātajās tabulās sakrīt ar rindu skaitu oriģinālajā tabulā NDZ_6.\n")
-} else {
-  stop("PĀRBAUDE NAV IZIETA.\n
-           Rindu summa no sešiniekiem atvasinātajās tabulās NESAKRĪT ar rindu skaitu oriģinālajā tabulā NDZ_6.\n")}
-  rm(x, r, x6, check_rows)
+} else {stop("PĀRBAUDE NAV IZIETA.
+             Rindu summa no sešiniekiem atvasinātajās tabulās NESAKRĪT ar rindu skaitu oriģinālajā tabulā NDZ_6.\n")}
+rm(x, r, x6, check_rows)
   
 #1 Apakštabulu x6_uzVieniniekiem apstrādā caur funkciju processingOnes().
 if(nrow(x6_uzVieniniekiem) > 0) {
-  x6_uzVieniniekiem %>% 
-    arrange(PS_code, NM_code, NDZ_sanemsanas_datums) %>% 
-    sendTo_tempNDZ(processingOnes(o))
+  x6_uzVieniniekiem %>% arrange(PS_code, NM_code, NDZ_sanemsanas_datums) %>% processingOnes(o) %>% sendTo_tempNDZ()
 } else {cat("Tabula x6_uzVieniniekiem ir tukša.\n")}
 rm(x6_uzVieniniekiem)
   
 #2 Apakštabulu x6_uzDivniekiem sūta caur funkciju processingTwoes().
 if(nrow(x6_uzDivniekiem) > 0) {
-  x6_uzDivniekiem %>% 
-    arrange(PS_code, NM_code, NDZ_sanemsanas_datums) %>% 
-    processingTwoes(o)
+  x6_uzDivniekiem %>% arrange(PS_code, NM_code, NDZ_sanemsanas_datums) %>% processingTwoes(o) 
+  cat("Atvasinātā tabula x6_uzDivniekiem pārsūtīta apstrādei uz processingTwoes().\n")
 } else {cat("Tabula x6_uzDivniekiem ir tukša.\n")}
 rm(x6_uzDivniekiem)
   
 #3 Apakštabulu x6_uzTris sūta caur funkciju processingThrees().
 if(nrow(x6_uzTris) > 0) {
   x6_uzTris %>% arrange(PS_code, NM_code, NDZ_sanemsanas_datums) %>% processingThrees(o)
+  cat("Atvasinātā tabula x6_uzTris pārsūtīta uz processingFours().\n")
 } else {cat("Tabula x6_uzTris ir tukša.\n")}
 rm(x6_uzTris)
 
 #4 Apakštabulu x6_uzCetri sūta caur funkciju processingFours().
 if(nrow(x6_uzCetri) > 0) {
   x6_uzCetri %>% arrange(PS_code, NM_code, NDZ_sanemsanas_datums) %>% processingFours(o)
+  cat("Atvasinātā tabula x6_uzCetri pārsūtīta apstrādei caur processingFours().\n")
 } else {cat("Tabula x6_uzCetri ir tukša.\n")}
 rm(x6_uzCetri)
   
 #5 Apakštabulu x6_uzPieciniekiem sūta caur funkciju processingFives().
 if(nrow(x6_uzPieciniekiem) > 0) {
   x6_uzPieciniekiem %>% arrange(PS_code, NM_code, NDZ_sanemsanas_datums) %>% processingFives(o)
+  cat("Atvasinātā tabula x6_uzPieciniekiem no sešinieku pārsūtīta apstrādei caur processingFives().\n")
 } else {cat("Tabula x6_uzPieciniekiem ir tukša.\n")}
 rm(x6_uzPieciniekiem)
 }
