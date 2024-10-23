0c914311c556d8f64b23c6ee850ec355c784577f
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-10-23 21:03:09 +0300

    Create starpkodi3_91_92.R

---
+++ NDZ_kodi11/functions/2_starpkoduApstrade/starpkodi3/starpkodi3_91_92.R
@@ -0,0 +1,40 @@
+starpkodi3_91_92 <- function(y, t, prev, v) {
+  
+  if (t$zinkod[3] == "25") {
+        if (all(diff(t$NDZ_sanemsanas_datums) != 0)) {
+          yt <- y2[v, ]
+          yt$dienas <- sum(as.numeric(difftime(t$NDZ_sanemsanas_datums[1], prev, units = "days")), as.numeric(diff(t$NDZ_sanemsanas_datums[2:3]))) 
+        } else {stop("Starpkodi3_91_92: Trūkst izstrādes koda.")}
+  } else if (t$zinkod[3] == "50") {
+        if (all(diff(t$NDZ_sanemsanas_datums) != 0)) {
+          yt <- y2[v, ]
+          yt$dienas <- sum(as.numeric(difftime(t$NDZ_sanemsanas_datums[1], prev, units = "days")) - 1, #jo atvaļinājums
+                           as.numeric(diff(t$NDZ_sanemsanas_datums[2:3])))
+        } else {stop("Starpkodi3_91_92: Trūkst izstrādes koda.")}
+  
+      
+    
+#  } else if (t$zinkod[3] == "50") {
+#    if (diff(t$NDZ_sanemsanas_datums[1:2]) == 0 &&
+#        diff(t$NDZ_sanemsanas_datums[2:3]) == 0)) { 
+#      yt <- y2[v, ]
+#      yt$dienas <- as.numeric(difftime(t$NDZ_sanemsanas_datums[1], prev, units = "days")) - 1 #jo atvaļinājums
+#    }
+#  } else if (t$zinkod[3] == "21") {}
+#  if(all(diff(t$NDZ_sanemsanas_datums) != 0)) {
+#    yt <- y2[v, ]
+#    yt$dienas <- as.numeric(difftime(t$NDZ_sanemsanas_datums[3], t$NDZ_sanemsanas_datums[2], units = "days"))
+#  }
+#}
+  #    #} else if (t$zinkod[3] == "40" && all(diff(t$NDZ_sanemsanas_datums) != 0)) {
+  #days1 <- as.numeric(difftime(as.Date(t$NDZ_sanemsanas_datums[1]), prev, units = "days")) - 1 #mīnus 1, jo pēdējā datumā viņš jau ir atvaļinājumā
+  #days2 <- as.numeric(difftime(as.Date(t$NDZ_sanemsanas_datumsz[3]), as.Date(t$NDZ_sanemsanas_datums[2]), units = "days")) 
+  #days <- days1 + days2 
+  #rm(days1, days2)
+  
+  #yt <- y2[v, ]
+  #yt$dienas <- days
+  } else {stop("Starpkodi3_91_92: Trūkst izstrādes koda.")}  
+  
+return(yt)
+}
