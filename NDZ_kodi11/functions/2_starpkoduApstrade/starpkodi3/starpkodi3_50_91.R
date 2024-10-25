df3e491b3191ceaba9321c09eb089b85ce4bbf5a
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-10-25 21:41:59 +0300

    Create starpkodi3_50_91.R

---
+++ NDZ_kodi11/functions/2_starpkoduApstrade/starpkodi3/starpkodi3_50_91.R
@@ -0,0 +1,21 @@
+starpkodi3_50_91 <- function(y, t, prev,v) {
+  
+  if (t$zinkod[3] == "92") {
+          if (all(diff(t$NDZ_sanemsanas_datums) != 0)) {
+            if (t$PS_code[1] == '_________' && t$NM_code[1] == '_________') {
+              t$zinkod[1] <- '92'; t$sak_beidz[1] <- '1'
+              yt <- y[v,]
+              yt$dienas <- sum(as.numeric(diff(t$NDZ_sanemsanas_datums[1:2])),
+                               as.numeric(difftime(t$last_date[3], t$NDZ_sanemsanas_datums[3], units = "days")) + 1)
+            } else {stop("starpkodi3_50_91: Iztrūkst apstrādes koda.")}
+          } else {stop("starpkodi3_50_91: Iztrūkst apstrādes koda.")}    
+  #} else if (t$zinkod[3] == "51") {
+        #  if (diff(t$NDZ_sanemsanas_datums[1:2]) != 0 && diff(t$NDZ_sanemsanas_datums[2:3]) == 0) {
+        #    yt <- y[v, ]
+        #    yt$dienas <- as.numeric(difftime(t$sak_beidz[1], prev, units = "days")) - 1 # jo atvaļinājums
+        #  } else {stop("starpkodi3_50: Iztrūkst apstrādes koda.")}
+  } else {stop("starpkodi3_50: Iztrūkst apstrādes koda.")}
+  
+  rm(y, t, prev,v)
+  return(yt)
+}
