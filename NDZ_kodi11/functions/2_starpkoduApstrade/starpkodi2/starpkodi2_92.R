8deab58612acd21d4f39229a4860c721217588ce
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-10-25 21:35:26 +0300

    Update starpkodi2_92.R

--- NDZ_kodi11/functions/2_starpkoduApstrade/starpkodi2/starpkodi2_92.R
+++ NDZ_kodi11/functions/2_starpkoduApstrade/starpkodi2/starpkodi2_92.R
@@ -1,25 +1,22 @@
 starpkodi2_92 <- function(y, t, prev, v) {
   
-  if (t$zinkod[2] == "21") {
+  if (t$zinkod[2] %in% c("21", "25")) {
     if(diff(t$NDZ_sanemsanas_datums) != 0) {
       yt <- y[v,]
       yt$dienas <- as.numeric(diff(t$NDZ_sanemsanas_datums)) + 1
     } else {stop("starpkodi2_92: Iztrūkst apstrādes koda")}
   } else {stop("starpkodi2_92: Iztrūkst apstrādes koda")}
   
-
-  
   
   #if (t$zinkod[2] == "40") {
     #  yt <- y2[v,] 
-    #  yt$dienas <- as.numeric(difftime(t$beidz_darbu[2], t$sak_darbu[1], units = "days"))
+    #  yt$dienas <- as.numeric(difftime(t$beidz[2], t$sak[1], units = "days"))
     #} else if (t$zinkod[2] == "25") {
     #  yt <- y2[v,] 
-    #  yt$dienas <- as.numeric(difftime(t$beidz_darbu[2], t$sak_darbu[1], units = "days")) + 1 
+    #  yt$dienas <- as.numeric(difftime(t$beidz[2], t$sak[1], units = "days")) + 1 #jo darbs
     #} else  else if (t$zinkod[1] == "92" && t$zinkod[2] == "50") {
     #  yt <- y2[v,] 
-    #  yt$dienas <- as.numeric(difftime(t$beidz_darbu[2], t$sak_darbu[1], units = "days"))
-    #
+    #  yt$dienas <- as.numeric(difftime(t$beidz[2], t$sak[1], units = "days"))
   
  return(yt) 
 }
