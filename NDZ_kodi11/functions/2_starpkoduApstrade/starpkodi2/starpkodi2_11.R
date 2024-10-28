0a7d561ad223a07e7d4d9c615e96e220219bfde8
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-10-28 21:05:17 +0200

    Update starpkodi2_11.R

--- NDZ_kodi11/functions/2_starpkoduApstrade/starpkodi2/starpkodi2_11.R
+++ NDZ_kodi11/functions/2_starpkoduApstrade/starpkodi2/starpkodi2_11.R
@@ -1,36 +1,40 @@
-starpkodi2_11 <- function(y2, t, prev, v) {
-  
-  if (t$zinkod[2] == "41" && t$PS_code[1] == '________' && t$NM_code[1] == '________') {
-    yt <- y2[v,] 
-    yt$dienas <- as.numeric(difftime(t$last_date[2], t$sak[2], units = "days")) + 1 
-  } else if (t$zinkod[2] == "51" && t$PS_code[1] == '________' && t$NM_code[1] == '________') {
-    yt <- y2[v:(v+1),] 
-    yt <- yt[yt$zinkod == "11", ] 
-  } else if (t$zinkod[2] == "51" && t$PS_code[1] %in% c('________', '________') && t$NM_code[1] == '________') {
-    yt <- y2[v:(v+1),] 
-    yt <- yt[yt$zinkod == "11", ] 
-  } else if (t$zinkod[2] == "51" && diff(t$NDZ_sanemsanas_datums) != 0 && t$PS_code[1] == '_________' && t$NM_code[1] == '____________') {
-    yt <- y2[v:(v+1),] 
-    yt <- yt[yt$zinkod == "50", ]
-  } else if(t$zinkod[2] == "50") {
-    yt <- y2[v,]
-    yt$dienas <- as.numeric(difftime(t$beidz[2], t$sak[1], units = "days"))
-  } else if (t$zinkod[2] == "91") {
-    yt <- y2[v,] 
-    yt$dienas <- as.numeric(difftime(t$beidz[2], t$sak[1], units = "days"))
+starpkodi2_11 <- function(y, t, prev, v) {
+
+  if (t$zinkod[2] == "50") {
+      yt <- y[v,]
+      yt$dienas <- as.numeric(diff(t$NDZ_sanemsanas_datums[1:2]))
   } else if (t$zinkod[2] == "40") {
-    #Indivīds sāk darbu un aiziet bērna kopšanas atvaļinājumā.
-    yt <- y2[v,] 
-    yt$dienas <- as.numeric(difftime(t$beidz[2], t$sak[1], units = "days"))
-  } else if (t$zinkod[2] == "53") {
-    yt <- y2[v,] 
-    yt$dienas <- as.numeric(difftime(t$beidz[2], t$sak[1], units = "days"))
-  } else if (t$zinkod[2] == "41" && t$PS_code[1] == '________' & t$NM_code[1] == '____________') {
-    yt <- y2[v,] 
-    yt$dienas <- as.numeric(difftime(t$NDZ_sanemsanas_datums[2], t$sak[1], units = "days"))
-  } else {
-    stop("Starpkodi2_11: Trūkst izstrādes koda.")
-  }
+      yt <- y2[v,] 
+      yt$dienas <- as.numeric(diff(t$NDZ_sanemsanas_datums[1:2]))
+  } else if (t$zinkod[2] == "51") {
+    if (t$PS_code[1] == '___________' && t$NM_code[1] == '____________') {
+      yt <- y2[v,] 
+      yt$dienas <- as.numeric(difftime(t$last_date[2], t$NDZ_sanemsanas_datums[2], units = "days")) + 1
+    }
+  #} else if (t$zinkod[2] == "51")  {
+  #    if(t$PS_code[1] %in% c('___________', '___________') && t$NM_code[1] == '___________') {
+  #      yt <- y2[v:(v+1),] 
+  #      yt <- yt[yt$zinkod == "11", ]
+  #    } else if (diff(t$NDZ_sanemsanas_datums) != 0 && t$PS_code[1] == '___________' && t$NM_code[1] == '___________') {
+  #      yt <- y2[v:(v+1),] 
+  #      yt <- yt[yt$zinkod == "50", ]
+  #    } else {stop("Starpkodi2_11: Trūkst izstrādes koda.")}
+  #if (t$zinkod[2] == "41" && t$PS_code[1] == '___________' && t$NM_code[1] == '___________') {
+   # yt <- y2[v,] 
+  #  yt$dienas <- as.numeric(difftime(t$last_date[2], t$sak_darbu[2], units = "days")) + 1 
+  #} else if (t$zinkod[2] == "51" && t$PS_code[1] == '___________' && t$NM_code[1] == '___________') {
+   # yt <- y2[v:(v+1),] 
+    #yt <- yt[yt$zinkod == "11", ] 
+  #} else if (t$zinkod[2] == "91") {
+#    yt <- y2[v,] 
+#    yt$dienas <- as.numeric(difftime(t$beidz_darbu[2], t$sak_darbu[1], units = "days"))
+#  } else if (t$zinkod[2] == "53") {
+#    yt <- y2[v,] 
+#    yt$dienas <- as.numeric(difftime(t$beidz_darbu[2], t$sak_darbu[1], units = "days"))
+#  } else if (t$zinkod[2] == "41" && t$PS_code[1] == 'PKF4BDF5543' & t$NM_code[1] == '90000074812') {
+#    yt <- y2[v,] 
+#    yt$dienas <- as.numeric(difftime(t$NDZ_sanemsanas_datums[2], t$sak_darbu[1], units = "days"))
+  } else {stop("Starpkodi2_11: Trūkst izstrādes koda.")}
   
   if(is.na(yt$PS_code[1])) {stop("Dienas NA.")}
   yt$zinkod <- "combined"  #jo starpkodu dienu sarēķins
