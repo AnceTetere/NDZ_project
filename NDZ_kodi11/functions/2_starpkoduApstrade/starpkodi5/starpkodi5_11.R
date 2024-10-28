ab009d2836cc7db569c2f562927f30ce389b616d
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-10-28 20:46:15 +0200

    Update starpkodi5_11.R

--- NDZ_kodi11/functions/2_starpkoduApstrade/starpkodi5/starpkodi5_11.R
+++ NDZ_kodi11/functions/2_starpkoduApstrade/starpkodi5/starpkodi5_11.R
@@ -1,10 +1,12 @@
 starpkodi5_11 <- function(y, t, prev, v) {
-  
+
   if (t$zinkod[2] == "50") {
      yt <- starpkodi5_11_50(y, t, prev, v)
+  } else if (t$zinkod[2] == "25") {
+    yt <- starpkodi5_11_25(y, t, prev, v)
   #} else if(t$zinkod[2] == "81" && t$zinkod[3] == "82" && t$zinkod[4] == "81" && 
   #          t$zinkod[5] == "82" && all(!diff(t$NDZ_sanemsanas_datums) == 0)) {
-  #  days1 <- as.numeric(difftime(as.Date(t$NDZ_sanemsanas_datums[3]), as.Date(t$NDZ_sanemsanas_datums[2]), units = "days")) + 1 NDZ
+  #  days1 <- as.numeric(difftime(as.Date(t$NDZ_sanemsanas_datums[3]), as.Date(t$NDZ_sanemsanas_datums[2]), units = "days")) + 1 
   #  days2 <- as.numeric(difftime(as.Date(t$NDZ_sanemsanas_datums[5]), as.Date(t$NDZ_sanemsanas_datums[4]), units = "days")) + 1
   #  days <- days1 + days2
   #  rm(days1, days2)
@@ -31,7 +33,7 @@ starpkodi5_11 <- function(y, t, prev, v) {
   #          all(diff(t$NDZ_sanemsanas_datums[2:5]) != 0) &&
   #          all(diff(t$NDZ_sanemsanas_datums[1:2]) == 0)) {
   #  days1 <- as.numeric(difftime(t$NDZ_sanemsanas_datums[4], t$NDZ_sanemsanas_datums[3], units = "days"))
-  #  days2 <- as.numeric(difftime(t$last_date[5], t$NDZ_sanemsanas_datums[5], units = "days")) + 1
+  #  days2 <- as.numeric(difftime(t$last_date[5], t$NDZ_sanemsanas_datums[5], units = "days")) + 1 
   #  
   #  yt <- y[v, ]
   #  yt$dienas <- sum(days1, days2)
