2bd3c7dd5ae56501aaefdcf45e4135092cea94a2
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-10-29 22:21:12 +0200

    Update starpkodi3_25.R

--- NDZ_kodi11/functions/2_starpkoduApstrade/starpkodi3/starpkodi3_25.R
+++ NDZ_kodi11/functions/2_starpkoduApstrade/starpkodi3/starpkodi3_25.R
@@ -11,7 +11,7 @@ starpkodi3_25 <- function(y, t, prev, v) {
     
 #    if (t$zinkod[2] == "50" && t$zinkod[3] == "51" && all(diff(t$NDZ_sanemsanas_datums) == 0)) {
 #    yt <- y[v, ]
-#    yt$dienas <- as.numeric(difftime(t$beidz[1], prev, units = "days")) - 1 
+#    yt$dienas <- as.numeric(difftime(t$beidz[1], prev, units = "days")) - 1 #Viss pareizi, nepieskaitu viens. Rēķinot no prev, tas pieskaitās automātiski, un es atņemu.
 #    } else if (t$zinkod[2] == "50" && t$zinkod[3] == "51" && diff(t$NDZ_sanemsanas_datums[2:3]) != 0 && diff(t$NDZ_sanemsanas_datums[1:2]) == 0) {
 #      yt <- y[v, ]
 #      yt$dienas <- as.numeric(difftime(t$beidz[1], prev, units = "days")) - 1 
@@ -24,7 +24,7 @@ starpkodi3_25 <- function(y, t, prev, v) {
 #             all(diff(t$NDZ_sanemsanas_datums[1:2]) == 0) && 
 #             all(diff(t$NDZ_sanemsanas_datums[2:3]) != 0)) {
 #    yt <- y[v, ]
-#    yt$dienas <- as.numeric(difftime(t$last_date[3], t$sak_darbu[3], units = "days")) + 1 #jo darbs
+#    yt$dienas <- as.numeric(difftime(t$last_date[3], t$sak[3], units = "days")) + 1 #jo darbs
 #    } else } else if (t$zinkod[2] %in% c("92","41") && t$zinkod[3] %in% c("92","41") && all(diff(t$NDZ_sanemsanas_datums) == 0)) {
 #      yt <- y[v, ]
 #      yt$dienas <- 0
@@ -43,7 +43,7 @@ starpkodi3_25 <- function(y, t, prev, v) {
 #    
 #  if (t$zinkod[2] == "11" && t$zinkod[3] == "50" && all(diff(t$NDZ_sanemsanas_datums) != 0)) {
 #    days1 <- as.numeric(difftime(t$beidz[1], prev, units = "days"))
-#    days2 <- as.numeric(difftime(t$beidz[3], t$sak_darbu[2], units = "days"))
+#    days2 <- as.numeric(difftime(t$beidz[3], t$sak[2], units = "days"))
 #    
 #    yt <- y[v, ]
 #    yt$dienas <- sum(days1, days2)
