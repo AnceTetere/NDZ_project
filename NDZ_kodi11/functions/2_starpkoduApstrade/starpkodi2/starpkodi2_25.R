72bfdc31637fc35cdf74ac8230246fd6577245e9
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-10-30 22:15:17 +0200

    Update starpkodi2_25.R

--- NDZ_kodi11/functions/2_starpkoduApstrade/starpkodi2/starpkodi2_25.R
+++ NDZ_kodi11/functions/2_starpkoduApstrade/starpkodi2/starpkodi2_25.R
@@ -6,8 +6,13 @@ starpkodi2_25 <- function(y, t, prev, v) {
 #  } else if (t$zinkod[2] == "92" && t$NDZ_sanemsanas_datums[1] == t$NDZ_sanemsanas_datums[2]) {
 #    yt <- y[v,] 
 #    yt$dienas <- 0
-#  } else 
-  if (t$zinkod[2] %in% c("41", "51")) {
+
+  if (t$zinkod[2] %in% c("40", "50", "53", "91")) {
+        if (diff(t$NDZ_sanemsanas_datums[1:2]) != 0) {
+          yt <- y[v,]
+          yt$dienas <- as.numeric(t$last_date[2], t$NDZ_sanemsanas_datums[2], units = "days") + 1 
+        } else {stop("Starpkodi2_25: Trūkst izstrādes koda.")}
+  } else if (t$zinkod[2] %in% c("41", "51", "54", "92")) {
       if (diff(t$NDZ_sanemsanas_datums[1:2]) != 0) {
           yt <- y[v,]
           yt$dienas <- 0
