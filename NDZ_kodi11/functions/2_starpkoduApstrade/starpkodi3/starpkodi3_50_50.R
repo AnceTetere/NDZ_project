c7cf551f2092e003ec8b192d67f31a6cb34248ac
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-11-04 20:50:31 +0200

    Update starpkodi3_50_50.R

--- NDZ_kodi11/functions/2_starpkoduApstrade/starpkodi3/starpkodi3_50_50.R
+++ NDZ_kodi11/functions/2_starpkoduApstrade/starpkodi3/starpkodi3_50_50.R
@@ -1,6 +1,6 @@
 starpkodi3_50_50 <- function(y, t, prev, v) {
   
-  if (t$zinkod[3] == "25") {
+  if (t$zinkod[3] %in% c("21", "25")) {
            if (all(diff(t$NDZ_sanemsanas_datums) != 0)) {
              yt <- y[v:(v+1), ]
              yt <- yt[yt$zinkod == "50", ]
@@ -8,12 +8,13 @@ starpkodi3_50_50 <- function(y, t, prev, v) {
   #} else if (t$zinkod[3] == "21") {  
           # if (all(diff(t$NDZ_sanemsanas_datums) != 0)) {
           #   yt <- y[v, ]
-          #   yt$dienas <- as.numeric(difftime(t$sak_beidz[2], prev, units = "days")) - 1 #jo atvaļinājums
+          #   yt$dienas <- as.numeric(difftime(t$sak_beidz[2], prev, units = "days")) - 1 
           # } else if (diff(t$NDZ_sanemsanas_datums[2:3]) != 0 && diff(t$NDZ_sanemsanas_datums[1:2]) == 0) {
           #   yt <- y[v, ]
-          #   yt$dienas <- as.numeric(difftime(t$sak_beidz[2], prev, units = "days")) - 1 #jo atvaļinājums  
+          #   yt$dienas <- as.numeric(difftime(t$sak_beidz[2], prev, units = "days")) - 1   
           # } else {stop("starpkodi3_50: Iztrūkst apstrādes koda.")} 
   } else {stop("starpkodi3_50_50: Iztrūkst apstrādes koda.")} 
   
   rm(y, t, prev, v)
   return(yt)
+}
