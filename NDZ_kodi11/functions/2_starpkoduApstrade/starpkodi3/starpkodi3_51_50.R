87b65d8f42c199049b0106f031979fa80a636d31
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-11-04 20:27:34 +0200

    Update starpkodi3_51_50.R

--- NDZ_kodi11/functions/2_starpkoduApstrade/starpkodi3/starpkodi3_51_50.R
+++ NDZ_kodi11/functions/2_starpkoduApstrade/starpkodi3/starpkodi3_51_50.R
@@ -1,27 +1,29 @@
 starpkodi3_51_50 <- function(y, t, prev, v) {
   
-    if (t$zinkod[3] == "21") {
-         if (all(!diff(t$NDZ_sanemsanas_datums) == 0)) {
-             yt <- y[v, ]
-             yt$dienas <- as.numeric(diff(t$NDZ_sanemsanas_datums[1:2]))
-         } else {stop("Starpkodi3_51_50: Trūkst izstrādes koda.")}
-    } else if (t$zinkod[3] == "25") {
+if (t$zinkod[3] %in% c("21", "22", "23", "24", "25", "29")){
          if (all(diff(t$NDZ_sanemsanas_datums) != 0)) {
            yt <- y2[v, ]
            yt$dienas <- as.numeric(diff(t$NDZ_sanemsanas_datums[1:2]))
+         }  else if (diff(t$NDZ_sanemsanas_datums[1:2]) == 0 && diff(t$NDZ_sanemsanas_datums[2:3]) != 0) {
+           yt <- y2[v, ]
+           yt$dienas <- 0
+         } else if (all(!diff(t$NDZ_sanemsanas_datums) == 0)) {
+           yt <- y[v, ]
+           yt$dienas <- as.numeric(diff(t$NDZ_sanemsanas_datums[1:2]))
          } else {stop("Starpkodi3_51_50: Trūkst izstrādes koda.")}
-    } else {stop("Starpkodi3_51_50: Trūkst izstrādes koda.")}
+} else if (t$zinkod[3] %in% c("40", "50", "53", "91")) {
+        if (diff(t$NDZ_sanemsanas_datums[1:2]) != 0) {
+          yt <- y[v, ]
+          yt$dienas <- as.numeric(diff(t$NDZ_sanemsanas_datums[2:3]))
+        } else {stop("Starpkodi3_51_50: Trūkst izstrādes koda.")}
+} else if (t$zinkod[3] %in% c("41", "51", "54", "92")) {
+       if (all(diff(t$NDZ_sanemsanas_datums) != 0)) {
+         yt <- y[v, ]
+         yt$dienas <- sum(as.numeric(diff(t$NDZ_sanemsanas_datums[1:2])),
+                          as.numeric(difftime(t$last_date[3], t$NDZ_sanemsanas_datums[3], units = "days")) + 1)
+       } else {stop("Starpkodi3_51_50: Trūkst izstrādes koda.")}
+} else {stop("Starpkodi3_51_50: Trūkst izstrādes koda.")}
 
-if(is.na(yt$PS_code[1])) {stop("Dienas NA.")}
+rm(y, t, prev, v)
 return(yt) 
 } 
-
-#}  else if (t$zinkod[3] == "21" && all(diff(t$NDZ_sanemsanas_datums[1:2]) == 0) && all(diff(t$NDZ_sanemsanas_datums[2:3]) != 0)) {
-#    yt <- y2[v, ]
-#    yt$dienas <- 0
-#}else if (t$zinkod[3] == "53" && all(diff(t$NDZ_sanemsanas_datums) == 0)) {
-#  yt <- y2[v, ]
-#  yt$dienas <- as.numeric(difftime(t$NDZ_sanemsanas_datums[3], prev, units = "days")) - 1  
-#
-
-#} else 
