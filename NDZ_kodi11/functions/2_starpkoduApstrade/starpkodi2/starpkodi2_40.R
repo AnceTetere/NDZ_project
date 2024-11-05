9460035bf873567def22a487943761c066071e1f
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-11-05 22:32:43 +0200

    Update starpkodi2_40.R

--- NDZ_kodi11/functions/2_starpkoduApstrade/starpkodi2/starpkodi2_40.R
+++ NDZ_kodi11/functions/2_starpkoduApstrade/starpkodi2/starpkodi2_40.R
@@ -1,11 +1,8 @@
 starpkodi2_40 <- function(y, t, prev, v) {
 
-  if (t$zinkod[2] == "50") {
-         if (diff(t$NDZ_sanemsanas_datums[1:2]) == 0 &&
-             (t$PS_code[1] == '_________' && t$NM_code[1] == '_________')) {
-             yt <- y[v,]
-           yt$dienas <- as.numeric(difftime(t$NDZ_sanemsanas_datums[1], prev, units = "days")) - 1 
-         } else {stop("Starpkodi2_40: Trūkst izstrādes koda.")}
+  if (t$zinkod[2] %in% c("21", "22", "23", "24", "25", "29")) {
+      yt <- y[v,]
+      yt$dienas <- as.numeric(difftime(t$NDZ_sanemsanas_datums[1], prev, units = "days")) - 1
   } else {stop("Starpkodi2_40: Trūkst izstrādes koda.")}
   
   rm(y, t, prev, v)
@@ -13,21 +10,13 @@ starpkodi2_40 <- function(y, t, prev, v) {
 }
 
   
-#           t$PS_code[1] == '_________' && t$NM_code[1] == '_________') {
+#           t$PS_code[1] == '______________' && t$NM_code[1] == '______________') {
 #  yt <- y2[v,] 
-#  yt$dienas <- as.numeric(difftime(t$beidz[1], prev, units = "days")) - 1 
-#if (t$zinkod[2] == "25") {
-#  yt <- y2[v:(v+1),] 
-#  yt <- yt[yt$zinkod == "40", ]
-#} else 
+#  yt$dienas <- as.numeric(difftime(t$beidz_darbu[1], prev, units = "days")) - 1 
 
-#} else if (t$zinkod[1] == "40" && t$zinkod[2] == "23") {
-#  yt <- y2[v,] 
-#  yt$dienas <- as.numeric(difftime(t$beidz[1], prev, units = "days")) - 1 
-
-#} else if (t$zinkod[1] == "40" && t$zinkod[2] == "51" && t$PS_code[1] == '_________' && t$NM_code[1] == '_________') {
-#  days1 <- as.numeric(difftime(t$beidz[1], prev, units = "days")) -1 
-#  days2 <- as.numeric(difftime(t$last_date[2], t$sak[2], units = "days")) + 1 
+#} else if (t$zinkod[1] == "40" && t$zinkod[2] == "51" && t$PS_code[1] == '______________' && t$NM_code[1] == '______________') {
+#  days1 <- as.numeric(difftime(t$beidz_darbu[1], prev, units = "days")) -1 
+#  days2 <- as.numeric(difftime(t$last_date[2], t$sak_darbu[2], units = "days")) + 1 
 
 #  yt <- y2[v,] 
 #  yt$dienas <- sum(days1, days2)
