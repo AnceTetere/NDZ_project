7be32ec889e852d699df0108cf6b59b599646677
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-11-05 22:37:10 +0200

    Update starpkodi4.R

--- NDZ_kodi11/functions/2_starpkoduApstrade/starpkodi4/starpkodi4.R
+++ NDZ_kodi11/functions/2_starpkoduApstrade/starpkodi4/starpkodi4.R
@@ -2,13 +2,13 @@ starpkodi4 <- function(y, t, prev, v) {
   
 if (t$zinkod[1] == "11") {
     yt <- starpkodi4_11(y, t, prev, v)
- } else if (t$zinkod[1] == "40") {
-    yt <- starpkodi4_40(y, t, prev, v)
- } else if (t$zinkod[1] == "41") {
-   yt <- starpkodi4_41(y, t, prev, v)
- } else if (t$zinkod[1] == "50") {
+ #} else if (t$zinkod[1] == "40") {
+  #  yt <- starpkodi4_40(y, t, prev, v)
+ #IZŅEMTS } else if (t$zinkod[1] == "41") {
+   #yt <- starpkodi4_41(y, t, prev, v)
+ } else if (t$zinkod[1] %in% c("40", "50", "53", "91")) {
     yt <- starpkodi4_50(y, t, prev, v)
- } else if (t$zinkod[1] == "51") {
+ } else if (t$zinkod[1] %in% c("41", "51", "54", "92")) {
     yt <- starpkodi4_51(y, t, prev, v)
  } else if (t$zinkod[1] %in% c("21", "22", "23", "24", "25", "29")) {
    yt <- starpkodi4_25(y, t, prev, v)
@@ -46,7 +46,7 @@ if (t$zinkod[1] == "11") {
     
  } else {stop("Starpkodi4 iztrūkst apstrādes koda.")}
   
-  if(is.na(yt$PS_code[1])) {stop("Dienas NA.")}
+  if(is.na(yt$pseidokods[1])) {stop("Dienas NA.")}
   yt$zinkod <- "combined"  #jo starpkodu dienu sarēķins
   return(yt)
 }
