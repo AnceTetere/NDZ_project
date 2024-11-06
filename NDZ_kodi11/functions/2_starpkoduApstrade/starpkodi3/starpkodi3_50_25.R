ea36bb0814f463ff32875a33577ef468d489a5e5
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-11-06 23:23:53 +0200

    Update starpkodi3_50_25.R

--- NDZ_kodi11/functions/2_starpkoduApstrade/starpkodi3/starpkodi3_50_25.R
+++ NDZ_kodi11/functions/2_starpkoduApstrade/starpkodi3/starpkodi3_50_25.R
@@ -6,7 +6,7 @@ starpkodi3_50_25 <- function(y, t, prev, v) {
              yt$dienas <- sum(as.numeric(difftime(t$NDZ_sanemsanas_datums[1], prev, units = "days")) - 1, 
                               as.numeric(difftime(t$last_date[3], t$NDZ_sanemsanas_datums[3], units = "days")) + 1)
            } else {stop("starpkodi3_50_25: Iztrūkst apstrādes koda.")}
-  } else if (t$zinkod[3] == "51") {
+  } else if (t$zinkod[3] %in% c("41", "51", "54", "92")) {
            yt <- y[v, ]
            yt$dienas <- as.numeric(difftime(t$NDZ_sanemsanas_datums[1], prev, units = "days")) - 1
   } else {stop("starpkodi3_50_25: Iztrūkst apstrādes koda.")} 
