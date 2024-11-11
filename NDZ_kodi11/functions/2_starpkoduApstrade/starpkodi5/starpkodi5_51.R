60e3d887e989d5a786cef9127a542dd6fa770a14
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-11-12 00:11:30 +0200

    Update starpkodi5_51.R

--- NDZ_kodi11/functions/2_starpkoduApstrade/starpkodi5/starpkodi5_51.R
+++ NDZ_kodi11/functions/2_starpkoduApstrade/starpkodi5/starpkodi5_51.R
@@ -1,12 +1,20 @@
 starpkodi5_51 <- function(y, t, prev, v) {
   
+  yt <- y[v,]
+  
   if (t$zinkod[2] %in% c("40", "50", "53", "91")) {
           if (t$zinkod[3] %in% c("41", "51", "54", "92")) {
             if (t$zinkod[4] %in% c("21", "22", "23", "24", "25", "29")) {
               if (t$zinkod[5] %in% c("40", "50", "53", "91")) {
                 if (all(sapply(c(2,4), function(i) diff(t$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) &&
                     all(sapply(c(1,3), function(i) diff(t$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
-                  yt <- y[v,]
+                  yt$dienas <- sum(as.numeric(diff(t$NDZ_sanemsanas_datums[1:2])), 
+                                   as.numeric(diff(t$NDZ_sanemsanas_datums[3:4])))
+                } else {stop("starpkodi5_51: Iztrūkst apstrādes koda.")}
+              } else {stop("starpkodi5_51: Iztrūkst apstrādes koda.")}
+            } else if (t$zinkod[4] %in% c("40", "50", "53", "91")) {
+              if (t$zinkod[5] %in% c("21", "22", "23", "24", "25", "29")) {
+                if (all(diff(t$NDZ_sanemsanas_datums) != 0)) {
                   yt$dienas <- sum(as.numeric(diff(t$NDZ_sanemsanas_datums[1:2])), 
                                    as.numeric(diff(t$NDZ_sanemsanas_datums[3:4])))
                 } else {stop("starpkodi5_51: Iztrūkst apstrādes koda.")}
