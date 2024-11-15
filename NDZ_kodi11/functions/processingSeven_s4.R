b6acd220316930f842470db12495e58b28635573
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-11-15 18:03:12 +0200

    Update processingSeven_s4.R

--- NDZ_kodi11/functions/processingSeven_s4.R
+++ NDZ_kodi11/functions/processingSeven_s4.R
@@ -1,22 +1,36 @@
 processingSeven_s4 <- function(x7s4) {
+  
   x7s4_uzVieniniekiem <- data.frame()
   x7s4_uzDivniekiem <- data.frame()
   x7s4_uzTrijniekiem <- data.frame()
   x7s4_uzCetriniekiem <- data.frame()
   x7s4_uzPieciniekiem <- data.frame()
   x7s4_uzSesiniekiem <- data.frame()
-
+  #x7s4 <- x7
+  
   if (all(x7s4$sak_beidz == c("1", "2", "1", "1", "2", "1", "2")) && 
-      all(sapply(c(1,3:6), function(i) all(diff(x7s4$NDZ_sanemsanas_datums[i:(i+1)]) != 0)))) {
+      all(sapply(c(1,3:6), function(i) diff(x7s4$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
     x7s4_uzTrijniekiem <- rbind(x7s4_uzTrijniekiem, x7s4[1:3, ])
     x7s4_uzCetriniekiem <- rbind(x7s4_uzCetriniekiem, x7s4[4:7, ])
+  } else if (all(x7s4$sak_beidz == c("2", "1", "2", "1", "1", "2", "1"))) {
+       if (all(sapply(c(1,3), function(i) diff(x7s4$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) &&
+           all(sapply(c(2,4:6), function(i) diff(x7s4$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
+         x7s4_uzDivniekiem <- rbind(x7s4_uzDivniekiem, x7s4[c(2,1,4,3,5,6), ])
+         x7s4_uzVieniniekiem <- rbind(x7s4_uzVieniniekiem, x7s4[1, ])
+       } else {stop("processingSeven: Septiņnieku apstrādē, gadījumam rindās ", r, " līdz ", r + 6, " trūkst izstrādes koda.\n")}
   } else if (all(x7s4$sak_beidz == c("1", "2", "1", "2", "2", "1", "1")) && 
              all(diff(x7s4$NDZ_sanemsanas_datums) != 0)) {
     x7s4_uzCetriniekiem <- rbind(x7s4_uzCetriniekiem, x7s4[c(1:3,5), ])
     x7s4_uzVieniniekiem <- rbind(x7s4_uzVieniniekiem, x7s4[7, ])
-  } else if (all(x7s4$sak_beidz == c("2", "1", "1", "2", "1", "2", "1")) && all(diff(x7s4$NDZ_sanemsanas_datums) != 0)) {
-    x7s4_uzCetriniekiem <- rbind(x7s4_uzCetriniekiem, x7s4[3:6, ])
-    x7s4_uzVieniniekiem <- rbind(x7s4_uzVieniniekiem, x7s4[c(1,7), ])
+  } else if (all(x7s4$sak_beidz == c("2", "1", "1", "2", "1", "2", "1"))) {
+         if (all(diff(x7s4$NDZ_sanemsanas_datums) != 0)) {
+           x7s4_uzCetriniekiem <- rbind(x7s4_uzCetriniekiem, x7s4[3:6, ])
+           x7s4_uzVieniniekiem <- rbind(x7s4_uzVieniniekiem, x7s4[c(1,7), ])
+         } else if (all(sapply(2:6, function(i) diff(x7s4$NDZ_sanemsanas_datums[i:(i+1)]) != 0)) &&
+                    diff(x7s4$NDZ_sanemsanas_datums[1:2]) == 0) {
+                    x7s4_uzDivniekiem <- rbind(x7s4_uzDivniekiem, x7s4[c(2,1,3,4,5,6), ])
+                    x7s4_uzVieniniekiem <- rbind(x7s4_uzVieniniekiem, x7s4[7, ])
+         } else {stop("processingSeven_s4 trūkst izstrādes koda.\n")}
   } else if (all(x7s4$sak_beidz == c("1", "2", "1", "2", "1", "2", "1"))) {
           if (all(diff(x7s4$NDZ_sanemsanas_datums) != 0)) {
               x7s4_uzSesiniekiem <- rbind(x7s4_uzSesiniekiem, x7s4[1:6, ])
@@ -29,7 +43,11 @@ processingSeven_s4 <- function(x7s4) {
                      all(sapply(c(1,2,4,5,6), function(i) diff(x7s4$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
                       x7s4_uzSesiniekiem <- rbind(x7s4_uzSesiniekiem, x7s4[1:6, ])
                       x7s4_uzVieniniekiem <- rbind(x7s4_uzVieniniekiem, x7s4[7, ])
-          }else {stop("processingSeven: Septiņnieku apstrādē, gadījumam rindās ", r, " līdz ", r + 6, " trūkst izstrādes koda.\n")}
+          } else if (all(sapply(c(3,5), function(i) diff(x7s4$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) &&
+                     all(sapply(c(1,2,4,6), function(i) diff(x7s4$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
+                     x7s4_uzSesiniekiem <- rbind(x7s4_uzSesiniekiem, x7s4[1:6, ])
+                     x7s4_uzVieniniekiem <- rbind(x7s4_uzVieniniekiem, x7s4[7, ])
+          } else {stop("processingSeven: Septiņnieku apstrādē, gadījumam rindās ", r, " līdz ", r + 6, " trūkst izstrādes koda.\n")}
   } else {stop("processingSeven: Septiņnieku apstrādē, gadījumam rindās ", r, " līdz ", r + 6, " trūkst izstrādes koda.\n")}
   
   return(list(x7_uzVieniniekiem = x7s4_uzVieniniekiem,
