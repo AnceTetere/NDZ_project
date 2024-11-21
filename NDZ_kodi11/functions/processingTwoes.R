ef37f290d1a954fb08d62d9bc0f116e1c28ce12e
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-11-21 19:01:20 +0200

    Update processingTwoes.R

--- NDZ_kodi11/functions/processingTwoes.R
+++ NDZ_kodi11/functions/processingTwoes.R
@@ -1,21 +1,23 @@
 processingTwoes <- function(x, o) {
-  
+
   #1 Sakārto tabulu
   x <- arrange(x, PS_code, DN_code, NM_code, NDZ_sanemsanas_datums)
   rownames(x) <- NULL
   
   x2_uzVieniniekiem <- data.frame()
   x2_trueDoubles <- data.frame()
-  
+    
   for (r in seq(1, nrow(x), by = 2)) {
-    if (doublesTest(r, x)) { 
+    if (doublesTest(r, x)) { # doublesTest() pārbauda vai katras 2 rindas attiecas uz vienu un to pašu unikālo indivīdu.
       if ((x$sak_beidz[r] == "2" && x$sak_beidz[r+1] == "1" && diff(x$NDZ_sanemsanas_datums[r:(r+1)]) > 0) || 
           (x$sak_beidz[r] == x$sak_beidz[r + 1])) {
-        x2_uzVieniniekiem <- rbind(x2_uzVieniniekiem, x[c(r, r + 1),])
-      } else {x2_trueDoubles <- rbind(x2_trueDoubles, x[c(r, r + 1),])}
+        x2_uzVieniniekiem <- rbind(x2_uzVieniniekiem, x[c(r, (r + 1)),])
+      } else {x2_trueDoubles <- rbind(x2_trueDoubles, x[c(r, (r + 1)),])}
     } else {stop("Rindās ", r, " un ", r + 1, " pamatkodi atšķiras.\n")}
   }
       
+  cat("Pāra tabula sadalīta tabulās x2_uzVieniniekiem:",
+    nrow(x2_uzVieniniekiem), "rindas; \n un x2_trueDoubles: ", nrow(x2_trueDoubles), "rindas.\n")
   rm(x, r)
   
   #2 Tabulu x2_uzVieniniekiem sūta uz vieninieku apstrādi caur processingOnes().
@@ -27,8 +29,6 @@ processingTwoes <- function(x, o) {
   rm(x2_uzVieniniekiem)
 
 #3 Izstrādā x2_trueDoubles
-#3.1 Pārbaude vai šie visi ir īstie dubultnieki
-  
   if (nrow(x2_trueDoubles) > 0) {
     y <- arrange(x2_trueDoubles, PS_code, NM_code); test <- 0
     
