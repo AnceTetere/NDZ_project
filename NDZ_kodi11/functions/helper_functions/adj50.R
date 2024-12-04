85f170dba9ab6c9a29671da16f4ff003d7995ec0
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-12-04 22:21:30 +0200

    Delete NDZ_kodi11/functions/helper_functions/adj50.R

--- NDZ_kodi11/functions/helper_functions/adj50.R
+++
@@ -1,9 +0,0 @@
-adj50 <- function(NDZ) {
-  
-  if (NDZ$period[1] == "202102") {
-    NDZ <- NDZ %>% 
-      filter(!(PS_code == '___________' & NM_code == '___________' & zinkod == '51'))
-  }
-  
-  return(NDZ) 
-}
