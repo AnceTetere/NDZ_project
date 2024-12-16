ff6c79807f385ed74cdc25dbfc12041f86aa3cbd
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-12-16 19:59:41 +0200

    Update processingTens_s6.R

--- NDZ_kodi11/functions/processingTens_s6.R
+++ NDZ_kodi11/functions/processingTens_s6.R
@@ -1,7 +1,7 @@
 processingTens_s6 <- function(x10s6) {
   x10s6 <- arrange(x10s6, PS_code, DN_code, NM_code, NDZ_sanemsanas_datums)
   
-   x10s6_uzVieniniekiem <- data.frame()
+  x10s6_uzVieniniekiem <- data.frame()
   x10s6_uzDivniekiem <- data.frame()
   x10s6_uzSeptini <- data.frame()
   x10s6_uzAstoniekiem <- data.frame()
