40fa240b2d76f72dafee4eac99d9372c7e9503ba
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-12-22 14:22:17 +0200

    Update starpkodi3_11.R

--- NDZ_kodi11/functions/2_starpkoduApstrade/starpkodi3/starpkodi3_11.R
+++ NDZ_kodi11/functions/2_starpkoduApstrade/starpkodi3/starpkodi3_11.R
@@ -1,17 +1,15 @@
 starpkodi3_11 <- function(y, t, prev, v) {
   
-  if (t$zinkod[2] %in% c("11", "14", "16", "61")) {
+  if (t$zk[2] %in% c("11", "14", "16", "61")) {
     yt <- starpkodi3_11_16(y, t, prev, v)
-  } else if (t$zinkod[2] %in% c("21", "22", "23", "24", "25", "29")) {
+  } else if (t$zk[2] %in% c("21", "22", "23", "24", "25", "29")) {
     yt <- starpkodi3_11_29(y, t, prev, v)
-  } else if (t$zinkod[2] %in% c("40", "50", "53", "91")) {
+  } else if (t$zk[2] %in% c("40", "50", "53", "91")) {
     yt <- starpkodi3_11_50(y, t, prev, v)
+  } else if (t$zk[2]  %in% c("41", "51", "54", "92")) {
+    yt <- starpkodi3_11_51(y, t, prev, v)  
   } else {stop("Starpkodi3_11: Trūkst izstrādes koda.")}
   
   rm(y, t, prev, v)
   return(yt) 
-}    
-  
-  
-#  } else if (t$zinkod[2] %in% c("51", "41")) {
-#    yt <- starpkodi3_11_51(y, t, prev, v)
+}
