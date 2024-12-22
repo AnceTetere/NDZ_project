fd264ee3aa87a5b99d1e47db7aee07781216763d
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-12-22 12:00:33 +0200

    Update starpkodi4_25.R

--- NDZ_kodi11/functions/2_otraisCikls_STARPKODI/starpkodi4/starpkodi4_25.R
+++ NDZ_kodi11/functions/2_otraisCikls_STARPKODI/starpkodi4/starpkodi4_25.R
@@ -18,30 +18,29 @@ starpkodi4_25 <- function(y, t, prev, v) {
            } else {stop("9starpkodi4_25: Iztrūkst aptrādes koda.")}
     
   } else if (t$zk[2] == "11") {
-    if (t$zk[3] == "50") {
-      if (t$zk[4] == "51") {
-        if (all(diff(t$NDZ_sanemsanas_datums) != 0)) {
-          yt <- y[v, ]
-          yt$dd <- sum(as.numeric(difftime(t$NDZ_sanemsanas_datums[1], prev, units = "days")),
-                           as.numeric(diff(t$NDZ_sanemsanas_datums[2:3])),
-                           as.numeric(difftime(t$last_date[4], t$NDZ_sanemsanas_datums[4], units = "days")) + 1)
-          
-        } else {stop("starpkodi4_25: Iztrūkst aptrādes koda.")}
-      } else if (t$zk[3] == "11") {
-        if (t$zk[4] == "50") {
-          if (all(sapply(c(1,3), function(i) diff(t$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) &&
-              diff(t$NDZ_sanemsanas_datums[2:3]) != 0) {
-            if (t$PS_code[1] == '____________' && t$NM_code[1] == '____________') {
-              yt <- y[v, ]
-              yt$dd <- as.numeric(sum(difftime(t$NDZ_sanemsanas_datums[1], prev, units = "days"),
-                                          diff(t$NDZ_sanemsanas_datums[3:4])))
+          if (t$zk[3] == "50") {
+            if (t$zk[4] == "51") {
+              if (all(diff(t$NDZ_sanemsanas_datums) != 0)) {
+                yt <- y[v, ]
+                yt$dd <- sum(as.numeric(difftime(t$NDZ_sanemsanas_datums[1], prev, units = "days")),
+                                 as.numeric(diff(t$NDZ_sanemsanas_datums[2:3])),
+                                 as.numeric(difftime(t$last_date[4], t$NDZ_sanemsanas_datums[4], units = "days")) + 1)
+              } else {stop("starpkodi4_25: Iztrūkst aptrādes koda.")}
             } else {stop("starpkodi4_25: Iztrūkst aptrādes koda.")}
+        } else if (t$zk[3] == "11") {
+                if (t$zk[4] == "50") {
+                  if (all(sapply(c(1,3), function(i) diff(t$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) &&
+                      diff(t$NDZ_sanemsanas_datums[2:3]) != 0) {
+                      if (t$PS_code[1] == '__________' && t$NM_code[1] == '__________') {
+                        yt <- y[v, ]
+                        yt$dd <- as.numeric(sum(difftime(t$NDZ_sanemsanas_datums[1], prev, units = "days"),
+                                          diff(t$NDZ_sanemsanas_datums[3:4])))
+                     } else {stop("starpkodi4_25: Iztrūkst aptrādes koda.")}
+                  } else {stop("starpkodi4_25: Iztrūkst aptrādes koda.")}
+              } else {stop("starpkodi4_25: Iztrūkst aptrādes koda.")}
           } else {stop("starpkodi4_25: Iztrūkst aptrādes koda.")}
-        } else {stop("starpkodi4_25: Iztrūkst aptrādes koda.")}
     } else {stop("starpkodi4_25: Iztrūkst aptrādes koda.")}
-    } else {stop("starpkodi4_25: Iztrūkst aptrādes koda.")}
-  } else {stop("starpkodi4_25: Iztrūkst aptrādes koda.")}
-  
+
   rm(y, t, prev, v)
   return(yt)
 }
