c5cd4b6e1b1d641f2479a615a4abfcc645bdc82b
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-01-10 14:53:24 +0200

    Update processingEights_s4.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/processingEights_s4.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/processingEights_s4.R
@@ -1,21 +1,18 @@
 processingEights_s4 <- function(x8s4) {
-  x8s4 <- x8s4 %>% arrange(PS_code, DN_code, NM_code, NDZ_sanemsanas_datums)
-#x8s4 <- x8
-x8s4_uzVieniniekiem <- data.frame()
-x8s4_uzDivniekiem <- data.frame()
-x8s4_uzCetriniekiem <- data.frame()
-x8s4_uzSesi <- data.frame()
-x8s4_uzSeptini <- data.frame()
 
-if (x8s4$PS_code[1] == '__________' && x8s4$NM_code[1] == '__________') {
+  x8s4 <- x8s4 %>% arrange(PS_code, DN_code, NM_code, NDZ_sanemsanas_datums)
+  x8s4_uzVieniniekiem <- data.frame(); x8s4_uzDivniekiem <- data.frame(); x8s4_uzCetriniekiem <- data.frame()
+  x8s4_uzSesi <- data.frame(); x8s4_uzSeptini <- data.frame()
+
+if (x8s4$PS_code[1] == '_________' && x8s4$NM_code[1] == '_________') {
   if (all(x8s4$sak_beidz == c("2","1","2","2","1","2","1","1"))) {
     x8s4_uzVieniniekiem <- rbind(x8s4_uzVieniniekiem, x8s4[c(1,8), ])
     x8s4_uzCetriniekiem <- rbind(x8s4_uzCetriniekiem, x8s4[c(2,4,5,6),])
   } else {stop("processingEights_s4: Trūkst izstrādes koda.")}
-} else if(all(sapply(seq(1,8,by=2), function(i) diff(x8s4$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) &&
+} else if (all(sapply(seq(1,8,by=2), function(i) diff(x8s4$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) &&
    all(sapply(seq(2,7,by=2), function(i) diff(x8s4$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
   #cS (check sak_beidz) norāda kombinācijas, kurās rindās izejas tabulas ailē sak_beidz atrodas "1".
-  cS <- list(c(2, 4, 6, 8), c(1, 4, 6, 7), c(2, 3, 6, 8), c(2, 3, 5, 7), c(2, 3, 5, 8), c(1, 4, 5, 7), c(1, 3, 6, 8), c(1, 3, 5, 7), c(2, 3, 6, 7))
+  cS <- list(c(2, 4, 5, 8), c(2, 4, 6, 8), c(1, 4, 6, 7), c(2, 3, 6, 8), c(2, 3, 5, 7), c(2, 3, 5, 8), c(1, 4, 5, 7), c(1, 3, 6, 8), c(1, 3, 5, 7), c(2, 3, 6, 7))
   found_match <- FALSE
   
   #Salīdzina vai šajā apakštabulā kāda no cS kombinācijām atrodama.
@@ -51,7 +48,9 @@ if (x8s4$PS_code[1] == '__________' && x8s4$NM_code[1] == '__________') {
   x8s4_uzCetriniekiem  <- rbind(x8s4_uzCetriniekiem, x8s4)
   #stop("processingEights_s4: Trūkst izstrādes koda.")
 }
+
 rm(x8s4)
+
 return(list(x8_uzVieniniekiem = x8s4_uzVieniniekiem, 
             x8_uzDivniekiem = x8s4_uzDivniekiem, 
             x8_uzCetriniekiem = x8s4_uzCetriniekiem, 
