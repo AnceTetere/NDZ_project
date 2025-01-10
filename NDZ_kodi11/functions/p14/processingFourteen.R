b3e4d4c24557ca0df46d7e7f5117993323476ab4
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-01-10 18:22:08 +0200

    Update and rename NDZ_kodi11/functions/processingFourteen.R to NDZ_kodi11/functions/p14/processingFourteen.R

---
+++ NDZ_kodi11/functions/p14/processingFourteen.R
@@ -0,0 +1,60 @@
+processingFourteen <- function(x, o, kods) {
+  x <- x %>% arrange(PS_code, DN_code, NM_code, NDZ_sanemsanas_datums)
+
+  x14_uzVieniniekiem <- data.frame(); x14_uzDivi <- data.frame()
+  x14_uzDivpadsmit <- data.frame(); x14_uzTrispadsmit <- data.frame()
+  
+  for (r in seq(1, nrow(x), by = 14)) {
+    
+    x14 <- x[r:(r+13),] %>% arrange(PS_code, DN_code, NM_code, NDZ_sanemsanas_datums, zinkod)
+    
+    if (all(x14$sak_beidz[c(2, 3, 5, 8, 9, 12, 14)] == "1") && 
+        x14$NDZ_sanemsanas_datums[1] == x14$NDZ_sanemsanas_datums[2] &&
+        x14$NDZ_sanemsanas_datums[2] != x14$NDZ_sanemsanas_datums[3]) {
+      x14_uzDivi <- rbind(x14_uzDivi, x14[1:2, ])    
+      x14_uzDivpadsmit <- rbind(x14_uzDivpadsmit, x14[-(1:2), ])    
+    } else if (all(x14$sak_beidz[seq(1, 14, by = 2)] == "1") && 
+               x14$NDZ_sanemsanas_datums[1] <= x14$NDZ_sanemsanas_datums[2] &&
+               x14$NDZ_sanemsanas_datums[2] != x14$NDZ_sanemsanas_datums[3]) {
+      x14_uzDivi <- rbind(x14_uzDivi, x14[1:2, ])    
+      x14_uzDivpadsmit <- rbind(x14_uzDivpadsmit, x14[-(1:2), ])    
+    } else if (all(x14$sak_beidz[seq(2, 14, by = 2)] == "1") && 
+               all(diff(x14$NDZ_sanemsanas_datums) != 0)) {
+      x14_uzVieniniekiem <- rbind(x14_uzVieniniekiem, x14[1, ])    
+      x14_uzTrispadsmit <- rbind(x14_uzTrispadsmit, x14[-1, ])    
+    } else {
+      stop("14-nieku tabulas pārdalei trūkst izstrādes koda. Rindas: ", r, " līdz ", r + 13, "\n")
+    }
+  }
+  
+  #PĀRBAUDE: Vai rindu skaits no 14-niekiem atvasinātajās tabulās sakrīt ar rindām izejas tabulā x.
+  if (nrow(x14_uzVieniniekiem) + nrow(x14_uzDivi) + nrow(x14_uzDivpadsmit) + nrow(x14_uzTrispadsmit) == nrow(x)) {
+    cat("PĀRBAUDE IZIETA: Apakštabulu rindu summa sakrīt ar izejošo 13-nieku tabulu.\n"); rm(x, x14, r)
+  } else {
+    stop("ERROR: Apakštabulu rindu summa NESAKRĪT ar izejošo 14-nieku tabulu.\n")
+  }
+  
+  #1 Apakštabulu x14_uzVieniniekiem sūta caur processingOnes().
+  if (nrow(x14_uzVieniniekiem) > 0) {
+    x14_uzVieniniekiem %>% arrange(PS_code, NM_code, NDZ_sanemsanas_datums, zinkod) %>% processingOnes(o) %>% sendTo_tempNDZ(o)
+  } else {cat("Tabula x14_uzVieniniekiem ir tukša.\n")}
+  rm(x14_uzVieniniekiem)
+  
+  #2 Apakštabulu x14_uzDivi sūta caur processingTwoes().
+  if(nrow(x14_uzDivi) > 0) {
+    x14_uzDivi %>% arrange(PS_code, NM_code, NDZ_sanemsanas_datums, sak_beidz) %>% processingTwoes(o, kods)
+  } else {cat("Tabula x14_uzDivi ir tukša.\n")}
+  rm(x14_uzDivi)
+  
+  #5 Apakštabulu x14_uzDivpadsmit sūta caur processingTwelve().
+  if (nrow(x14_uzDivpadsmit) > 0) {
+    x14_uzDivpadsmit %>% arrange(PS_code, NM_code, NDZ_sanemsanas_datums, sak_beidz) %>% processingTwelve(o, kods)
+  } else {cat("Tabula x14_uzDivpadsmit ir tukša.\n")}
+  rm(x14_uzDivpadsmit)
+  
+  #6 Apakštabulu x14_uzTrispadsmit sūta caur processingThirteen().
+  if (nrow(x14_uzTrispadsmit) > 0) {
+    x14_uzTrispadsmit %>% arrange(PS_code, NM_code, NDZ_sanemsanas_datums, sak_beidz) %>% processingThirteen(o, kods)
+  } else {cat("Tabula x14_uzTrispadsmit ir tukša.\n")}
+  rm(x14_uzTrispadsmit)
+}
