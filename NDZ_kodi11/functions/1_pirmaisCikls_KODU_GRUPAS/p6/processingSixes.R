6febbf441d2ee1c8be69a9c88ee147a7eae4a20c
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-01-13 14:18:14 +0200

    Update and rename NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/processingSixes.R to NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p6/processingSixes.R

---
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p6/processingSixes.R
@@ -0,0 +1,83 @@
+processingSixes <- function(x, o, kods) {
+  x <- x %>% arrange(PS_code, DN_code, NM_code, NDZ_sanemsanas_datums)
+
+  x6_uzVieniniekiem <- data.frame(); x6_uzDivniekiem <- data.frame(); x6_uzTris <- data.frame(); x6_uzCetri <- data.frame(); x6_uzPieciniekiem <- data.frame()
+  check_rows <- 0
+
+  for (r in seq(1, nrow(x), by = 6)) {
+      x6 <- x[r:(r+5),] %>% arrange(PS_code, DN_code, NM_code, NDZ_sanemsanas_datums, sak_beidz)
+      
+    if (all(x6$sak_beidz[1:2] == c("2", "1")) && diff(x6$NDZ_sanemsanas_datums[1:2]) != 0) {
+        x6_uzVieniniekiem <- rbind(x6_uzVieniniekiem, x6[1, ])
+        x6_uzPieciniekiem <- rbind(x6_uzPieciniekiem, x6[2:6, ])
+    } else if (diff(x6$NDZ_sanemsanas_datums[1:2]) == 0 && 
+               diff(x6$NDZ_sanemsanas_datums[3:4]) != 0 && 
+               x6$sak_beidz[1] != x6$sak_beidz[2] && x6$sak_beidz[3] == "2") {
+      x6_uzDivniekiem <- x6[c(1,3), ] %>% rbind(x6_uzDivniekiem)
+      x6_uzTris <- x6[4:6, ] %>% rbind(x6_uzTris)
+    } else if (sum(x6$sak_beidz == "1") == 6) {
+      x_vieninieki <- codes_match(x6)
+      x6_uzVieniniekiem <- rbind(x6_uzVieniniekiem, x_vieninieki)
+      rm(x_vieninieki)
+    } else if (sum(x6$sak_beidz == "1") == 3) {
+         result <- processingSixes_s3(x6) 
+    } else if (sum(x6$sak_beidz == "1") == 4) {
+        result <- processingSixes_s4(x6)
+    } else if (sum(x6$sak_beidz == "1") == 5) {
+                if (all(x6$sak_beidz == c("1", "1", "1", "1", "1", "2"))) {
+                  if (all(diff(x6$NDZ_sanemsanas_datums[1:5]) != 0) && diff(x6$NDZ_sanemsanas_datums[5:6]) == 0) {
+                    if (x6$PS_code[1] == '______________' && x6$NM_code[1] == '______________') {
+                      x6_uzDivniekiem <- rbind(x6_uzDivniekiem, x6[5:6, ])
+                    } else {stop("processingSeven trūkst izstrādes koda.\n")}
+                  } else {stop("processingSixes: trūkst apstrādes koda sešinieku apakštabulai!\n Rinda ", r, " līdz ", r+5, "\n")}
+                } else {stop("processingSixes: trūkst apstrādes koda sešinieku apakštabulai!\n Rinda ", r, " līdz ", r+5, "\n")}
+    } else {stop("processingSixes: Sešinieku tabulā trūkst apstrādes koda sešinieku apakštabulai!\n Rinda ", r, " līdz ", r+5, "\n")}
+    
+      if(exists("result")) {
+        x6_uzVieniniekiem <- rbind(x6_uzVieniniekiem, result$x6_uzVieniniekiem)  
+        x6_uzDivniekiem <- rbind(x6_uzDivniekiem, result$x6_uzDivniekiem)
+        x6_uzCetri <- rbind(x6_uzCetri, result$x6_uzCetri)
+        x6_uzPieciniekiem <- rbind(x6_uzPieciniekiem, result$x6_uzPieciniekiem)
+      }
+      
+      check_rows <- check_rows + 6
+  }
+  
+#Pārbaude
+if(check_rows == nrow(x)) {
+  cat("PĀRBAUDE IZIETA:
+      Rindu summa no sešiniekiem atvasinātajās tabulās sakrīt ar rindu skaitu oriģinālajā tabulā NDZ_6.\n")
+} else {stop("PĀRBAUDE NAV IZIETA.
+             Rindu summa no sešiniekiem atvasinātajās tabulās NESAKRĪT ar rindu skaitu oriģinālajā tabulā NDZ_6.\n")}
+rm(x, r, x6, check_rows)
+  
+#1 Apakštabulu x6_uzVieniniekiem apstrādā caur funkciju processingOnes().
+if(nrow(x6_uzVieniniekiem) > 0) {
+  x6_uzVieniniekiem %>% arrange(PS_code, NM_code, NDZ_sanemsanas_datums) %>% processingOnes(o) %>% sendTo_tempNDZ(o)
+} else {cat("Tabula x6_uzVieniniekiem ir tukša.\n")}
+rm(x6_uzVieniniekiem)
+  
+#2 Apakštabulu x6_uzDivniekiem sūta caur funkciju processingTwoes().
+if(nrow(x6_uzDivniekiem) > 0) {
+  x6_uzDivniekiem %>% arrange(PS_code, NM_code, NDZ_sanemsanas_datums) %>% processingTwoes(o, kods)
+} else {cat("Tabula x6_uzDivniekiem ir tukša.\n")}
+rm(x6_uzDivniekiem)
+  
+#3 Apakštabulu x6_uzTris sūta caur funkciju processingThrees().
+if(nrow(x6_uzTris) > 0) {
+  x6_uzTris %>% arrange(PS_code, NM_code, NDZ_sanemsanas_datums) %>% processingThrees(o, kods)
+} else {cat("Tabula x6_uzTris ir tukša.\n")}
+rm(x6_uzTris)
+
+#4 Apakštabulu x6_uzCetri sūta caur funkciju processingFours().
+if(nrow(x6_uzCetri) > 0) {
+  x6_uzCetri %>% arrange(PS_code, NM_code, NDZ_sanemsanas_datums) %>% processingFours(o, kods)
+} else {cat("Tabula x6_uzCetri ir tukša.\n")}
+rm(x6_uzCetri)
+  
+#5 Apakštabulu x6_uzPieciniekiem sūta caur funkciju processingFives().
+if(nrow(x6_uzPieciniekiem) > 0) {
+  x6_uzPieciniekiem %>% arrange(PS_code, NM_code, NDZ_sanemsanas_datums) %>% processingFives(o, kods)
+} else {cat("Tabula x6_uzPieciniekiem ir tukša.\n")}
+rm(x6_uzPieciniekiem)
+}
