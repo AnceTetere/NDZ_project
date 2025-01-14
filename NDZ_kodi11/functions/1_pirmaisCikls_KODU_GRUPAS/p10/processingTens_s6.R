e8ae3f5091e7583b6a60cb7d48cf4cb4353645d7
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-01-14 15:09:56 +0200

    Update processingTens_s6.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p10/processingTens_s6.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p10/processingTens_s6.R
@@ -7,7 +7,7 @@ processingTens_s6 <- function(x10s6) {
   x10s6_uzAstoniekiem <- data.frame()
   
   if (all(x10s6$sak_beidz[c(2, 4, 7, 9)] == "2") && all(x10s6$sak_beidz[c(1, 3, 5, 6, 8, 10)] == "1") &&
-      all(sapply(c(1:3, 5:9), function(i) all(diff(x10s6$NDZ_sanemsanas_datums[i:i+1]) != 0))) && all(diff(x10s6$NDZ_sanemsanas_datums[4:5]) == 0)) {
+      all(sapply(c(1:3, 5:9), function(i) diff(x10s6$NDZ_sanemsanas_datums[i:(i+1)]) != 0)) && diff(x10s6$NDZ_sanemsanas_datums[4:5]) == 0) {
     x10s6_uzVieniniekiem <- rbind(x10s6_uzVieniniekiem, x10s6[10, ])
     x10s6_uzDivniekiem <- rbind(x10s6_uzDivniekiem, x10s6[1:2, ])
     x10s6_uzSeptini <- rbind(x10s6_uzSeptini, x10s6[3:9, ])
@@ -19,9 +19,7 @@ processingTens_s6 <- function(x10s6) {
              all(sapply(c(2,4,5,7,9), function(i) diff(x10s6$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
     x10s6_uzVieniniekiem <- rbind(x10s6_uzVieniniekiem, x10s6[c(1,10), ])
     x10s6_uzDivniekiem <- rbind(x10s6_uzDivniekiem, x10s6[5:6, ])
-  } else {
-    stop("processingTens_s6: Trūkst izstrādes koda.")
-  }
+  } else {stop("processingTens_s6: Trūkst izstrādes koda.")}
   
   rm(x10s6)
   return(list(x10_uzVieniniekiem = x10s6_uzVieniniekiem,
