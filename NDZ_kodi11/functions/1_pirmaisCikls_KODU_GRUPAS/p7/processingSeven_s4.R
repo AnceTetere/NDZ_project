62344ea7862be5c991ea5bb917096320ac6244d9
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-01-14 15:40:00 +0200

    Update processingSeven_s4.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p7/processingSeven_s4.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p7/processingSeven_s4.R
@@ -15,13 +15,16 @@ processingSeven_s4 <- function(x7s4) {
                      all(sapply(c(4,6), function(i) diff(x7s4$NDZ_sanemsanas_datums[i:(i+1)]) == 0))) {
                       x7s4_uzVieniniekiem <- rbind(x7s4_uzVieniniekiem, x7s4[6, ])
                       x7s4_uzSesiniekiem <- rbind(x7s4_uzSesiniekiem, x7s4[c(1,2,3,5,4,7), ])      
-          } else {stop("processingSeven: Septiņnieku apstrādē, gadījumam rindās ", r, " līdz ", r + 6, " trūkst izstrādes koda.\n")}
+          } else {stop("processingSeven trūkst izstrādes koda.\n")}
   } else if (all(x7s4$sak_beidz == c("2", "1", "2", "1", "1", "2", "1"))) {
             if (all(sapply(c(1,3), function(i) diff(x7s4$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) &&
                  all(sapply(c(2,4:6), function(i) diff(x7s4$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
                   x7s4_uzDivniekiem <- rbind(x7s4_uzDivniekiem, x7s4[c(2,1,4,3,5,6), ])
                   x7s4_uzVieniniekiem <- rbind(x7s4_uzVieniniekiem, x7s4[1, ])
-              } else {stop("processingSeven: Septiņnieku apstrādē, gadījumam rindās ", r, " līdz ", r + 6, " trūkst izstrādes koda.\n")}
+            } else if (all(diff(x7s4$NDZ_sanemsanas_datums) != 0)) {
+                  x7s4_uzDivniekiem <- rbind(x7s4_uzDivniekiem, x7s4[c(2,3,5,6), ])
+                  x7s4_uzVieniniekiem <- rbind(x7s4_uzVieniniekiem, x7s4[c(2,7), ])
+            } else {stop("processingSeven trūkst izstrādes koda.\n")}
   } else if (all(x7s4$sak_beidz == c("1", "2", "1", "2", "2", "1", "1"))) { 
                if (all(diff(x7s4$NDZ_sanemsanas_datums) != 0)) {
                  x7s4_uzCetriniekiem <- rbind(x7s4_uzCetriniekiem, x7s4[c(1:3,5), ])
@@ -30,7 +33,7 @@ processingSeven_s4 <- function(x7s4) {
                           diff(x7s4$NDZ_sanemsanas_datums[5:6]) == 0) {
                  x7s4_uzSesiniekiem <- rbind(x7s4_uzSesiniekiem, x7s4[c(1,2,3,4,6,5), ])
                  x7s4_uzVieniniekiem <- rbind(x7s4_uzVieniniekiem, x7s4[7, ])
-               } else {stop("processingSeven: Septiņnieku apstrādē, gadījumam rindās ", r, " līdz ", r + 6, " trūkst izstrādes koda.\n")}
+               } else {stop("processingSeven trūkst izstrādes koda.\n")}
   } else if (all(x7s4$sak_beidz == c("2", "1", "1", "2", "1", "2", "1"))) {
             if (all(diff(x7s4$NDZ_sanemsanas_datums) != 0)) {
                x7s4_uzCetriniekiem <- rbind(x7s4_uzCetriniekiem, x7s4[3:6, ])
@@ -41,44 +44,58 @@ processingSeven_s4 <- function(x7s4) {
                       x7s4_uzVieniniekiem <- rbind(x7s4_uzVieniniekiem, x7s4[7, ])
              } else if (all(sapply(c(1,2,4,5,6), function(i) diff(x7s4$NDZ_sanemsanas_datums[i:(i+1)]) != 0)) &&
                         diff(x7s4$NDZ_sanemsanas_datums[3:4]) == 0) {
-                        if (x7s4$PS_code[1] == '________' && x7s4$NM_code[1] == '________') {
+                        if (x7s4$PS_code[1] == '___________' && x7s4$NM_code[1] == '___________') {
                           x7s4_uzDivniekiem <- rbind(x7s4_uzDivniekiem, x7s4[c(3,4,5,6), ])
                           x7s4_uzVieniniekiem <- rbind(x7s4_uzVieniniekiem, x7s4[c(1,7), ])
                         } else {stop("processingSeven_s4 trūkst izstrādes koda.\n")}
              } else {stop("processingSeven_s4 trūkst izstrādes koda.\n")}
   } else if (all(x7s4$sak_beidz == c("1", "2", "1", "2", "1", "2", "1"))) {
-    result <- processingSeven_s4_1212121(x7s4)
-    if (exists("result")) {
-      x7s4_uzVieniniekiem <- rbind(x7s4_uzVieniniekiem, result$x7s4_uzVieniniekiem)
-      x7s4_uzSesiniekiem  <- rbind(x7s4_uzSesiniekiem, result$x7s4_uzSesiniekiem)
-      rm(result)}    
+          res <- processingSeven_s4_1212121(x7s4)
+          if (exists("res")) {
+            x7s4_uzVieniniekiem <- rbind(x7s4_uzVieniniekiem, res$x7s4_uzVieniniekiem)
+            x7s4_uzSesiniekiem  <- rbind(x7s4_uzSesiniekiem, res$x7s4_uzSesiniekiem)
+            rm(res)}    
   } else if (all(x7s4$sak_beidz == c("1", "1", "2", "1", "2", "2", "1"))) {
                 if (all(sapply(c(2,4), function(i) diff(x7s4$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) &&
                  all(sapply(c(1,3,5,6), function(i) diff(x7s4$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
-                  if (x7s4$PS_code[1] == '________' && x7s4$NM_code[1] == '________') {
+                  if (x7s4$PS_code[1] == '___________' && x7s4$NM_code[1] == '___________') {
                     x7s4_uzDivniekiem <- rbind(x7s4_uzSesiniekiem, x7s4[c(2,3,4,6), ])
                     x7s4_uzVieniniekiem <- rbind(x7s4_uzVieniniekiem, x7s4[7, ])
-                  } else {stop("processingSeven: Septiņnieku apstrādē, gadījumam rindās ", r, " līdz ", r + 6, " trūkst izstrādes koda.\n")}
+                  } else {stop("processingSeven trūkst izstrādes koda.\n")}
               } else if (all(sapply(c(2,4,6), function(i) diff(x7s4$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) &&
                          all(sapply(c(1,3,5), function(i) diff(x7s4$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
-                         if (x7s4$PS_code[1] == '________' && x7s4$NM_code[1] == '________') {
+                         if (x7s4$PS_code[1] == '___________' && x7s4$NM_code[1] == '___________') {
+                          #Te it kā viss kārtībā, bet pagaidām bremzēju.
                           x7s4_uzDivniekiem <- rbind(x7s4_uzSesiniekiem, x7s4[c(1,3,2,5,4,6), ])
                           x7s4_uzVieniniekiem <- rbind(x7s4_uzVieniniekiem, x7s4[7, ])
-                        } else {stop("processingSeven: Septiņnieku apstrādē, gadījumam rindās ", r, " līdz ", r + 6, " trūkst izstrādes koda.\n")}
-              } else {stop("processingSeven: Septiņnieku apstrādē, gadījumam rindās ", r, " līdz ", r + 6, " trūkst izstrādes koda.\n")}
+                        } else {stop("processingSeven trūkst izstrādes koda.\n")}
+              } else {stop("processingSeven trūkst izstrādes koda.\n")}
   } else if (all(x7s4$sak_beidz == c("1", "2", "1", "2", "1", "1", "2"))) {
               if (all(diff(x7s4$NDZ_sanemsanas_datums) != 0)) {
                 x7s4_uzSesiniekiem <- rbind(x7s4_uzSesiniekiem, x7s4[c(1,2,3,4,6,7), ])
-              } else {stop("processingSeven_s4: Septiņnieku apstrādē, gadījumam rindās ", r, " līdz ", r + 6, " trūkst izstrādes koda.\n")}
+              } else {stop("processingSeven_s4 trūkst izstrādes koda.\n")}
+  } else if (all(x7s4$sak_beidz == c("2", "1", "2", "1", "2", "1", "1"))) {
+            if (all(sapply(c(1,2,3,4,6), function(i) diff(x7s4$NDZ_sanemsanas_datums[i:(i+1)]) != 0)) &&
+                diff(x7s4$NDZ_sanemsanas_datums[5:6]) == 0) {
+                #It kā viss OK, bet neesmu vēl pārliecināta, ka šo var vispārināt.
+                if (x7s4$period[1] == "___________" && x7s4$PS_code[1] == '___________' && x7s4$NM_code[1] == '___________') {
+                  x7s4_uzVieniniekiem <- rbind(x7s4_uzVieniniekiem, x7s4[c(1,7),])
+                  x7s4_uzCetriniekiem <- rbind(x7s4_uzCetriniekiem, x7s4[c(2,3,4,5), ])
+                } else {stop("processingSeven_s4 trūkst izstrādes koda.\n")}
+    } else {stop("processingSeven_s4 trūkst izstrādes koda.\n")}
   } else if (all(x7s4$sak_beidz == c("2", "1", "1", "2", "2", "1", "1"))) {
-    if (all(sapply(c(1,3,5), function(i) diff(x7s4$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) &&
-        all(sapply(c(2,4,6), function(i) diff(x7s4$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
-      if (x7s4$period[1] == "________" && x7s4$PS_code[1] == "________" && x7s4$NM_code[1] == "________") {
-        x7s4_uzSesiniekiem <- rbind(x7s4_uzSesiniekiem, x7s4[c(2,1,3,4,6,5), ])
-        x7s4_uzVieniniekiem <- rbind(x7s4_uzVieniniekiem, x7s4[7, ])
-      } else {stop("processingSeven_s4 trūkst izstrādes koda.\n")}
-    } else {stop("processingSeven_s4: Septiņnieku apstrādē, gadījumam rindās ", r, " līdz ", r + 6, " trūkst izstrādes koda.\n")}
-  } else {stop("processingSeven_s4: Septiņnieku apstrādē, gadījumam rindās ", r, " līdz ", r + 6, " trūkst izstrādes koda.\n")}
+              if (all(sapply(c(1,3,5), function(i) diff(x7s4$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) &&
+                  all(sapply(c(2,4,6), function(i) diff(x7s4$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
+                if (x7s4$period[1] == "___________" && x7s4$PS_code[1] == "___________" && x7s4$NM_code[1] == "___________") {
+                  x7s4_uzSesiniekiem <- rbind(x7s4_uzSesiniekiem, x7s4[c(2,1,3,4,6,5), ])
+                  x7s4_uzVieniniekiem <- rbind(x7s4_uzVieniniekiem, x7s4[7, ])
+                } else {stop("processingSeven_s4 trūkst izstrādes koda.\n")}
+              } else {stop("processingSeven_s4 trūkst izstrādes koda.\n")}
+  } else if (x7s4$sak_beidz[1] == x7s4$sak_beidz[2]) {
+          if (diff(x7s4$NDZ_sanemsanas_datums[1:2] != 0)) {
+        x7s4_uzSesiniekiem <- rbind(x7s4_uzSesiniekiem, x7s4[-1, ])
+     } else {stop("processingSeven_s4 trūkst izstrādes koda.\n")}
+  } else {stop("processingSeven_s4 trūkst izstrādes koda.\n")}
   
   return(list(x7_uzVieniniekiem = x7s4_uzVieniniekiem,
               x7_uzDivniekiem   = x7s4_uzDivniekiem,
