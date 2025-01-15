a192f992528a74515fac03f8e92823317743154d
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-01-15 19:49:16 +0200

    Create processingFives_s3e3.R

---
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p5/processingFives_s3e3.R
@@ -0,0 +1,19 @@
+processingFives_s3e3 <- function(a) {
+  a1 <- data.frame(); a2 <- data.frame(); a4 <- data.frame()
+  
+  if (all(a$sak_beidz[c(1, 3, 5)] == "1")) {
+    a1 <- rbind(a1, a[5, ])
+    a2 <- rbind(a2, a[1:4, ])
+  } else if (all(a$sak_beidz[c(1, 3, 4)] == "1")) {
+    if (a$PS_code[1] == '_________' && a$NM_code[1] == '_________') {
+      a2 <- rbind(a2, a[c(1:3,5), ])
+    } else if (a$PS_code[1] == '_________' && a$NM_code[1] == '_________') {
+      a2 <- rbind(a2, a[c(1:3,5), ])
+    } else {stop("processingFives_s3e3 iztrūkst apstrādes koda")}
+  } else {stop("processingFives_s3e3 iztrūkst apstrādes koda")}
+  
+  rm(a)
+  return(list(x5s3_uzVieniniekiem = a1, 
+              x5s3_uzDivniekiem = a2,
+              x5s3_uzCetriniekiem = a4)) 
+}
