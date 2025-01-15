5a09baaf8582e3ee8fff274427a5ad33053ef961
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-01-15 21:24:08 +0200

    Update processingSeven_s4_1212121.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p7/processingSeven_s4_1212121.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p7/processingSeven_s4_1212121.R
@@ -20,10 +20,10 @@ processingSeven_s4_1212121 <- function(x7s4a) {
     x7s4a_uzVieniniekiem <- rbind(x7s4a_uzVieniniekiem, x7s4a[7, ])
   } else if (all(sapply(c(2,4,6), function(i) diff(x7s4a$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) &&
              all(sapply(c(1,3,5), function(i) diff(x7s4a$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
-    if (x7s4a$period[1] == "________" && x7s4a$PS_code[1] == "________" && x7s4a$NM_code[1] == "________") {
+    if (x7s4a$period[1] == "____________" && x7s4a$PS_code[1] == "____________" && x7s4a$NM_code[1] == "____________") {
       x7s4a_uzSesiniekiem <- rbind(x7s4a_uzSesiniekiem, x7s4a[c(3,2,5,4,7,6), ])
-    } else {stop("processingSeven: Septiņnieku apstrādē, gadījumam rindās ", r, " līdz ", r + 6, " trūkst izstrādes koda.\n")}
-  } else {stop("processingSeven: Septiņnieku apstrādē, gadījumam rindās ", r, " līdz ", r + 6, " trūkst izstrādes koda.\n")}
+    } else {stop("processingSeven_s4_1212121 trūkst izstrādes koda.\n")}
+  } else {stop("processingSeven_s4_1212121 trūkst izstrādes koda.\n")}
 
    
   return(list(x7s4_uzVieniniekiem = x7s4a_uzVieniniekiem,
