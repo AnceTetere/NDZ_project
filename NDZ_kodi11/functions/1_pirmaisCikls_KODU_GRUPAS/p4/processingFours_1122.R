c9c64e96b546c971a7ba36e51c4989050a4b9dc0
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-01-15 17:28:31 +0200

    Update processingFours_1122.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p4/processingFours_1122.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p4/processingFours_1122.R
@@ -3,22 +3,15 @@ processingFours_1122 <- function(a) {
   
   if (diff(a$NDZ_sanemsanas_datums[2:3]) == 0 && 
       all(sapply(c(1,3), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) != 0)) && 
-      a$PS_code[1] == '__________' && a$NM_code[1] == '________') {
+      a$PS_code[1] == '__________' && a$NM_code[1] == '___________') {
       a4_2 <- rbind(a4_2, a[c(1,3,2,4), ])
+  } else if (all(diff(a$NDZ_sanemsanas_datums) != 0)) {
+          if (a$period[1] == "__________" && a$PS_code[1] %in% c('__________', '____________') && a$NM_code[1] == '__________') {
+            a4_2 <- rbind(a4_2, a[c(2,4), ])
+          } else {stop("processingFours_1122 trūkst apstrādes koda.")}
   } else {stop("processingFours_1122 trūkst apstrādes koda.")}
 
   return(list(x4_uzVieniniekiem = a4_1,
               x4_trueDoubles = a4_2,
               x4_uzTrijniekiem= a4_3))
 }
-
-#else if (all(diff(a$NDZ_sanemsanas_datums) > 0)) {
-#  a4_2 <- rbind(a4_2, a[c(2,4), ])
-#} else if (diff(a$NDZ_sanemsanas_datums[3:4]) > 0) {
-#  a4_1 <- rbind(a4_1, a[4, ]) 
-#  a4_3 <- rbind(a4_3, a[1:3, ])
-#} else if (diff(a$NDZ_sanemsanas_datums[2:3]) >= 0) {
-#  a4_3 <- rbind(a4_3, a[2:4, ])
-#} else if (all(diff(a$NDZ_sanemsanas_datums[1:3]) != 0) && diff(a$NDZ_sanemsanas_datums[3:4]) == 0) {
-#  a4_2 <- rbind(a4_2, a[3:4, ])
-#}
