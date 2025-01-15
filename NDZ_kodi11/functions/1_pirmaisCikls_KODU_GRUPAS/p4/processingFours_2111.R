3404aa239615f89c8759fd5b452bf0dd2942922e
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-01-15 17:30:27 +0200

    Update processingFours_2111.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p4/processingFours_2111.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p4/processingFours_2111.R
@@ -1,12 +1,12 @@
 processingFours_2111 <- function(a) {
   a4_1 <- data.frame(); a4_2 <- data.frame(); a4_3 <- data.frame()
-  
+
 if (diff(a$NDZ_sanemsanas_datums[1:2]) > 0 && diff(a$NDZ_sanemsanas_datums[2:3]) > 0) {
-  a4_1 <- rbind(a4_1, a[c(1, 4), ])
+  a4_2 <- rbind(a4_2, a[c(1, 4), ])
 } else if (diff(a$NDZ_sanemsanas_datums[1:2]) == 0 && diff(a$NDZ_sanemsanas_datums[2:3]) > 0) {
   a4_1 <- rbind(a4_1, a[4, ])
   a4_2 <- rbind(a4_2, a[c(2,1), ])
-} else {stop("Šeit četrinieku izstrādes tabulas rindām ", r, " līdz ", r+3, " trūkst apstrādes koda.")}
+} else {stop("processingFours_2111 trūkst apstrādes koda.")}
 
   return(list(x4_uzVieniniekiem = a4_1,
               x4_trueDoubles = a4_2,
