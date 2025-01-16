51d02e8a58a053e9baa1639adaf29bd245efe614
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-01-16 21:07:33 +0200

    Update and rename NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/processingSeventeen.R to NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p17/processingSeventeen.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p17/processingSeventeen.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p17/processingSeventeen.R
@@ -1,4 +1,4 @@
-processingSeventeen <- function(x, o) {
+processingSeventeen <- function(x, o, kods) {
 
   x <- x %>% arrange(PS_code, DN_code, NM_code, NDZ_sanemsanas_datums)
   x17_uzVieniniekiem <- data.frame(); x17_uzDivniekiem <- data.frame(); x17_uzTrīspadsmit <- data.frame(); x17_uz15 <- data.frame(); x17_uzSespadsmit <- data.frame() 
@@ -6,7 +6,7 @@ processingSeventeen <- function(x, o) {
   for (r in seq(1, nrow(x), by = 17)) {
     x17 <- x[r:(r+16), ] %>% arrange(PS_code, DN_code, NM_code, NDZ_sanemsanas_datums)
     
-if(sum(x17$sak_beidz == "2") == 9){
+if (sum(x17$sak_beidz == "2") == 9) {
       if (all(x17$sak_beidz[1:2] == c("2", "1"))) {
         if (diff(x17$NDZ_sanemsanas_datums[1:2]) != 0) {
           x17_uzVieniniekiem <- rbind(x17_uzVieniniekiem, x17[1, ])
@@ -22,8 +22,8 @@ if(sum(x17$sak_beidz == "2") == 9){
           x17_uzDivniekiem <- rbind(x17_uzDivniekiem, x17[1:2, ])
           x17_uz15 <- rbind(x17_uz15, x17[-(1:2), ])
         } else {stop("17-nieku tabulās trūkst izstrādes koda.")}
-      } else if (x17$sak_beidz[17] == "1") {
-        if(diff(x17$NDZ_sanemsanas_datums[16:17] != 0)) {
+      } else if (x17$sak_beidz[17] == "1" && x17$sak_beidz[16] != "1") {
+        if (diff(x17$NDZ_sanemsanas_datums[16:17]) != 0) {
           x17_uzVieniniekiem <- rbind(x17_uzVieniniekiem, x17[17, ])
           x17_uzSespadsmit <- rbind(x17_uzSespadsmit, x17[-17, ])
         } else {stop("17-nieku tabulās trūkst izstrādes koda.")}
@@ -45,26 +45,26 @@ if(sum(x17$sak_beidz == "2") == 9){
   
   #2 Apakštabulu x17_uzDivniekiem sūta caur processingTwoes().
   if (nrow(x17_uzDivniekiem) > 0) {
-    x17_uzDivniekiem %>% arrange(PS_code, NM_code, NDZ_sanemsanas_datums) %>% processingTwoes(o)
+    x17_uzDivniekiem %>% arrange(PS_code, NM_code, NDZ_sanemsanas_datums) %>% processingTwoes(o, kods)
   } else {cat("Tabula x17_uzDivniekiem ir tukša.\n")}
   rm(x17_uzDivniekiem)
   
   #3 Apakštabulu x17_uzTrīspadsmit sūta caur processingThirteen().
   if (nrow(x17_uzTrīspadsmit) > 0) {
-   x17_uzTrīspadsmit %>% arrange(PS_code, NM_code, NDZ_sanemsanas_datums) %>% processingThirteen(o)
+   x17_uzTrīspadsmit %>% arrange(PS_code, NM_code, NDZ_sanemsanas_datums) %>% processingThirteen(o, kods)
   } else {cat("Tabula x17_uzTrīspadsmit ir tukša.\n")}
   rm(x17_uzTrīspadsmit)
   
   #4 Apakštabulu x17_uz15 sūta caur processingFifteen().
   if (nrow(x17_uz15) > 0) {
-    x17_uz15 %>% arrange(PS_code, NM_code, NDZ_sanemsanas_datums) %>% processingFifteen(o)
+    x17_uz15 %>% arrange(PS_code, NM_code, NDZ_sanemsanas_datums) %>% processingFifteen(o, kods)
   } else {cat("Tabula x17_uzTrīspadsmit ir tukša.\n")}
   rm(x17_uz15)
   
   #5 Apakštabulu x17_uzSespadsmit sūta caur processingSixteen().
   if (nrow(x17_uzSespadsmit) > 0) {
     x17_uzSespadsmit %>% arrange(PS_code, NM_code, NDZ_sanemsanas_datums) %>% 
-      processingSixteen(o)
+      processingSixteen(o, kods)
   } else {cat("Tabula x17_uzSespadsmit ir tukša.\n")}
   rm(x17_uzSespadsmit)
 }
