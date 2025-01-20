e153b7a59e0bfcc9788396a66c1e60d5d02a4b61
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-01-20 15:39:27 +0200

    Create starpkodi4_50_51_25.R

---
+++ NDZ_kodi11/functions/2_otraisCikls_STARPKODI/starpkodi4/starpkodi4_50_51_25.R
@@ -0,0 +1,31 @@
+starpkodi4_50_51_25 <- function(y, t, prev, v) {
+  
+  if (t$zinkod[4] %in% c("41", "51", "54", "92")) {
+            if (all(sapply(c(1,3), function(i) all(diff(t$NDZ_sanemsanas_datums[i:(i+1)]) == 0))) && 
+                diff(t$NDZ_sanemsanas_datums[2:3]) != 0) {
+              yt <- y[v, ]
+              yt$dienas <- 0
+            } else if (all(diff(t$NDZ_sanemsanas_datums[1:3]) != 0)) {
+              yt <- y[v, ]
+              yt$dienas <- as.numeric(sum(difftime(t$NDZ_sanemsanas_datums[1], prev, units = "days"), 
+                                          diff(t$NDZ_sanemsanas_datums[2:3])))
+            } else {stop("starpkodi4_50_51_25: Trūkst izstrādes koda.")}
+  } else if (t$zinkod[4] %in% c("40", "50", "53", "91")) {
+    if (all(diff(t$NDZ_sanemsanas_datums[1:3]) != 0) && diff(t$NDZ_sanemsanas_datums[3:4]) == 0) {
+      yt <- y[v, ]
+      yt$dienas <- as.numeric(sum(difftime(t$NDZ_sanemsanas_datums[1], prev, units = "days"), 
+                                  diff(t$NDZ_sanemsanas_datums[2:3])))
+    } else {stop("starpkodi4_50_51_25: Trūkst izstrādes koda.")}
+  #} else if (t$zinkod[4] == "11") {
+           #   if (all(diff(t$NDZ_sanemsanas_datums) != 0)) {
+           #     yt <- y[v, ]
+           #     yt$dienas <- sum(as.numeric(difftime(t$NDZ_sanemsanas_datums[1], prev, units = "days")) - 1, 
+           #                      as.numeric(diff(t$NDZ_sanemsanas_datums[2:3])) + 1, 
+           #                      as.numeric(difftime(t$last_date[4], t$NDZ_sanemsanas_datums[4], units = "days")) + 1)
+           #   } else {stop("starpkodi4_50_51_25: Trūkst izstrādes koda.")}
+  } else {stop("starpkodi4_50_51_25: Trūkst izstrādes koda.")}
+    
+    
+  rm(y, t, prev, v)
+  return(yt) 
+}
