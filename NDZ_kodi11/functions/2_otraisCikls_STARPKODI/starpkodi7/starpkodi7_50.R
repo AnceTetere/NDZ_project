724b67eac133615474b3c3c7626be91cd0407521
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-01-22 20:36:44 +0200

    Create starpkodi7_50.R

---
+++ NDZ_kodi11/functions/2_otraisCikls_STARPKODI/starpkodi7/starpkodi7_50.R
@@ -0,0 +1,13 @@
+starpkodi7_50 <- function(y, t, prev, v) {
+  
+  if (t$zinkod[2] %in% c("40", "50", "53", "91")) {
+    stop("Starpkodi7_51 iztrūkst apstrādes koda.")
+  } else if (t$zinkod[2] %in% c("11", "14", "16", "61")) {
+    stop("Starpkodi7_51 iztrūkst apstrādes koda.")
+  } else if (t$zinkod[2] %in% c("41", "51", "54", "92")) {
+    yt <- starpkodi7_50_51(y, t, prev, v)
+  } else {stop("Starpkodi7_51 iztrūkst apstrādes koda.")}
+  
+  rm(y, t, prev, v)
+  return(yt)
+}
