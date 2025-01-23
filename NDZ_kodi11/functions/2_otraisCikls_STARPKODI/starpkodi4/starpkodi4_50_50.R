c4b017500bda2068a553247a9dedbaedc34ae2cb
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-01-23 15:26:51 +0200

    Update starpkodi4_50_50.R

--- NDZ_kodi11/functions/2_otraisCikls_STARPKODI/starpkodi4/starpkodi4_50_50.R
+++ NDZ_kodi11/functions/2_otraisCikls_STARPKODI/starpkodi4/starpkodi4_50_50.R
@@ -10,6 +10,13 @@ starpkodi4_50_50 <- function(y, t, prev, v) {
                            as.numeric(diff(t$NDZ_sanemsanas_datums[3:4])) + 1) 
         } else {stop("starpkodi4_50_50: Trūkst izstrādes koda.")}
       } else {stop("starpkodi4_50_50: Trūkst izstrādes koda.")}
+    } else if (t$zinkod[4] %in% c("41", "51", "54", "92")) {
+      if (all(diff(t$NDZ_sanemsanas_datums) != 0)) {
+        if (t$period[1] == "__________" && t$PS_code[1] == '__________' && t$NM_code[1] == '__________') {
+          yt$dienas <- sum(as.numeric(difftime(t$NDZ_sanemsanas_datums[1], prev, units = "days")) - 1, 
+                           as.numeric(difftime(t$last_date[4], t$NDZ_sanemsanas_datums[4], units = "days")) + 1) 
+        } else {stop("starpkodi4_50_50: Trūkst izstrādes koda.")}
+      } else {stop("starpkodi4_50_50: Trūkst izstrādes koda.")}
     } else if (t$zinkod[4] %in% c("40", "50", "53", "91")) {
       if (diff(t$NDZ_sanemsanas_datums[1:2]) == 0 && all(diff(t$NDZ_sanemsanas_datums[2:4]) != 0)) {
         if(t$PS_code[1] == '__________' && t$NM_code[1] == '__________') {
@@ -43,7 +50,7 @@ starpkodi4_50_50 <- function(y, t, prev, v) {
   } else if (t$zinkod[3] %in% c("40", "50", "53", "91")) {
     if (t$zinkod[4] %in% c("41", "51", "54", "92")) {
       if(all(sapply(c(1,3), function(i) diff(t$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) && diff(t$NDZ_sanemsanas_datums[2:3]) != 0) {
-        if (t$period[1] == "__________" && t$PS_code[1] == '__________' && t$NM_code[1] == '__________') {
+        if (t$period[1] == "__________" && t$PS_code[1] == '__________' && t$NM_code[1] == '__________) {
           yt$dienas <- as.numeric(sum(difftime(t$NDZ_sanemsanas_datums[1], prev, units = "days") - 1,
                                       diff(t$NDZ_sanemsanas_datums[3:4]) + 1))
         } else {stop("starpkodi4_50_50: Trūkst izstrādes koda.")}
