b5e3eb296e031b7a103a5624d418164ed2841c81
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-01-23 15:48:13 +0200

    Update and rename NDZ_kodi11/functions/2_otraisCikls_STARPKODI/starpkodi3/starpkodi3.R to NDZ_kodi11/functions/2_otraisCikls_STARPKODI/dubultkodi/starpkodi3/starpkodi3.R

---
+++ NDZ_kodi11/functions/2_otraisCikls_STARPKODI/dubultkodi/starpkodi3/starpkodi3.R
@@ -0,0 +1,16 @@
+starpkodi3 <- function(y, t, prev, v) {
+  
+  if (t$zinkod[1] %in% c("11", "14", "16", "61")) {
+    yt <- starpkodi3_11(y, t, prev, v)
+  } else if (t$zinkod[1] %in% c("41", "51", "54", "92")) {
+    yt <- starpkodi3_51(y, t, prev, v)
+  } else if (t$zinkod[1] %in% c("40", "50", "53", "91")) {
+     yt <- starpkodi3_50(y, t, prev, v)
+  } else if (t$zinkod[1] %in% c("21", "22", "23", "24", "25", "29")) {
+    yt <- starpkodi3_25(y, t, prev, v)
+  } else {stop("Starpkodi3: Trūkst izstrādes koda.")}
+  
+  if(is.na(yt$pseidokods[1])) {stop("Dienas NA.")}
+  yt$zinkod <- "combined"  #jo starpkodu dienu sarēķins
+  
+  return(yt)
