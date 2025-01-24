4d6f0c0302fd427aaea1e1a7412dcdc760be73dd
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-01-24 13:55:19 +0200

    Create processingFives_s2e4.R

---
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p5/processingFives_s2e4.R
@@ -0,0 +1,19 @@
+processingFives_s2e4 <- function(a, kods) {  
+  #a <- a for testing
+  a <- a %>% arrange(PS_code, DN_code, NM_code, NDZ_sanemsanas_datums)
+  a1 <- data.frame(); a2 <- data.frame(); a4 <- data.frame()
+  
+  if (all(a$sak_beidz[c(1,4)] == "1")) {
+    a4 <- rbind(a4, a[c(1,3:5), ])
+    if (kods %in% c("40", "50", "53")) {ZERO_plus(a %>% slice(5))}
+  } else if (all(a$sak_beidz[c(2,4)] == "1")) {
+    a1 <- rbind(a1, a[1, ])
+    a4 <- rbind(a4, a[2:5, ])
+    if (kods %in% c("40", "50", "53")) {ZERO_plus(a %>% slice(5))}
+  } else {stop("processingFives_s2: Trūkst apstrādes koda.")}
+  
+  rm(a, kods)
+  return(list(x5s2_uzVieniniekiem = a1,
+              x5s2_uzDivniekiem = a2,
+              x5s2_uzCetriniekiem = a4))
+}
