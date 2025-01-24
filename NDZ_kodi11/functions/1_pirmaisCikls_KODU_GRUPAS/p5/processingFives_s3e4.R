62224b2f882115229fa5bc6948095bd13553ed4d
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-01-24 14:45:33 +0200

    Update processingFives_s3e4.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p5/processingFives_s3e4.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p5/processingFives_s3e4.R
@@ -1,16 +1,24 @@
-processingFives_s3e4 <- function(a) {
+#GIT 20250124
+processingFives_s3e4 <- function(a, kods) {
   a1 <- data.frame(); a2 <- data.frame(); a4 <- data.frame()
-  
+  #a <- x5s3 for testing
   if (all(a$sak_beidz[c(1,3,5)] == "1")) {
-    if((a$PS_code[1] == "____________" && a$NM_code[1] == "____________") ||
-       (a$PS_code[1] == "____________" && a$NM_code[1] == "____________")) {
+            if((a$PS_code[1] == "_________" && a$NM_code[1] == "_________") ||
+               (a$PS_code[1] == "_________" && a$NM_code[1] == "_________")) {
+              a1 <- rbind(a1, a[5, ])
+              a4 <- rbind(a4, a[-5, ])
+            } else {stop("processingFives_s3 iztrūkst apstrādes koda")}
+    if (kods %in% c("40", "50", "53")) {ZERO_minus(a %>% slice(1))}
+  } else if (all(a$sak_beidz[3:5] == "1")) {
+           if(a$PS_code[1] == "_________" && a$NM_code[1] == "_________") {
+             a1 <- rbind(a1, a[1, ])
+           } else {stop("processingFives_s3e4 iztrūkst apstrādes koda")}
+  } else if (all(a$sak_beidz[c(1,2,5)] == "1")) {
+    if (a$period[1] == "_________" && a$PS_code[1] %in% c("_________", "_________") && a$NM_code[1] == "_________") {
       a1 <- rbind(a1, a[5, ])
-      a4 <- rbind(a4, a[-5, ])
-    } else {stop("processingFives_s3 iztrūkst apstrādes koda")}
-  } else if (all(a$sak_beidz == c("2", "2", "1", "1", "1"))) {
-    if(a$PS_code[1] == "____________" && a$NM_code[1] == "____________") {
-      a1 <- rbind(a1, a[1, ])
+      a4 <- rbind(a4, a[c(1,3,2,4),])
     } else {stop("processingFives_s3e4 iztrūkst apstrādes koda")}
+    if (kods %in% c("40", "50", "53")) {ZERO_minus(a %>% slice(1))}
   } else {stop("processingFives_s3e4 iztrūkst apstrādes koda")} 
   
   rm(a)
