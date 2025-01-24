5036b7daac4c9e7fe6d9127fcf9ddb4ea2d04194
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-01-24 13:14:44 +0200

    Update processingFives_s3e2.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p5/processingFives_s3e2.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p5/processingFives_s3e2.R
@@ -1,11 +1,11 @@
-processingFives_s3e2 <- function(a) {
+processingFives_s3e2 <- function(a, kods) {
   a1 <- data.frame(); a2 <- data.frame(); a4 <- data.frame()
-  
-  if(all(a$sak_beidz[c(2,4,5)] == "1")) {
-    a1 <- rbind(a1, a[c(1, 5), ])
+  #a <- x5s3 testiem
+  if (all(a$sak_beidz[c(2,4,5)] == "1")) {
+    a1 <- rbind(a1, a[c(1,5), ])
     a2 <- rbind(a2, a[2:3, ])
   } else if (all(a$sak_beidz[c(2,3,5)] == "1")) {
-    a1 <- rbind(a1, a[c(1, 5), ])
+    a1 <- rbind(a1, a[c(1,5), ])
     a2 <- rbind(a2, a[3:4, ])
   } else if (all(a$sak_beidz[2:4] == "1")) {
     a1 <- rbind(a1, a[1, ])
@@ -13,18 +13,24 @@ processingFives_s3e2 <- function(a) {
   } else if (all(a$sak_beidz[c(1,3,5)] == "1")) {
     a1 <- rbind(a1, a[5, ])
     a2 <- rbind(a2, a[1:4, ])
+    if (kods %in% c("40", "50", "53")) {ZERO_minus(a %>% slice(1))}
   } else if (all(a$sak_beidz[c(1,3,4)] == "1")) {
     a4 <- rbind(a4, a[c(1,2,4,5),])
+    if (kods %in% c("40", "50", "53")) {ZERO_minus(a %>% slice(1)); ZERO_plus(slice(5))}
   } else if (all(a$sak_beidz[c(1,2,4)] == "1")) {
-    if (a$PS_code[1] == '________' && a$NM_code[1] == '________') {
+    if (a$PS_code[1] == '__________' && a$NM_code[1] == '__________') {
       a[1, 'sak_beidz'] <- "2"
       a[1, 'zinkod'] <- "50"
       a1 <- rbind(a1, a[1,])
       a4 <- rbind(a4, a[2:5,])
+      if (kods %in% c("40", "50", "53")) {ZERO_minus(a %>% slice(1)); ZERO_plus(slice(5))}
+    } else if (a$period[1] == "__________" && a$PS_code[1] == '__________' && a$NM_code[1] == '__________') {
+      a4 <- rbind(a4, a[2:5,])
+      #Ja atbrīvo, tad pievieno šo. if (kods %in% c("40", "50", "53")) {ZERO_minus(a %>% slice(1)); ZERO_plus(slice(5))}
     } else {stop("processingFives_s3e2 iztrūkst apstrādes koda")}
   } else {stop("processingFives_s3e2 iztrūkst apstrādes koda")} 
 
-    rm(a)
+    rm(a, kods)
     return(list(x5s3_uzVieniniekiem = a1, 
                 x5s3_uzDivniekiem = a2,
                 x5s3_uzCetriniekiem = a4))
