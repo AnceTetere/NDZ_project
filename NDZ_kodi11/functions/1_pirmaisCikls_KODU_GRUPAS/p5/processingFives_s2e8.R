053656243f1228bf6cecaf5feb64fae20036d122
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-01-24 14:18:58 +0200

    Create processingFives_s2e8.R

---
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p5/processingFives_s2e8.R
@@ -0,0 +1,24 @@
+processingFives_s2e8 <- function(a, kods) {  
+  #a <- a for testing
+  a <- a %>% arrange(PS_code, DN_code, NM_code, NDZ_sanemsanas_datums)
+  a1 <- data.frame(); a2 <- data.frame(); a4 <- data.frame()
+  
+} else if (diff(a$NDZ_sanemsanas_datums[4:5]) == 0 &&
+           all(diff(a$NDZ_sanemsanas_datums[1:4]) != 0)) {
+  if (all(a$sak_beidz[c(2,5)] == "1")) {
+    if ((a$PS_code[1] == '________' && a$NM_code[1] == '________')||
+        (a$PS_code[1] == '________' && a$NM_code[1] == '________')) {
+      a1 <- rbind(a1, a[1,])  
+      a2 <- rbind(a2, a[c(2,3,5,4),])
+    } else {stop("processingFives_s2e8.R: Trūkst apstrādes koda.")}
+  } else if (all(a$sak_beidz[c(2,4)] == "1")) {
+    a1 <- rbind(a1, a[1,])  
+    a2 <- rbind(a2, a[2:5,])
+    if (kods %in% c("40", "50", "53")) {ZERO_plus(a %>% slice(5))}
+  } else {stop("processingFives_s2e8.R: Trūkst apstrādes koda.")}
+  
+  rm(a, kods)
+  return(list(x5s2_uzVieniniekiem = a1,
+              x5s2_uzDivniekiem = a2,
+              x5s2_uzCetriniekiem = a4))
+}
