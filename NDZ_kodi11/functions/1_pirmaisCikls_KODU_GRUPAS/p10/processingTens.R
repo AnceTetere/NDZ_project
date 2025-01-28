ff5b74083acbfaa2edbb4af8c010778d24b6f5e4
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-01-28 12:46:59 +0200

    Update processingTens.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p10/processingTens.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p10/processingTens.R
@@ -2,12 +2,13 @@ processingTens <- function(x, o, kods) {
   cat("-------------SĀK 10-nieku APSTRĀDI.")
   x <- x %>% arrange(PS_code, DN_code, NM_code, NDZ_sanemsanas_datums)
 
-  x10_uzVieniniekiem <- data.frame(); x10_uzDivniekiem <- data.frame(); x10_uzSeptini <- data.frame(); x10_uzAstoniekiem <- data.frame()
+  x10_uzVieniniekiem <- data.frame(); x10_uzDivniekiem <- data.frame(); x10_uzPieci <- data.frame(); x10_uzSeptini <- data.frame(); x10_uzAstoniekiem <- data.frame()
   check_rows <- 0
 
   result <- function(y) {
     x10_uzVieniniekiem <<- rbind(x10_uzVieniniekiem, y$x10_uzVieniniekiem)
     x10_uzDivniekiem <<- rbind(x10_uzDivniekiem, y$x10_uzDivniekiem)
+    x10_uzPieci <<- rbind(x10_uzPieci, y$x10_uzPieci)
     x10_uzSeptini <<- rbind(x10_uzSeptini, y$x10_uzSeptini)
     x10_uzAstoniekiem <<- rbind(x10_uzAstoniekiem, y$x10_uzAstoniekiem)
     rm(y)
@@ -15,9 +16,9 @@ processingTens <- function(x, o, kods) {
   
   for (r in seq(1, nrow(x), by = 10)) {
     x10 <- x[r:(r+9),] %>% arrange(PS_code, DN_code, NM_code, NDZ_sanemsanas_datums)
-    
-    if(sum(x10$sak_beidz == "1") == 5) {
-      result(processingTens_s5(x10))
+  
+    if (sum(x10$sak_beidz == "1") == 5) {
+            result(processingTens_s5(x10, o, kods))
     } else if (sum(x10$sak_beidz == "1") == 4) {
               if (all(sapply(seq(1, 10, by = 2), function(i) diff(x10$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) &&
                   all(sapply(seq(2, 8, by = 2), function(i) diff(x10$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
@@ -63,13 +64,19 @@ if(nrow(x10_uzDivniekiem) > 0) {
 } else {cat("Tabula x10_uzDivniekiem ir tukša.\n")}
 rm(x10_uzDivniekiem)
   
-#3 Apakštabulu x10_uzSeptini sūta caur processingSeven().
+#3 Apakštabulu x10_uzPieci sūta caur processingFives().
+if(nrow(x10_uzPieci) > 0) {
+  x10_uzPieci %>% arrange(PS_code, NM_code, NDZ_sanemsanas_datums) %>% processingFives(o, kods)
+} else {cat("Tabula x10_uzPieci ir tukša.\n")}
+rm(x10_uzPieci)
+
+#4 Apakštabulu x10_uzSeptini sūta caur processingSeven().
 if(nrow(x10_uzSeptini) > 0) {
   x10_uzSeptini %>% arrange(PS_code, NM_code, NDZ_sanemsanas_datums) %>% processingSeven(o, kods)
 } else {cat("Tabula x10_uzSeptini ir tukša.\n")}
 rm(x10_uzSeptini)
   
-#4 Apakštabulu x10_uzAstoniekiem sūta caur processingEights().
+#5 Apakštabulu x10_uzAstoniekiem sūta caur processingEights().
 if(nrow(x10_uzAstoniekiem) > 0) {
   x10_uzAstoniekiem %>% arrange(PS_code, NM_code, NDZ_sanemsanas_datums) %>% processingEights(o, kods)
 } else {cat("Tabula x10_uzAstoniekiem ir tukša.\n")}
