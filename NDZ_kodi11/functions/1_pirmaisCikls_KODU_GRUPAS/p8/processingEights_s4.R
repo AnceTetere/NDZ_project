2a2fb56b885823e58a447f97d7e31739abc778d6
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-01-28 17:28:54 +0200

    Update processingEights_s4.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p8/processingEights_s4.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p8/processingEights_s4.R
@@ -1,10 +1,10 @@
-processingEights_s4 <- function(x8s4) {
-
+processingEights_s4 <- function(x8s4, o, kods) {
   x8s4 <- x8s4 %>% arrange(PS_code, DN_code, NM_code, NDZ_sanemsanas_datums)
-  x8s4_uzVieniniekiem <- data.frame(); x8s4_uzDivniekiem <- data.frame(); x8s4_uzCetriniekiem <- data.frame()
-  x8s4_uzSesi <- data.frame(); x8s4_uzSeptini <- data.frame()
+#x8s4 <- x8
+x8s4_uzVieniniekiem <- data.frame(); x8s4_uzDivniekiem <- data.frame(); x8s4_uzCetriniekiem <- data.frame()
+x8s4_uzSesi <- data.frame(); x8s4_uzSeptini <- data.frame()
 
-if (x8s4$PS_code[1] == '_________' && x8s4$NM_code[1] == '_________') {
+if (x8s4$PS_code[1] == '___________' && x8s4$NM_code[1] == '___________') {
   if (all(x8s4$sak_beidz == c("2","1","2","2","1","2","1","1"))) {
     x8s4_uzVieniniekiem <- rbind(x8s4_uzVieniniekiem, x8s4[c(1,8), ])
     x8s4_uzCetriniekiem <- rbind(x8s4_uzCetriniekiem, x8s4[c(2,4,5,6),])
@@ -12,7 +12,7 @@ if (x8s4$PS_code[1] == '_________' && x8s4$NM_code[1] == '_________') {
 } else if (all(sapply(seq(1,8,by=2), function(i) diff(x8s4$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) &&
    all(sapply(seq(2,7,by=2), function(i) diff(x8s4$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
   #cS (check sak_beidz) norāda kombinācijas, kurās rindās izejas tabulas ailē sak_beidz atrodas "1".
-  cS <- list(c(2, 4, 5, 8), c(2, 4, 6, 8), c(1, 4, 6, 7), c(2, 3, 6, 8), c(2, 3, 5, 7), c(2, 3, 5, 8), c(1, 4, 5, 7), c(1, 3, 6, 8), c(1, 3, 5, 7), c(2, 3, 6, 7))
+  cS <- list(c(1, 4, 6, 8), c(2, 4, 5, 8), c(2, 4, 6, 8), c(1, 4, 6, 7), c(2, 3, 6, 8), c(2, 3, 5, 7), c(2, 3, 5, 8), c(1, 4, 5, 7), c(1, 3, 6, 8), c(1, 3, 5, 7), c(2, 3, 6, 7))
   found_match <- FALSE
   
   #Salīdzina vai šajā apakštabulā kāda no cS kombinācijām atrodama.
@@ -26,6 +26,10 @@ if (x8s4$PS_code[1] == '_________' && x8s4$NM_code[1] == '_________') {
     if(all(x8s4$sak_beidz[c(3,4,7,8)] == "1") && x8s4$zinkod[1] %in% c("40", "41", "90", "91")) {
       x8s4_uzVieniniekiem <- rbind(x8s4_uzVieniniekiem, x8s4[c(1, 8), ])
       x8s4_uzDivniekiem <- rbind(x8s4_uzDivniekiem, x8s4[4:5, ])
+    } else if(all(x8s4$sak_beidz[c(1,4,5,8)] == "1") && x8s4$zinkod[1] %in% c("11")) {
+      if (x8s4$period[1] == "___________" && x8s4$PS_code[1] == "___________" && x8s4$NM_code[1] == "___________") {
+        x8s4_uzDivniekiem <- rbind(x8s4_uzDivniekiem, x8s4[c(1,2,4,3,5,6,8,7), ])
+      } else {stop("processingEights_s4: Trūkst izstrādes koda.")}
     } else {stop("processingEights_s4: Trūkst izstrādes koda.")}
     rm(cS, s, found_match)
   }
@@ -48,9 +52,7 @@ if (x8s4$PS_code[1] == '_________' && x8s4$NM_code[1] == '_________') {
   x8s4_uzCetriniekiem  <- rbind(x8s4_uzCetriniekiem, x8s4)
   #stop("processingEights_s4: Trūkst izstrādes koda.")
 }
-
 rm(x8s4)
-
 return(list(x8_uzVieniniekiem = x8s4_uzVieniniekiem, 
             x8_uzDivniekiem = x8s4_uzDivniekiem, 
             x8_uzCetriniekiem = x8s4_uzCetriniekiem, 
