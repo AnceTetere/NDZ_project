4aa014eef2343adf6ea85dd99f7ca1d5cccff955
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-01-29 16:35:34 +0200

    Update processingTens_s5.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p10/processingTens_s5.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p10/processingTens_s5.R
@@ -3,39 +3,28 @@ processingTens_s5 <- function(x10s5, o, kods) {
   x10s5_1 <- data.frame(); x10s5_2 <- data.frame(); x10s5_5 <- data.frame(); x10s5_7 <- data.frame(); x10s5_8 <- data.frame()
   #x10s5 <- x10
   
+  result <- function(y) {
+    x10s5_1 <<- rbind(x10s5_1, y$x10s5_1)
+    x10s5_2 <<- rbind(x10s5_2, y$x10s5_2)
+    x10s5_5 <<- rbind(x10s5_5, y$x10s5_5)
+    x10s5_7 <<- rbind(x10s5_7, y$x10s5_7)
+    x10s5_8 <<- rbind(x10s5_7, y$x10s5_7)
+    rm(y)
+  }  
   if (all(x10s5$sak_beidz[1:2] == c("1","2"))) {
-           if (x10s5$sak_beidz[3] != "2") {
-              if (all(diff(x10s5$NDZ_sanemsanas_datums[1:3]) != 0)) {
-                      x10s5_2 <- rbind(x10s5_2, x10s5[1:2, ])
-                      x10s5_8 <- rbind(x10s5_8, x10s5[3:10, ])
-                      if (kods %in% c("40", "50", "53") && o == "10") {ZERO_minus(x10s5 %>% slice(1))}
-              } else if (diff(x10s5$NDZ_sanemsanas_datums[1:2]) == 0 && diff(x10s5$NDZ_sanemsanas_datums[2:3]) != 0) {
-                     if ((x10s5$period[1] == "______" && x10s5$PS_code[1] == "_______" && x10s5$NM_code[1] == "______") ||
-                         (x10s5$period[1] == "______" && x10s5$PS_code[1] == "______" && x10s5$NM_code[1] == "______") ||
-                         (x10s5$period[1] == "______" && x10s5$PS_code[1] == "______" && x10s5$NM_code[1] == "______") ||
-                         (x10s5$period[1] == "______" && x10s5$PS_code[1] == "______" && x10s5$NM_code[1] == "______")) {
-                         x10s5_2 <- rbind(x10s5_2, x10s5[1:2, ])
-                         x10s5_8 <- rbind(x10s5_8, x10s5[3:10, ])
-                     } else {stop("processingTens_s5: Desmitnieku tabulas pārdalei trūkst izstrādes koda\n")}
-                     if (kods %in% c("40", "50", "53") && o == "10") {ZERO_minus(x10s5 %>% slice(1))}
-                   } else {stop("processingTens_s5: Desmitnieku tabulas pārdalei trūkst izstrādes koda\n")}
-               } else if (x10s5$sak_beidz[3] == "2") {
-                   if (all(sapply(c(1,2,4), function(i) diff(x10s5$NDZ_sanemsanas_datums[i:(i+1)]) != 0)) && diff(x10s5$NDZ_sanemsanas_datums[3:4]) == 0) {
-                       if (x10s5$period[1] == "______" && x10s5$PS_code[1] == "______" && x10s5$NM_code[1] == "______") {
-                       x10s5_2 <- rbind(x10s5_2, x10s5[1:2, ])
-                       x10s5_8 <- rbind(x10s5_8, x10s5[c(4,3,5:10), ])
-                     } else {stop("processingTens_s5: Desmitnieku tabulas pārdalei trūkst izstrādes koda\n")}
-                     if (kods %in% c("40", "50", "53") && o == "10") {ZERO_minus(x10s5 %>% slice(1))}
-                   } else {stop("processingTens_s5: Desmitnieku tabulas pārdalei trūkst izstrādes koda\n")}
-              } else {stop("processingTens_s5: Desmitnieku tabulas pārdalei trūkst izstrādes koda\n")}
+          processingTens_s5e1(a, o, kods)
   } else if (all(x10s5$sak_beidz[1:2] == c("2","1"))) {
             if (x10s5$sak_beidz[3] != "2") {
               if (diff(x10s5$NDZ_sanemsanas_datums[1:2]) == 0 && diff(x10s5$NDZ_sanemsanas_datums[2:3]) != 0) {
-                if ((x10s5$period[1] == "______" && x10s5$PS_code[1] == "______" && x10s5$NM_code[1] == "______") ||
-                    (x10s5$period[1] == "______" && x10s5$PS_code[1] == "______" && x10s5$NM_code[1] == "______") ||
-                    (x10s5$period[1] == "______" && x10s5$PS_code[1] == "______" && x10s5$NM_code[1] == "______") ||
-                    (x10s5$period[1] == "______" && x10s5$PS_code[1] == "______" && x10s5$NM_code[1] == "______") ||
-                    (x10s5$period[1] == "______" && x10s5$PS_code[1] == "______" && x10s5$NM_code[1] == "______")) {
+                if ((x10s5$period[1] == "_________" && x10s5$PS_code[1] == "_________" && x10s5$NM_code[1] == "_________") ||
+                    (x10s5$period[1] == "_________" && x10s5$PS_code[1] == "_________" && x10s5$NM_code[1] == "_________") ||
+                    (x10s5$period[1] == "_________" && x10s5$PS_code[1] == "_________" && x10s5$NM_code[1] == "_________") ||
+                    (x10s5$period[1] == "_________" && x10s5$PS_code[1] == "_________" && x10s5$NM_code[1] == "_________") ||
+                    (x10s5$period[1] == "_________" && x10s5$PS_code[1] == "_________" && x10s5$NM_code[1] == "_________") ||
+                    (x10s5$period[1] == "_________" && x10s5$PS_code[1] == "_________" && x10s5$NM_code[1] == "_________") ||
+                    (x10s5$period[1] == "_________" && x10s5$PS_code[1] == "_________" && x10s5$NM_code[1] == "_________") ||
+                    (x10s5$period[1] == "_________" && x10s5$PS_code[1] == "_________" && x10s5$NM_code[1] == "_________") ||
+                    (x10s5$period[1] == "_________" && x10s5$PS_code[1] == "_________" && x10s5$NM_code[1] == "_________")) {
                   x10s5_2 <- rbind(x10s5_2, x10s5[c(2,1), ])
                   x10s5_8 <- rbind(x10s5_8, x10s5[3:10, ])
                 } else {stop("processingTens_s5: Desmitnieku tabulas pārdalei trūkst izstrādes koda\n")}
@@ -44,7 +33,7 @@ processingTens_s5 <- function(x10s5, o, kods) {
                 if (x10s5$sak_beidz[4] == "2") {
                   if (x10s5$sak_beidz[5] == "2") {
                     if (x10s5$sak_beidz[6] == "1") {
-                        if (x10s5$period[1] == "______" && x10s5$PS_code[1] == "______" && x10s5$NM_code[1] == "______") {
+                        if (x10s5$period[1] == "_________" && x10s5$PS_code[1] == "_________" && x10s5$NM_code[1] == "_________") {
                           x10s5_1 <- rbind(x10s5_1, x10s5[1, ])
                           x10s5_2 <- rbind(x10s5_2, x10s5[c(3,5), ])
                           x10s5_5 <- rbind(x10s5_5, x10s5[6:10, ])
@@ -59,7 +48,10 @@ processingTens_s5 <- function(x10s5, o, kods) {
                            x10s5_2 <- rbind(x10s5_2, x10s5[2:3, ])
                            x10s5_7 <- rbind(x10s5_7, x10s5[4:10, ])
                          } else if (diff(x10s5$NDZ_sanemsanas_datums[1:2]) == 0 && diff(x10s5$NDZ_sanemsanas_datums[2:3]) != 0) {
-                           if (x10s5$period[1] == "______" && x10s5$PS_code[1] == "______" && x10s5$NM_code[1] == "______") {
+                           if ((x10s5$period[1] == "_________" && x10s5$PS_code[1] == "_________" && x10s5$NM_code[1] == "_________") ||
+                               (x10s5$period[1] == "_________" && x10s5$PS_code[1] == "_________" && x10s5$NM_code[1] == "_________") ||
+                               (x10s5$period[1] == "_________" && x10s5$PS_code[1] == "_ " && x10s5$NM_code[1] == "_________") ||
+                               (x10s5$period[1] == "_________" && x10s5$PS_code[1] == "_________" && x10s5$NM_code[1] == "_________")) {
                              x10s5_2 <- rbind(x10s5_2, x10s5[c(2,1), ])
                              x10s5_8 <- rbind(x10s5_8, x10s5[3:10, ])
                            } else {stop("processingTens_s5: Desmitnieku tabulas pārdalei trūkst izstrādes koda\n")}
