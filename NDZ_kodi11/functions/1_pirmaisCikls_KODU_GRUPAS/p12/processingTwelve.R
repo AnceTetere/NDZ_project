b0c4d8ebdceb9e8ef9882785e35cbfc60cba4cd3
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-02-03 15:20:36 +0200

    Update processingTwelve.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p12/processingTwelve.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p12/processingTwelve.R
@@ -1,13 +1,14 @@
 processingTwelve <- function(x, o, kods) {
   cat("-------------SĀK 12-nieku APSTRĀDI. \n")
   x <- arrange(x, PS_code, DN_code, NM_code, NDZ_sanemsanas_datums)
-  x12_uzVieniniekiem <- data.frame(); x12_uzDivniekiem <- data.frame(); x12_uzSeptini <- data.frame(); x12_uzDevini <- data.frame(); x12_uzDesmitniekiem <- data.frame(); x12_uzVienpadsmit <- data.frame()
+  x12_uzVieniniekiem <- data.frame(); x12_uzDivniekiem <- data.frame(); x12_uzSeptini <- data.frame(); x12_uzAstoni <- data.frame(); x12_uzDevini <- data.frame(); x12_uzDesmitniekiem <- data.frame(); x12_uzVienpadsmit <- data.frame()
   check_rows <- 0
   
   result <- function(y) {
     x12_uzVieniniekiem <<- rbind(x12_uzVieniniekiem, y$x12_uzVieniniekiem)
     x12_uzDivniekiem <<- rbind(x12_uzDivniekiem, y$x12_uzDivniekiem)
     x12_uzSeptini <<- rbind(x12_uzSeptini, y$x12_uzSeptini)
+    x12_uzAstoni <<- rbind(x12_uzAstoni, y$x12_uzAstoni)
     x12_uzDesmitniekiem <<- rbind(x12_uzDesmitniekiem, y$x12_uzDesmitniekiem)
     x12_uzVienpadsmit <<- rbind(x12_uzVienpadsmit, y$x12_uzVienpadsmit)
     rm(y)
@@ -46,20 +47,26 @@ if (nrow(x12_uzSeptini) > 0) {
     x12_uzSeptini %>% arrange(PS_code, NM_code, NDZ_sanemsanas_datums) %>% processingSeven(o, kods)
 } else {cat("Tabula x12_uzSeptini ir tukša.\n")}
 rm(x12_uzSeptini)
-  
-#4 Apakštabulu x12_uzDevini sūta caur processingNines().
+
+#4 Apakštabulu x12_uzSeptini sūta caur processingSeven().
+if (nrow(x12_uzAstoni) > 0) {
+  x12_uzAstoni %>% arrange(PS_code, NM_code, NDZ_sanemsanas_datums) %>% processingEights(o, kods)
+} else {cat("Tabula x12_uzAstoni ir tukša.\n")}
+rm(x12_uzAstoni)
+
+#5 Apakštabulu x12_uzDevini sūta caur processingNines().
 if (nrow(x12_uzDevini) > 0) {
   x12_uzDevini %>% arrange(PS_code, NM_code, NDZ_sanemsanas_datums) %>% processingNines(o, kods)
 } else {cat("Tabula x12_uzDevini ir tukša.\n")}
 rm(x12_uzDevini)
 
-#5 Apakštabulu x12_uzDesmitniekiem sūta caur processingTens().
+#6 Apakštabulu x12_uzDesmitniekiem sūta caur processingTens().
 if (nrow(x12_uzDesmitniekiem) > 0) {
     x12_uzDesmitniekiem %>% arrange(PS_code, NM_code, NDZ_sanemsanas_datums) %>% processingTens(o, kods)
 } else {cat("Tabula x12_uzDesmitniekiem ir tukša.\n")}
 rm(x12_uzDesmitniekiem)
   
-#6 Apakštabulu x12_uzVienpadsmit sūta caur processingEleven().
+#7 Apakštabulu x12_uzVienpadsmit sūta caur processingEleven().
 if (nrow(x12_uzVienpadsmit) > 0) {
     x12_uzVienpadsmit %>% arrange(PS_code, NM_code, NDZ_sanemsanas_datums) %>% processingEleven(o, kods)
 } else {cat("Tabula x12_uzVienpadsmit ir tukša.\n")}
