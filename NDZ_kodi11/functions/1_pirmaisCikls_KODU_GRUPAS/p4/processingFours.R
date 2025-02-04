7d472fbbdee1721f19952a2ded9120e0c4dc4a3f
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-02-04 16:44:23 +0200

    Update processingFours.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p4/processingFours.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p4/processingFours.R
@@ -19,50 +19,54 @@ for (r in seq(1, nrow(x), by = 4)) {
                rindās no", r, "līdz", r + 3, "nesakrīt pseidokoda, NM_code, DN_code, period kombinācija visās četrās rindās.")
   } else if (all(x4$sak_beidz == c("2", "1", "2", "2"))) {
             result(processingFours_2122(x4))
-            if (kods %in% c("40", "50", "53")) {ZERO_plus(x4 %>% slice(4))}
+            if (kods %in% c("40", "50", "53") && o == "4") {ZERO_plus(x4 %>% slice(4))}
   } else if (all(x4$sak_beidz == c("1", "2", "1", "2"))) {
             x4_trueDoubles <- rbind(x4_trueDoubles, x4)
-            if (kods %in% c("40", "50", "53")) {ZERO_plus(x4 %>% slice(4)); ZERO_minus(x1 %>% slice(1))}
+            if (kods %in% c("40", "50", "53") && o == "4") {ZERO_plus(x4 %>% slice(4)); ZERO_minus(x4 %>% slice(1))}
   } else if (all(x4$sak_beidz == c("1", "1", "2", "2"))) {
             result(processingFours_1122(x4))
-            if (kods %in% c("40", "50", "53")) {ZERO_plus(x4 %>% slice(4)); ZERO_minus(x1 %>% slice(1))}
   } else if (all(x4$sak_beidz == c("2", "1", "2", "1"))) {
             result(processingFours_2121(x4))
   } else if (all(x4$sak_beidz == c("1", "1", "2", "1"))) {
             result(processingFours_1121(x4))
-            if (kods %in% c("40", "50", "53")) {ZERO_minus(x1 %>% slice(2))}
+            if (kods %in% c("40", "50", "53") && o == "4") {ZERO_minus(x4 %>% slice(2))}
   } else if (all(x4$sak_beidz == c("1", "2", "1", "1"))) {
             result(processingFours_1211(x4))  
-            if (kods %in% c("40", "50", "53")) {ZERO_minus(x1 %>% slice(1))}
-  } else if (all(x4$sak_beidz == "1") || all(x4$sak_beidz == "2")) {
-            x4_uzVieniniekiem <- rbind(x4_uzVieniniekiem, codes_match(x4))
-            if (kods %in% c("40", "50", "53")) {ZERO_minus(x1 %>% slice(1))}
+            if (kods %in% c("40", "50", "53") && o == "4") {ZERO_minus(x4 %>% slice(1))}
+ # } else if (all(x4$sak_beidz == "1") || all(x4$sak_beidz == "2")) {
+#            x4_uzVieniniekiem <- rbind(x4_uzVieniniekiem, codes_match(x4[1, ]))
+#            if (kods %in% c("40", "50", "53") && o == "4") {ZERO_minus(x4 %>% slice(1))}
   } else if (all(x4$sak_beidz == c("1", "1", "1", "2"))) {
             x4_trueDoubles <- rbind(x4_trueDoubles, x4[3:4, ])
-            if (kods %in% c("40", "50", "53")) {ZERO_plus(x4 %>% slice(4)); ZERO_minus(x1 %>% slice(1))}
+            if (kods %in% c("40", "50", "53") && o == "4") {ZERO_plus(x4 %>% slice(4)); ZERO_minus(x4 %>% slice(1))}
   } else if (all(x4$sak_beidz == c("1", "2", "2", "1"))) {
             result(processingFours_1221(x4))
-            if (kods %in% c("40", "50", "53")) {ZERO_minus(x1 %>% slice(1))}
+            if (kods %in% c("40", "50", "53") && o == "4") {ZERO_minus(x4 %>% slice(1))}
   } else if (all(x4$sak_beidz == c("2", "1", "1", "1"))) {
             result(processingFours_2111(x4))
   } else if (all(x4$sak_beidz == c("2", "2", "1", "1"))) {
             result(processingFours_2211(x4))
   } else if (all(x4$sak_beidz == c("2", "1", "2", "2"))) {
             result(processingFours_2122(x4))
-            if (kods %in% c("40", "50", "53")) {ZERO_plus(x4 %>% slice(4))}
+            if (kods %in% c("40", "50", "53") && o == "4") {ZERO_plus(x4 %>% slice(4))}
   } else if (all(x4$sak_beidz == c("1", "2", "2", "2"))) {
             if ("26" %in% x4$zinkod) {
               x4_trueDoubles <- rbind(x4_trueDoubles, x4[x4$sak_beidz == "1" | x4$zinkod == "26", ])
             } else if(all(diff(x4$NDZ_sanemsanas_datums) != 0)){
               x4_trueDoubles <- rbind(x4_trueDoubles, x4[c(1,4), ])
-              if (kods %in% c("40", "50", "53")) {ZERO_plus(x4 %>% slice(4)); ZERO_minus(x1 %>% slice(1))}
+              if (kods %in% c("40", "50", "53") && o == "4") {ZERO_plus(x4 %>% slice(4)); ZERO_minus(x4 %>% slice(1))}
             } else {stop("Šeit četrinieku izstrādes tabulas rindām ", r, " līdz ", r+3, " trūkst apstrādes koda.")}
     } else if (all(x4$sak_beidz == c("2", "1", "1", "2"))) { 
               result(processingFours_2112(x4))
-              if (kods %in% c("40", "50", "53")) {ZERO_plus(x4 %>% slice(4))}
+              if (kods %in% c("40", "50", "53") && o == "4") {ZERO_plus(x4 %>% slice(4))}
+    } else if (all(x4$sak_beidz == c("2", "2", "1", "2"))) { 
+              result(processingFours_2212(x4, o, kods))
+    } else if (all(x4$sak_beidz == c("2", "2", "2", "1"))) { 
+              result(processingFours_2221(x4, o, kods))
     } else {stop(cat("Šeit četrinieku izstrādes tabulas rindām", r, "līdz", r+3, "trūkst apstrādes koda."))}
   check_rows <- check_rows + 4
 }
+
 
 #PĀRBAUDE
 if(nrow(x) == check_rows) {
