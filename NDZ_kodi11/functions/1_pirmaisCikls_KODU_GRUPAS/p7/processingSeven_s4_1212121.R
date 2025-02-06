a7de9ce777dc1cc0702493d18f7efe690dc8664e
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-02-06 19:29:29 +0200

    Update processingSeven_s4_1212121.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p7/processingSeven_s4_1212121.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p7/processingSeven_s4_1212121.R
@@ -23,14 +23,18 @@ processingSeven_s4_1212121 <- function(a, o, kods) {
           if (kods %in% c("40", "50", "53") && o == "7") {ZERO_minus(a %>% slice(1))}
   } else if (all(sapply(c(2,4,6), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) &&
              all(sapply(c(1,3,5), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
-    if (a$period[1] == "______" && a$PS_code[1] == "________" && a$NM_code[1] == "______") {
-      a6 <- rbind(a6, a[c(3,2,5,4,7,6), ])
-    } else if ((a$period[1] == "______" && a$PS_code[1] == "______" && a$NM_code[1] == "______") ||
-               (a$period[1] == "______" && a$PS_code[1] == "______" && a$NM_code[1] == "______")) {
-      a6 <- rbind(a6, a[1:6, ])
-      a1 <- rbind(a1, a[7, ])
-      if (kods %in% c("40", "50", "53") && o == "7") {ZERO_minus(a %>% slice(1))}
-    } else {stop("processingSeven_s4_1212121 trūkst izstrādes koda.\n")}
+                if (a$period[1] == "______" && a$pseidokods[1] == "______" && a$nmrkod[1] == "______") {
+                a6 <- rbind(a6, a[c(3,2,5,4,7,6), ])
+               } else if ((a$period[1] == "______" && a$pseidokods[1] == "______" && a$nmrkod[1] == "______") ||
+                          (a$period[1] == "______" && a$pseidokods[1] == "______" && a$nmrkod[1] == "______") ||
+                          (a$period[1] == "______" && a$pseidokods[1] == "______" && a$nmrkod[1] == "______") ||
+                          (a$period[1] == "______" && a$pseidokods[1] == "______" && a$nmrkod[1] == "______") ||
+                          (a$period[1] == "______" && a$pseidokods[1] == "______" && a$nmrkod[1] == "______") ||
+                          (a$period[1] == "______" && a$pseidokods[1] == "______" && a$nmrkod[1] == "______")) {
+                            a6 <- rbind(a6, a[1:6, ])
+                            a1 <- rbind(a1, a[7, ])
+                            if (kods %in% c("40", "50", "53") && o == "7") {ZERO_minus(a %>% slice(1))}
+              } else {stop("processingSeven_s4_1212121 trūkst izstrādes koda.\n")}
   } else {stop("processingSeven_s4_1212121 trūkst izstrādes koda.\n")}
 
   rm(a, o, kods)
