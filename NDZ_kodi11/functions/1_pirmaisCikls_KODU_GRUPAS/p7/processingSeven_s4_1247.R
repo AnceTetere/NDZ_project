fefad8fb0c890703c3ba380ce29b4ef0bfbf0e8d
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-02-06 19:40:20 +0200

    Update processingSeven_s4_1247.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p7/processingSeven_s4_1247.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p7/processingSeven_s4_1247.R
@@ -1,21 +1,22 @@
 processingSeven_s4_1247 <- function(a, o, kods) {
-  
+  #a <- x7s4
   a1 <- data.frame(); a2 <- data.frame(); a3 <- data.frame(); a4 <- data.frame(); a5 <- data.frame(); a6 <- data.frame()
   
   if (all(sapply(c(2,4), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) &&
       all(sapply(c(1,3,5,6), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
-    if (a$PS_code[1] == '________' && a$NM_code[1] == '______') {
-      a2 <- rbind(a_uzSesiniekiem, a[c(2,3,4,6), ])
-      a1 <- rbind(a1, a[7, ])
-      if (kods %in% c("40", "50", "53") && o == "7") {ZERO_minus(a %>% slice(1))}
-    } else {stop("processingSeven trūkst izstrādes koda.\n")}
+            if (a$pseidokods[1] == '______' && a$nmrkod[1] == '______') {
+              a2 <- rbind(a6, a[c(2,3,4,6), ])
+              a1 <- rbind(a1, a[7, ])
+              if (kods %in% c("40", "50", "53") && o == "7") {ZERO_minus(a %>% slice(1))}
+            } else {stop("processingSeven trūkst izstrādes koda.\n")}
   } else if (all(sapply(c(2,4,6), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) &&
              all(sapply(c(1,3,5), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
-    if (a$PS_code[1] == '______' && a$NM_code[1] == '______') {
-      a2 <- rbind(a_uzSesiniekiem, a[c(1,3,2,5,4,6), ])
-      a1 <- rbind(a1, a[7, ])
-      if (kods %in% c("40", "50", "53") && o == "7") {ZERO_minus(a %>% slice(1))}
-    } else {stop("processingSeven trūkst izstrādes koda.\n")}
+             if ((a$pseidokods[1] == '______' && a$nmrkod[1] == '______') ||
+                 (a$period[1] == '______' && a$pseidokods[1] == '______' && a$nmrkod[1] == '______')) {
+                 a2 <- rbind(a6, a[c(1,3,2,5,4,6), ])
+                 a1 <- rbind(a1, a[7, ])
+                 if (kods %in% c("40", "50", "53") && o == "7") {ZERO_minus(a %>% slice(1))}
+             } else {stop("processingSeven trūkst izstrādes koda.\n")}
   } else {stop("processingSeven trūkst izstrādes koda.\n")}
   
   rm(a, o, kods)
