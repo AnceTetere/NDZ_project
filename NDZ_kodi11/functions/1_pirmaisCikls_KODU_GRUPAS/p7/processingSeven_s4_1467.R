df176d97851402ccb8af8c9433e1a6390776eaaf
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-02-11 19:05:42 +0200

    Create processingSeven_s4_1467.R

---
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p7/processingSeven_s4_1467.R
@@ -0,0 +1,20 @@
+processingSeven_s4_1467 <- function(a, o, kods) {
+  
+  a1 <- data.frame(); a2 <- data.frame(); a3 <- data.frame(); a4 <- data.frame(); a5 <- data.frame(); a6 <- data.frame()
+  
+  if (all(diff(a$NDZ_sanemsanas_datums) != 0)) {
+    if (a$period[1] == "_______" && a$PS_code[1] == "_______" && a$NM_code[1] == "_______") {
+      a6 <- rbind(a6, a[c(1,3,4,5), ])
+      a1 <- rbind(a1, a[7, ])
+      if (kods %in% c("40", "50", "53") && o == "7") {ZERO_minus(a %>% slice(1))}
+    } else {stop("processingSeven_s4 trūkst izstrādes koda.\n")}
+  } else {stop("processingSeven_s4 trūkst izstrādes koda.\n")}
+  
+  rm(a, o, kods)
+  return(list(x7s4_uzVieniniekiem = a1,
+              x7s4_uzDivniekiem = a2,
+              x7s4_uzTrijniekiem = a3,
+              x7s4_uzCetriniekiem = a4, 
+              x7s4_uzPieciniekiem = a5,
+              x7s4_uzSesiniekiem = a6))
+}
