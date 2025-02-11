15d8a3dc5085e7162a574800406701830a17f6b0
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-02-11 18:36:29 +0200

    Update processingSeven_s4_1356.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p7/processingSeven_s4_1356.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p7/processingSeven_s4_1356.R
@@ -7,8 +7,11 @@ processingSeven_s4_1356 <- function(a, o, kods) {
           if (kods %in% c("40", "50", "53") && o == "7") {ZERO_plus(a %>% slice(7)); ZERO_minus(a %>% slice(1))}
   } else if (all(sapply(c(1,3,5), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) != 0)) ||
              all(sapply(c(2,4,6), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) == 0))) {
-         if ((a$period[1] == '______' && a$pseidokods[1] == '______' && a$nmrkod[1] == '______') ||
-             (a$period[1] == '______' && a$pseidokods[1] == '______' && a$nmrkod[1] == '______')) {
+         if ((a$period[1] == '___________' && a$PS_code[1] == '___________' && a$NM_code[1] == '___________') ||
+             (a$period[1] == '___________' && a$PS_code[1] == '___________' && a$NM_code[1] == '___________') ||
+             (a$period[1] == '___________' && a$PS_code[1] == '___________' && a$NM_code[1] == '___________') ||
+             (a$period[1] == '___________' && a$PS_code[1] == '___________' && a$NM_code[1] == '___________') ||
+             (a$period[1] == '___________' && a$PS_code[1] == '___________' && a$NM_code[1] == '___________')) {
            a1 <- rbind(a1, a[6])
            a6 <- rbind(a6, a[c(1,2,3,4,5,7), ])
            if (kods %in% c("40", "50", "53") && o == "7") {ZERO_minus(a %>% slice(1))}
