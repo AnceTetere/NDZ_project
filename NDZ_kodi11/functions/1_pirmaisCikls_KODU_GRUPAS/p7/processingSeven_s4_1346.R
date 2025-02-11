b6976f25f4fdabbef52da220437008bac79e68eb
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-02-11 19:09:58 +0200

    Create processingSeven_s4_1346.R

---
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p7/processingSeven_s4_1346.R
@@ -0,0 +1,27 @@
+processingSeven_s4_1346 <- function(a, o, kods) {
+  a1 <- data.frame(); a2 <- data.frame(); a3 <- data.frame(); a4 <- data.frame(); a5 <- data.frame(); a6 <- data.frame()
+  
+  if (all(sapply(c(1,3,5), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) != 0)) &&
+      all(sapply(c(2,4,6), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) == 0))) {
+    if ((a$period[1] == "__________" && a$PS_code[1] == "__________" && a$NM_code[1] == "__________") ||
+        (a$period[1] == "__________" && a$PS_code[1] == "__________" && a$NM_code[1] == "__________") ||
+        (a$period[1] == "__________" && a$PS_code[1] == "__________" && a$NM_code[1] == "__________") ||
+        (a$period[1] == "__________" && a$PS_code[1] == "__________" && a$NM_code[1] == "__________") ||
+        (a$period[1] == "__________" && a$PS_code[1] == "__________" && a$NM_code[1] == "__________") ||
+        (a$period[1] == "__________" && a$PS_code[1] == "__________" && a$NM_code[1] == "__________") ||
+        (a$period[1] == "__________" && a$PS_code[1] == "__________" && a$NM_code[1] == "__________")) {
+      a <- a[c(1,2,3,5,4,7,6),]
+      a6 <- rbind(a6, a[1:6, ])
+      a1 <- rbind(a1, a[7, ])
+      if (kods %in% c("40", "50", "53") && o == "7") {ZERO_minus(a %>% slice(1))}
+    } else {stop("processingSeven_s4 trūkst izstrādes koda.\n")}
+  } else {stop("processingSeven_s4 trūkst izstrādes koda.\n")}
+  
+  rm(a, o, kods)
+  return(list(x7s4_uzVieniniekiem = a1,
+              x7s4_uzDivniekiem = a2,
+              x7s4_uzTrijniekiem = a3,
+              x7s4_uzCetriniekiem = a4, 
+              x7s4_uzPieciniekiem = a5,
+              x7s4_uzSesiniekiem = a6))
+}
