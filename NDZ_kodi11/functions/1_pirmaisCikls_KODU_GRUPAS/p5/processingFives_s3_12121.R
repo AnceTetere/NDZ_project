8ff7827482f4ecce03b6b4a014d74a58055396dc
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-02-28 19:05:12 +0200

    Update processingFives_s3_12121.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p5/processingFives_s3_12121.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p5/processingFives_s3_12121.R
@@ -1,17 +1,20 @@
 processingFives_s3_12121 <- function(x5s3s) {
   x5s3s <- x5s3s %>% arrange(PS_code, DN_code, NM_code, NDZ_sanemsanas_datums)
 
-  x5s3s_uzVieniniekiem <- data.frame()
-  x5s3s_uzDivniekiem <- data.frame()
-  x5s3s_uzCetriniekiem <- data.frame()
+  x5s3s_uzVieniniekiem <- data.frame(); x5s3s_uzDivniekiem <- data.frame(); x5s3s_uzCetriniekiem <- data.frame()
   
 if (diff(x5s3s$NDZ_sanemsanas_datums[3:4]) == 0 &&
       all(sapply(c(1,2,4), function(i) diff(x5s3s$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
-    if((x5s3s$PS_code[1] %in% c('______________', '______________', '______________') && x5s3s$NM_code[1] %in% c('______________')) ||
-       (x5s3s$PS_code[1] == '______________' && x5s3s$NM_code[1] == '______________') ||
-       (x5s3s$PS_code[1] == '______________' && x5s3s$NM_code[1] == '______________')) {
+    if((x5s3s$PS_code[1] %in% c('_____', 
+                                   '_____', 
+                                   '_____', 
+                                   '_____', 
+                                   '_____') && x5s3s$NM_code[1] %in% c('_____')) ||
+       (x5s3s$PS_code[1] == '_____' && x5s3s$NM_code[1] == '_____') ||
+       (x5s3s$PS_code[1] == '_____' && x5s3s$NM_code[1] == '_____')) {
       x5s3s_uzVieniniekiem <- rbind(x5s3s_uzVieniniekiem, x5s3s[5,])
       x5s3s_uzDivniekiem <- rbind(x5s3s_uzDivniekiem, x5s3s[1:4,])
+      if (kods %in% c("40", "50", "53") && o == "5") {ZERO_minus(x5s3s %>% slice(1))}
     } else {stop("processingFives_s3_12121: Iztrūkst apstrādes koda!\n")}
 } else if (diff(x5s3s$NDZ_sanemsanas_datums[1:2]) == 0 &&
            all(sapply(2:4, function(i) diff(x5s3s$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
@@ -19,10 +22,10 @@ if (diff(x5s3s$NDZ_sanemsanas_datums[3:4]) == 0 &&
       x5s3s_uzDivniekiem <- rbind(x5s3s_uzDivniekiem, x5s3s[1:4,])
 } else if (all(sapply(c(1,3), function(i) diff(x5s3s$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) &&
            all(sapply(c(2,4), function(i) diff(x5s3s$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
-      if((x5s3s$PS_code[1] == '______________' && x5s3s$NM_code[1] == '______________') ||
-          (x5s3s$PS_code[1] == '______________' && x5s3s$NM_code[1] == '______________') ||
-          (x5s3s$PS_code[1] == '______________' && x5s3s$NM_code[1] == '______________') ||
-          (x5s3s$PS_code[1] == '______________' && x5s3s$NM_code[1] == '______________')) {
+      if((x5s3s$PS_code[1] == '_____' && x5s3s$NM_code[1] == '_____') ||
+          (x5s3s$PS_code[1] == '_____' && x5s3s$NM_code[1] == '_____') ||
+          (x5s3s$PS_code[1] == '_____' && x5s3s$NM_code[1] == '_____') ||
+          (x5s3s$PS_code[1] == '_____' && x5s3s$NM_code[1] == '_____')) {
         x5s3s_uzVieniniekiem <- rbind(x5s3s_uzVieniniekiem, x5s3s[5,])
         x5s3s_uzDivniekiem <- rbind(x5s3s_uzDivniekiem, x5s3s[1:4,])
      } else {stop("processingFives_s3_12121: Iztrūkst apstrādes koda!\n")}  
