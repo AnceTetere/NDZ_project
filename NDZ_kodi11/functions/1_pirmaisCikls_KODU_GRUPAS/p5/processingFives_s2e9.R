3bf9ac312f5cf1f73e691b9c8f02fb7ca980c929
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-02-28 19:11:28 +0200

    Update processingFives_s2e9.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p5/processingFives_s2e9.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p5/processingFives_s2e9.R
@@ -1,25 +1,33 @@
-processingFives_s2e9 <- function(a, kods) {  
-  #a <- a for testing
+processingFives_s2e9 <- function(a, o, kods) {  
+  #a <- x5s2 for testing
   a <- a %>% arrange(PS_code, DN_code, NM_code, NDZ_sanemsanas_datums)
   a1 <- data.frame(); a2 <- data.frame(); a4 <- data.frame()
   
-  if (all(a$sak_beidz == c("2", "2", "1", "2", "1"))) {
-    if (a$PS_code[1] == '________' && a$NM_code[1] == '________1') {
+  if (all(a$sak_beidz[c(3,5)] == "1")) {
+    if ((a$PS_code[1] == '_____' && a$NM_code[1] == '_____') ||
+        (a$PS_code[1] == '_____' && a$NM_code[1] == '_____')) {
       a1 <- rbind(a1, a[1,])  
       a2 <- rbind(a2, a[c(3,2,5,4),])
     } else {stop("processingFives_s2e9: Trūkst apstrādes koda.")}
   } else if (all(a$sak_beidz == c("2", "1", "2", "1", "2"))) {
-    if (a$PS_code[1] == '________' && a$NM_code[1] == '________' && a$period[1] == "________") {
+    if (a$PS_code[1] == '_____' && a$NM_code[1] == '_____' && a$period[1] == "_____") {
       a1 <- rbind(a1, a[1,])  
       a2 <- rbind(a2, a[2:5,])
     } else {stop("processingFives_s2e9: Trūkst apstrādes koda.")}
     if (kods %in% c("40", "50", "53")) {ZERO_plus(a %>% slice(5))}
   } else if (all(a$sak_beidz == c("2", "1", "2", "2", "1"))) {
-    if (a$PS_code[1] == '________' && a$NM_code[1] == '________' && a$period[1] == "________") {
+    if (a$PS_code[1] == '_____' && a$NM_code[1] == '_____' && a$period[1] == "_____") {
       a <- a %>% slice(1,2,3,5,4)
       a1 <- rbind(a1, a[1,])  
       a2 <- rbind(a2, a[2:5,])
       if (kods %in% c("40", "50", "53")) {ZERO_plus(a %>% slice(5))}
+    } else {stop("processingFives_s2e9: Trūkst apstrādes koda.")}
+  } else if (all(a$sak_beidz == c("2", "2", "1", "1", "2"))) {
+    if (a$PS_code[1] == '_____' && a$NM_code[1] == '_____' && a$period[1] == "_____") {
+      a <- a %>% slice(1,3,2,4,5)
+      a1 <- rbind(a1, a[1,])  
+      a2 <- rbind(a2, a[2:5,])
+      if (kods %in% c("40", "50", "53") && o == "5") {ZERO_plus(a %>% slice(5))}
     } else {stop("processingFives_s2e9: Trūkst apstrādes koda.")}
   } else {stop("processingFives_s2e9: Trūkst apstrādes koda.")}
   
