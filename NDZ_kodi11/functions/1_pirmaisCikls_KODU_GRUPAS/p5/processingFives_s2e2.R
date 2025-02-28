2e0c411f7412567c6eeff28516adc6cf864366f1
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-02-28 19:17:11 +0200

    Update processingFives_s2e2.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p5/processingFives_s2e2.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p5/processingFives_s2e2.R
@@ -1,15 +1,16 @@
 processingFives_s2e2 <- function(a, kods) {  
-  #a <- x5s2 for testing
+  #a <- a for testing
   a <- a %>% arrange(PS_code, DN_code, NM_code, NDZ_sanemsanas_datums)
   a1 <- data.frame(); a2 <- data.frame(); a4 <- data.frame()
   
-  if (all(x5s2$sak_beidz[1:5] == c("2", "1", "2", "1", "2")) && x5s2$zinkod[3] == "26") {
-    x5s2_uzVieniniekiem <- rbind(x5s2_uzVieniniekiem, x5s2[1, ])
-    x5s2_uzCetriniekiem <- rbind(x5s2_uzCetriniekiem, x5s2[-1, ])
+  if (all(a$sak_beidz[1:5] == c("2", "1", "2", "1", "2")) && a$zinkod[3] == "26") {
+    a1 <- rbind(a1, a[1, ])
+    a4 <- rbind(a4, a[-1, ])
   } else {stop("processingFives_s2: Trūkst apstrādes koda.")}
   
   rm(a, kods)
   return(list(x5s2_uzVieniniekiem = a1,
               x5s2_uzDivniekiem = a2,
               x5s2_uzCetriniekiem = a4))
+
 }
