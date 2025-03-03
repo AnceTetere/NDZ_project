ab02d89d35d66e01c33b90225c25b314f9bc5317
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-03-03 13:56:12 +0200

    Update processingSeven_s4_2467.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p7/processingSeven_s4_2467.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p7/processingSeven_s4_2467.R
@@ -1,18 +1,17 @@
 processingSeven_s4_2467 <- function(a, o, kods) {
   
   a1 <- data.frame(); a2 <- data.frame(); a3 <- data.frame(); a4 <- data.frame(); a5 <- data.frame(); a6 <- data.frame()
-    
+  
+  
   if (all(sapply(c(1,2,3,4,6), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) != 0)) &&
       diff(a$NDZ_sanemsanas_datums[5:6]) == 0) {
-        #It kā viss OK, bet neesmu vēl pārliecināta, ka šo var vispārināt.
-        if (a$period[1] == "___________" && a$PS_code[1] == '___________' && a$NM_code[1] == '___________') {
+        if (a$period[1] == "______" && a$PS_code[1] == '______' && a$NM_code[1] == '______') {
           a1 <- rbind(a1, a[c(1,7),])
           a4 <- rbind(a4, a[c(2,3,4,5), ])
         } else {stop("processingSeven_s4_2467 trūkst izstrādes koda.\n")}
   } else if (all(diff(a$NDZ_sanemsanas_datums) != 0)) {
-        #It kā viss OK, bet neesmu vēl pārliecināta, ka šo var vispārināt.
-        if ((a$period[1] == "___________" && a$PS_code[1] == '___________' && a$NM_code[1] == '___________') ||
-            (a$period[1] == "___________" && a$PS_code[1] == '___________' && a$NM_code[1] == '___________')) {
+        if ((a$period[1] == "______" && a$PS_code[1] == '______' && a$NM_code[1] == '______') ||
+            (a$period[1] == "______" && a$PS_code[1] == '______' && a$NM_code[1] == '______')) {
           a1 <- rbind(a1, a[c(1,7),])
           a4 <- rbind(a4, a[c(2,3,4,5), ])
         } else {stop("processingSeven_s4_2467 trūkst izstrādes koda.\n")}
