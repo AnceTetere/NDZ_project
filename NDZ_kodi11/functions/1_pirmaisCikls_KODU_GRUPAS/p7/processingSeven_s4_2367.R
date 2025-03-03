7fd96b864e4c2a62c9fbc0ce6870ab1fde4f6756
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-03-03 13:39:55 +0200

    Update processingSeven_s4_2367.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p7/processingSeven_s4_2367.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p7/processingSeven_s4_2367.R
@@ -1,10 +1,12 @@
 processingSeven_s4_2367 <- function(a, o, kods) {
   
   a1 <- data.frame(); a2 <- data.frame(); a3 <- data.frame(); a4 <- data.frame(); a5 <- data.frame(); a6 <- data.frame()
+  #a <- x7s4
   
   if (all(sapply(c(1,3,5), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) &&
       all(sapply(c(2,4,6), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
-    if (a$period[1] == "___________" && a$PS_code[1] == "___________" && a$NM_code[1] == "___________") {
+    if ((a$period[1] == "______" && a$PS_code[1] == "______" && a$NM_code[1] == "______") ||
+        (a$period[1] == "______" && a$PS_code[1] == "______" && a$NM_code[1] == "______")) {
       a6 <- rbind(a6, a[c(2,1,3,4,6,5), ])
       a1 <- rbind(a1, a[7, ])
       if (kods %in% c("40", "50", "53") && o == "7") {ZERO_minus(a %>% slice(2))}
