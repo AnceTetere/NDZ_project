65d0097571251c6ce0e65a5d30b4e518f6fa34d4
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-03-03 13:44:49 +0200

    Update processingSeven_s4_1347.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p7/processingSeven_s4_1347.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p7/processingSeven_s4_1347.R
@@ -4,7 +4,7 @@ processingSeven_s4_1347 <- function(a, o, kods) {
   
   if (all(sapply(c(1,3,5), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) != 0)) &&
       all(sapply(c(2,4,6), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) == 0))) {
-    if (a$period[1] == "_________" && a$NM_code[1] == "__________") {
+    if (a$period[1] == "______" && a$NM_code[1] == "______") {
       a <- a[c(1,2,3,5,4,6,7),]
       a6 <- rbind(a6, a[1:6, ])
       a1 <- rbind(a1, a[7, ])
