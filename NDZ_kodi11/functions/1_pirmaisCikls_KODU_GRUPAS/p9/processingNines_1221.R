1eeeac88fac77bd92845e7b35a08c6d1ac11d1c1
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-03-04 16:34:09 +0200

    Update processingNines_1221.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p9/processingNines_1221.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p9/processingNines_1221.R
@@ -1,21 +1,31 @@
 processingNines_1221 <- function(a, o, kods) {  
-  #a <- a for testing
+
   a <- a %>% arrange(PS_code, DN_code, NM_code, NDZ_sanemsanas_datums)
   a1 <- data.frame(); a2 <- data.frame(); a6 <- data.frame(); a7 <- data.frame(); a8 <- data.frame()
   
-  if (diff(a$NDZ_sanemsanas_datums[1:2]) != 0 && diff(a$NDZ_sanemsanas_datums[8:9]) != 0) {
-    a1 <- rbind(a1, a[9, ])
-    a8 <- rbind(a8, a[1:8, ])
-    if (kods %in% c("40", "50", "53") && o == "9") {ZERO_minus(a %>% slice(1))}
+  if (all(a$sak_beidz == c("1", "2", "2", "1", "1", "2", "1", "2", "1")) && 
+           all(sapply(seq(1, 4, by = 2), function(i) all(diff(a$NDZ_sanemsanas_datums[i:i+1]) == 0))) &&
+           all(diff(a$NDZ_sanemsanas_datums[4:9]) != 0)) {
+              a1 <- rbind(a1, a[9, ])
+              a8 <- rbind(a8, a[1:8, ])
+               if (kods %in% c("40", "50", "53") && o == "9") {ZERO_minus(a %>% slice(1))}
+  } else if (diff(a$NDZ_sanemsanas_datums[1:2]) != 0 && diff(a$NDZ_sanemsanas_datums[8:9]) != 0) {
+              a1 <- rbind(a1, a[9, ])
+              a8 <- rbind(a8, a[1:8, ])
+               if (kods %in% c("40", "50", "53") && o == "9") {ZERO_minus(a %>% slice(1))}
   } else if (all(sapply(c(2,4,8), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) && diff(a$NDZ_sanemsanas_datums[1:2]) != 0) {
-    #JO PIRMOREIZ
-    if (a$period[1] == "_________" && a$PS_code[1] == "_________" && a$NM_code[1] == "_________") {
-      a1 <- rbind(a1, a[9, ])
-      a8 <- rbind(a8, a[1:8, ])
+               if (a$period[1] == "______" && a$PS_code[1] == "___________" && a$NM_code[1] == "______") {
+                 a1 <- rbind(a1, a[9, ])
+                 a8 <- rbind(a8, a[1:8, ])
+                 if (kods %in% c("40", "50", "53") && o == "9") {ZERO_minus(a %>% slice(1))}
+               } else {stop("processingNines_1221 iztrūkst apstrādes kods. \n")}
+  } else if (diff(a$NDZ_sanemsanas_datums[1:2]) == 0 && all(diff(a$NDZ_sanemsanas_datums[2:9]) != 0)) {
+    if (a$period[1] == "______" && a$PS_code[1] == "______" && a$NM_code[1] == "______") {
+      a1 <- a[9, ]
+      a8 <- a[1:8, ]
       if (kods %in% c("40", "50", "53") && o == "9") {ZERO_minus(a %>% slice(1))}
     } else {stop("processingNines_1221 iztrūkst apstrādes kods. \n")}
   } else {stop("processingNines_1221 iztrūkst apstrādes kods. \n")}
-  } else {stop("processingNines_1221: Trūkst apstrādes koda.")}
   
   rm(a, kods)
   return(list(x9_uzVieniniekiem = a1,
@@ -23,5 +33,4 @@ processingNines_1221 <- function(a, o, kods) {
               x9_uzSesi = a6,
               x9_uzSeptini = a7,
               x9_uzAstoniekiem = a8))
-  
 }
