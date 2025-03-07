30981c8518bab0fb6927ca836affd489cc20b29e
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-03-07 19:11:44 +0200

    Update processingSeven.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p7/processingSeven.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p7/processingSeven.R
@@ -1,4 +1,5 @@
 processingSeven <- function(x, o, kods) {
+  
   x <- x %>% arrange(PS_code, DN_code, NM_code, NDZ_sanemsanas_datums)
   x7_uzVieniniekiem <- data.frame(); x7_uzDivniekiem <- data.frame(); x7_uzTrijniekiem <- data.frame(); x7_uzCetriniekiem <- data.frame(); x7_uzPieciniekiem <- data.frame(); x7_uzSesiniekiem <- data.frame()
   check_rows <- 0
@@ -35,7 +36,7 @@ processingSeven <- function(x, o, kods) {
               } else {stop("processingSeven trūkst izstrādes koda.\n")}
             } else if (all(x7$sak_beidz[1:3] == c("2", "1", "1"))) {
                    if (diff(x7$NDZ_sanemsanas_datums[1:2]) == 0) {
-                     if (x7$PS_code[1] == '_________' && x7$NM_code[1] == '_________') {
+                     if (x7$PS_code[1] == '______' && x7$NM_code[1] == '______') {
                         x7_uzVieniniekiem <- rbind(x7_uzVieniniekiem, x7[1, ])
                         x7_uzSesiniekiem <- rbind(x7_uzSesiniekiem, x7[-1, ])
                      } else {stop("processingSeven trūkst izstrādes koda.\n")}
@@ -87,4 +88,3 @@ if(nrow(x7_uzSesiniekiem) > 0) {
 } else {cat("Tabula x7_uzSesiniekiem ir tukša.\n")}
 rm(x7_uzSesiniekiem)
 }
-
