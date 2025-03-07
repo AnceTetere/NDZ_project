739e0ab931b6343df4afb85b44fe5856940b5ce1
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-03-07 19:05:09 +0200

    Update processingSeven_s4_1457.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p7/processingSeven_s4_1457.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p7/processingSeven_s4_1457.R
@@ -5,7 +5,8 @@ processingSeven_s4_1457 <- function(a, o, kods) {
   
   if (all(sapply(c(1,2,4,5,6), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) != 0)) &&
              diff(a$NDZ_sanemsanas_datums[3:4]) == 0) {
-           if (a$period[1] == "______" && a$PS_code[1] == '______' && a$NM_code[1] == '______') {
+           if ((a$period[1] == "______" && a$PS_code[1] == '______' && a$NM_code[1] == '______') ||
+               (a$period[1] == "______" && a$PS_code[1] == '______' && a$NM_code[1] == '______')) {
              a <- a[c(1,2,4,3,5,6,7),]
              a1 <- rbind(a1, a[7,])
              a6 <- rbind(a6, a[1:6, ])
@@ -21,11 +22,11 @@ processingSeven_s4_1457 <- function(a, o, kods) {
           } else {stop("processingSeven_s4_1457 trūkst izstrādes koda.\n")}
   } else if (all(sapply(c(1,3), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) &&
              all(sapply(c(2,4,5,6), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
-    if (a$period[1] == "______" && a$PS_code[1] == '______' && a$NM_code[1] == '______') {
-      a <- a[c(1,2,4,3,5,6,7),]
-      a1 <- a[7,]; a6 <- a[1:6, ]
-      if (kods %in% c("40", "50", "53") && o == "7") {ZERO_minus(a %>% slice(1))}
-    } else {stop("processingSeven_s4_1457 trūkst izstrādes koda.\n")}
+           if (a$period[1] == "______" && a$PS_code[1] == '______' && a$NM_code[1] == '______') {
+             a <- a[c(1,2,4,3,5,6,7),]
+             a1 <- a[7,]; a6 <- a[1:6, ]
+             if (kods %in% c("40", "50", "53") && o == "7") {ZERO_minus(a %>% slice(1))}
+           } else {stop("processingSeven_s4_1457 trūkst izstrādes koda.\n")}
   } else {stop("processingSeven_s4_1457 trūkst izstrādes koda.\n")}
   
   rm(a, o, kods)
