0a43b43d17ce9ec6823c0494e5e361bc27607676
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-03-07 19:13:26 +0200

    Update processingEights_s4.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p8/processingEights_s4.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p8/processingEights_s4.R
@@ -1,6 +1,5 @@
 processingEights_s4 <- function(x8s4, o, kods) {
   x8s4 <- x8s4 %>% arrange(PS_code, DN_code, NM_code, NDZ_sanemsanas_datums)
-
 x8s4_uzVieniniekiem <- data.frame(); x8s4_uzDivniekiem <- data.frame(); x8s4_uzCetriniekiem <- data.frame()
 x8s4_uzSesi <- data.frame(); x8s4_uzSeptini <- data.frame()
 
@@ -12,7 +11,7 @@ result <- function(x) {
   x8s4_uzSeptini <<- x$x8s4_uzSeptini
   rm(x)}
 
-if (x8s4$PS_code[1] == '_____' && x8s4$NM_code[1] == '_____') {
+if (x8s4$PS_code[1] == '______' && x8s4$NM_code[1] == '______') {
   if (all(x8s4$sak_beidz == c("2","1","2","2","1","2","1","1"))) {
     x8s4_uzVieniniekiem <- rbind(x8s4_uzVieniniekiem, x8s4[c(1,8), ])
     x8s4_uzCetriniekiem <- rbind(x8s4_uzCetriniekiem, x8s4[c(2,4,5,6),])
@@ -23,6 +22,7 @@ if (x8s4$PS_code[1] == '_____' && x8s4$NM_code[1] == '_____') {
            cS <- list(c(2, 4, 6, 7), c(1, 4, 6, 8), c(2, 4, 5, 8), c(2, 4, 6, 8), c(1, 4, 6, 7), c(2, 3, 6, 8), c(2, 3, 5, 7), c(2, 3, 5, 8), c(1, 4, 5, 7), c(1, 3, 6, 8), c(1, 3, 5, 7), c(2, 3, 6, 7))
            found_match <- FALSE
            
+           #Salīdzina vai šajā apakštabulā kāda no cS kombinācijām atrodama.
            for (s in cS) {
              if (all(x8s4$sak_beidz[s] == "1")) {
                x8s4_uzCetriniekiem <- rbind(x8s4_uzCetriniekiem, x8s4)
@@ -34,7 +34,7 @@ if (x8s4$PS_code[1] == '_____' && x8s4$NM_code[1] == '_____') {
                x8s4_uzVieniniekiem <- rbind(x8s4_uzVieniniekiem, x8s4[c(1, 8), ])
                x8s4_uzDivniekiem <- rbind(x8s4_uzDivniekiem, x8s4[4:5, ])
              } else if (all(x8s4$sak_beidz[c(1,4,5,8)] == "1") && x8s4$zinkod[1] %in% c("11")) {
-               if (x8s4$period[1] == "_____" && x8s4$PS_code[1] == "_____" && x8s4$NM_code[1] == "_____") {
+               if (x8s4$period[1] == "______" && x8s4$PS_code[1] == "______" && x8s4$NM_code[1] == "______") {
                  x8s4_uzDivniekiem <- rbind(x8s4_uzDivniekiem, x8s4[c(1,2,4,3,5,6,8,7), ])
                } else {stop("processingEights_s4: Trūkst izstrādes koda.")}
              } else {stop("processingEights_s4: Trūkst izstrādes koda.")}
@@ -50,7 +50,7 @@ if (x8s4$PS_code[1] == '_____' && x8s4$NM_code[1] == '_____') {
            } else if (all(sapply(c(1,3,5,6), function(i) diff(x8s4$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) &&
                       all(sapply(c(2,4,7), function(i) diff(x8s4$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
              if (kods == "40") {
-               if (x8s4$period[1] == "_____" && x8s4$PS_code[1] == "_____" && x8s4$NM_code[1] == "_____") {
+               if (x8s4$period[1] == "______" && x8s4$PS_code[1] == "______" && x8s4$NM_code[1] == "______") {
                  x8s4_uzVieniniekiem <- rbind(x8s4_uzVieniniekiem, x8s4[c(2,8), ])
                  x8s4_uzDivniekiem <- rbind(x8s4_uzDivniekiem, x8s4[c(3,7), ])
                } else {stop("processingEights_s4: Trūkst izstrādes koda.")}
