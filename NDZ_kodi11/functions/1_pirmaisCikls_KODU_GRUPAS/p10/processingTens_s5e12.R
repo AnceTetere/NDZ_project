2817c43ed74c1c94def986cb9e9b21c842653ba9
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-03-11 17:09:40 +0200

    Update processingTens_s5e12.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p10/processingTens_s5e12.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p10/processingTens_s5e12.R
@@ -4,37 +4,35 @@ processingTens_s5e12 <- function(a, o, kods) {
 
  if (a$sak_beidz[3] != "2") {
          if (all(diff(a$NDZ_sanemsanas_datums[1:3]) != 0)) {
-              a2 <- rbind(a2, a[1:2, ])
-              a8 <- rbind(a8, a[3:10, ])
+              a2 <- a[1:2, ]; a8 <- a[3:10, ]
               if (kods %in% c("40", "50", "53") && o == "10") {ZERO_minus(a %>% slice(1))}
          } else if (diff(a$NDZ_sanemsanas_datums[1:2]) == 0 && diff(a$NDZ_sanemsanas_datums[2:3]) != 0) {
-           if (a$NM_code[1] == "_____" ||
-               (a$period[1] == "_____" && a$PS_code[1] == "_____" && a$NM_code[1] == "_____") ||
+            if (a$NM_code[1] == "__________" ||
+               (a$period[1] == "________" && a$PS_code[1] == "________" && a$NM_code[1] == "________") ||
                (a$period[1] == "_____" && a$PS_code[1] == "_____" && a$NM_code[1] == "_____") ||
                (a$period[1] == "_____" && a$PS_code[1] == "_____" && a$NM_code[1] == "_____")) {
-                 a2 <- a[1:2, ]
-                 a8 <- a[3:10, ]
+                 a2 <- a[1:2, ]; a8 <- a[3:10, ]
+                 if (kods %in% c("40", "50", "53") && o == "10") {ZERO_minus(a %>% slice(1))}
            } else {stop("processingTens_s5: Desmitnieku tabulas pārdalei trūkst izstrādes koda\n")}
-           if (kods %in% c("40", "50", "53") && o == "10") {ZERO_minus(a %>% slice(1)); ZERO_plus(a %>% slice(10))}
          } else {stop("processingTens_s5: Desmitnieku tabulas pārdalei trūkst izstrādes koda\n")}
   } else if (all(a$sak_beidz[3:4] == c("2", "1"))) {
         if (all(sapply(c(1,2,4), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) != 0)) && diff(a$NDZ_sanemsanas_datums[3:4]) == 0) {
-          if ((a$period[1] == "_____" && a$PS_code[1] == "_____" && a$NM_code[1] == "40103757447") ||
-              (a$period[1] == "_____" && a$PS_code[1] == "_____" && a$NM_code[1] == "_____") ||
-              (a$period[1] == "_____" && a$PS_code[1] == "_____" && a$NM_code[1] == "_____") ||
-              (a$period[1] == "_____" && a$PS_code[1] == "_____" && a$NM_code[1] == "_____") ||
-              (a$period[1] == "_____" && a$PS_code[1] == "_____" && a$NM_code[1] == "_____") ||
-              (a$period[1] == "_____" && a$PS_code[1] == "_____" && a$NM_code[1] == "_____")) {
-                a2 <- a[1:2, ]
-                a8 <- a[c(4,3,5:10), ]
+          if ((a$period[1] == "_____" && a$PS_code[1] == "_____" && a$NM_code[1] == "_____") ||
+              (a$period[1] == "_____" && a$PS_code[1] == "_____" && a$NM_code[1] == "__________") ||
+              (a$period[1] == "_____" && a$PS_code[1] == "_____" && a$NM_code[1] == "__________") ||
+              (a$period[1] == "_____" && a$PS_code[1] == "_____" && a$NM_code[1] == "__________") ||
+              (a$period[1] == "_____" && a$PS_code[1] == "_____" && a$NM_code[1] == "__________") ||
+              (a$period[1] == "_____" && a$PS_code[1] == "_____" && a$NM_code[1] == "__________")) {
+                a2 <- a[1:2, ]; a8 <- a[c(4,3,5:10), ]
           } else {stop("processingTens_s5: Desmitnieku tabulas pārdalei trūkst izstrādes koda\n")}
       if (kods %in% c("40", "50", "53") && o == "10") {ZERO_minus(a %>% slice(1)); ZERO_plus(a %>% slice(9))}
     } else if (all(sapply(c(1,3), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) && 
                all(sapply(c(2,4), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
-      if ((a$period[1] == "_____" && a$PS_code[1] == "_____" && a$NM_code[1] == "_____") ||
-          (a$period[1] == "_____" && a$PS_code[1] == "_____" && a$NM_code[1] == "_____")) {
-              a2 <- rbind(a2, a[1:2, ])
-              a8 <- rbind(a8, a[c(4,3,5:10), ])
+      #BLOĶĒJU, JO PIRMOREIZ
+      if ((a$period[1] == "_____" && a$PS_code[1] == "_____ && a$NM_code[1] == "__________") ||
+          (a$period[1] == "_____" && a$PS_code[1] == "_____" && a$NM_code[1] == "__________") ||
+          (a$period[1] == "_____" && a$PS_code[1] == "_____" && a$NM_code[1] == "__________")) {
+              a2 <- a[1:2, ]; a8 <- a[c(4,3,5:10), ]
       } else {stop("processingTens_s5: Desmitnieku tabulas pārdalei trūkst izstrādes koda\n")}
       if (kods %in% c("40", "50", "53") && o == "10") {ZERO_minus(a %>% slice(1))}
     } else {stop("processingTens_s5: Desmitnieku tabulas pārdalei trūkst izstrādes koda\n")}
