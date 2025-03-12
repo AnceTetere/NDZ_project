9efa04879be405f5db18f5586f8c0394fb28311e
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-03-12 21:18:00 +0200

    Update processingFives_s2e5.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p5/processingFives_s2e5.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p5/processingFives_s2e5.R
@@ -6,7 +6,7 @@ processingFives_s2e5 <- function(a, kods) {
   if (all(a$sak_beidz[4:5] == "1")) {
     a1 <- rbind(a1, a[c(1,5), ])
   } else if (all(a$sak_beidz[3:4] == "1")) {
-    if (a$PS_code[1] == '_____' && a$NM_code[1] == '_____') {
+    if (a$PS_code[1] == '_____' && a$NM_code[1] == '______') {
       a1 <- rbind(a1, a[1, ])
       a2 <- rbind(a2, a[4:5, ])
     } else {stop("processingFives_s2e5: Trūkst apstrādes koda.")}
