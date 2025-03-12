c61c9f71dda58b1d686ae13d0bf93230d2a714ff
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-03-12 21:24:29 +0200

    Update processingFives_s2e8.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p5/processingFives_s2e8.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p5/processingFives_s2e8.R
@@ -1,12 +1,15 @@
 processingFives_s2e8 <- function(a, o, kods) {  
+  #a <- x5s2 for testing
   a <- a %>% arrange(PS_code, DN_code, NM_code, NDZ_sanemsanas_datums)
   a1 <- data.frame(); a2 <- data.frame(); a4 <- data.frame()
   
   if (all(a$sak_beidz[c(2,5)] == "1")) {
-          if ((a$PS_code[1] == '_____' && a$NM_code[1] == '_____')||
+          if ((a$PS_code[1] == '_____' && a$NM_code[1] == '_____') ||
+              (a$PS_code[1] == '_____' && a$NM_code[1] == '_____') ||
               (a$PS_code[1] == '_____' && a$NM_code[1] == '_____')) {
             a1 <- rbind(a1, a[1,])  
             a2 <- rbind(a2, a[c(2,3,5,4),])
+            if (kods %in% c("40", "50", "53") && o == "5") {ZERO_plus(a %>% slice(4))}
           } else {stop("processingFives_s2e8.R: Trūkst apstrādes koda.")}
   } else if (all(a$sak_beidz[c(2,4)] == "1")) {
            a1 <- rbind(a1, a[1,])  
