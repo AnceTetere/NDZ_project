2eb35aba60e196bf69c818f784fb957604b88463
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-03-12 21:33:56 +0200

    Update processingFives_s3.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p5/processingFives_s3.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p5/processingFives_s3.R
@@ -1,5 +1,6 @@
 processingFives_s3 <- function(x5s3, o, kods) {
   x5s3 <- x5s3 %>% arrange(PS_code, DN_code, NM_code, NDZ_sanemsanas_datums)
+  #x5s3 <- x5
   x5s3_uzVieniniekiem <- data.frame(); x5s3_uzDivniekiem <- data.frame(); x5s3_uzCetriniekiem <- data.frame()
 
   result <- function(y) {
@@ -31,15 +32,7 @@ if (all(x5s3$sak_beidz[c(1,3,5)] == "1")) {
 } else if (all(diff(x5s3$NDZ_sanemsanas_datums[2:4]) != 0) && all(sapply(c(1,4), function(i) diff(x5s3$NDZ_sanemsanas_datums[i:(i+1)]) == 0))) {
             result(processingFives_s3e10(x5s3, o, kods))
 } else if (diff(x5s3$NDZ_sanemsanas_datums[3:4]) == 0 && all(sapply(c(1,2,4), function(i) diff(x5s3$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
-          if (all(x5s3$sak_beidz == c("2", "1", "2", "1", "1"))) {
-            x5s3_uzVieniniekiem <- rbind(x5s3_uzVieniniekiem, x5s3[c(1,5),])
-            x5s3_uzDivniekiem <- rbind(x5s3_uzDivniekiem, x5s3[2:3,])
-          } else if (all(x5s3$sak_beidz == c("1", "2", "2", "1", "1"))) {
-            x5s3 <- x5s3[c(1,2,4,3,5), ]
-            x5s3_uzVieniniekiem <- rbind(x5s3_uzVieniniekiem, x5s3[5,])
-            x5s3_uzDivniekiem <- rbind(x5s3_uzDivniekiem, x5s3[1:4,])
-            if (kods %in% c("40", "50", "53") && o == "5") {ZERO_minus(x5s3 %>% slice(1))}
-          } else {stop("processingFives_s3: Iztrūkst apstrādes koda!\n")}
+            result(processingFives_s3e11(x5s3, o, kods))
 } else if (diff(x5s3$NDZ_sanemsanas_datums[1:2]) == 0 && all(sapply(2:4, function(i) diff(x5s3$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
                 result(processingFives_s3e1(x5s3, o, kods))
 } else if (all(sapply(c(1,3), function(i) diff(x5s3$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) && all(sapply(c(2,4), function(i) diff(x5s3$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
@@ -83,7 +76,7 @@ if (all(x5s3$sak_beidz[c(1,3,5)] == "1")) {
             } else {stop("processingFives_s3: Iztrūkst apstrādes koda!\n")}
 } else {stop("processingFives_s3: Iztrūkst apstrādes koda!\n")}
   
-rm(x5s3, kods)
+rm(x5s3, o, kods)
 return(list(x5_uzVieniniekiem = x5s3_uzVieniniekiem, 
             x5_uzDivniekiem = x5s3_uzDivniekiem, 
             x5_uzCetriniekiem = x5s3_uzCetriniekiem))
