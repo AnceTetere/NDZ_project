61f520531d495296cf0b2db6eac810f4cf810e58
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-03-13 19:12:05 +0200

    Update processingSeven_s4_2467.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p7/processingSeven_s4_2467.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p7/processingSeven_s4_2467.R
@@ -1,16 +1,17 @@
 processingSeven_s4_2467 <- function(a, o, kods) {
   
   a1 <- data.frame(); a2 <- data.frame(); a3 <- data.frame(); a4 <- data.frame(); a5 <- data.frame(); a6 <- data.frame()
-    
+  
+  
   if (all(sapply(c(1,2,3,4,6), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) != 0)) &&
       diff(a$NDZ_sanemsanas_datums[5:6]) == 0) {
-        if (a$period[1] == "______" && a$PS_code[1] == '______' && a$NM_code[1] == '______') {
+        if (a$period[1] == "_____" && a$PS_code[1] == '_____' && a$NM_code[1] == '_____') {
           a1 <- rbind(a1, a[c(1,7),])
           a4 <- rbind(a4, a[c(2,3,4,5), ])
         } else {stop("processingSeven_s4_2467 trūkst izstrādes koda.\n")}
   } else if (all(diff(a$NDZ_sanemsanas_datums) != 0)) {
-        if ((a$period[1] == "______" && a$PS_code[1] == '______' && a$NM_code[1] == '______') ||
-            (a$period[1] == "______" && a$PS_code[1] == '______' && a$NM_code[1] == '______')) {
+        if ((a$period[1] == "_____" && a$PS_code[1] == '_____' && a$NM_code[1] == '_____') ||
+            (a$period[1] == "_____" && a$PS_code[1] == '_____' && a$NM_code[1] == '_____')) {
           a1 <- rbind(a1, a[c(1,7),])
           a4 <- rbind(a4, a[c(2,3,4,5), ])
         } else {stop("processingSeven_s4_2467 trūkst izstrādes koda.\n")}
