9bf365a1bfaef5a71732b86ba6b4ef439010eb29
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-03-13 19:14:51 +0200

    Update processingSeven_s4_1212121.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p7/processingSeven_s4_1212121.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p7/processingSeven_s4_1212121.R
@@ -20,23 +20,23 @@ processingSeven_s4_1212121 <- function(a, o, kods) {
           if (kods %in% c("40", "50", "53") && o == "7") {ZERO_minus(a %>% slice(1))}
   } else if (all(sapply(c(2,4,6), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) &&
              all(sapply(c(1,3,5), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
-                if (a$period[1] == "______" && a$PS_code[1] == "______ && a$NM_code[1] == "______") {
+                if (a$period[1] == "_____" && a$PS_code[1] == "_____" && a$NM_code[1] == "_____") {
                 a6 <- a[c(3,2,5,4,7,6), ]
-               } else if (a$period[1] == "______" && a$NM_code[1] == "______") {
+               } else if (a$period[1] == "_____" && a$NM_code[1] == "_____") {
                             a6 <- a[1:6, ]; a1 <- a[7, ]
                             if (kods %in% c("40", "50", "53") && o == "7") {ZERO_minus(a %>% slice(1))}
               } else {stop("processingSeven_s4_1212121 trūkst izstrādes koda.\n")}
   } else if (all(sapply(seq(1,6,by=2), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) &&
              all(sapply(seq(2,7,by=2), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
-             if ((a$period[1] == "______" && a$PS_code[1] == "______" && a$NM_code[1] == "______") ||
-                 (a$period[1] == "______" && a$PS_code[1] == "______" && a$NM_code[1] == "______")) {
+             if ((a$period[1] == "_____" && a$PS_code[1] == "_____" && a$NM_code[1] == "_____") ||
+                 (a$period[1] == "_____" && a$PS_code[1] == "_____" && a$NM_code[1] == "_____")) {
                a1 <- a[7, ]; a6 <- a[1:6, ]
                if (kods %in% c("40", "50", "53") && o == "7") {ZERO_minus(a %>% slice(1))}
              } else {stop("processingSeven_s4_1212121 trūkst izstrādes koda.\n")}
   } else if (diff(a$NDZ_sanemsanas_datums[5:6]) == 0 &&
              all(sapply(c(1:4,6), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
-            if ((a$period[1] == "______" && a$PS_code[1] == "______" && a$NM_code[1] == "______") ||
-                (a$period[1] == "______" && a$PS_code[1] == "______" && a$NM_code[1] == "______")) {
+            if ((a$period[1] == "_____" && a$PS_code[1] == "_____" && a$NM_code[1] == "_____") ||
+                (a$period[1] == "_____" && a$PS_code[1] == "_____" && a$NM_code[1] == "_____")) {
                   a1 <- a[7, ]; a6 <- a[1:6, ]
                   if (kods %in% c("40", "50", "53") && o == "7") {ZERO_minus(a %>% slice(1))}
             } else {stop("processingSeven_s4_1212121 trūkst izstrādes koda.\n")}
