3a3539d6d5b33f472cae473cdcc5a6c38e4f2af0
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-03-24 19:34:29 +0200

    Create starpkodi10.R

---
+++ NDZ_kodi11/functions/2_otraisCikls_STARPKODI/dubultkodi/starpkodi10/starpkodi10.R
@@ -0,0 +1,105 @@
+starpkodi10 <- function(y, t, prev, v) {
+  
+  yt <- y[v, ]
+  
+  if (t$zinkod[1] %in% c("11", "14", "16", "61")) { 
+   if (t$zinkod[2] %in% c("40", "50", "53", "91")) { 
+    if (t$zinkod[3] %in% c("41", "51", "54", "92")) {
+     if (t$zinkod[4] %in% c("41", "51", "54", "92")) {
+      if (t$zinkod[5] %in% c("40", "50", "53", "91")) {
+       if (t$zinkod[6] %in% c("41", "51", "54", "92")) {
+        if (t$zinkod[7] %in% c("41", "51", "54", "92")) {
+         if (t$zinkod[8] %in% c("40", "50", "53", "91")) {
+          if (t$zinkod[9] %in% c("41", "51", "54", "92")) {
+           if (t$zinkod[10] %in% c("41", "51", "54", "92")) {
+            if (all(diff(t$NDZ_sanemsanas_datums[c(1:7,9:10)]) != 0) && diff(t$NDZ_sanemsanas_datums[8:9]) == 0) {
+               yt$dienas <- sum(sapply(c(1,4,7), function(i) diff(t$NDZ_sanemsanas_datums[i:(i+1)])),
+                                difftime(t$last_date[10], t$NDZ_sanemsanas_datums[10], units = "days") + 1)
+             } else {stop("Starpkodi10 iztrūkst apstrādes koda.")}
+             } else {stop("Starpkodi10 iztrūkst apstrādes koda.")}
+             } else {stop("Starpkodi10 iztrūkst apstrādes koda.")}
+             } else {stop("Starpkodi10 iztrūkst apstrādes koda.")}
+             } else {stop("Starpkodi10 iztrūkst apstrādes koda.")}
+             } else {stop("Starpkodi10 iztrūkst apstrādes koda.")}
+             } else {stop("Starpkodi10 iztrūkst apstrādes koda.")}
+             } else {stop("Starpkodi10 iztrūkst apstrādes koda.")}
+    } else if (t$zinkod[4] %in% c("40", "50", "53", "91")) {
+      if (t$zinkod[5] %in% c("41", "51", "54", "92")) {
+        if (t$zinkod[6] %in% c("40", "50", "53", "91")) {
+          if (t$zinkod[7] %in% c("41", "51", "54", "92")) {
+            if (t$zinkod[8] %in% c("40", "50", "53", "91")) {
+              if (t$zinkod[9] %in% c("41", "51", "54", "92")) {
+                if (t$zinkod[10] %in% c("40", "50", "53", "91")) {
+                  if (all(diff(t$NDZ_sanemsanas_datums) != 0)) {
+                    yt$dienas <- sum(sapply(seq(1,10,by=2), function(i) diff(t$NDZ_sanemsanas_datums[i:(i+1)])))
+                  } else {stop("Starpkodi10 iztrūkst apstrādes koda.")}
+                } else {stop("Starpkodi10 iztrūkst apstrādes koda.")}
+              } else {stop("Starpkodi10 iztrūkst apstrādes koda.")}
+            } else {stop("Starpkodi10 iztrūkst apstrādes koda.")}
+          } else {stop("Starpkodi10 iztrūkst apstrādes koda.")}
+        } else {stop("Starpkodi10 iztrūkst apstrādes koda.")}
+      } else {stop("Starpkodi10 iztrūkst apstrādes koda.")}
+    } else {stop("Starpkodi10 iztrūkst apstrādes koda.")}
+   } else {stop("Starpkodi10 iztrūkst apstrādes koda.")}
+} else if (t$zinkod[1] %in% c("41", "51", "54", "92")) { 
+    if (t$zinkod[2] %in% c("40", "50", "53", "91")) { 
+      if (t$zinkod[3] %in% c("41", "51", "54", "92")) {
+        if (t$zinkod[4] %in% c("40", "50", "53", "91")) {
+          if (t$zinkod[5] %in% c("41", "51", "54", "92")) {
+            if (t$zinkod[6] %in% c("40", "50", "53", "91")) {
+              if (t$zinkod[7] %in% c("41", "51", "54", "92")) {
+                if (t$zinkod[8] %in% c("40", "50", "53", "91")) {
+                  if (t$zinkod[9] %in% c("41", "51", "54", "92")) {
+                    if (t$zinkod[10] %in% c("21", "22", "23", "24", "25", "29")) {
+              yt$dienas <- sum(sapply(seq(1,10,by=2), function(i) diff(t$NDZ_sanemsanas_datums[i:(i+1)]))) + 1
+      } else {stop("Starpkodi10 iztrūkst apstrādes koda.")}
+    } else {stop("Starpkodi10 iztrūkst apstrādes koda.")}
+  } else {stop("Starpkodi10 iztrūkst apstrādes koda.")}
+  } else {stop("Starpkodi10 iztrūkst apstrādes koda.")}
+  } else {stop("Starpkodi10 iztrūkst apstrādes koda.")}
+  } else {stop("Starpkodi10 iztrūkst apstrādes koda.")}
+  } else {stop("Starpkodi10 iztrūkst apstrādes koda.")}
+  } else {stop("Starpkodi10 iztrūkst apstrādes koda.")}
+  } else {stop("Starpkodi10 iztrūkst apstrādes koda.")}
+  } else {stop("Starpkodi10 iztrūkst apstrādes koda.")}
+  
+  #if (t$zinkod[1] == "51" && 
+  #    t$zinkod[2] == "50" && 
+  #    t$zinkod[3] == "51" && 
+  #    t$zinkod[4] == "50" && 
+  #    t$zinkod[5] == "51" && 
+  #    t$zinkod[6] == "50" && 
+  #    t$zinkod[7] == "51" && 
+  #    t$zinkod[8] == "50" && 
+  #    t$zinkod[9] %in% c("21", "22", "23", "24", "25", "29") && 
+  #    t$zinkod[10] == "51" && 
+  #    all(!diff(t$NDZ_sanemsanas_datums[1:9]) == 0) &&
+  #    t$NDZ_sanemsanas_datums[9] == t$NDZ_sanemsanas_datums[10]) {
+  #  yt <- y[v:(v+1), ]
+  #  yt <- yt[yt$zinkod == "50", ]
+  #} else if (t$zinkod[1] == "51" && 
+  #           t$zinkod[2] == "50" && 
+  #           t$zinkod[3] == "51" && 
+  #           t$zinkod[4] == "50" && 
+  #           t$zinkod[5] == "51" && 
+  #           t$zinkod[6] == "50" && 
+  #           t$zinkod[7] == "51" && 
+  #           t$zinkod[8] == "50" && 
+  #           t$zinkod[9] == "51" && 
+  #           t$zinkod[10] == "25" && 
+  #           all(diff(t$NDZ_sanemsanas_datums) != 0)) {
+  #  days1 <- as.numeric(difftime(t$NDZ_sanemsanas_datums[2], t$NDZ_sanemsanas_datums[1], units = "days"))
+  #  days2 <- as.numeric(difftime(t$NDZ_sanemsanas_datums[4], t$NDZ_sanemsanas_datums[3], units = "days"))
+  #  days3 <- as.numeric(difftime(t$NDZ_sanemsanas_datums[6], t$NDZ_sanemsanas_datums[5], units = "days"))
+  #  days4 <- as.numeric(difftime(t$NDZ_sanemsanas_datums[8], t$NDZ_sanemsanas_datums[7], units = "days"))
+  #  days5 <- as.numeric(difftime(t$NDZ_sanemsanas_datums[10], t$NDZ_sanemsanas_datums[9], units = "days")) + 1
+  #  
+  #  yt <- y[v, ]
+  #  yt$dienas <- sum(days1, days2, days3, days4, days5)
+  #  rm(days1, days2, days3, days4, days5)
+  #} else {stop("Starpkodi10 iztrūkst apstrādes koda.")}
+  
+  if(is.na(yt$PS_code[1])) {stop("Dienas NA.")}
+  yt$zinkod <- "combined"  #jo starpkodu dienu sarēķins
+  return(yt)
+}
