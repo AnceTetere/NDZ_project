5a0b5602ec212c53f04ecb93ae9e38212e73cacb
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-04-02 19:00:12 +0300

    Update processingCodes.R

--- NDZ_kodi11/functions/processingCodes.R
+++ NDZ_kodi11/functions/processingCodes.R
@@ -1,16 +1,18 @@
 processingCodes <- function(NDZ, kods) {
+
   result <- occurencesSplit(NDZ)
   tabs_zdn <- result$tabs_zdn
   NDZ_list <- result$NDZ_list
   rm(NDZ, result)
 
-#2 Funkcija tempNDZ() izveido tukšu datni. 
   tempNDZ()
   
-#i <- 5 #testēšanai
+#i <- 1 #testēšanai
 #for(i in 1:4) { #TESTIEM
-    
+  
+  options(warn = 2) #Warnings into errors: Pārliec uz warn = 1, lai atceltu  
 for(i in 1:length(tabs_zdn)) {
+# Ielādē attiecīgo tabulu  
   x <- NDZ_list[[i]]    ### loadTables nekļūst lieka, jo tiek lietota 1-majā skriptā - skati vai to var aizvietot <- Ievēro, ja tu šādi lieto, funkcija load_table() kļūst lieka
 
       if(nchar(tabs_zdn[i]) == 5) {
@@ -25,6 +27,7 @@ for(i in 1:length(tabs_zdn)) {
   
     switch(   
       o,
+      # Caur sekojošo apstrādi sarēķina dienas un noglabā tabulā temp_NDZ, kuru būvē.
       "1"= cat(sendTo_tempNDZ(processingOnes(x, o), o)),
       "2"= processingTwoes(x, o, kods),  
       "3"= processingThrees(x, o, kods),  
@@ -32,7 +35,7 @@ for(i in 1:length(tabs_zdn)) {
       "5"= processingFives(x, o, kods),
       "6"= processingSixes(x, o, kods), 
       "7"= processingSeven(x, o, kods), 
-      "8"= processingEights(x, o, kods), # !! BLOĶĒTS processingEights_s4 testēšanai. Jāatbrīvo, lai ietu: tā nav kļūda.
+      "8"= processingEights(x, o, kods), # !! BLOĶĒTS processingEights_s4 testēšanai. Jāatbīvo lai ietu: tā nav kļūda.
       "9"= processingNines(x, o, kods),
       "10"= processingTens(x, o, kods),
       "11"= processingEleven(x, o, kods), 
@@ -43,7 +46,7 @@ for(i in 1:length(tabs_zdn)) {
       "16"= processingSixteen(x, o, kods),
       "17"= processingSeventeen(x, o, kods),
       "18"= processingEighteen(x, o, kods),
-      "19"= stop("NAV KODA PRIEKŠ NDZ_", o),
+      "19"= processingNineteen(x, o, kods),
       "20"= stop("NAV KODA PRIEKŠ NDZ_", o),
       "21"= stop("NAV KODA PRIEKŠ NDZ_", o),
       "22"= stop("NAV KODA PRIEKŠ NDZ_", o),
@@ -55,3 +58,31 @@ for(i in 1:length(tabs_zdn)) {
   rm(NDZ_list, tabs_zdn, o, i, x)
   return(cat("Kodu kopums", kods, "apstrādāts."))
 }
+
+#calculations
+#kods <- kodu_vektors[1] #FOR TESTING
+
+create_tempZERO()
+iztrukstKodi <- character(0)
+
+for(kods in kodu_vektors) {
+  NDZ <- NDZ_original(kods) #77454
+  
+  if (nrow(NDZ) > 0) {
+    cat(processingCodes(NDZ, kods))
+    cat(month_byCode(kods))
+  } else {
+    cat(paste0(year, ". gada ", month, ". mēnesī nav neviena ieraksta  ziņojuma kodam ", kods, "."), "\n")
+    iztrukstKodi <- append(iztrukstKodi, kods)
+  }
+}
+
+kodu_vektors <- kodu_vektors[!(kodu_vektors %in% iztrukstKodi)]
+rm(iztrukstKodi, NDZ, kods)
+source("R/scripts/4_months.R")
+source("R/scripts/5_output.R")
+
+
+rm(list = mtab_nos, mtab_nos, kodu_vektors, kods, month, year, md, x)
+rm(list = ls())
+#rm(list = ls()[sapply(ls(), function(x) is.function(get(x)))])
