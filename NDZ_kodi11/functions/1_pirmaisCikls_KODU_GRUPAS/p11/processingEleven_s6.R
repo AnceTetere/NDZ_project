15efeeea4fc390d86579981e8707684b33db14d8
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-04-12 21:14:24 +0300

    Update processingEleven_s6.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p11/processingEleven_s6.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p11/processingEleven_s6.R
@@ -1,7 +1,5 @@
 processingEleven_s6 <- function(a, o, kods) {
-  
   a1 <- data.frame(); a2 <- data.frame(); a9 <- data.frame(); a10 <- data.frame()
-  #a <- x11
   
     if (all(a$sak_beidz[c(1,3,5,8,10,11)] == "1")) {
               if (all(sapply(c(1,2,3,4,6,8,10), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) != 0)) &&
@@ -47,13 +45,25 @@ processingEleven_s6 <- function(a, o, kods) {
                          diff(a$NDZ_sanemsanas_datums[7:8]) == 0) {
                        a1 <- a[11, ]; a10 <- a[-11,]
                        if (kods %in% c("40", "50", "53") && o == "11") {ZERO_minus(a %>% slice(1))}
+              } else if (all(sapply(c(2,4:10), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) != 0)) &&
+                         all(sapply(c(1,3), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) == 0))) {
+                        a1 <- a[11, ]; a10 <- a[-11,]
+                        if (kods %in% c("40", "50", "53") && o == "11") {ZERO_minus(a %>% slice(1))}
               } else {stop("processingEleven: Vienpadsmitnieku tabulas pārdalei trūkst izstrādes koda. Rindas: ", r, " līdz ", r + 10, "\n")}
-    } else if (all(a$sak_beidz[c(2,3,5,7,9,11)] == "1") && 
-               all(sapply(c(2,4,5,6,7,8,9,10), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) != 0)) &&
+    } else if (all(a$sak_beidz[c(2,3,5,7,9,11)] == "1")) {
+              if (all(sapply(c(2,4,5,6,7,8,9,10), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) != 0)) &&
                all(sapply(c(1,3), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) == 0))) {
                 a <- a[c(2,1,3,4,5,6,7,8,9,10,11),]
                 a1 <- a[11, ]; a10 <- a[-11,]
                if (kods %in% c("40", "50", "53") && o == "11") {ZERO_minus(a %>% slice(1))}
+              } else if (all(diff(a$NDZ_sanemsanas_datums[2:11]) != 0) &&
+                         diff(a$NDZ_sanemsanas_datums[1:2]) == 0) {
+                          if (a$period[1] == "_____" && a$PS_code[1] == "__________" && a$NM_code[1] == "__________") {
+                            a <- a[c(2,1,3,4,5,6,7,8,9,10,11),]
+                            a1 <- a[11, ]; a10 <- a[-11,]
+                            if (kods %in% c("40", "50", "53") && o == "11") {ZERO_minus(a %>% slice(1))}
+                          } else {stop("processingEleven: Vienpadsmitnieku tabulas pārdalei trūkst izstrādes koda. Rindas: ", r, " līdz ", r + 10, "\n")}
+              } else {stop("processingEleven: Vienpadsmitnieku tabulas pārdalei trūkst izstrādes koda. Rindas: ", r, " līdz ", r + 10, "\n")}
     } else if (all(a$sak_beidz[c(2,3,5,7,10,11)] == "1") && 
                all(sapply(c(2,4,5,6,7,8,10), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) != 0)) &&
                all(sapply(c(1,3,9), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) == 0))) {
@@ -71,8 +81,15 @@ processingEleven_s6 <- function(a, o, kods) {
                   a1 <- a[1, ]; a10 <- a[-1,]
                 } else {stop("processingEleven_s6 trūkst izstrādes koda. \n")}
               } else {stop("processingEleven_s6 trūkst izstrādes koda. \n")}
+    } else if (all(a$sak_beidz[1:4] == c("1", "2", "2", "1"))) {
+              if (all(sapply(c(1,2,4,5,6,8,9,10), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) != 0)) &&
+                  all(sapply(c(3,7), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) == 0))) {
+                    if (a$period[1] == "_____" && a$PS_code[1] == "__________" && a$NM_code[1] == "__________") {
+                    a2 <- a[1:2, ]; a9 <- a[3:11,]
+                } else {stop("processingEleven_s6 trūkst izstrādes koda. \n")}
+              } else {stop("processingEleven_s6 trūkst izstrādes koda. \n")}
     } else {stop("processingEleven_s6 trūkst izstrādes koda. \n")}
-  
+  class(a)
   rm(a, o, kods)
   return(list(x11_uzVieniniekiem = a1,
               x11_2 = a2,
