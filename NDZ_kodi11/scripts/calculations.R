08cbd39b856b2f7b3f67464d8c934163eb19075a
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-04-12 21:08:35 +0300

    Update calculations.R

--- NDZ_kodi11/scripts/calculations.R
+++ NDZ_kodi11/scripts/calculations.R
@@ -1,6 +1,27 @@
+#kods <- kodu_vektors[3] #FOR TESTING
+
+create_tempZERO()
+iztrukstKodi <- character(0)
+
 for(kods in kodu_vektors) {
-  source(paste0(path, "R\\scripts\\1_NDZ_original.R"))
-  source(paste0(path, "R\\scripts\\2_processingCodes.R"))
+  NDZ <- NDZ_original(kods)
+  
+  if (nrow(NDZ) > 0) {
+    cat(processingCodes(NDZ, kods))
+    cat(month_byCode(kods))
+  } else {
+    cat(paste0(year, ". gada ", month, ". mēnesī nav neviena ieraksta  ziņojuma kodam ", kods, "."), "\n")
+    iztrukstKodi <- append(iztrukstKodi, kods)
+  }
 }
 
-source(paste0(path, "R\\scripts\\3_months.R"))
+kodu_vektors <- kodu_vektors[!(kodu_vektors %in% iztrukstKodi)]
+rm(iztrukstKodi, NDZ, kods)
+source("R/scripts/4_months.R")
+source("R/scripts/5_output.R")
+
+
+rm(list = mtab_nos, mtab_nos, kodu_vektors, kods, month, year, md, x)
+rm(list = ls())
+#rm(list = ls()[sapply(ls(), function(x) is.function(get(x)))])
+
