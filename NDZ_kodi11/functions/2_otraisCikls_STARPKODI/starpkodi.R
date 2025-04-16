172647ab791ce80043db30874ff4bfb5d31ef997
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-04-17 00:33:31 +0300

    Update and rename starpkodi.R to starpkodi.R

--- NDZ_kodi11/functions/2_otraisCikls_STARPKODI/starpkodi.R
+++ NDZ_kodi11/functions/2_otraisCikls_STARPKODI/starpkodi.R
@@ -1,20 +1,26 @@
+#FOR TESTS: y_2plus = y2; n = 2; rm(y2)
+
 starpkodi <- function(y_2plus, n) { 
-  y_2plus <- arrange(y_2plus, PS_code, DN_code, NM_code)
+  y_2plus <- y_2plus %>% arrange(PS_code, dnperk, NM_code)
   
   prev <- as.Date(paste0(substr(y_2plus$period[1], 1, 4), "-", substr(y_2plus$period[1], 5, 6), "-01")) - 1
   z <- data.frame()
   
+  #options(warn = 1)
   for(v in seq(1, nrow(y_2plus), by = n)) {
+
     t <- subTable(y_2plus, n, v)
+
     if(!(all(t$PS_code == t$PS_code[1] & t$NM_code == t$NM_code[1]))) {
-      stop()}
+      stop("Pseidokodu nesakritība! Tabula: y_2plus; rinda:", v)}
 
     z <- switch(
       as.character(n),
       "2" = dubultkodi(y_2plus, t, prev, v, z),
       "3" = tripletkodi(y_2plus, t, prev, v, z),
-      default = stop()
+      default = stop("Starpkodi neatpazīst atšķirīgo kodu tabulu y_2plus.")
     )
+    print(v)
   }
   rm(prev, t, y_2plus)
   return(z) 
