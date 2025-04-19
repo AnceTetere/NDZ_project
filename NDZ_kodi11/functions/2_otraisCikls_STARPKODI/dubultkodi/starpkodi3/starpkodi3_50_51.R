7f4dc3e08f664a0c7802462421c0894b22df2a9c
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-04-19 17:13:09 +0300

    Update starpkodi3_50_51.R

--- NDZ_kodi11/functions/2_otraisCikls_STARPKODI/dubultkodi/starpkodi3/starpkodi3_50_51.R
+++ NDZ_kodi11/functions/2_otraisCikls_STARPKODI/dubultkodi/starpkodi3/starpkodi3_50_51.R
@@ -1,19 +1,18 @@
-starpkodi3_50_51 <- function(y, t, prev, v) {
+ starpkodi3_50_51 <- function(y, t, prev, v) {
 
   yt <- y[v, ]
   
     if (t$zinkod[3] %in% c("21", "22", "23", "24", "25", "29")) {
                if (all(diff(t$NDZ_sanemsanas_datums) != 0)) {
-                 yt$dienas <- as.numeric(sum(difftime(t$NDZ_sanemsanas_datums[1], prev, units = "days") - 1,  
+                 yt$dienas <- as.numeric(sum(difftime(t$NDZ_sanemsanas_datums[1], prev, units = "days") - 1, 
                                              diff(t$NDZ_sanemsanas_datums[2:3])))
-                 
                } else if (diff(t$NDZ_sanemsanas_datums[1:2]) == 0 && diff(t$NDZ_sanemsanas_datums[2:3]) != 0) {
-                      if (t$period[1] == '_____' && t$PS_code[1] %in% c('_____________', 
-                                                                            '_____________', 
-                                                                            '_____________',
-                                                                            '_____________',
-                                                                            '_____________',
-                                                                            '_____________') && 
+                      if (t$period[1] == '_____' && t$PS_code[1] %in% c('__________', 
+                                                                            '__________', 
+                                                                            '__________',
+                                                                            '__________',
+                                                                            '__________',
+                                                                            '__________') && 
                           t$NM_code[1] %in% c('__________', 
                                              '__________', 
                                              '__________',
@@ -61,17 +60,16 @@ starpkodi3_50_51 <- function(y, t, prev, v) {
                                         as.numeric(difftime(t$last_date[3], t$NDZ_sanemsanas_datums[3], units = "days")) + 1)
                } else {stop("starpkodi3_50_51: Iztrūkst apstrādes koda.")}
              } else if (diff(t$NDZ_sanemsanas_datums[2:3]) == 0 && diff(t$NDZ_sanemsanas_datums[1:2]) != 0) {
-               
-             } else {stop("starpkodi3_50_51: Iztrūkst apstrādes koda.")}     
-  } else {stop("starpkodi3_50_51: Iztrūkst apstrādes koda.")}
+               if (t$period[1] == "_____" && t$PS_code[1] == "__________" && t$NM_code[1] == "__________"){
+                 yt$dienas <- sum(as.numeric(difftime(t$NDZ_sanemsanas_datums[1], prev, units = "days")),
+                                  as.numeric(difftime(t$last_date[3], t$NDZ_sanemsanas_datums[3], units = "days")))                 
+               } else {stop("starpkodi3_50_51: Iztrūkst apstrādes koda.")}
+              } else {stop("starpkodi3_50_51: Iztrūkst apstrādes koda.")}     
+            } else {stop("starpkodi3_50_51: Iztrūkst apstrādes koda.")}     
   
 rm(y, t, prev, v)
 return(yt) 
 }
-
-
-
-
 
 
 #} else 
