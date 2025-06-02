39d9879b429454dbf18d963cc5bb622e8c91fdeb
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-06-02 19:42:19 +0300

    Update and rename NDZ_kodi11/functions/2_otraisCikls_STARPKODI/tripletkodi/tripletkodi4_50.R to NDZ_kodi11/functions/2_otraisCikls_STARPKODI/tripletkodi/tripletkodi4/tripletkodi4_50.R

--- NDZ_kodi11/functions/2_otraisCikls_STARPKODI/tripletkodi/tripletkodi4/tripletkodi4_50.R
+++ NDZ_kodi11/functions/2_otraisCikls_STARPKODI/tripletkodi/tripletkodi4/tripletkodi4_50.R
@@ -8,7 +8,7 @@ tripletkodi4_50 <- function(y3, t, prev, v) {
           if (diff(t$NDZ_sanemsanas_datums[1:2]) == 0 && all(diff(t$NDZ_sanemsanas_datums[2:4]) != 0)) {
             yt$dienas <- as.numeric(difftime(t$NDZ_sanemsanas_datums[1], prev, units = "days")) - 1
           } else if (all(sapply(c(1,3), function(i) diff(t$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) && diff(t$NDZ_sanemsanas_datums[2:3]) != 0) {
-		  yt$dienas <- as.numeric(difftime(t$NDZ_sanemsanas_datums[1], prev, units = "days")) - 1
+            yt$dienas <- as.numeric(difftime(t$NDZ_sanemsanas_datums[1], prev, units = "days")) - 1
           } else {stop("Tripletkodi4 iztrūkst apstrādes koda.")}
         } else {stop("Tripletkodi4 iztrūkst apstrādes koda.")}
       } else if (t$zinkod[3] %in% c("41", "51", "54", "92")) {
@@ -23,7 +23,7 @@ tripletkodi4_50 <- function(y3, t, prev, v) {
       if (t$zinkod[3] %in% c("21", "22", "23", "24", "25", "29")) {
         if (t$zinkod[4] %in% c("41", "51", "54", "92")) {
           if (all(sapply(c(1,3), function(i) diff(t$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) && diff(t$NDZ_sanemsanas_datums[2:3]) != 0) {
-            if (t$PS_code[1] == '_________' && t$NM_code[1] == '_________') {
+            if (t$PS_code[1] == "___________"   && t$NM_code[1] == "___________" ) {
               yt$dienas <- sum(as.numeric(difftime(t$NDZ_sanemsanas_datums[1], prev, units = "days")) - 1,
                                as.numeric(diff(t$NDZ_sanemsanas_datums[2:3])))
             } else {stop("Tripletkodi4 iztrūkst apstrādes koda.")}
@@ -38,7 +38,9 @@ tripletkodi4_50 <- function(y3, t, prev, v) {
       } else if (t$zinkod[3] %in% c("40", "50", "53", "91")) {
         if (t$zinkod[4] %in% c("21", "22", "23", "24", "25", "29")) {
           if (all(diff(t$NDZ_sanemsanas_datums) != 0)) {
-            if (t$period[1] == '_____' && t$PS_code[1] =='__________' && t$NM_code[1] == '__________') {
+            if ((t$period[1] == "______"  && t$PS_code[1] == "___________"  && t$NM_code[1] == "___________" ) ||
+                (t$period[1] == "______"  && t$PS_code[1] == "___________"  && t$NM_code[1] == "___________" ) ||
+                (t$period[1] == "______"  && t$PS_code[1] == "___________"  && t$NM_code[1] == "___________" )) {
               yt$dienas <- sum(as.numeric(difftime(t$NDZ_sanemsanas_datums[1], prev, units = "days")) - 1,
                                as.numeric(diff(t$NDZ_sanemsanas_datums[2:3])))
             } else {stop("Tripletkodi4 iztrūkst apstrādes koda.")}
