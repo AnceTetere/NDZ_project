714cb7c4ca18e8967373c3e5046653a816c54f6a
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-06-03 15:03:30 +0300

    Update adj50.R

--- NDZ_kodi11/functions/paligfunkcijas/NDZ/adj50.R
+++ NDZ_kodi11/functions/paligfunkcijas/NDZ/adj50.R
@@ -1,127 +1,138 @@
 adj50 <- function(NDZ) {
     
- if (isTRUE(NDZ$period[1] == "______")) {
+ if (isTRUE(NDZ$period[1] == '______')) {
+    
+ NDZ <- NDZ %>% 
+      dplyr::filter(!(PS_code %in% c"___________", "___________", "___________", "___________" & NM_code ==  '______________' & zinkod == '51'))
     
     NDZ <- NDZ %>% 
-      dplyr::filter(!(PS_code %in% c("___________", "___________", "___________", "___________") & NM_code == "______________" & zinkod == "51"))
+      dplyr::filter(!(PS_code %in% c"___________", "___________" & NM_code == c"___________", "___________" & zinkod == '41'))
+    NDZ <- NDZ %>% 
+      dplyr::filter(!(PS_code ==  '______________' & NM_code ==  '______________' & zinkod == '25'))
     
     NDZ <- NDZ %>% 
-      dplyr::filter(!(PS_code %in% c("___________", "___________") & NM_code == c("___________", "___________1") & zinkod == "41"))
-    NDZ <- NDZ %>% 
-      dplyr::filter(!(PS_code == "______________" & NM_code == "______________" & zinkod == "25"))
-    
-    NDZ <- NDZ %>% 
-      dplyr::filter(!(PS_code == "______________" & NM_code == "______________" & zinkod == "92"))  
+      dplyr::filter(!(PS_code ==  '______________' & NM_code ==  '______________' & zinkod == '92'))  
 
- } else if (isTRUE(NDZ$period[1] == "______")) {
+ } else if (isTRUE(NDZ$period[1] == '______')) {
    NDZ <- NDZ %>% 
-     dplyr::filter(PS_code == "____________" & NM_code == "______________" & zinkod == "11") %>% 
-     mutate(zinkod = "50")
+     dplyr::filter(PS_code ==  '______________' & NM_code ==  '______________' & zinkod == '11') %>% 
+     mutate(zinkod = '50')
 
- } else if (isTRUE(NDZ$period[1] == "______" & kods == "11")) {
+ } else if (isTRUE(NDZ$period[1] == '______' & kods == "11")) {
    NDZ <- NDZ %>% 
-     dplyr::filter(!(PS_code == "______________" & NM_code == "______________" & NDZ_sanemsanas_datums == "______________"))
+     dplyr::filter(!(PS_code ==  '______________' & NM_code ==  '______________' & NDZ_sanemsanas_datums == '___________'))
 
- } else if (isTRUE(NDZ$period[1] == "______" & kods == "11")) {
+ } else if (isTRUE(NDZ$period[1] == '______' & kods == "11")) {
    NDZ <- NDZ %>% 
-     dplyr::filter(!(PS_code == "______________" & NM_code == "______________" & NDZ_sanemsanas_datums == "______________"))
+     dplyr::filter(!(PS_code ==  '______________' & NM_code ==  '______________' & NDZ_sanemsanas_datums == '___________'))
    
- } else if (isTRUE(NDZ$period[1] == "______") && kods == "11") {
+ } else if (isTRUE(NDZ$period[1] == '______') && kods == "11") {
    NDZ <- NDZ %>% 
-     dplyr::filter(PS_code == "______________" & NM_code == "______________" & zinkod == "29") %>% 
-     mutate(zinkod = "40")
+     dplyr::filter(PS_code ==  '______________' & NM_code ==  '______________' & zinkod == '29') %>% 
+     mutate(zinkod = '40')
    
-  } else if (isTRUE(NDZ$period[1] == "______" & kods == "50")) {
+  } else if (isTRUE(NDZ$period[1] == '______' & kods == "50")) {
     NDZ <- NDZ %>% 
-      dplyr::filter(!(PS_code == "______________" & NM_code == "______________" & zinkod == "51"))
+      dplyr::filter(!(PS_code ==  '______________' & NM_code ==  '______________' & zinkod == '51'))
     
-  } else if (isTRUE(NDZ$period[1] == "______" & kods == "11")) {
+  } else if (isTRUE(NDZ$period[1] == '______' & kods == "11")) {
     NDZ <- NDZ %>% 
-      dplyr::filter(!(PS_code == "______________" & NM_code == "______________" & zinkod == "11" & NDZ_sanemsanas_datums == "______________"))
+      dplyr::filter(!(PS_code ==  '______________' & NM_code ==  '______________' & zinkod == '11' & NDZ_sanemsanas_datums == "______________"))
 
-  } else if (isTRUE(NDZ$period[1] == "______" & kods == "11")) {
+  } else if (isTRUE(NDZ$period[1] == '______' & kods == "11")) {
     NDZ <- NDZ %>% 
-      dplyr::filter(!(PS_code == "______________" & NM_code == "______________" & zinkod == "11" & NDZ_sanemsanas_datums == "______________"))
+      dplyr::filter(!(PS_code ==  '______________' & NM_code ==  '______________' & zinkod == '11' & NDZ_sanemsanas_datums == "______________"))
     
+#SEKOJOŠĀS RINDAS IZŅĒMU
+#  } else if (isTRUE(NDZ$period[1] == '______' & NDZ$zinkod[1] == "40")) {
     
-    } else if (isTRUE(NDZ$period[1] == "______" & kods == "40")) {
-      NDZ <- NDZ %>% 
-        dplyr::filter(!(PS_code == "______________"& NM_code == "______________" & zinkod == "91" & NDZ_sanemsanas_datums == "______________"))
-      
-      NDZ <- NDZ %>% 
-        dplyr::filter(!(PS_code == "______________"& NM_code == "______________" & zinkod == "40" & NDZ_sanemsanas_datums == "______________"))
-      
-      NDZ <- NDZ %>% 
-        dplyr::filter(!(PS_code == "______________"& NM_code == "______________" & zinkod == "91" & NDZ_sanemsanas_datums == "______________"))
-    
-    } else if (isTRUE(NDZ$period[1] == "______") && kods == "11") {
-      NDZ <- NDZ %>% 
-        dplyr::filter(!(PS_code == "______________"& NM_code == "______________" & zinkod == "11" & NDZ_sanemsanas_datums == "______________"))
-      
-      NDZ <- NDZ %>% 
-        dplyr::filter(!(PS_code == "______________"& NM_code == "______________" & zinkod == "11" & NDZ_sanemsanas_datums == "______________"))
-      
-      NDZ <- NDZ %>% 
-        dplyr::filter(!(PS_code == "______________"& NM_code == "______________" & zinkod == "25" & NDZ_sanemsanas_datums == "______________"))
    
-    } else if (isTRUE(NDZ$period[1] == "______" & kods == "11")) {
+    #NDZ <- NDZ %>% 
+     # dplyr::filter(!(PS_code ==  '______________' & NM_code ==  '______________')) ]
+
+    
+    } else if (isTRUE(NDZ$period[1] == '______' & kods == "40")) {
+      NDZ <- NDZ %>% 
+        dplyr::filter(!(PS_code ==  '______________'& NM_code ==  '______________' & zinkod == '91' & NDZ_sanemsanas_datums == "______________"))
+      
+      NDZ <- NDZ %>% 
+        dplyr::filter(!(PS_code ==  '______________'& NM_code ==  '______________' & zinkod == '40' & NDZ_sanemsanas_datums == "______________"))
+      
+      NDZ <- NDZ %>% 
+        dplyr::filter(!(PS_code ==  '______________'& NM_code ==  '______________' & zinkod == '91' & NDZ_sanemsanas_datums == "______________"))
+    
+    } else if (isTRUE(NDZ$period[1] == '______') && kods == "11") {
+      NDZ <- NDZ %>% 
+        dplyr::filter(!(PS_code ==  '______________'& NM_code ==  '______________' & zinkod == '11' & NDZ_sanemsanas_datums == "______________"))
+      
+      NDZ <- NDZ %>% 
+        dplyr::filter(!(PS_code ==  '______________'& NM_code ==  '______________' & zinkod == '11' & NDZ_sanemsanas_datums == "______________"))
+      
+      NDZ <- NDZ %>% 
+        dplyr::filter(!(PS_code ==  '______________'& NM_code ==  '______________' & zinkod == '25' & NDZ_sanemsanas_datums == "______________"))
+   
+    } else if (isTRUE(NDZ$period[1] == '______' & kods == "11")) {
       NDZ <- NDZ %>%
         dplyr::filter(!(
-          (PS_code == "______________" & NM_code == "______________" & zinkod == "11" & NDZ_sanemsanas_datums == "______________") |
-            (PS_code == "______________" & NM_code == "______________" & zinkod == "11" & NDZ_sanemsanas_datums == "______________") |
-            (PS_code == "______________" & NM_code == "______________" & zinkod == "11" & NDZ_sanemsanas_datums == "______________") |
-            (PS_code == "______________" & NM_code == "______________" & zinkod == "11" & NDZ_sanemsanas_datums == "______________")
+          (PS_code ==  '______________' & NM_code ==  '______________' & zinkod == '11' & NDZ_sanemsanas_datums == "______________") |
+            (PS_code ==  '______________' & NM_code ==  '______________' & zinkod == '11' & NDZ_sanemsanas_datums == "______________") |
+            (PS_code ==  '______________' & NM_code ==  '______________' & zinkod == '11' & NDZ_sanemsanas_datums == "______________") |
+            (PS_code ==  '______________' & NM_code ==  '______________' & zinkod == '11' & NDZ_sanemsanas_datums == "______________")
         ))
       
-    } else if (isTRUE(NDZ$period[1] == "______" & kods == "40")) {
+    } else if (isTRUE(NDZ$period[1] == '______' & kods == "40")) {
       NDZ <- NDZ %>% 
-        dplyr::filter(!(PS_code == "______________"& NM_code == "______________" & zinkod == "92" & NDZ_sanemsanas_datums == "______________"))
+        dplyr::filter(!(PS_code ==  '______________'& NM_code ==  '______________' & zinkod == '92' & NDZ_sanemsanas_datums == "______________"))
 
-    } else if (isTRUE(NDZ$period[1] == "______" & kods == "50")) {
+    } else if (isTRUE(NDZ$period[1] == '______' & kods == "50")) {
       NDZ <- NDZ %>% 
-        dplyr::filter(!(PS_code == "______________" && NM_code == "______________" & zinkod == "51" & NDZ_sanemsanas_datums == "______________"))
+        dplyr::filter(!(PS_code ==  '______________' && NM_code ==  '______________' & zinkod == '51' & NDZ_sanemsanas_datums == "______________"))
 
-    } else if (isTRUE(NDZ$period[1] == "______" & kods == "11")) {
+    } else if (isTRUE(NDZ$period[1] == '______' & kods == "11")) {
       NDZ <- NDZ %>% 
-        dplyr::filter(!(PS_code == "______________"& NM_code == "______________" & zinkod == "11" & NDZ_sanemsanas_datums == "______________"))
+        dplyr::filter(!(
+          (PS_code ==  '______________'& NM_code ==  '______________' & zinkod == '11' & NDZ_sanemsanas_datums == "______________") |
+          (PS_code ==  '______________'& NM_code ==  '______________' & zinkod == '11' & NDZ_sanemsanas_datums == "______________")
+        ))
       
-    } else if (isTRUE(NDZ$period[1] == "______" & kods == "40")) {
+    } else if (isTRUE(NDZ$period[1] == '______' & kods == "40")) {
       NDZ <- NDZ %>% 
-        dplyr::filter(!(PS_code == "______________"& NM_code == "______________" & zinkod == "91" & NDZ_sanemsanas_datums == "______________"))
+        dplyr::filter(!(PS_code ==  '______________'& NM_code ==  '______________' & zinkod == '91' & NDZ_sanemsanas_datums == "______________"))
       NDZ <- NDZ %>% 
-        dplyr::filter(!(PS_code == "______________"& NM_code == "______________" & zinkod == "91" & NDZ_sanemsanas_datums == "______________"))
+        dplyr::filter(!(PS_code ==  '______________'& NM_code ==  '______________' & zinkod == '91' & NDZ_sanemsanas_datums == "______________"))
       NDZ <- NDZ %>% 
-        dplyr::filter(!(PS_code == "______________"& NM_code == "______________" & zinkod == "92" & NDZ_sanemsanas_datums == "______________"))
+        dplyr::filter(!(PS_code ==  '______________'& NM_code ==  '______________' & zinkod == '92' & NDZ_sanemsanas_datums == "______________"))
       NDZ <- NDZ %>% 
-        dplyr::filter(!(PS_code == "______________"& NM_code == "______________" & zinkod == "92" & NDZ_sanemsanas_datums == "______________"))
+        dplyr::filter(!(PS_code ==  '______________'& NM_code ==  '______________' & zinkod == '92' & NDZ_sanemsanas_datums == "______________"))
       
-    } else if (isTRUE(NDZ$period[1] == "______" & kods == "50")) {
+    } else if (isTRUE(NDZ$period[1] == '______' & kods == "50")) {
       NDZ <- NDZ %>% 
-        dplyr::filter(!(PS_code == "______________"& NM_code == "______________" & zinkod == "51" & NDZ_sanemsanas_datums == "______________"))
+        dplyr::filter(!(PS_code ==  '______________'& NM_code ==  '______________' & zinkod == '51' & NDZ_sanemsanas_datums == "______________"))
       
       NDZ <- NDZ %>% 
-        dplyr::filter(!(PS_code == "______________"& NM_code == "______________" & zinkod == "50" & NDZ_sanemsanas_datums == "______________"))
+        dplyr::filter(!(PS_code ==  '______________'& NM_code ==  '______________' & zinkod == '50' & NDZ_sanemsanas_datums == "______________"))
 
       NDZ <- NDZ %>% 
-        dplyr::filter(!(PS_code == "______________"& NM_code == "______________" & zinkod == "51" & NDZ_sanemsanas_datums == "______________"))
+        dplyr::filter(!(PS_code ==  '______________'& NM_code ==  '______________' & zinkod == '51' & NDZ_sanemsanas_datums == "______________"))
 
       NDZ <- NDZ %>% 
-        dplyr::filter(!(PS_code == "______________"& NM_code == "______________" & zinkod == "51" & NDZ_sanemsanas_datums == "______________"))
+        dplyr::filter(!(PS_code ==  '______________'& NM_code ==  '______________' & zinkod == '51' & NDZ_sanemsanas_datums == "______________"))
       
-    } else if (isTRUE(NDZ$period[1] == "______" & kods == "53")) {
+    } else if (isTRUE(NDZ$period[1] == '______' & kods == "53")) {
       NDZ <- NDZ %>% 
-        dplyr::filter(!(PS_code == "______________"& NM_code == "______________" & zinkod == "53" & NDZ_sanemsanas_datums == "______________"))
+        dplyr::filter(!(PS_code ==  '______________'& NM_code ==  '______________' & zinkod == '53' & NDZ_sanemsanas_datums == "______________"))
   
-    } else if (isTRUE(NDZ$period[1] == "______") && kods == "50") {
-            
+    } else if (isTRUE(NDZ$period[1] == '______') && kods == "50") {
+      
+      
       NDZ <- NDZ %>% 
         mutate(
           NDZ_sanemsanas_datums = if_else(
-            PS_code == "______________" &
-              NM_code == "______________" &
-              zinkod == "50" &
-              NDZ_sanemsanas_datums == as.Date("______________"),
-            as.Date("______________"),
+            PS_code ==  '______________' &
+              NM_code ==  '______________' &
+              zinkod == '50' &
+              NDZ_sanemsanas_datums == as.Date'___________',
+            as.Date'___________',
             NDZ_sanemsanas_datums
           )
         )
@@ -130,72 +141,72 @@ adj50 <- function(NDZ) {
       NDZ <- NDZ %>% 
         mutate(
           zinkod = ifelse(
-            PS_code == "______________" &
-              NM_code == "______________" &
-              zinkod == "51" &
-              NDZ_sanemsanas_datums == as.Date("______________"),
-            "50",
+            PS_code ==  '______________' &
+              NM_code ==  '______________' &
+              zinkod == '51' &
+              NDZ_sanemsanas_datums == as.Date'___________',
+            '50',
             zinkod
           ),
           sak_beidz = ifelse(
-            PS_code == "______________" &
-              NM_code == "______________" &
-              zinkod == "50" &
-              NDZ_sanemsanas_datums == as.Date("______________"),
-            "2",
+            PS_code ==  '______________' &
+              NM_code ==  '______________' &
+              zinkod == '50' &
+              NDZ_sanemsanas_datums == as.Date'___________',
+            '2',
             sak_beidz
           )
         )
       
       NDZ <- NDZ %>% 
-        dplyr::filter(!(PS_code == "______________"& NM_code == "______________" & zinkod == "51" & NDZ_sanemsanas_datums == "______________"))
+        dplyr::filter(!(PS_code ==  '______________'& NM_code ==  '______________' & zinkod == '51' & NDZ_sanemsanas_datums == "______________"))
 
-    } else if (isTRUE(NDZ$period[1] == "______" & kods == "53")) {
+    } else if (isTRUE(NDZ$period[1] == '______' & kods == "53")) {
       NDZ <- NDZ %>% 
-        dplyr::filter(!(PS_code == "______________"& NM_code == "______________" & zinkod == "53" & NDZ_sanemsanas_datums == "______________"))
+        dplyr::filter(!(PS_code ==  '______________'& NM_code ==  '______________' & zinkod == '53' & NDZ_sanemsanas_datums == "______________"))
       
-    } else if (isTRUE(NDZ$period[1] == "______" & kods == "50")) {
+    } else if (isTRUE(NDZ$period[1] == '______' & kods == "50")) {
       NDZ <- NDZ %>% 
-        dplyr::filter(!(PS_code == "______________"& NM_code == "______________" & zinkod == "51" & NDZ_sanemsanas_datums == "______________"))
+        dplyr::filter(!(PS_code ==  '______________'& NM_code ==  '______________' & zinkod == '51' & NDZ_sanemsanas_datums == "______________"))
       
-    } else if (isTRUE(NDZ$period[1] == "______" & kods == "40")) {
+    } else if (isTRUE(NDZ$period[1] == '______' & kods == "40")) {
       NDZ <- NDZ %>%
         mutate(
           zinkod = ifelse(
-            PS_code == "______________" &
-            NM_code == "______________" &
-            zinkod == "51" &
-            NDZ_sanemsanas_datums == as.Date("______________"), 
-          "41", 
+            PS_code ==  '______________' &
+            NM_code ==  '______________' &
+            zinkod == '51' &
+            NDZ_sanemsanas_datums == as.Date'___________', 
+          '41', 
           zinkod))
 
-    } else if (isTRUE(NDZ$period[1] == "______" & kods == "11")) {
+    } else if (isTRUE(NDZ$period[1] == '______' & kods == "11")) {
       
       NDZ <- NDZ %>% 
-        dplyr::filter(!(PS_code == "______________" & NM_code == "______________" & zinkod == "25" & NDZ_sanemsanas_datums == "______________"))
+        dplyr::filter(!(PS_code == "___________" & NM_code ==  '______________' & zinkod == "25" & NDZ_sanemsanas_datums == "______________"))
       
-    } else if (isTRUE(NDZ$period[1] == "______") && kods == "11") {
+    } else if (isTRUE(NDZ$period[1] == '______') && kods == "11") {
       
       NDZ <- NDZ %>% 
-        dplyr::filter(!(PS_code == "______________"& NM_code == "______________" & zinkod == "11" & NDZ_sanemsanas_datums == "______________"))
+        dplyr::filter(!(PS_code ==  '______________'& NM_code ==  '______________' & zinkod == '11' & NDZ_sanemsanas_datums == "______________"))
 
-    } else if (isTRUE(NDZ$period[1] == "______") && kods == "50") {
+    } else if (isTRUE(NDZ$period[1] == '______') && kods == "50") {
       NDZ <- NDZ %>%
         mutate(
           zinkod = ifelse(
-            PS_code == "______________" &
-              NM_code == "______________" &
-              zinkod == "50" &
-              NDZ_sanemsanas_datums == "______________",
-            "51",
+            PS_code ==  '______________' &
+              NM_code ==  '______________' &
+              zinkod == '50' &
+              NDZ_sanemsanas_datums == '___________',
+            '51',
             zinkod
           ),
           sak_beidz = ifelse(
-            PS_code == "______________" &
-              NM_code == "______________" &
-              zinkod == "51" &
-              NDZ_sanemsanas_datums == as.Date("______________"),
-            "1",
+            PS_code ==  '______________' &
+              NM_code ==  '______________' &
+              zinkod == '51' &
+              NDZ_sanemsanas_datums == as.Date'___________',
+            '1',
             sak_beidz
           )
         )
@@ -203,9 +214,10 @@ adj50 <- function(NDZ) {
 
       
       
-    } else if (isTRUE(NDZ$period[1] == "______" & NDZ$zinkod[1] == "40")) {
+    } else if (isTRUE(NDZ$period[1] == '______' & NDZ$zinkod[1] == "40")) {
+
     NDZ <- NDZ %>% 
-      dplyr::filter(!(PS_code == "______________" & NM_code == "______________" & zinkod == "25"))
+      dplyr::filter(!(PS_code ==  '______________' & NM_code ==  '______________' & zinkod == '25'))
   }
   return(NDZ) 
 }
