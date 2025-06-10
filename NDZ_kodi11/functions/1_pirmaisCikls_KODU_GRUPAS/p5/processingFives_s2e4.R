0aef1de747c5d9e1384903f3a7b02e05b1f73654
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-06-10 15:20:04 +0300

    Update processingFives_s2e4.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p5/processingFives_s2e4.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p5/processingFives_s2e4.R
@@ -1,4 +1,4 @@
-processingFives_s2e4 <- function(a, kods) {  
+processingFives_s2e4 <- function(a, o, kods) {  
   a <- a %>% arrange(PS_code, DN_code, NM_code, NDZ_sanemsanas_datums)
   a1 <- data.frame(); a2 <- data.frame(); a4 <- data.frame()
   
@@ -6,9 +6,8 @@ processingFives_s2e4 <- function(a, kods) {
     a4 <- rbind(a4, a[c(1,3:5), ])
     if (kods %in% c("40", "50", "53")) {ZERO_plus(a %>% slice(5))}
   } else if (all(a$sak_beidz[c(2,4)] == "1")) {
-    a1 <- rbind(a1, a[1, ])
-    a4 <- rbind(a4, a[2:5, ])
-    if (kods %in% c("40", "50", "53")) {ZERO_plus(a %>% slice(5))}
+    a1 <- a[1, ]; a4 <- a[2:5, ]
+    if (kods %in% c("40", "50", "53") && o == "5") {ZERO_plus(a %>% slice(5))}
   } else {stop("processingFives_s2: Trūkst apstrādes koda.")}
   
   rm(a, kods)
