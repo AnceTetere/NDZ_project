a3a6e0222955b7114c2467d7103017186c97d5d3
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-08-07 18:53:02 +0300

    Update processingTens_s7.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p10/processingTens_s7.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p10/processingTens_s7.R
@@ -2,7 +2,8 @@ processingTens_s7 <- function(x10s7, o, kods) {
   
   x10s7 <- arrange(x10s7, PS_code, DN_code, NM_code, NDZ_sanemsanas_datums)
   #x10s7 <- x10
-  x10s7_uzVieniniekiem <- data.frame(); x10s7_uzDivniekiem <- data.frame(); x10s7_uzSeptini <- data.frame(); x10s7_uzAstoniekiem <- data.frame()
+  x10s7_uzVieniniekiem <- data.frame(); x10s7_uzDivniekiem <- data.frame(); x10s7_uzPieci <- data.frame(); x10s7_uzSesi <- data.frame(); 
+  x10s7_uzSeptini <- data.frame(); x10s7_uzAstoniekiem <- data.frame()
   
   if (all(x10s7$sak_beidz[1:5] == c("1", "2", "1", "1", "2"))) {
     if (all(diff(x10s7$NDZ_sanemsanas_datums[1:5]) != 0)) {
@@ -14,6 +15,8 @@ processingTens_s7 <- function(x10s7, o, kods) {
   rm(x10s7, o, kods)
   return(list(x10_uzVieniniekiem = x10s7_uzVieniniekiem,
               x10_uzDivniekiem = x10s7_uzDivniekiem,
+              x10_uzPieci = x10s7_uzPieci,
+              x10_uzSesi = x10s7_uzSesi,
               x10_uzSeptini = x10s7_uzSeptini,
               x10_uzAstoniekiem = x10s7_uzAstoniekiem))
 }
