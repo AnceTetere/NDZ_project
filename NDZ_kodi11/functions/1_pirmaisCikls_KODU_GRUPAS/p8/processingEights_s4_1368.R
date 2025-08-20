504117b3740556e590d371383f696f8f3a5ba022
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-08-20 18:09:09 +0300

    Update processingEights_s4_1368.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p8/processingEights_s4_1368.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p8/processingEights_s4_1368.R
@@ -1,4 +1,4 @@
- processingEights_s4_1368 <- function(a, o, kods) {
+processingEights_s4_1368 <- function(a, o, kods) {
 
   #a <- x8s4
   a1 <- data.frame(); a2 <- data.frame(); a4 <- data.frame(); a5 <- data.frame(); a6 <- data.frame(); a7 <- data.frame()
@@ -11,14 +11,12 @@
               } else {stop("processingEights_s4_1368: Trūkst izstrādes koda.")}
     } else if (diff(a$NDZ_sanemsanas_datums[3:4]) == 0 &&
                all(sapply(c(1,2,4,5,6,7), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
-                
                 if (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') {
                  a1 <- a[8, ]; a2 <- a[c(1,2,3,5,6,7), ]
                  if (kods %in% c("40", "50", "53") && o == "8") {ZERO_plus(a %>% slice(5))} 
                 } else {stop("processingEights_s4_1368: Trūkst izstrādes koda.")}
     } else if (all(sapply(c(3,5,7), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) &&
                all(sapply(c(1,2,4,6), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
-                
                  if ((a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
                      (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
                      (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________')) {
@@ -27,7 +25,6 @@
                 } else {stop("processingEights_s4_1368: Trūkst izstrādes koda.")}
     } else if (all(sapply(c(5,7), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) &&
                all(sapply(c(1,2,3,4,6), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
-              
               if ((a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
                   (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
                   (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
