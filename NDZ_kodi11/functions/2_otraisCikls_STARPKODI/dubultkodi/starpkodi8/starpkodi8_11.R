adc2c8b04eb02164737b081a6ad3c8b826585727
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-08-21 17:33:58 +0300

    Update starpkodi8_11.R

--- NDZ_kodi11/functions/2_otraisCikls_STARPKODI/dubultkodi/starpkodi8/starpkodi8_11.R
+++ NDZ_kodi11/functions/2_otraisCikls_STARPKODI/dubultkodi/starpkodi8/starpkodi8_11.R
@@ -10,8 +10,8 @@ starpkodi8_11 <- function(y, t, prev, v) {
             if (t$zinkod[7] %in% c("21", "22", "23", "24", "25", "29")) {
               if (t$zinkod[8] %in% c("41", "51", "54", "92")) {
                 if (diff(t$NDZ_sanemsanas_datums[7:8]) == 0 && all(diff(t$NDZ_sanemsanas_datums[1:7]) != 0)) {
-                  if ((NDZ$period[1] == '______' && t$PS_code[1] ==  '______________' && t$NM_code[1] ==  '______________') ||
-                      (NDZ$period[1] == '______' && t$PS_code[1] ==  '______________' && t$NM_code[1] ==  '______________')) {
+                  if ((t$period[1] == '______' && t$PS_code[1] ==  '______________' && t$NM_code[1] ==  '______________') ||
+                      (t$period[1] == '______' && t$PS_code[1] ==  '______________' && t$NM_code[1] ==  '______________')) {
                         yt$dienas <- sum(sapply(seq(1,7,by=2), function(i) diff(t$NDZ_sanemsanas_datums[i:(i+1)])))                  
                     } else {stop("Starpkodi8_11 iztrūkst apstrādes koda.")}
                 } else {stop("Starpkodi8_11 iztrūkst apstrādes koda.")}
@@ -19,8 +19,18 @@ starpkodi8_11 <- function(y, t, prev, v) {
             } else if (t$zinkod[7] %in% c("41", "51", "54", "92")) {
               if (t$zinkod[8] %in% c("40", "50", "53", "91")) {
                 if (diff(t$NDZ_sanemsanas_datums[1:2]) == 0 && all(diff(t$NDZ_sanemsanas_datums[2:8]) != 0)) {
-                  if (NDZ$period[1] == '______' && t$PS_code[1] ==  '______________' && t$NM_code[1] ==  '______________') {
+                  if (t$period[1] == '______' && t$PS_code[1] ==  '______________' && t$NM_code[1] ==  '______________') {
                     yt$dienas <- sum(sapply(seq(1,7,by=2), function(i) diff(t$NDZ_sanemsanas_datums[i:(i+1)])))                  
+                  } else {stop("Starpkodi8_11 iztrūkst apstrādes koda.")}
+                } else {stop("Starpkodi8_11 iztrūkst apstrādes koda.")}
+              } else if (t$zinkod[8] %in% c("21", "22", "23", "24", "25", "29")) {
+                if (diff(t$NDZ_sanemsanas_datums[1:2]) == 0 && all(diff(t$NDZ_sanemsanas_datums[2:8]) != 0)) {
+                  if (t$period[1] == '______' && t$PS_code[1] ==  '______________' &&  t$NM_code[1] ==  '______________') {
+                    yt$dienas <- sum(sapply(seq(1,7,by=2), function(i) diff(t$NDZ_sanemsanas_datums[i:(i+1)]))) + 1              
+                  } else {stop("Starpkodi8_11 iztrūkst apstrādes koda.")}
+                } else if (all(diff(t$NDZ_sanemsanas_datums) != 0)) {
+                  if (t$period[1] == '______' && t$PS_code[1] ==  '______________' &&  t$NM_code[1] ==  '______________') {
+                    yt$dienas <- sum(sapply(seq(1,7,by=2), function(i) diff(t$NDZ_sanemsanas_datums[i:(i+1)]))) + 1              
                   } else {stop("Starpkodi8_11 iztrūkst apstrādes koda.")}
                 } else {stop("Starpkodi8_11 iztrūkst apstrādes koda.")}
               } else {stop("Starpkodi8_11 iztrūkst apstrādes koda.")}
