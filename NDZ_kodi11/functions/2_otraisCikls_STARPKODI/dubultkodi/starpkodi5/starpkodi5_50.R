591286e3130d0cbf9703adb1660e5aa13d0a0f9e
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-08-21 17:16:25 +0300

    Update starpkodi5_50.R

--- NDZ_kodi11/functions/2_otraisCikls_STARPKODI/dubultkodi/starpkodi5/starpkodi5_50.R
+++ NDZ_kodi11/functions/2_otraisCikls_STARPKODI/dubultkodi/starpkodi5/starpkodi5_50.R
@@ -11,6 +11,7 @@ starpkodi5_50 <- function(y, t, prev, v) {
            if (t$zinkod[4] %in% c("41", "51", "54", "92")) {
              if (t$zinkod[5] %in% c("21", "22", "23", "24", "25", "29")) {
                if (diff(t$NDZ_sanemsanas_datums[2:3]) == 0 && all(sapply(c(1,3,4), function(i) diff(t$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
+                 #CITĀDI VISS KĀRTĪBĀ
                  if ((t$period[1] == '______' && t$PS_code[1] ==  '______________' && t$NM_code[1] ==  '______________') ||
                      (t$period[1] == '______' && t$PS_code[1] ==  '______________' && t$NM_code[1] ==  '______________')) {
                    t <- t[c(1,3,2,4,5), ]
@@ -34,8 +35,8 @@ starpkodi5_50 <- function(y, t, prev, v) {
                                     sapply(c(2,4), function(i) diff(t$NDZ_sanemsanas_datums[i:(i+1)]))) + 1
                  } else {stop("Starpkodi5_50: iztrūkst apstrādes koda.")} 
                } else if (all(sapply(c(2,4), function(i) diff(t$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) && 
-                          all(sapply(c(1,3), function(i) diff(t$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {                 
-                 if (t$period[1] == '______' && t$PS_code[1] ==  '______________' && t$NM_code[1] ==  '___
+                          all(sapply(c(1,3), function(i) diff(t$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
+                 if (t$period[1] == '______' && t$PS_code[1] ==  '______________' && t$NM_code[1] ==  '______________') {
                    t <- t[c(1,3,2,4,5), ]
                    yt$dienas <- sum(as.numeric(difftime(t$NDZ_sanemsanas_datums[1], prev, units = "days")) - 1, 
                                     sapply(c(2,4), function(i) diff(t$NDZ_sanemsanas_datums[i:(i+1)]))) + 1
