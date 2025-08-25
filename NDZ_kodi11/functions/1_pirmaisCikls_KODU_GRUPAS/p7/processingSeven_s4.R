9be44d55436c3f1de36df555b18c21c0824b13cc
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-08-25 23:42:21 +0300

    Update processingSeven_s4.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p7/processingSeven_s4.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p7/processingSeven_s4.R
@@ -1,7 +1,6 @@
 processingSeven_s4 <- function(x7s4, o, kods) {
   x7s4_uzVieniniekiem <- data.frame(); x7s4_uzDivniekiem <- data.frame(); x7s4_uzTrijniekiem <- data.frame(); x7s4_uzCetriniekiem <- data.frame(); x7s4_uzPieciniekiem <- data.frame(); x7s4_uzSesiniekiem <- data.frame()
   #x7s4 <- x7
-  
   x7s4 <- x7s4 %>% dplyr::arrange(NDZ_sanemsanas_datums)
   
   result <- function(y) {
@@ -33,36 +32,38 @@ processingSeven_s4 <- function(x7s4, o, kods) {
   } else if (all(x7s4$sak_beidz[c(2,3,6,7)] == "1")) {
            result(processingSeven_s4_2367(x7s4, o, kods))
   } else if (all(x7s4$sak_beidz[c(1,2,4,6)] == "1")) {
-          result(processingSeven_s4_1246(x7s4, o, kods))
+           result(processingSeven_s4_1246(x7s4, o, kods))
   } else if (all(x7s4$sak_beidz[c(1,4,6,7)] == "1")) {
-          result(processingSeven_s4_1467(x7s4, o, kods))
+           result(processingSeven_s4_1467(x7s4, o, kods))
   } else if (all(x7s4$sak_beidz[c(1,2,5,7)] == "1")) {
-          result(processingSeven_s4_1257(x7s4, o, kods))
+           result(processingSeven_s4_1257(x7s4, o, kods))
   } else if (all(x7s4$sak_beidz[c(1,3,4,7)] == "1")) {
-          result(processingSeven_s4_1347(x7s4, o, kods))
+           result(processingSeven_s4_1347(x7s4, o, kods))
   } else if (all(x7s4$sak_beidz[c(1,2,5,6)] == "1")) {
-          result(processingSeven_s4_1256(x7s4, o, kods))
+           result(processingSeven_s4_1256(x7s4, o, kods))
   } else if (all(x7s4$sak_beidz[c(1,4,5,6)] == "1")) {
-          result(processingSeven_s4_1456(x7s4, o, kods))
+           result(processingSeven_s4_1456(x7s4, o, kods))
   } else if (all(x7s4$sak_beidz[c(1,4,5,7)] == "1")) {
-          result(processingSeven_s4_1457(x7s4, o, kods))
+           result(processingSeven_s4_1457(x7s4, o, kods))
   } else if (all(x7s4$sak_beidz[c(2,4,5,7)] == "1")) {
-          result(processingSeven_s4_2457(x7s4, o, kods))
+           result(processingSeven_s4_2457(x7s4, o, kods))
   } else if (all(x7s4$sak_beidz[c(2,3,5,6)] == "1")) {
-          result(processingSeven_s4_2356(x7s4, o, kods))
+           result(processingSeven_s4_2356(x7s4, o, kods))
   } else if (all(x7s4$sak_beidz[c(2,4,5,6)] == "1")) {
-          result(processingSeven_s4_2456(x7s4, o, kods))
+           result(processingSeven_s4_2456(x7s4, o, kods))
   } else if (all(x7s4$sak_beidz[c(2,3,4,6)] == "1")) {
-          result(processingSeven_s4_2346(x7s4, o, kods))
+           result(processingSeven_s4_2346(x7s4, o, kods))
   } else if (all(x7s4$sak_beidz[c(2,5,6,7)] == "1")) {
-          result(processingSeven_s4_2567(x7s4, o, kods))
+           result(processingSeven_s4_2567(x7s4, o, kods))
+  } else if (all(x7s4$sak_beidz[c(3,4,6,7)] == "1")) {
+           result(processingSeven_s4_3467(x7s4, o, kods))
   } else {stop("processingSeven_s4 trūkst izstrādes koda.\n")}
   
   rm(x7s4, o, kods)
-  return(list(x7s4_uzVieniniekiem = x7s4_uzVieniniekiem,
-              x7s4_uzDivniekiem   = x7s4_uzDivniekiem,
-              x7s4_uzTrijniekiem  = x7s4_uzTrijniekiem,
-              x7s4_uzCetriniekiem = x7s4_uzCetriniekiem, 
-              x7s4_uzPieciniekiem = x7s4_uzPieciniekiem,
-              x7s4_uzSesiniekiem  = x7s4_uzSesiniekiem))
+  return(list(x7_uzVieniniekiem = x7s4_uzVieniniekiem,
+              x7_uzDivniekiem   = x7s4_uzDivniekiem,
+              x7_uzTrijniekiem  = x7s4_uzTrijniekiem,
+              x7_uzCetriniekiem = x7s4_uzCetriniekiem, 
+              x7_uzPieciniekiem = x7s4_uzPieciniekiem,
+              x7_uzSesiniekiem  = x7s4_uzSesiniekiem))
 }
