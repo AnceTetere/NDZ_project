cbb85e764d6e8a41bcb0d87b5349ce58790ea152
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-08-25 23:47:26 +0300

    Update processingSeven_s4_2357.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p7/processingSeven_s4_2357.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p7/processingSeven_s4_2357.R
@@ -1,21 +1,41 @@
 processingSeven_s4_2357 <- function(a, o, kods) {
   
   a1 <- data.frame(); a2 <- data.frame(); a3 <- data.frame(); a4 <- data.frame(); a5 <- data.frame(); a6 <- data.frame()
-  #a <- x7s4 
+  #a <- x7s4
+  
   if (all(diff(a$NDZ_sanemsanas_datums) != 0)) {
             a4 <- a[3:6, ]; a1 <- a[c(1,7), ]
   } else if (all(sapply(2:6, function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) != 0)) &&  diff(a$NDZ_sanemsanas_datums[1:2]) == 0) {
             a2 <- a[c(2,1,3,4,5,6), ]; a1 <- a[7, ]
   } else if (all(sapply(c(1,2,4,5,6), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) != 0)) && diff(a$NDZ_sanemsanas_datums[3:4]) == 0) {
-            if ((a$PS_code[1] == '__________' && a$NM_code[1] == '__________') ||
-                (a$period[1] == '_____' && a$PS_code[1] == '__________' && a$NM_code[1] == '__________') ||
-                (a$period[1] == '_____' && a$PS_code[1] == '__________' && a$NM_code[1] == '__________')) {
+            if ((a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
+                (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
+                (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________')) {
                   a2 <- a[c(3:6), ]; a1 <- a[c(1,7), ]
             } else {stop("processingSeven_s4 trūkst izstrādes koda.\n")}
   } else if (all(sapply(c(1,5), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) && 
              all(sapply(c(2,3,4,6), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
-            if (a$period[1] == "_____" && a$PS_code[1] == 'PK7C1D87E55' && a$NM_code[1] == '__________') {
-             a2 <- a[c(2,1,3,4,5,6), ]; a1 <- a[7, ]
+             if ((a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
+                 (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________')) {
+                   a2 <- a[c(2,1,3,4,5,6), ]; a1 <- a[7, ]
+            } else {stop("processingSeven_s4 trūkst izstrādes koda.\n")}
+  } else if (all(sapply(seq(1,6,by=2), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) && 
+             all(sapply(c(2,4,6), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
+              if (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') {
+                a2 <- a[c(2,1,3,4,5,6), ]; a1 <- a[7, ]
+              } else {stop("processingSeven_s4 trūkst izstrādes koda.\n")}
+  } else if (all(sapply(c(1,6), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) && 
+             all(diff(a$NDZ_sanemsanas_datums[2:6]) != 0)) {
+              if (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') {
+                  a2 <- a[c(2,1,3,4,5,6), ]; a1 <- a[7, ]
+              } else {stop("processingSeven_s4 trūkst izstrādes koda.\n")}
+  } else if (all(sapply(c(1,3), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) && 
+             all(sapply(c(2,4,5,6), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
+               #JO PIRMOREIZ - bet te var būt arī svādāk - uzmanies pirms atbrīvo
+               if ((a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
+                   (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________')) {
+                   a6 <- a[c(2,1,3,4,5,6), ]; a1 <- a[7, ]
+                   if (kods %in% c("40", "50", "53") && o == "7") {ZERO_minus(a %>% slice(2))}
     } else {stop("processingSeven_s4 trūkst izstrādes koda.\n")}
   } else {stop("processingSeven_s4 trūkst izstrādes koda.\n")}
   
