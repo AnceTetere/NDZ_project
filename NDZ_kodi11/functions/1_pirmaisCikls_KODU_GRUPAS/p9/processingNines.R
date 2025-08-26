9cfd0355e1982e0e1e2f4b4c4f4ef0ffd63cf71e
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-08-26 14:36:27 +0300

    Update processingNines.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p9/processingNines.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p9/processingNines.R
@@ -1,11 +1,12 @@
 processingNines <- function(x, o, kods) {
   x <- arrange(x, PS_code, DN_code, NM_code, NDZ_sanemsanas_datums)
-  x9_uzVieniniekiem <- data.frame(); x9_uzDivi <- data.frame();  x9_uzSesi <- data.frame(); x9_uzSeptini <- data.frame(); x9_uzAstoniekiem <- data.frame()
+  x9_uzVieniniekiem <- data.frame(); x9_uzDivi <- data.frame();  x9_uzPieci <- data.frame(); x9_uzSesi <- data.frame(); x9_uzSeptini <- data.frame(); x9_uzAstoniekiem <- data.frame()
   check_rows <- 0
-  
+
   result <- function(res) {
     x9_uzVieniniekiem <<- rbind(x9_uzVieniniekiem, res$x9_uzVieniniekiem)
     x9_uzDivi <<- rbind(x9_uzDivi, res$x9_uzDivi)
+    x9_uzPieci <<- rbind(x9_uzPieci, res$x9_uzPieci)
     x9_uzSesi <<- rbind(x9_uzSesi, res$x9_uzSesi)
     x9_uzSeptini <<- rbind(x9_uzSeptini, res$x9_uzSeptini)
     x9_uzAstoniekiem <<- rbind(x9_uzAstoniekiem, res$x9_uzAstoniekiem)
@@ -15,53 +16,27 @@ processingNines <- function(x, o, kods) {
 
     x9 <- x[r:(r+8),] %>% arrange(PS_code, DN_code, NM_code, NDZ_sanemsanas_datums)
     
-     if (all(x9$sak_beidz == c("2", "1", "2", "1", "1", "2", "2", "1", "1")) && 
-               all(sapply(seq(1, 8, by = 2), function(i) all(diff(x9$NDZ_sanemsanas_datums[i:i+1]) == 0))) &&
-               all(sapply(seq(2, 9, by = 2), function(i) all(diff(x9$NDZ_sanemsanas_datums[i:i+1]) != 0)))) {
-      x9_uzVieniniekiem <- rbind(x9_uzVieniniekiem, x9[9, ])
-      x9_uzAstoniekiem <- rbind(x9_uzAstoniekiem, x9[1:8, ])
-    } else if (all(x9$sak_beidz[c(1,2,7,8,9)] == c("1", "2", "2", "1", "1")) && diff(x9$NDZ_sanemsanas_datums[7:8]) == 0) {
-      x9_uzVieniniekiem <- rbind(x9_uzVieniniekiem, x9[9, ])
-      x9_uzAstoniekiem <- rbind(x9_uzAstoniekiem, x9[1:8, ])
-      if (kods %in% c("40", "50", "53") && o == "9") {ZERO_minus(x9 %>% slice(1))}
-    } else if (all(x9$sak_beidz[c(1,2,8,9)] == c("2", "1", "2", "1")) && 
-               diff(x9$NDZ_sanemsanas_datums[1:2]) == 0 && 
-               diff(x9$NDZ_sanemsanas_datums[8:9]) != 0) {
-      x9_uzVieniniekiem <- rbind(x9_uzVieniniekiem, x9[9, ])
-      x9_uzAstoniekiem <- rbind(x9_uzAstoniekiem, x9[1:8, ])
-      if (kods %in% c("40", "50", "53") && o == "9") {ZERO_minus(x9 %>% slice(1))}
-    } else if (all(x9$sak_beidz[c(1,2,8,9)] == c("1", "2", "2", "1"))) {
-              result(processingNines_1221(x9, o, kods))
-    } else if (all(x9$sak_beidz[1:2] == c("2", "1")) && diff(x9$NDZ_sanemsanas_datums[1:2]) != 0) {
-      x9_uzVieniniekiem <- rbind(x9_uzVieniniekiem, x9[1, ])
-      x9_uzAstoniekiem <- rbind(x9_uzAstoniekiem, x9[2:9, ])
-    } else if (all(x9$sak_beidz == c("2", "2", "1", "1", "2", "1", "2", "1", "2")) && all(diff(x9$NDZ_sanemsanas_datums) != 0)) {
-      x9_uzVieniniekiem <- rbind(x9_uzVieniniekiem, x9[2, ])
-      x9_uzSesi <- rbind(x9_uzSesi, x9[4:9, ])
-      if (kods %in% c("40", "50", "53") && o == "9") {ZERO_plus(x9 %>% slice(9))}
-    } else if (all(x9$sak_beidz == c("1", "2", "1", "1", "2", "1", "2", "1", "2"))) {
-          if (diff(x9$NDZ_sanemsanas_datums[2:3]) == 0) {
-            if (x9$period[1] == "______" && x9$PS_code[1] == "______" && x9$NM_code[1] == "______") {
-              x9_uzAstoniekiem <- rbind(x9_uzAstoniekiem, x9[c(1:2, 4:9), ])
-              if (kods %in% c("40", "50", "53") && o == "9") {ZERO_minus(x9 %>% slice(1)); ZERO_plus(x9 %>% slice(9))}
-            } else {stop("processingNines() iztrūkst apstrādes kods. \n")}
-          } else {stop("processingNines() iztrūkst apstrādes kods. \n")}
-    } else if (all(x9$sak_beidz[1:3] == c("1", "1", "2")) && x9$sak_beidz[4] != "2") {
-            if (all(diff(x9$NDZ_sanemsanas_datums[1:4]) != 0)) {
-                x9_uzDivi <- rbind(x9_uzDivi, x9[c(1,3), ])
-                x9_uzSesi <- rbind(x9_uzSesi, x9[4:9, ])
-            } else {stop("processingNines() iztrūkst apstrādes kods. \n")}
-    } else if (all(x9$sak_beidz[1:4] == c("2", "2", "1", "1")) && all(x9$sak_beidz[8:9] == c("2", "1"))) {
-            if (diff(x9$NDZ_sanemsanas_datums[2:3]) == 0 && all(sapply(c(1,3), function(i) diff(x9$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
-              if (x9$period[1] == "______" && x9$PS_code[1] == "______" && x9$NM_code[1] == "______") {
-              x9_uzVieniniekiem <- rbind(x9_uzVieniniekiem, x9[1, ])
-              x9_uzAstoniekiem <- rbind(x9_uzSesi, x9[c(3,2,4,5,6,7,9,8), ])
-              } else {stop("processingNines() iztrūkst apstrādes kods. \n")}
-            } else {stop("processingNines() iztrūkst apstrādes kods. \n")}
+    if (all(x9$sak_beidz[1:3] == c("1", "1", "2")) && x9$sak_beidz[4] != "2") {
+               result(processingNines_112X2(x9, o, kods))
+    } else if (all(x9$sak_beidz[1:4] == c("1", "2", "1", "1"))) {
+               result(processingNines_1211(x9, o, kods))
+    } else if (all(x9$sak_beidz[1:4] == c("2", "2", "1", "1"))) {
+               result(processingNines_2211(x9, o, kods))
     } else if (all(x9$sak_beidz[1:4] == c("1", "2", "1", "2"))) {
-              result(processingNines_1212(x9, o, kods))
+               result(processingNines_1212(x9, o, kods))
+    } else if (all(x9$sak_beidz[1:4] == c("2", "2", "1", "2"))) {
+               result(processingNines_2212(x9, o, kods))
+    } else if (all(x9$sak_beidz[1:4] == c("2", "1", "1", "2"))) {
+               result(processingNines_2112(x9, o, kods))
+    } else if (all(x9$sak_beidz[1:4] == c("1", "2", "2", "1"))) {
+               result(processingNines_1221(x9, o, kods))
+    } else if (all(x9$sak_beidz[1:4] == c("2", "1", "2", "1"))) {
+               result(processingNines_2121(x9, o, kods))
+    } else if (all(x9$sak_beidz[1:4] == c("2", "1", "2", "2"))) {
+               result(processingNines_2122(x9, o, kods))
     } else {stop("processingNines() iztrūkst apstrādes kods: rindas ", r, ":", r+8, "!\n")}
-    check_rows = check_rows + 9
+
+        check_rows = check_rows + 9
   }
   
 #PĀRBAUDE: Vai rindu skaits no deviņniekiem atvasinātajās tabulās sakrīt ar rindām izejas tabulā x.
@@ -79,24 +54,30 @@ rm(x9_uzVieniniekiem)
 #2 Apakštabulu x9_uzDivi sūta caur processingTwoes().
 if(nrow(x9_uzDivi) > 0) {
   x9_uzDivi %>% arrange(PS_code, NM_code, NDZ_sanemsanas_datums) %>% processingTwoes(o, kods)
-} else {cat("Tabula x9_uzDivi ir tukša.")}
+} else {cat("Tabula x9_uzDivi ir tukša.\n")}
 rm(x9_uzDivi) 
 
-#3 Apakštabulu x9_uzSesi sūta caur processingSixes().
+#3 Apakštabulu x9_uzPieci sūta caur processingFives().
+if(nrow(x9_uzPieci) > 0) {
+  x9_uzPieci %>% arrange(PS_code, NM_code, NDZ_sanemsanas_datums) %>% processingFives(o, kods)
+} else {cat("Tabula x9_uzPieci ir tukša.\n")}
+rm(x9_uzPieci) 
+
+#4 Apakštabulu x9_uzSesi sūta caur processingSixes().
 if(nrow(x9_uzSesi) > 0) {
   x9_uzSesi %>% arrange(PS_code, NM_code, NDZ_sanemsanas_datums) %>% processingSixes(o, kods)
-} else {cat("Tabula x9_uzSesi ir tukša.")}
+} else {cat("Tabula x9_uzSesi ir tukša.\n")}
 rm(x9_uzSesi) 
 
-#4 Apakštabulu x9_uzSeptini sūta caur processingSeven().
+#5 Apakštabulu x9_uzSeptini sūta caur processingSeven().
 if(nrow(x9_uzSeptini) > 0) {
   x9_uzSeptini %>% arrange(PS_code, NM_code, NDZ_sanemsanas_datums) %>% processingSeven(o, kods)
-} else {cat("Tabula x9_uzSeptini ir tukša.")}
+} else {cat("Tabula x9_uzSeptini ir tukša.\n")}
 rm(x9_uzSeptini) 
 
-#4 Apakštabulu x9_uzAstoniekiem sūta caur processingEights().
+#6 Apakštabulu x9_uzAstoniekiem sūta caur processingEights().
 if(nrow(x9_uzAstoniekiem) > 0) {
   x9_uzAstoniekiem %>% arrange(PS_code, NM_code, NDZ_sanemsanas_datums) %>% processingEights(o, kods)
-} else {cat("Tabula x9_uzAstoniekiem ir tukša.")}
+} else {cat("Tabula x9_uzAstoniekiem ir tukša.\n")}
 rm(x9_uzAstoniekiem) 
 }
