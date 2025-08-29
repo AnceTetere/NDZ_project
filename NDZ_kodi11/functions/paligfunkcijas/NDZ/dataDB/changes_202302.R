77232cc75786f9e0384115bfd4492a100e9ab72b
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-08-29 16:56:28 +0300

    Create changes_202302.R

---
+++ NDZ_kodi11/functions/paligfunkcijas/NDZ/dataDB/changes_202302.R
@@ -0,0 +1,19 @@
+changes_202302 <- function(NDZ, kods) {
+  
+  if (kods == "11") {
+    NDZ <- NDZ %>% 
+      dplyr::filter(!(
+        (PS_code ==  '______________' & NM_code ==  '______________' & NDZ_sanemsanas_datums == '2023-02-15' & zinkod == '25') |
+        (PS_code ==  '______________' & NM_code ==  '______________' & NDZ_sanemsanas_datums == '2023-02-08' & zinkod == '26') |
+        (PS_code ==  '______________' & NM_code ==  '______________' & NDZ_sanemsanas_datums == '2023-02-08' & zinkod == '26') |
+        (PS_code ==  '______________' & NM_code ==  '______________' & NDZ_sanemsanas_datums == '2023-02-08' & zinkod == '26') |
+        (PS_code ==  '______________' & NM_code ==  '______________' & NDZ_sanemsanas_datums == '2023-02-08' & zinkod == '26') |
+        (PS_code ==  '______________' & NM_code ==  '______________' & NDZ_sanemsanas_datums == '2023-02-08' & zinkod == '26')  
+        ))
+  } else if (kods == '50') {
+    NDZ <- NDZ %>% 
+      dplyr::filter(!(PS_code ==  '______________' & NM_code ==  '______________' & zinkod == '51' & NDZ_sanemsanas_datums == '2023-02-18'))
+  }
+  
+  return(NDZ)
+}
