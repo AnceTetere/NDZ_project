8c4ce59e5f8c949191861b6d9a77a1e009547f5c
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-09-03 21:34:39 +0300

    Update processingSeven_s4_1457.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p7/processingSeven_s4_1457.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p7/processingSeven_s4_1457.R
@@ -10,36 +10,36 @@ processingSeven_s4_1457 <- function(a, o, kods) {
   } else if (all(sapply(seq(1,6,by=2), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) &&
              all(sapply(seq(2,7,by=2), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
               #TE viss OK, bloķēju, jo pirmoreiz.
-              if (a$period[1] == '______' && aPS_code ==  '______________' && aNM_code ==  '______________') {
+              if (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') {
                 a1 <- a[7,]; a6 <- a[c(1,2,4,3,5,6),]
                 if (kods %in% c("40", "50", "53") && o == "7") {ZERO_minus(a %>% slice(1))}
               } else {stop("processingSeven_s4_1457 trūkst izstrādes koda.\n")}
   } else if (all(sapply(c(1,3), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) &&
              all(sapply(c(2,4,5,6), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
               #TE viss OK, bloķēju, jo pirmoreiz.
-              if ((a$period[1] == '______' && aPS_code ==  '______________' && aNM_code ==  '______________') ||
-                  (a$period[1] == '______' && aPS_code ==  '______________' && aNM_code ==  '______________') ||
-                  (a$period[1] == '______' && aPS_code ==  '______________' && aNM_code ==  '______________') ||
-                  (a$period[1] == '______' && aPS_code ==  '______________' && aNM_code ==  '______________') ||
-                  (a$period[1] == '______' && aPS_code ==  '______________' && aNM_code ==  '______________') ||
-                  (a$period[1] == '______' && aPS_code ==  '______________' && aNM_code ==  '______________')) {
+              if ((a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
+                  (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
+                  (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
+                  (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
+                  (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
+                  (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________')) {
                      a1 <- a[7,]; a6 <- a[c(1,2,4,3,5,6),]
                      if (kods %in% c("40", "50", "53") && o == "7") {ZERO_minus(a %>% slice(1))}
               } else {stop("processingSeven_s4_1457 trūkst izstrādes koda.\n")}
   } else if (all(sapply(c(3,5), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) &&
              all(sapply(c(1,2,4,6), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
               #TE viss OK, bloķēju, jo pirmoreiz.
-              if ((a$period[1] == '______' && aPS_code ==  '______________' && aNM_code ==  '______________') ||
-                  (a$period[1] == '______' && aPS_code ==  '______________' && aNM_code ==  '______________') ||
-                  (a$period[1] == '______' && aPS_code ==  '______________' && aNM_code ==  '______________') ||
-                  (a$period[1] == '______' && aPS_code ==  '______________' && aNM_code ==  '______________') ||
-                  (a$period[1] == '______' && aPS_code ==  '______________' && aNM_code ==  '______________')) {
+              if ((a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
+                  (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
+                  (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
+                  (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
+                  (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________')) {
                   a1 <- a[7,]; a6 <- a[c(1,2,4,3,5,6),]
                   if (kods %in% c("40", "50", "53") && o == "7") {ZERO_minus(a %>% slice(1))}
               } else {stop("processingSeven_s4_1457 trūkst izstrādes koda.\n")}
   } else if (all(diff(a$NDZ_sanemsanas_datums) != 0)) {
               #TE viss OK, bloķēju, jo pirmoreiz.
-              if (a$period[1] == '______' && aPS_code ==  '______________' && aNM_code ==  '______________') {
+              if (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') {
                a1 <- a[7,]; a4 <- a[c(1,3,5,6),]
                if (kods %in% c("40", "50", "53") && o == "7") {ZERO_minus(a %>% slice(1))}
              } else {stop("processingSeven_s4_1457 trūkst izstrādes koda.\n")}
@@ -52,5 +52,4 @@ processingSeven_s4_1457 <- function(a, o, kods) {
               x7s4_uzCetriniekiem = a4, 
               x7s4_uzPieciniekiem = a5,
               x7s4_uzSesiniekiem = a6))
-}
- 
+} 
