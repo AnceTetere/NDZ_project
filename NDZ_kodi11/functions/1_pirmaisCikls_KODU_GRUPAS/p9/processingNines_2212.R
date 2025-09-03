25ef471ad81a9661c6e3865d49811daeae19a01f
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-09-03 21:41:27 +0300

    Update processingNines_2212.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p9/processingNines_2212.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p9/processingNines_2212.R
@@ -1,4 +1,5 @@
 processingNines_2212 <- function(a, o, kods) {  
+  #a <- x9 for testing !!!PIEVĒRS UZMANĪBU KOMPANIJAI
   a <- a %>% arrange(PS_code, DN_code, NM_code, NDZ_sanemsanas_datums)
   a1 <- data.frame(); a2 <- data.frame(); a6 <- data.frame(); a7 <- data.frame(); a8 <- data.frame()
   
@@ -6,22 +7,27 @@ if (diff(a$NDZ_sanemsanas_datums[2:3]) == 0 && all(sapply(c(1,3), function(i) di
       #JO PIRMOREIZ
       if ((a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
           (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
-          (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||  
+          (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||  #Šis ir dīvains gadījyms - paseko, kā tas iet cauri.
           (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________')) {
             a1 <- a[1, ]; a8 <- a[c(3,2,4:9), ]
       } else {stop("processingNines() iztrūkst apstrādes kods. \n")}
 } else if (all(sapply(c(1,3,5,7), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) != 0)) &&
     all(sapply(c(2,4,6,8), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) == 0))) {
+    #JO PIRMOREIZ
     if (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') {
         a1 <- a[1, ]; a8 <- a[c(3,2,4:9),]
    } else {stop("processingNines() iztrūkst apstrādes kods. \n")}
 } else if (all(sapply(c(1,3,4,6,7), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) != 0)) &&
            all(sapply(c(2,5,8), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) == 0))) {
+           #JO PIRMOREIZ
+           #Bet nu reāli te nevar saprast, kas notiek
            if (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') {
                a1 <- a[1, ]; a8 <- a[c(3,2,4:9),]
           } else {stop("processingNines() iztrūkst apstrādes kods. \n")}
 } else if (all(sapply(c(1,3), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) != 0)) &&
            all(sapply(c(2,4), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) == 0))) {
+           #JO PIRMOREIZ 
+           #Bet nu reāli te nevar saprast, kas notiek
            if (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') {
                 a1 <- a[1, ]; a2 <- a[c(3,2),]; a6 <- a[4:9,]
            } else {stop("processingNines() iztrūkst apstrādes kods. \n")}
