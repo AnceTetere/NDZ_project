94f82a1594149e259269ee46f13f22d27025fc3b
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-09-03 21:42:21 +0300

    Update changes_202302.R

--- NDZ_kodi11/functions/paligfunkcijas/NDZ/dataDB/changes_202302.R
+++ NDZ_kodi11/functions/paligfunkcijas/NDZ/dataDB/changes_202302.R
@@ -5,9 +5,6 @@ changes_202302 <- function(NDZ, kods) {
       dplyr::filter(!(
         (PS_code ==  '______________' & NM_code ==  '______________' & NDZ_sanemsanas_datums == '2023-02-15' & zinkod == '25') |
         (PS_code ==  '______________' & NM_code ==  '______________' & NDZ_sanemsanas_datums == '2023-02-08' & zinkod == '26') |
-        (PS_code ==  '______________' & NM_code ==  '______________' & NDZ_sanemsanas_datums == '2023-02-08' & zinkod == '26') |
-        (PS_code ==  '______________' & NM_code ==  '______________' & NDZ_sanemsanas_datums == '2023-02-08' & zinkod == '26') |
-        (PS_code ==  '______________' & NM_code ==  '______________' & NDZ_sanemsanas_datums == '2023-02-08' & zinkod == '26') |
         (PS_code ==  '______________' & NM_code ==  '______________' & NDZ_sanemsanas_datums == '2023-02-08' & zinkod == '26')  
         ))
   } else if (kods == '50') {
