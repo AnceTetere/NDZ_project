76b3de69c9e1a81cd7fe5f104a386fbd39ac9995
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-09-03 21:33:15 +0300

    Update processingFives_s3e10.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p5/processingFives_s3e10.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p5/processingFives_s3e10.R
@@ -1,10 +1,28 @@
 processingFives_s3e10 <- function(a, o, kods) {
   a1 <- data.frame(); a2 <- data.frame(); a4 <- data.frame()
-  #a <- a for testing
+  #a <- x5s3 for testing
   
   if (all(a$sak_beidz[3:5] == "1")) {
-    a1 <- rbind(a1, a[c(1, 5), ])
-  } else {stop("processingFives_s3: Iztrūkst apstrādes koda!\n")}  
+         #JO PIRMOREIZ
+         if ((a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
+             (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
+             (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________')) {
+              a1 <- a[c(1,5),]
+        } else {stop("processingFives_s3e10: Iztrūkst apstrādes koda!\n")}
+  } else if (all(a$sak_beidz[c(2,3,5)] == "1")) {
+        if ((a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
+            (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
+            (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________')) {
+             #JO PIRMOREIZ
+             a1 <- a[5, ]; a4 <- a[c(2,1,3,4),]
+        } else {stop("processingFives_s3e10: Iztrūkst apstrādes koda!\n")}
+  } else if (all(a$sak_beidz[c(1,3,4)] == "1")) {
+          #Te datumi ir samainījušies vietām.
+           if (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') {
+              #JO PIRMOREIZ
+               a1 <- a[4, ]; a4 <- a[c(1,2,3,5),]
+           } else {stop("processingFives_s3e10: Iztrūkst apstrādes koda!\n")}  
+  } else {stop("processingFives_s3e10: Iztrūkst apstrādes koda!\n")}  
 
   
   rm(a)
