93c32f2e8418a1868fe7ff0d5ca95baa67bfa53b
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-09-04 17:14:09 +0300

    Update processingFours_2221.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p4/processingFours_2221.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p4/processingFours_2221.R
@@ -1,12 +1,15 @@
 processingFours_2221 <- function(a, o, kods) {
   a4_1 <- data.frame(); a4_2 <- data.frame(); a4_3 <- data.frame()
+  #a <- x4
   
- if (all(diff(a$ZDN_sanemsanas_datums) != 0)) {
-      #JO PIRMO REIZ.
-      if (a$period[1] == '_______' && a$PS_code[1] == '_______' && a$NM_code[1] == '_______') {
+ if (all(diff(a$NDZ_sanemsanas_datums) != 0)) {
+      #JO PIRMOREIZ.
+      if ((a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
+          (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
+          (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________')) {
         a4_1 <- rbind(a4_1, a[3:4, ])     
       } else {stop("processingFours_2212 trūkst apstrādes koda.")}
-} else {stop("processingFours_2212 trūkst apstrādes koda.")}
+ } else {stop("processingFours_2221 trūkst apstrādes koda.")}
   
   return(list(x4_uzVieniniekiem = a4_1,
               x4_trueDoubles = a4_2,
