bf789a8ebca23d0f992ee5447977351e722e7a0e
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-09-04 17:13:48 +0300

    Update processingFours_2212.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p4/processingFours_2212.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p4/processingFours_2212.R
@@ -1,12 +1,21 @@
-processingFours_2212 <- function(a) {
+processingFours_2212 <- function(a, o, kods) {
   a4_1 <- data.frame(); a4_2 <- data.frame(); a4_3 <- data.frame()
-
+  #a <- x4
+  
   if (diff(a$NDZ_sanemsanas_datums[1:2]) > 0 && diff(a$NDZ_sanemsanas_datums[2:3]) == 0) {
     a4_1 <- rbind(a4_1, a[1, ])
     a4_2 <- rbind(a4_2, a[3:4, ])
-  } else if (all(diff(a$NDZ_sanemsanas_datums) > 0)) {
-    a4_1 <- rbind(a4_1, a[2, ])
-    a4_2 <- rbind(a4_2, a[3:4, ])
+    if (kods %in% c("40", "50", "53") & o == "4") {ZERO_plus(a %>% slice(4))}
+  } else if (all(diff(a$NDZ_sanemsanas_datums) != 0)) {
+            a4_1 <- rbind(a4_1, a[2, ])
+            a4_2 <- rbind(a4_2, a[3:4, ])
+            if (kods %in% c("40", "50", "53") & o == "4") {ZERO_plus(a %>% slice(4))}
+  } else if (diff(a$NDZ_sanemsanas_datums[1:2]) == 0 && all(diff(a$NDZ_sanemsanas_datums[2:4]) != 0)) {
+            #JO PIRMOREIZ
+            if (kods == "40" && a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') {
+              a4_1 <- a[2, ]; a4_2 <- a[3:4, ]
+              if (kods %in% c("40", "50", "53") & o == "4") {ZERO_plus(a %>% slice(4))}
+            } else {stop("processingFours_2212 trūkst apstrādes koda.")}
   } else {stop("processingFours_2212 trūkst apstrādes koda.")}
   
   return(list(x4_uzVieniniekiem = a4_1,
