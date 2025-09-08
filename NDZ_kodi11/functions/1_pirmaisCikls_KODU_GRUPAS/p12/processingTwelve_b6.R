9696e261e695288bf1190c4374ab47d0571dae14
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-09-08 23:28:25 +0300

    Update processingTwelve_b6.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p12/processingTwelve_b6.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p12/processingTwelve_b6.R
@@ -1,3 +1,5 @@
+
+
 processingTwelve_b6 <- function(a, o, kods) {
   
   a1 <- data.frame(); a2 <- data.frame(); a7 <- data.frame(); a8 <- data.frame(); a10 <- data.frame(); a11 <- data.frame()
@@ -13,6 +15,7 @@ processingTwelve_b6 <- function(a, o, kods) {
           } else {stop("12-nieku tabulas pārdalei trūkst izstrādes koda.")}
         } else if (a$sak_beidz[3] == "1") {
                  if (diff(a$NDZ_sanemsanas_datums[1:2]) == 0) {
+                    #JO PIRMOREIZ - nobloķēšu tikai ar to kompāniju, jo tur visiem ir līdzīga sistemātika.
                      if (a$NM_code[1] ==  '______________') {
                          a2 <- a[c(2,1), ]; a10 <- a[3:12, ]
                     } else {stop("12-nieku tabulas pārdalei trūkst izstrādes koda.")}
@@ -32,6 +35,7 @@ processingTwelve_b6 <- function(a, o, kods) {
                                  if (all(sapply(c(1,3,5,7,9,11), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) && 
                                      all(sapply(c(2,4,6,8,10), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
                                    if (kods == "40") {
+                                      #JO PIRMOREIZ - BK atvaļinājuma kodi lietoti dubultā
                                        if ((a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
                                            (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________')) {
                                            a1 <- a[c(1,11), ]; a <- a[c(3,5,7,9),]
@@ -124,4 +128,4 @@ processingTwelve_b6 <- function(a, o, kods) {
               x12_uzAstoni = a8,
               x12_uzDesmitniekiem = a10,
               x12_uzVienpadsmit = a11))
-}
+} 
