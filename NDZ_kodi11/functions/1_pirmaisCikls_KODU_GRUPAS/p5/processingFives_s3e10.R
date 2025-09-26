9f99683827e834cde580cafa803cd15ccd7fb7e4
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-09-26 17:12:16 +0300

    Update processingFives_s3e10.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p5/processingFives_s3e10.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p5/processingFives_s3e10.R
@@ -3,14 +3,12 @@ processingFives_s3e10 <- function(a, o, kods) {
   #a <- x5s3 for testing
   
   if (all(a$sak_beidz[3:5] == "1")) {
-         #JO PIRMOREIZ
          if ((a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
              (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
              (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________')) {
               a1 <- a[c(1,5),]
         } else {stop("processingFives_s3e10: Iztrūkst apstrādes koda!\n")}
   } else if (all(a$sak_beidz[c(2,3,5)] == "1")) {
-        #Te ir jocīgi - neesmu pārliecināta, ka šo var vispārināt.
         if ((a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
             (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
             (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________')) {
@@ -18,7 +16,6 @@ processingFives_s3e10 <- function(a, o, kods) {
              a1 <- a[5, ]; a4 <- a[c(2,1,3,4),]
         } else {stop("processingFives_s3e10: Iztrūkst apstrādes koda!\n")}
   } else if (all(a$sak_beidz[c(1,3,4)] == "1")) {
-
            if (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') {
               #JO PIRMOREIZ
                a1 <- a[4, ]; a4 <- a[c(1,2,3,5),]
