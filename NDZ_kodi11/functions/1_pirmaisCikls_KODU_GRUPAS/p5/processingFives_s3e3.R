54ce33145253d7ecf1f538de61744e7ffa412569
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-09-26 17:13:01 +0300

    Update processingFives_s3e3.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p5/processingFives_s3e3.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p5/processingFives_s3e3.R
@@ -6,23 +6,22 @@ processingFives_s3e3 <- function(a, o, kods) {
           a1 <- a[5, ]; a2 <- a[1:4, ]
           if (kods %in% c("40", "50", "53") && o == "5") {ZERO_minus(a %>% slice(1))}
   } else if (all(a$sak_beidz[c(1,3,4)] == "1")) {
-
-         #if (a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') {
-         #  a2 <- a[c(1:3,5), ]
-        # } else 
-           if ((a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
-                    (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________')) {
-           a2 <- a[c(1,2,4,5), ]
-           if (kods %in% c("40", "50", "53") && o == "5") {ZERO_minus(a %>% slice(1)); ZERO_plus(a %>% slice(5))}
-         } else {stop("processingFives_s3e3 iztrūkst apstrādes koda")}
+             #if (a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') {
+             #  a2 <- a[c(1:3,5), ]
+             # } else 
+              if ((a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
+                  (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
+                  (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
+                  (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________')) {
+                     a2 <- a[c(1,2,4,5), ]
+                     if (kods %in% c("40", "50", "53") && o == "5") {ZERO_minus(a %>% slice(1)); ZERO_plus(a %>% slice(5))}
+             } else {stop("processingFives_s3e3 iztrūkst apstrādes koda")}
   } else if (all(a$sak_beidz[c(1,2,5)] == "1")) {
-          #JO PIRMO REIZI
           if (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') {
             a2 <- a[c(2,3,5,4), ]
             if (kods %in% c("40", "50", "53") && o == "5") {ZERO_minus(a %>% slice(2)); ZERO_plus(a %>% slice(5))}
           } else {stop("processingFives_s3e3 iztrūkst apstrādes koda")}
   } else if (all(a$sak_beidz[c(1,2,4)] == "1")) {
-         #JO PIRMO REIZI
          if (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') {
            a2 <- a[2:4, ]
            if (kods %in% c("40", "50", "53") && o == "5") {ZERO_minus(a %>% slice(2)); ZERO_plus(a %>% slice(5))}
