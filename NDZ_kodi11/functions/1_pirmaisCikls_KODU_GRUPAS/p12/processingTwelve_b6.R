58bff5d17dbf4213df342e834818330fb8e5013d
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-11-27 23:15:46 +0200

    Update processingTwelve_b6.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p12/processingTwelve_b6.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p12/processingTwelve_b6.R
@@ -13,7 +13,6 @@ processingTwelve_b6 <- function(a, o, kods) {
           } else {stop("12-nieku tabulas pārdalei trūkst izstrādes koda.")}
         } else if (a$sak_beidz[3] == "1") {
                  if (diff(a$NDZ_sanemsanas_datums[1:2]) == 0) {
-                    #JO PIRMOREIZ - nobloķēšu tikai ar to kompāniju, jo tur visiem ir līdzīga sistemātika.
                      if (a$NM_code[1] ==  '______________') {
                          a2 <- a[c(2,1), ]; a10 <- a[3:12, ]
                     } else {stop("12-nieku tabulas pārdalei trūkst izstrādes koda.")}
@@ -33,7 +32,6 @@ processingTwelve_b6 <- function(a, o, kods) {
                                  if (all(sapply(c(1,3,5,7,9,11), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) && 
                                      all(sapply(c(2,4,6,8,10), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
                                    if (kods == "40") {
-                                      #JO PIRMOREIZ - BK atvaļinājuma kodi lietoti dubultā
                                        if ((a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
                                            (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________')) {
                                            a1 <- a[c(1,11), ]; a <- a[c(3,5,7,9),]
@@ -67,13 +65,11 @@ processingTwelve_b6 <- function(a, o, kods) {
                         if (all(diff(a$NDZ_sanemsanas_datums[1:4]) != 0)) {
                                    a2 <- a[c(1,3), ]; a9 <- a[4:12, ]
                         } else if (diff(a$NDZ_sanemsanas_datums[1:2]) == 0 && diff(a$NDZ_sanemsanas_datums[2:3]) != 0) {
-                                   #BLOĶĒJU, JO PIRMOREIZ
                                    if ((a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
                                        (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________')) {
                                         a2 <- a[1:2, ]; a10 <- a[3:12, ]
                                     } else {stop("12-nieku tabulas pārdalei trūkst izstrādes koda.")}
                         } else if (all(diff(a$NDZ_sanemsanas_datums[1:3]) != 0) && diff(a$NDZ_sanemsanas_datums[3:4]) == 0) {
-                                  #BLOĶĒJU, JO PIRMOREIZ
                                   if ((a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
                                       (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
                                       (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________')) {
@@ -85,20 +81,17 @@ processingTwelve_b6 <- function(a, o, kods) {
                               if (a$sak_beidz[6] == "2") {
                                 if (all(sapply(c(1,3,5), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) && 
                                     all(sapply(c(2,4), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
-                                        #BLOĶĒJU, JO PIRMOREIZ
                                          if ((a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
                                              (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________')) {
                                                 a2 <- a[1:2, ]; a10 <- a[c(4,3, 5:12), ]
                                          } else {stop("12-nieku tabulas pārdalei trūkst izstrādes koda.")}           
                                 } else if (all(sapply(c(3,5), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) && 
                                            all(sapply(c(1,2,4), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
-                                               #BLOĶĒJU, JO PIRMOREIZ
                                                 if (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') {
                                                        a2 <- a[1:2, ]; a10 <- a[c(4,3, 5:12), ]
                                                } else {stop("12-nieku tabulas pārdalei trūkst izstrādes koda.")}  
                                 }  else if (diff(a$NDZ_sanemsanas_datums[3:4]) == 0 && 
                                             all(sapply(c(1,2,4:6), function(i) diff(a$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
-                                            #BLOĶĒJU, JO PIRMOREIZ
                                             if ((a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
                                                 (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
                                                 (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
@@ -106,7 +99,6 @@ processingTwelve_b6 <- function(a, o, kods) {
                                                  a2 <- a[1:2, ]; a10 <- a[c(4,3, 5:12), ]
                                             } else {stop("12-nieku tabulas pārdalei trūkst izstrādes koda.")}  
                                 } else if (all(diff(a$NDZ_sanemsanas_datums[1:6]) != 0)) {
-                                           #JO PIRMOREIZ
                                            if (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') {
                                                a2 <- a[c(1,3), ]; a8 <- a[5:12, ]
                                            } else {stop("12-nieku tabulas pārdalei trūkst izstrādes koda.")}  
@@ -126,4 +118,4 @@ processingTwelve_b6 <- function(a, o, kods) {
               x12_uzAstoni = a8,
               x12_uzDesmitniekiem = a10,
               x12_uzVienpadsmit = a11))
-} 
+}
