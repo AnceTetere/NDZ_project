39c0060b68aad3c044f52d95251d4e720f904d79
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-11-27 23:12:06 +0200

    Update changes_202206.R

--- NDZ_kodi11/functions/paligfunkcijas/NDZ/dataDB/changes_202206.R
+++ NDZ_kodi11/functions/paligfunkcijas/NDZ/dataDB/changes_202206.R
@@ -1,15 +1,15 @@
-changes_202206 <- function(NDZ, kods) {
+ changes_202206 <- function(NDZ, kods) {
   
   if (kods == "11") {
              
              NDZ <- NDZ %>% 
                dplyr::filter(!(
-                 (PS_code ==  '______________' & NM_code ==  '______________' & zinkod == '11' & NDZ_sanemsanas_datums == "2022-06-01") | 
-                 (PS_code ==  '______________' & NM_code ==  '______________' & zinkod == '11' & NDZ_sanemsanas_datums == "2022-06-28") | 
-                 (PS_code ==  '______________' & NM_code ==  '______________' & zinkod == '11' & NDZ_sanemsanas_datums == "2022-06-23") | 
-                 (PS_code ==  '______________' & NM_code ==  '______________' & zinkod == '11' & NDZ_sanemsanas_datums == "2022-06-01") | 
+                 (PS_code ==  '______________' & NM_code ==  '______________' & zinkod == '11' & NDZ_sanemsanas_datums == "2022-06-01") | #Te dzilāka izpēte rāda, ka cauri gadiem indivīds tiek pieņemts darbā uz vienu dienu (visdrīzāk pašnodarbinātais). Sekojošais kods noatbilst šai sistemātikai. Tur ir kļūda; pieņemu lēmumu to dzēst.
+                 (PS_code ==  '______________' & NM_code ==  '______________' & zinkod == '11' & NDZ_sanemsanas_datums == "2022-06-28") | #Te nevar saprast. 202206 indivīds tiek pieņemts darbā, bet tad atkal pieņemts darbā 202302. Dzilāka izpēte liek domāt, ka 202206 darbā pieņemšanas kods varētu būt kļūda. Bet tā var arī nebūt kļūda.
+                 (PS_code ==  '______________' & NM_code ==  '______________' & zinkod == '11' & NDZ_sanemsanas_datums == "2022-06-23") | #Te nevar saprast. 20220623 indivīds tiek atlaists un pieņemts darbā, bet tad atkal pieņemts darbā 20220712. Dzilāka izpēte liek domāt, ka 202206 darbā pieņemšanas kods varētu būt kļūda. Bet tā var arī nebūt kļūda.
+                 (PS_code ==  '______________' & NM_code ==  '______________' & zinkod == '11' & NDZ_sanemsanas_datums == "2022-06-01") | #Dzilāka izpēte rāda, ka šis kods varētu būt kļūda, jo cilvēks jau ir nodarbinātībā un bieži ņem bezalgas atvaļinājumus, un pa reizei pie atgriešanās parādas šis 11 kods vienā datumā ar 51, lai arī nekāda atlaišana nav notikusi.
                  (PS_code ==  '______________' & NM_code ==  '______________' & zinkod == '11' & NDZ_sanemsanas_datums == "2022-06-24") |  
-                 (PS_code ==  '______________' & NM_code ==  '______________' & zinkod == '11' & NDZ_sanemsanas_datums == "2022-06-21")    
+                 (PS_code ==  '______________' & NM_code ==  '______________' & zinkod == '11' & NDZ_sanemsanas_datums == "2022-06-21")    #Šis kods maldina, jo darbs tiek sākts augustā
                 ))
   
                 
@@ -18,15 +18,15 @@ changes_202206 <- function(NDZ, kods) {
               NDZ <- NDZ %>% 
                 mutate(
                   zinkod = ifelse(
-                    PS_code  %in%  c'______________', '______________' &
-                      NM_code  %in%  c'______________', '______________' &
+                    PS_code  %in%  c'______________', '______________') &
+                      NM_code  %in%  c'______________', '______________') &
                       zinkod == '40' &
                       NDZ_sanemsanas_datums %in% c(as.Date('2022-06-28'), as.Date('2022-06-15')),
                     '41', 
                     zinkod),
                   sak_beidz = ifelse(
-                    PS_code  %in%  c'______________', '______________' &
-                      NM_code  %in%  c'______________', '______________' &
+                    PS_code  %in%  c'______________', '______________') &
+                      NM_code  %in%  c'______________', '______________') &
                       zinkod == '41' &
                       NDZ_sanemsanas_datums %in% c(as.Date('2022-06-28'), as.Date('2022-06-15')),
                     '1',
@@ -41,8 +41,8 @@ changes_202206 <- function(NDZ, kods) {
              
              NDZ <- NDZ %>% 
                dplyr::filter(!(
-                 (PS_code ==  '______________'& NM_code ==  '______________' & zinkod == '51' & NDZ_sanemsanas_datums == "2022-06-01") | #Te un nākošajā mēnesī, kodam 51 nav jēga, jo iepriekšējā mēnesī darbinieks jau ir atlaists.
-                 (PS_code ==  '______________'& NM_code ==  '______________' & zinkod == '50' & NDZ_sanemsanas_datums == "2022-06-01")   #Te indivīds aiziet bezalgas atvaļinājumā nākamajā mēnesī, neatgriežoties no šī. Par cik šis kods nav izskaidrojams un atstājot maldina. pieņemu lēmumu to izņemt.
+                 (PS_code ==  '______________'& NM_code ==  '______________' & zinkod == '51' & NDZ_sanemsanas_datums == "2022-06-01") | 
+                 (PS_code ==  '______________'& NM_code ==  '______________' & zinkod == '50' & NDZ_sanemsanas_datums == "2022-06-01")  
                 ))
              
              NDZ <- NDZ %>% 
@@ -56,7 +56,7 @@ changes_202206 <- function(NDZ, kods) {
              
              NDZ <- NDZ %>% 
                dplyr::filter(!(
-                 (PS_code ==  '______________'& NM_code ==  '______________' & zinkod == '51' & NDZ_sanemsanas_datums == "2022-06-01")  līdz ar to, tas maldina.
+                 (PS_code ==  '______________'& NM_code ==  '______________' & zinkod == '51' & NDZ_sanemsanas_datums == "2022-06-01")  
                ))
     
   } else if (kods == "53") {
@@ -68,4 +68,4 @@ changes_202206 <- function(NDZ, kods) {
   }
   
   return(NDZ)
-} 
+}
