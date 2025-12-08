fdcd65162303feb81fde2d43cf2a26c856fcf67c
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-12-08 19:27:57 +0200

    Update processingFives_s2e6.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p5/processingFives_s2e6.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p5/processingFives_s2e6.R
@@ -1,38 +1,27 @@
 processingFives_s2e6 <- function(a, o, kods) {  
   #a <- x5s2 for testing
-  a <- a %>% arrange(PS_code, DN_code, NM_code, NDZ_sanemsanas_datums)
+  a <- a %>% arrange(pseidokods, dnperk, nmrkod, ZDN_sanemsanas_datums)
   a1 <- data.frame(); a2 <- data.frame(); a4 <- data.frame()
   
   if (all(a$sak_beidz[3:4] == "1")) {
            a1 <- a[1,]; a2 <- a[c(3,2,4,5),]
            if (kods %in% c("40", "50", "53") && o == "5") {ZERO_plus(a %>% slice(5))}
   } else if (all(a$sak_beidz[c(2,4)] == "1")) {
-           if ((a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
-               (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
-               (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
-               (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
-               (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
-               (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
-               (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
-               (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
-               (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
-               (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________')) {
                   a1 <- a[1,]; a2 <- a[2:5,]
                   if (kods %in% c("40", "50", "53") && o == "5") {ZERO_plus(a %>% slice(5))}
-            } else {stop("processingFives_s2e6: Trūkst apstrādes koda.")}
   } else if (all(a$sak_beidz[c(3,5)] == "1")) {
-           if ((a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
-               (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
-               (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________')) {
+           if ((a$period[1] == "202203" && a$pseidokods[1] == '______________' && a$nmrkod[1] == '______________') ||
+               (a$period[1] == "202208" && a$pseidokods[1] == '______________' && a$nmrkod[1] == '______________') ||
+               (a$period[1] == "202208" && a$pseidokods[1] == '______________' && a$nmrkod[1] == '______________')) {
                  a <- a[c(1,3,2,4,5),]; rownames(a) <- NULL
                  a1 <- a[c(1,5),]; a2 <- a[c(2,4),]
            } else {stop("processingFives_s2e6: Trūkst apstrādes koda.")}
   } else if (all(a$sak_beidz[c(2,5)] == "1")) {
-          if ((a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
-              (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
-              (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
-              (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________') ||
-              (a$period[1] == '______' && a$PS_code[1] ==  '______________' && a$NM_code[1] ==  '______________')) {
+          if ((a$period[1] == '202201' && a$pseidokods[1] == '______________' && a$nmrkod[1] == '______________') ||
+              (a$period[1] == '202201' && a$pseidokods[1] == '______________' && a$nmrkod[1] == '______________') ||
+              (a$period[1] == '202205' && a$pseidokods[1] == '______________' && a$nmrkod[1] == '______________') ||
+              (a$period[1] == '202207' && a$pseidokods[1] == '______________' && a$nmrkod[1] == '______________') ||
+              (a$period[1] == '202208' && a$pseidokods[1] == '______________' && a$nmrkod[1] == '______________')) {
                 a1 <- a[c(1,5),]; a2 <- a[c(2,4),]
           } else {stop("processingFives_s2e6: Trūkst apstrādes koda.")}
   } else {stop("processingFives_s2e6: Trūkst apstrādes koda.")}
