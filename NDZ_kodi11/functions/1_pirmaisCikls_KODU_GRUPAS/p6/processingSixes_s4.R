202c3509b275ae7b7628b13f9aaa1755cd5aa932
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-12-09 17:01:45 +0200

    Update processingSixes_s4.R

--- NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p6/processingSixes_s4.R
+++ NDZ_kodi11/functions/1_pirmaisCikls_KODU_GRUPAS/p6/processingSixes_s4.R
@@ -4,32 +4,44 @@ processingSixes_s4 <- function(x6s4, o, kods) {
   x6s4_uzCetri <- data.frame(); x6s4_uzPieciniekiem <- data.frame()
   
   if (all(x6s4$sak_beidz[c(1,2,4,6)] == "1")) { 
-           if (all(diff(x6s4$NDZ_sanemsanas_datums) != 0)) {
+        if (all(diff(x6s4$NDZ_sanemsanas_datums) != 0)) {
+          #JO PIRMOREIZ
+            if (x6s4$period[1] == '202202' && x6s4$PS_code[1] == '______________' && x6s4$NM_code[1] == '______________') {
                 x6s4_uzCetri <- x6s4[2:5, ]
                 x6s4_uzVieniniekiem <- x6s4[6, ]
-                if (kods %in% c("40", "50", "53") && o == "6") {ZERO_minus(x6s4 %>% slice(1))}
-           } else if (diff(x6s4$NDZ_sanemsanas_datums[1:2]) == 0 && all(diff(x6s4$NDZ_sanemsanas_datums[2:6]) != 0)) {
+                if (kods %in% c("40", "50", "53") && o == "6") {ZERO_minus(x6s4 %>% slice(2))}
+            } else {stop("processingSixes_s4 trūkst apstrādes koda. \n")}
+        } else if (diff(x6s4$NDZ_sanemsanas_datums[1:2]) == 0 && all(diff(x6s4$NDZ_sanemsanas_datums[2:6]) != 0)) {
+                 #JO PIRMOREIZ
                  if (kods == "40") {
-                   if (x6s4$period[1] == '______' && x6s4$PS_code[1] ==  '______________' && x6s4$NM_code[1] ==  '______________') {
+                   if (x6s4$period[1] == '202208' && x6s4$PS_code[1] == '______________' && x6s4$NM_code[1] == '______________') {
                         x6s4_uzCetri <- x6s4[2:5, ]; x6s4_uzVieniniekiem <- x6s4[6, ]
                      if (kods %in% c("40", "50", "53") && o == "6") {ZERO_minus(x6s4 %>% slice(1))}
                    } else {stop("processingSixes_s4 trūkst apstrādes koda. \n")}
                  } else {stop("processingSixes_s4 trūkst apstrādes koda. \n")}
-            } else {stop("processingSixes_s4 trūkst apstrādes koda. \n")}
+             } else if (all(sapply(c(2,4), function(i) diff(x6s4$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) && 
+                        all(sapply(c(1,3,5), function(i) diff(x6s4$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
+                        #JO PIRMOREIZ
+                        if (with(x6s4, period[1] == '202201' && PS_code[1] == '______________' && NM_code[1] == '______________')) {
+                              x6s4_uzCetri <- x6s4[2:5, ]; x6s4_uzVieniniekiem <- x6s4[6, ]
+                              if (kods %in% c("40", "50", "53") && o == "6") {ZERO_minus(x6s4 %>% slice(2))}
+                        } else {stop("processingSixes_s4 trūkst apstrādes koda. \n")}
+          } else {stop("processingSixes_s4 trūkst apstrādes koda. \n")}
   } else if (all(x6s4$sak_beidz == c("1","1","2","1","1","2"))) {
             if (all(sapply(c(2,5), function(i) diff(x6s4$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) &&
                 all(sapply(c(1,3,4), function(i) diff(x6s4$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
-                    if (x6s4$period[1] == '______' && x6s4$PS_code[1] ==  '______________' && x6s4$NM_code[1] ==  '______________') {
+                    if (x6s4$period[1] == '202202' && x6s4$PS_code[1] == '______________' && x6s4$NM_code[1] == '______________') {
                         x6s4_uzCetri <- x6s4[c(1,3,5,6), ]
                         if (kods %in% c("40", "50", "53") && o == "6") {ZERO_minus(x6s4 %>% slice(1)); ZERO_plus(x6s4 %>% slice(6))}
                     } else {stop("processingSixes_s4 trūkst apstrādes koda. \n")}
             } else if (all(sapply(c(1,2,4,5), function(i) diff(x6s4$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) &&
                        diff(x6s4$NDZ_sanemsanas_datums[3:4]) != 0) {
-                        if (x6s4$period[1] == '______' && x6s4$PS_code[1] ==  '______________' && x6s4$NM_code[1] ==  '______________') {
+                        if (x6s4$period[1] == '202202' && x6s4$PS_code[1] == '______________' && x6s4$NM_code[1] == '______________') {
                           x6s4_uzCetri <- x6s4[c(3,2,6,4), ]
                        } else {stop("processingSixes_s4 trūkst apstrādes koda. \n")}
             } else if (all(diff(x6s4$NDZ_sanemsanas_datums[1:5]) != 0) && diff(x6s4$NDZ_sanemsanas_datums[5:6]) == 0) {
-                       if (x6s4$period[1] == '______' && x6s4$PS_code[1] ==  '______________' && x6s4$NM_code[1] ==  '______________') {
+                       #JO PIRMOREIZ
+                       if (x6s4$period[1] == '202208' && x6s4$PS_code[1] == '______________' && x6s4$NM_code[1] == '______________') {
                            x6s4_uzCetri <- x6s4[c(2,3,5,6), ]
                            if (kods %in% c("40", "50", "53") && o == "6") {ZERO_minus(x6s4 %>% slice(2)); ZERO_plus(x6s4 %>% slice(6))}
               } else {stop("processingSixes_s4 trūkst apstrādes koda. \n")}
@@ -40,12 +52,14 @@ processingSixes_s4 <- function(x6s4, o, kods) {
                        x6s4_uzCetri <- x6s4[1:4, ]; x6s4_uzVieniniekiem <- x6s4[6, ]
                        if (kods %in% c("40", "50", "53") && o == "6") {ZERO_minus(x6s4 %>% slice(1))}
               } else if (all(diff(x6s4$NDZ_sanemsanas_datums) != 0)) {
-                if (x6s4$period[1] == '______' && x6s4$PS_code[1] ==  '______________' && x6s4$NM_code[1] ==  '______________') {
+                #JO PIRMOREIZ
+                if (x6s4$period[1] == "202203" && x6s4$PS_code[1] == '______________' && x6s4$NM_code[1] == '______________') {
                        x6s4_uzCetri <- x6s4[1:4, ]; x6s4_uzVieniniekiem <- x6s4[6, ]
                        if (kods %in% c("40", "50", "53") && o == "6") {ZERO_minus(x6s4 %>% slice(1))}
                 } else {stop("processingSixes_s4 trūkst apstrādes koda. \n")}
               } else if (diff(x6s4$NDZ_sanemsanas_datums[1:2]) == 0 && all(diff(x6s4$NDZ_sanemsanas_datums[2:6]) != 0)) {
-                if (x6s4$period[1] == '______' && x6s4$PS_code[1] ==  '______________' && x6s4$NM_code[1] ==  '______________') {
+                #JO PIRMO REIZ
+                if (x6s4$period[1] == "202204" && x6s4$PS_code[1] == '______________' && x6s4$NM_code[1] == '______________') {
                   x6s4_uzCetri <- x6s4[1:4, ]; x6s4_uzVieniniekiem <- x6s4[6, ]
                   if (kods %in% c("40", "50", "53") && o == "6") {ZERO_minus(x6s4 %>% slice(1))}
                 } else {stop("processingSixes_s4 trūkst apstrādes koda. \n")}
@@ -53,22 +67,24 @@ processingSixes_s4 <- function(x6s4, o, kods) {
   } else if (all(x6s4$sak_beidz[c(1,3,4,6)] == "1")) {
            if (all(sapply(c(1,4), function(i) diff(x6s4$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) &&
                all(sapply(c(2,3,5), function(i) diff(x6s4$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
-              if (x6s4$period[1] == '______' && x6s4$PS_code[1] ==  '______________' && x6s4$NM_code[1] ==  '______________') {
+              if (x6s4$period[1] == '202201' && x6s4$PS_code[1] == '______________' && x6s4$NM_code[1] == '______________') {
                    x6s4 <- x6s4[c(2,4,5,6),]
                    x6s4_uzVieniniekiem <- x6s4[c(1,4),]; x6s4_uzDivniekiem <- x6s4[2:3,]
                 
-            } else if ((x6s4$period[1] == '______' && x6s4$PS_code[1] ==  '______________' && x6s4$NM_code[1] ==  '______________') ||
-                       (x6s4$period[1] == '______' && x6s4$PS_code[1] ==  '______________' && x6s4$NM_code[1] ==  '______________')) {
+            } else if ((x6s4$period[1] == '202203' && x6s4$PS_code[1] == '______________' && x6s4$NM_code[1] == '______________') ||
+                       (x6s4$period[1] == '202208' && x6s4$PS_code[1] == '______________' && x6s4$NM_code[1] == '______________')) {
+                       #JO PIRMOREIZ
                        x6s4_uzVieniniekiem <- x6s4[6,]; x6s4_uzCetri <- x6s4[c(1,2,4,5), ]
                        if (kods %in% c("40", "50", "53") && o == "6") {ZERO_minus(x6s4 %>% slice(1))}
                        } else {stop("processingSixes_s4: trūkst apstrādes koda. \n")}
           } else if (all(diff(x6s4$NDZ_sanemsanas_datums) != 0)) {
-                     if ((x6s4$PS_code[1] ==  '______________' && x6s4$NM_code[1] ==  '______________') ||
-                         (x6s4$period[1] == '______' && x6s4$PS_code[1] ==  '______________' && x6s4$NM_code[1] ==  '______________') ||
-                         (x6s4$period[1] == '______' && x6s4$PS_code[1] ==  '______________' && x6s4$NM_code[1] ==  '______________') ||
-                         (x6s4$period[1] == '______' && x6s4$PS_code[1] ==  '______________' && x6s4$NM_code[1] ==  '______________') ||
-                         (x6s4$period[1] == '______' && x6s4$PS_code[1] ==  '______________' && x6s4$NM_code[1] ==  '______________') ||
-                         (x6s4$period[1] == '______' && x6s4$PS_code[1] ==  '______________' && x6s4$NM_code[1] ==  '______________')) {
+                     #JA VISS KĀRTĪBĀ, ATBRĪVO
+                     if ((x6s4$PS_code[1] == '______________' && x6s4$NM_code[1] == '______________') ||
+                         (x6s4$period[1] == '202202' && x6s4$PS_code[1] == '______________' && x6s4$NM_code[1] == '______________') ||
+                         (x6s4$period[1] == '202203' && x6s4$PS_code[1] == '______________' && x6s4$NM_code[1] == '______________') ||
+                         (x6s4$period[1] == '202203' && x6s4$PS_code[1] == '______________' && x6s4$NM_code[1] == '______________') ||
+                         (x6s4$period[1] == '202202' && x6s4$PS_code[1] == '______________' && x6s4$NM_code[1] == '______________') ||
+                         (x6s4$period[1] == '202204' && x6s4$PS_code[1] == '______________' && x6s4$NM_code[1] == '______________')) {
                             x6s4_uzCetri <- x6s4[c(1,2,4,5), ]; x6s4_uzVieniniekiem <- x6s4[6, ]
                             if (kods %in% c("40", "50", "53") && o == "6") {ZERO_minus(x6s4 %>% slice(1))}
                      } else {stop("processingSixes_s4: trūkst apstrādes koda. \n")}
@@ -77,18 +93,21 @@ processingSixes_s4 <- function(x6s4, o, kods) {
              if (kods == "40") {
                if (all(sapply(c(3,5), function(i) diff(x6s4$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) &&
                    all(sapply(c(1,2,4), function(i) diff(x6s4$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
-                         if (x6s4$period[1] == '______' && x6s4$PS_code[1] ==  '______________' && x6s4$NM_code[1] ==  '______________') {
+                         #JO PIRMOREIZ
+                         if (x6s4$period[1] == '202205' && x6s4$PS_code[1] == '______________' && x6s4$NM_code[1] == '______________') {
                              x6s4_uzVieniniekiem <- x6s4[6,]; x6s4_uzDivniekiem <- x6s4[c(2,4), ]
                              if (kods %in% c("40", "50", "53") && o == "6") {ZERO_minus(x6s4 %>% slice(1))}
                          } else {stop("processingSixes_s4 trūkst apstrādes koda. \n")}
                } else if (all(sapply(seq(1,6,by=2), function(i) diff(x6s4$NDZ_sanemsanas_datums[i:(i+1)]) == 0)) &&
                           all(sapply(c(2,4), function(i) diff(x6s4$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
-                          if (x6s4$period[1] == '______' && x6s4$PS_code[1] ==  '______________' && x6s4$NM_code[1] ==  '______________') {
+                          #JO PIRMOREIZ
+                          if (x6s4$period[1] == '202209' && x6s4$PS_code[1] == '______________' && x6s4$NM_code[1] == '______________') {
                               x6s4_uzVieniniekiem <- x6s4[6,]; x6s4_uzDivniekiem <- x6s4[c(2,4), ]
                               if (kods %in% c("40", "50", "53") && o == "6") {ZERO_minus(x6s4 %>% slice(1))}
                          } else {stop("processingSixes_s4 trūkst apstrādes koda. \n")}
                } else if (diff(x6s4$NDZ_sanemsanas_datums[3:4]) == 0 && all(sapply(c(1,2,4,5), function(i) diff(x6s4$NDZ_sanemsanas_datums[i:(i+1)]) != 0))) {
-                          if (x6s4$period[1] == '______' && x6s4$PS_code[1] ==  '______________' && x6s4$NM_code[1] ==  '______________') {
+                         #JO PIRMOREIZ
+                          if (x6s4$period[1] == '202209' && x6s4$PS_code[1] == '______________' && x6s4$NM_code[1] == '______________') {
                               x6s4_uzVieniniekiem <- x6s4[6,]; x6s4_uzDivniekiem <- x6s4[c(2,4), ]
                               if (kods %in% c("40", "50", "53") && o == "6") {ZERO_minus(x6s4 %>% slice(1))}
                           } else {stop("processingSixes_s4 trūkst apstrādes koda. \n")}
@@ -100,4 +119,4 @@ processingSixes_s4 <- function(x6s4, o, kods) {
                 x6_uzDivniekiem = x6s4_uzDivniekiem,
                 x6_uzCetri = x6s4_uzCetri,
                 x6_uzPieciniekiem = x6s4_uzPieciniekiem))
-  } 
+  }
